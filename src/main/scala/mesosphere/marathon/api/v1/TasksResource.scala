cc032cd476f28787fc34fd0895f2d408a86cb785
Tobi Knaup <tobi@knaup.me>
2013-10-23 19:54:56 -0700

    Finish  /v1/tasks

--- src/main/scala/mesosphere/marathon/api/v1/TasksResource.scala
+++ src/main/scala/mesosphere/marathon/api/v1/TasksResource.scala
@@ -2,43 +2,44 @@ package mesosphere.marathon.api.v1
 
 import javax.ws.rs._
 import mesosphere.marathon.MarathonSchedulerService
-import javax.ws.rs.core.{Context, Response, MediaType}
-import javax.inject.{Named, Inject}
-import javax.validation.Valid
+import javax.ws.rs.core.MediaType
+import javax.inject.Inject
 import com.codahale.metrics.annotation.Timed
-import com.google.common.eventbus.EventBus
-import mesosphere.marathon.event.{EventModule, ApiPostEvent}
-import javax.servlet.http.HttpServletRequest
-import mesosphere.marathon.tasks.{TaskIDUtil, TaskTracker}
+import mesosphere.marathon.tasks.TaskTracker
 import java.util.logging.Logger
 import org.apache.mesos.Protos.TaskID
 
 @Path("v1/tasks")
 @Produces(Array(MediaType.APPLICATION_JSON))
 class TasksResource @Inject()(
-    @Named(EventModule.busName) eventBus: Option[EventBus],
     service: MarathonSchedulerService,
     taskTracker: TaskTracker) {
 
+  import Implicits._
+
   val log = Logger.getLogger(getClass.getName)
+
 
   @GET
   @Timed
   def list = {
-    taskTracker.list()
+    taskTracker.list().map { case ((key, setOfTasks)) =>
+      // TODO teach Jackson how to serialize a MarathonTask instead
+      (key, setOfTasks.map(s => s: Map[String, Object]))
+    }
   }
 
-  @GET
-  @Path("killTasks")
+  @POST
+  @Path("kill")
   @Timed
   def killTasks(@QueryParam("appId") appId: String,
-                @QueryParam("hostname") hostname: String) = {
-    val tasks = taskTracker.list().filter(  _._1 == appId || appId == "*"  )
-      .filter(_._2 == hostname || hostname == "*").flatMap(_._2)
-
-    tasks.foreach(task => {
-      log.warning(f"Killing task: ${task.getId} on host: ${task.getHost}")
-      service.driver.killTask(TaskID.newBuilder().setValue(task.getId).build())
-    })
+                @QueryParam("host") host: String) = {
+    taskTracker.get(appId)
+      .filter(_.getHost == host || host == "*")
+      .map(task => {
+        log.info(f"Killing task ${task.getId} on host ${task.getHost}")
+        service.driver.killTask(TaskID.newBuilder.setValue(task.getId).build)
+        task: Map[String, Object]
+      })
   }
 }
