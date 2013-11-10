99cdcffc4c88fa816593f8c191a864a524d5f835
Tobi Knaup <tobi@knaup.me>
2013-11-10 09:24:40 -1000

    Handler NotFoundException

--- src/main/scala/mesosphere/marathon/api/v1/MarathonExceptionMapper.scala
+++ src/main/scala/mesosphere/marathon/api/v1/MarathonExceptionMapper.scala
@@ -1,10 +1,11 @@
 package mesosphere.marathon.api.v1
 
 import javax.ws.rs.ext.{Provider, ExceptionMapper}
-import javax.ws.rs.core.Response
+import javax.ws.rs.core.{MediaType, Response}
 import scala.concurrent.TimeoutException
 import mesosphere.marathon.UnknownAppException
 import java.util.logging.{Level, Logger}
+import com.sun.jersey.api.NotFoundException
 
 /**
  * @author Tobi Knaup
@@ -18,12 +19,16 @@ class MarathonExceptionMapper extends ExceptionMapper[Exception] {
   def toResponse(exception: Exception): Response = {
     log.log(Level.WARNING, "", exception)
     val statusCode = exception match {
-      case e: IllegalArgumentException => 422  // Unprocessable entity
+      case e: IllegalArgumentException => 422 // Unprocessable entity
       case e: TimeoutException => 504 // Gateway timeout
-      case e: UnknownAppException => 404
+      case e: UnknownAppException => 404 // Not found
+      case e: NotFoundException => 404 // Not found
       case _ => 500 // Internal server error
     }
-    val entity = Map("message" -> exception.getMessage)
-    Response.status(statusCode).entity(entity).build
+    val entity = exception match {
+      case e: NotFoundException => Map("message" -> s"URI not found: ${e.getNotFoundUri.getRawPath}")
+      case _ => Map("message" -> exception.getMessage)
+    }
+    Response.status(statusCode).`type`(MediaType.APPLICATION_JSON).entity(entity).build
   }
 }
