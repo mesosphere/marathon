9936dabc951a690a97ad7ce2c163333e667bcabe
Brenden Matthews <brenden@diddyinc.com>
2013-11-20 11:37:59 -0800

    Added task reconciliation.

--- src/main/scala/mesosphere/marathon/MarathonSchedulerService.scala
+++ src/main/scala/mesosphere/marathon/MarathonSchedulerService.scala
@@ -36,8 +36,10 @@ class MarathonSchedulerService @Inject()(
   // TODO use a thread pool here
   import ExecutionContext.Implicits.global
 
-  // Time to wait before trying to balance app tasks after driver starts
-  val balanceWait = Duration(30, "seconds")
+  // Time to wait before trying to reconcile app tasks after driver starts
+  val registrationWait = Duration(60, "seconds")
+  val periodicWait = Duration(config.reconciliationPeriod(), "minutes")
+  val reconciliationTimer = new Timer("reconciliationTimer")
 
   val log = Logger.getLogger(getClass.getName)
 
@@ -48,6 +50,7 @@ class MarathonSchedulerService @Inject()(
     .setFailoverTimeout(config.mesosFailoverTimeout())
     .setUser("") // Let Mesos assign the user
     .setCheckpoint(config.checkpoint())
+
 
   // Set the framework ID
   frameworkIdUtil.fetch() match {
@@ -116,11 +119,12 @@ class MarathonSchedulerService @Inject()(
     log.info("Shutting down")
     abdicateCmd.map(_.execute)
     stopDriver()
+    reconciliationTimer.cancel
   }
 
   def runDriver() {
     log.info("Running driver")
-    scheduleTaskBalancing()
+    scheduleTaskReconciliation
     driver.run()
   }
 
@@ -161,15 +165,15 @@ class MarathonSchedulerService @Inject()(
   }
   //End Leader interface
 
-  private def scheduleTaskBalancing() {
-    val timer = new Timer()
+  private def scheduleTaskReconciliation {
     val task = new TimerTask {
       def run() {
-        scheduler.balanceTasks(driver)
-        scheduleTaskBalancing
+        scheduler.reconcileTasks(driver)
       }
     }
-    timer.schedule(task, balanceWait.toMillis)
+    reconciliationTimer.schedule(task,
+                                 new java.util.Date(System.currentTimeMillis + registrationWait.toMillis),
+                                 periodicWait.toMillis)
   }
 
   private def offerLeaderShip() {
