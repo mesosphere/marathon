98f19efe514842606cdddcfc35912e4b5ede3309
Ross Allen <rossgit@gmail.com>
2013-12-09 13:12:34 -0800

    Extend BackboneMixin to support models and collections

--- src/main/resources/assets/js/mixins/BackboneMixin.js
+++ src/main/resources/assets/js/mixins/BackboneMixin.js
@@ -3,10 +3,13 @@ define([
   return {
     componentDidMount: function() {
       this._boundForceUpdate = this.forceUpdate.bind(this, null);
-      this.props.collection.on("all", this._boundForceUpdate, this);
+      this.getBackboneObject().on("all", this._boundForceUpdate, this);
     },
     componentWillUnmount: function() {
-      this.props.collection.off("all", this._boundForceUpdate);
+      this.getBackboneObject().off("all", this._boundForceUpdate);
+    },
+    getBackboneObject: function() {
+      return this.props.collection || this.props.model;
     }
   };
 });
