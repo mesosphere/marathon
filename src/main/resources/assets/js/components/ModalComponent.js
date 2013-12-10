a82e2a27af0b5d088a219869a2dc3fec1aeaa0cb
Ross Allen <rossgit@gmail.com>
2013-12-10 12:37:17 -0800

    Close modals with ESC

--- src/main/resources/assets/js/components/ModalComponent.js
+++ src/main/resources/assets/js/components/ModalComponent.js
@@ -1,9 +1,20 @@
 /** @jsx React.DOM */
 
 define([
+  "jquery",
   "React"
-], function(React) {
+], function($, React) {
   return React.createClass({
+    componentDidMount: function() {
+      var _this = this;
+
+      $(document).on("keyup.modalComponent", function(event) {
+        if (event.keyCode === 27) { _this.destroy(); }
+      });
+    },
+    componentWillUnmount: function() {
+      $(document).off(".modalComponent");
+    },
     destroy: function(event) {
       var domNode = this.getDOMNode();
       if (this.props.onDestroy != null) this.props.onDestroy();
