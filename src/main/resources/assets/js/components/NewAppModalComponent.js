f7020a1309e56f3e3bf33084521c9d1c4ad770ff
Ross Allen <rossgit@gmail.com>
2014-01-02 15:01:16 -0800

    Parse Number values to sort properly
    Form values are always Strings and must be parsed. Without parsing, some
    models have Strings for values and others have Numbers. That leads to
    odd sorting because "30" comes after 2 but before 4.

--- src/main/resources/assets/js/components/NewAppModalComponent.js
+++ src/main/resources/assets/js/components/NewAppModalComponent.js
@@ -28,6 +28,13 @@ define([
       // URIs should be an Array of Strings.
       if ("uris" in modelAttrs) modelAttrs.uris = modelAttrs.uris.split(",");
 
+      // mem, cpus, and instances are all Numbers and should be parsed as such.
+      if ("mem" in modelAttrs) modelAttrs.mem = parseFloat(modelAttrs.mem);
+      if ("cpus" in modelAttrs) modelAttrs.cpus = parseFloat(modelAttrs.cpus);
+      if ("instances" in modelAttrs) {
+        modelAttrs.instances = parseInt(modelAttrs.instances, 10);
+      }
+
       this.props.onCreate(modelAttrs);
       this.destroy();
     },
