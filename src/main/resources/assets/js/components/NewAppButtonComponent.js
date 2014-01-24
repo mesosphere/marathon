cb2e259751f80d9576af5c38021f722c5fe3a09e
Ross Allen <rossgit@gmail.com>
2014-01-24 10:24:13 -0800

    Shrink header logo and "New app" button
    The header should take up as little space as possible.

--- src/main/resources/assets/js/components/NewAppButtonComponent.js
+++ src/main/resources/assets/js/components/NewAppButtonComponent.js
@@ -23,8 +23,8 @@ define([
     },
     render: function() {
       return (
-        <button type="button" className="btn btn-primary btn-lg add-button" onClick={this.showModal}>
-          + New
+        <button type="button" className="btn btn-primary header-btn" onClick={this.showModal}>
+          + New App
         </button>
       );
     }
