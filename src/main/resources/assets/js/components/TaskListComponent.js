bc1155971ad61e82e3729efd3d267f2f9e806a19
Ross Allen <rossgit@gmail.com>
2014-02-11 17:50:58 -0800

    Show task status and updated timestamp in list
    * Collapse host and ports to a line underneath the main task row.
    * Add "Status" and "Updated" columns to task list.
    * "Status" is one of "Started" and "Staged".
    * "Updated" is the timestamp related to the current status.
    * Columns in the task list are now sortable forward and in reverse.

--- src/main/resources/assets/js/components/TaskListComponent.js
+++ src/main/resources/assets/js/components/TaskListComponent.js
@@ -8,6 +8,18 @@ define([
   var STATE_ERROR = 1;
   var STATE_SUCCESS = 2;
 
+  function taskHostPortsToString(task) {
+    var portsString;
+    var ports = task.get("ports");
+    if (ports.length > 1) {
+      portsString = ":[" + ports.join(", ") + "]";
+    } else if (ports.length === 1) {
+      portsString = ":" + ports[0];
+    }
+
+    return task.get("host") + portsString;
+  }
+
   return React.createClass({
     componentWillMount: function() {
       this.fetchTasks();
@@ -27,7 +39,8 @@ define([
     },
     getInitialState: function() {
       return {
-        fetchState: STATE_LOADING
+        fetchState: STATE_LOADING,
+        showTimestamps: false
       };
     },
     getResource: function() {
@@ -46,6 +59,7 @@ define([
     mixins: [BackboneMixin],
     render: function() {
       var taskNodes;
+      var _this = this;
       var tasksLength = this.props.collection.length;
 
       // If there are no tasks, they can't all be selected. Otherwise, assume
@@ -56,21 +70,21 @@ define([
       if (this.state.fetchState === STATE_LOADING) {
         taskNodes =
           <tr>
-            <td className="text-center" colSpan="4">
+            <td className="text-center" colSpan="5">
               Loading...
             </td>
           </tr>;
       } else if (this.state.fetchState === STATE_ERROR) {
         taskNodes =
           <tr>
-            <td className="text-center text-danger" colSpan="4">
+            <td className="text-center text-danger" colSpan="5">
               Error fetching tasks. Refresh the list to try again.
             </td>
           </tr>;
       } else if (tasksLength === 0) {
         taskNodes =
           <tr>
-            <td className="text-center" colSpan="4">
+            <td className="text-center" colSpan="5">
               No tasks running.
             </td>
           </tr>;
@@ -88,6 +102,27 @@ define([
             allTasksSelected = false;
           }
 
+          var statusNode;
+          if (task.isStarted()) {
+            statusNode =
+              <span className="badge badge-default">
+                {task.get("status")}
+              </span>;
+          } else if (task.isStaged()) {
+            statusNode =
+              <span className="badge badge-warning">
+                {task.get("status")}
+              </span>;
+          }
+
+          var updatedAtNode;
+          if (task.get("updatedAt") != null) {
+            updatedAtNode =
+              <time timestamp={task.get("updatedAt")}>
+                {task.get("updatedAt").toISOString()}
+              </time>;
+          }
+
           return (
             <tr key={task.cid} className={className} onClick={this.handleTrClick.bind(this, task)}>
               <td width="1">
@@ -95,14 +130,24 @@ define([
                   checked={active}
                   onChange={this.handleCheckboxClick.bind(this, task)} />
               </td>
-              <td>{task.get("id")}</td>
-              <td>{task.get("host")}</td>
-              <td>{task.get("ports").join(",")}</td>
+              <td>
+                {task.get("id")}<br />
+                <span className="text-muted">
+                  {taskHostPortsToString(task)}
+                </span>
+              </td>
+              <td>{statusNode}</td>
+              <td className="text-right">{updatedAtNode}</td>
             </tr>
           );
         }, this);
       }
 
+      var comparator = this.props.collection.comparatorAttribute;
+      var sortSymbol =
+        (this.props.collection.comparatorString.substr(0, 1) === "-") ?
+        "▼" :
+        "▲";
       return (
         <table className="table table-selectable">
           <thead>
@@ -113,9 +158,15 @@ define([
                   disabled={tasksLength === 0}
                   onChange={this.props.onAllTasksToggle} />
               </th>
-              <th>ID</th>
-              <th>Hosts</th>
-              <th>Ports</th>
+              <th onClick={this.sortCollectionBy.bind(null, "id")}>
+                ID {(comparator === "id") ? sortSymbol : null}
+              </th>
+              <th onClick={this.sortCollectionBy.bind(null, "status")}>
+                Status {(comparator === "status") ? sortSymbol : null}
+              </th>
+              <th className="text-right" onClick={this.sortCollectionBy.bind(null, "updatedAt")}>
+                {(comparator === "updatedAt") ? sortSymbol : null} Updated
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -126,6 +177,17 @@ define([
     },
     setFetched: function() {
       this.setState({fetched: true});
+    },
+    sortCollectionBy: function(comparator) {
+      if (comparator === this.props.collection.comparatorAttribute) {
+        this.props.collection.sortReverse();
+      } else {
+        this.props.collection.setComparator(comparator);
+        this.props.collection.sort();
+      }
+    },
+    toggleShowTimestamps: function() {
+      this.setState({showTimestamps: !this.state.showTimestamps});
     }
   });
 });
