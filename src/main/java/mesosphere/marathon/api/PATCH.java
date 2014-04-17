0ef1968b9e7e2a4ed57909bc06ecbc6f0f091183
drexin <dario.rexin@r3-tech.de>
2014-04-17 12:06:41 +0200

    #203 Add PATCH support for updating app instances

---
+++ src/main/java/mesosphere/marathon/api/PATCH.java
@@ -0,0 +1,13 @@
+package mesosphere.marathon.api;
+
+import javax.ws.rs.HttpMethod;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@HttpMethod("PATCH")
+public @interface PATCH {
+}
