b2ac0000410f7508eddbc8c44f867a171d3bb7bc
drexin <dario.rexin@r3-tech.de>
2014-04-29 12:27:38 +0200

    #NO_ISSUE Constraints refactoring

--- src/main/scala/mesosphere/mesos/Constraints.scala
+++ src/main/scala/mesosphere/mesos/Constraints.scala
@@ -1,128 +1,121 @@
 package mesosphere.mesos
 
 import scala.collection.JavaConverters._
-import mesosphere.marathon.Protos.Constraint
+import mesosphere.marathon.Protos.{MarathonTask, Constraint}
 import mesosphere.marathon.Protos.Constraint.Operator
 import java.util.logging.Logger
 import org.apache.mesos.Protos.Offer
+import scala.util.Try
 
 object Int {
-  def unapply(s: String): Option[Int] = try {
-    Some(s.toInt)
-  } catch {
-    case e: java.lang.NumberFormatException => None
-  }
+  def unapply(s: String): Option[Int] = Try(s.toInt).toOption
 }
 
-// TODO this needs a refactor
 object Constraints {
 
   private[this] val log = Logger.getLogger(getClass.getName)
-  val groupByDefault = 0
+  val GroupByDefault = 0
 
-  def getIntValue(s: String, default: Int): Int = s match {
+  private def getIntValue(s: String, default: Int): Int = s match {
     case "inf" => Integer.MAX_VALUE
     case Int(x) => x
     case _ => default
   }
 
-  def meetsConstraint(tasks: Iterable[mesosphere.marathon.Protos.MarathonTask],
-                      offer: Offer,
-                      constraint: Constraint): Boolean = {
-
+  private final class ConstraintsChecker(tasks: Iterable[MarathonTask], offer: Offer, constraint: Constraint) {
     val field = constraint.getField
     val value = constraint.getValue
+    lazy val attr = offer.getAttributesList.asScala.find(_.getName == field)
 
-    if (field == "hostname") {
-      return constraint.getOperator match {
-        case Operator.LIKE =>
-          offer.getHostname.matches(value)
-        case Operator.UNIQUE =>
-          // All running tasks must have a hostname that is different from the one in the offer
-          tasks.forall(_.getHost != offer.getHostname)
+    def isMatch: Boolean =
+      if (field == "hostname") {
+        checkHostName
+      } else if (attr.nonEmpty) {
+        checkAttribute
+      } else {
+        // This will be reached in case we want to schedule for an attribute
+        // that's not supplied.
+        false
+      }
+
+    private def checkGroupBy = {
+      val minimum = List(GroupByDefault, getIntValue(value, GroupByDefault)).max
+      // Group tasks by the constraint value
+      val groupedTasks = tasks.groupBy(
+        _.getAttributesList.asScala
+          .find(_.getName == field)
+          .map(_.getText.getValue)
+      )
+
+      // Order groupings by smallest first
+      val orderedByCount = groupedTasks.toSeq.sortBy(_._2.size)
+      def minValue = orderedByCount.headOption.map(_._1).getOrElse("")
+
+      // Return true if any of these are also true:
+      // a) this offer matches the smallest grouping when there
+      // are >= minimum groupings
+      // b) the constraint value from the offer is not yet in the grouping
+      val condA =
+        orderedByCount.size >= minimum &&
+          // true if the smallest group has this attribute value
+          (minValue == attr.get.getText.getValue ||
+            // or all groups are the same size
+            orderedByCount.headOption.map(_._2.size) == orderedByCount.lastOption.map(_._2.size))
+
+      def condB = !orderedByCount.exists(_._1.exists(_ == attr.get.getText.getValue))
+
+      condA || condB
+    }
+
+    private def checkHostName =
+      constraint.getOperator match {
+        case Operator.LIKE => offer.getHostname.matches(value)
+        // All running tasks must have a hostname that is different from the one in the offer
+        case Operator.UNIQUE => tasks.forall(_.getHost != offer.getHostname)
         case Operator.CLUSTER =>
           // Hostname must match or be empty
           (value.isEmpty || value == offer.getHostname) &&
           // All running tasks must have the same hostname as the one in the offer
           tasks.forall(_.getHost == offer.getHostname)
       }
-    }
 
-    val attr = offer.getAttributesList.asScala.find(_.getName == field)
-
-    if (attr.nonEmpty) {
-      val matches = matchTaskAttributes(tasks, field, attr.get.getText.getValue)
+    private def checkAttribute = {
+      def matches = matchTaskAttributes(tasks, field, attr.get.getText.getValue)
       constraint.getOperator match {
         case Operator.UNIQUE => matches.isEmpty
         case Operator.CLUSTER =>
           // If no value is set, accept the first one. Otherwise check for it.
           (value.isEmpty || attr.get.getText.getValue == value) &&
-          // All running tasks should have the matching attribute
-          matches.size == tasks.size
-        case Operator.GROUP_BY =>
-          val minimum = List(groupByDefault, getIntValue(value, groupByDefault)).max
-          // Group tasks by the constraint value
-          val groupedTasks = tasks.groupBy(
-            x =>
-              x.getAttributesList.asScala
-                .find(y =>
-                y.getName == field)
-                .map(y =>
-                y.getText.getValue)
-          )
-
-          // Order groupings by smallest first
-          val orderedByCount = groupedTasks.toSeq.sortBy(_._2.size)
-          val minValue = orderedByCount.headOption.map(_._1).getOrElse("")
-
-          // Return true if any of these are also true:
-          // a) this offer matches the smallest grouping when there
-          // are >= minimum groupings
-          // b) the constraint value from the offer is not yet in the grouping
-          val condA =
-            orderedByCount.size >= minimum &&
-            // true if the smallest group has this attribute value
-            (minValue == attr.get.getText.getValue ||
-            // or all groups are the same size
-              orderedByCount.headOption.map(_._2.size) == orderedByCount.lastOption.map(_._2.size))
-
-          val condB =
-            !orderedByCount.exists(x =>
-              x._1.getOrElse("") == attr.get.getText.getValue)
-
-          condA || condB
-        case Operator.LIKE => {
+            // All running tasks should have the matching attribute
+            matches.size == tasks.size
+        case Operator.GROUP_BY => checkGroupBy
+        case Operator.LIKE =>
           if (value.nonEmpty) {
             attr.get.getText.getValue.matches(value)
           } else {
             log.warning("Error, value is required for LIKE operation")
             false
           }
-        }
       }
-    } else {
-      // This will be reached in case we want to schedule for an attribute
-      // that's not supplied.
-      false
     }
+
+    /**
+     * Filters running tasks by matching their attributes to this field & value.
+     * @param tasks
+     * @param field
+     * @param value
+     * @return
+     */
+    private def matchTaskAttributes(tasks: Iterable[MarathonTask], field: String, value: String) =
+      tasks.filter {
+        _.getAttributesList.asScala
+          .filter { y =>
+          y.getName == field &&
+            y.getText.getValue == value
+        }.nonEmpty
+      }
   }
 
-  /**
-   * Filters running tasks by matching their attributes to this field & value.
-   * @param tasks
-   * @param field
-   * @param value
-   * @return
-   */
-  private def matchTaskAttributes(tasks: Iterable[mesosphere.marathon.Protos.MarathonTask],
-                         field: String,
-                         value: String) = {
-    tasks
-      .filter(x =>
-      (x.getAttributesList.asScala)
-        .filter(y => {
-          y.getName == field &&
-          y.getText.getValue == value})
-        .nonEmpty)
-  }
+  def meetsConstraint(tasks: Iterable[MarathonTask], offer: Offer, constraint: Constraint): Boolean =
+    new ConstraintsChecker(tasks, offer, constraint).isMatch
 }
