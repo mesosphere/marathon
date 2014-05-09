92d254da3387fe165665eee15bc9ac02a1ece960
Matthias Veit <matthias_veit@yahoo.de>
2014-05-09 17:38:33 +0200

    Group: Rest CRUD operations

--- src/main/scala/mesosphere/marathon/api/v2/GroupsResource.scala
+++ src/main/scala/mesosphere/marathon/api/v2/GroupsResource.scala
@@ -1,32 +1,42 @@
 package mesosphere.marathon.api.v2
 
 import javax.ws.rs._
-import javax.ws.rs.core.{Context, MediaType}
-import javax.inject.{Named, Inject}
-import mesosphere.marathon.event.EventModule
-import com.google.common.eventbus.EventBus
-import mesosphere.marathon.MarathonSchedulerService
-import mesosphere.marathon.tasks.TaskTracker
-import mesosphere.marathon.health.HealthCheckManager
-import javax.servlet.http.HttpServletRequest
+import javax.ws.rs.core.MediaType
+import javax.inject.Inject
 import javax.validation.Valid
+import mesosphere.marathon.upgrade.UpgradeManager
 
 @Path("v2/groups")
 @Consumes(Array(MediaType.APPLICATION_JSON))
-class GroupsResource @Inject()(
-    @Named(EventModule.busName) eventBus: Option[EventBus],
-    service: MarathonSchedulerService,
-    taskTracker: TaskTracker,
-    healthCheckManager: HealthCheckManager) {
+@Produces(Array(MediaType.APPLICATION_JSON))
+class GroupsResource @Inject()(upgradeManager: UpgradeManager) {
+
+  @GET
+  def list() = {
+    List(group("1"), group("2"), group("3"))
+  }
+
+  @GET
+  @Path("{id}")
+  def group(@PathParam("id") id: String) = {
+    Group(id, ScalingStrategy(Seq(AbsoluteStepping(13)), 123), Seq.empty)
+  }
 
   @POST
-  @Path("{id}")
-  @Produces(Array(MediaType.APPLICATION_JSON))
-  def deploy(
-    @Context req: HttpServletRequest,
-    @PathParam("id") id: String,
-    @Valid group: Group
-  ) = {
+  def install( @Valid group: Group ) = {
+//    upgradeManager.install(group)
     group
+  }
+
+  @PUT
+  @Path("{id}")
+  def upgrade( @PathParam("id") id: String, @Valid group: Group ) = {
+//    upgradeManager.upgrade(group, group)
+  }
+
+  @DELETE
+  @Path("{id}")
+  def delete( @PathParam("id") id: String ) = {
+    //upgradeManager.delete(id)
   }
 }
