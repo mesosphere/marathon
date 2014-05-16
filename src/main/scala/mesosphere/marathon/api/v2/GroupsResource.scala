ade2052dd4a051f11531c8df0fad6d5ae1699108
Matthias Veit <matthias_veit@yahoo.de>
2014-05-16 11:18:16 +0200

    -GroupsResource: do not wait when update and patch

--- src/main/scala/mesosphere/marathon/api/v2/GroupsResource.scala
+++ src/main/scala/mesosphere/marathon/api/v2/GroupsResource.scala
@@ -1,42 +1,53 @@
 package mesosphere.marathon.api.v2
 
 import javax.ws.rs._
-import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.{Response, MediaType}
 import javax.inject.Inject
 import javax.validation.Valid
 import mesosphere.marathon.state.GroupManager
-import scala.concurrent.{Future, Await}
+import scala.concurrent.Await.result
 import scala.concurrent.duration._
 import mesosphere.marathon.api.PATCH
 
 @Path("v2/groups")
 @Consumes(Array(MediaType.APPLICATION_JSON))
-@Produces(Array(MediaType.APPLICATION_JSON))
 class GroupsResource @Inject()(groupManager: GroupManager) {
 
-  @GET
-  def list() : Iterable[Group] = groupManager.list()
+  val defaultWait = 3.seconds
 
   @GET
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  def list() : Iterable[Group] = result(groupManager.list(), defaultWait)
+
+  @GET
+  @Produces(Array(MediaType.APPLICATION_JSON))
   @Path("{id}")
-  def group(@PathParam("id") id: String) : Option[Group] = groupManager.group(id)
+  def group(@PathParam("id") id: String) : Option[Group] = result(groupManager.group(id), defaultWait)
 
   @POST
-  def create( @Valid group: Group ) : Group = groupManager.create(group)
+  def create( @Valid group: Group ) : Response = {
+    groupManager.create(group)
+    Response.noContent().build()
+  }
 
   @PUT
   @Path("{id}")
-  def upgrade( @PathParam("id") id: String, @Valid group: Group ) : Group = groupManager.upgrade(id, group)
-
+  def upgrade( @PathParam("id") id: String, @Valid group: Group ) : Response = {
+    groupManager.upgrade(id, group)
+    Response.noContent().build()
+  }
 
   @PATCH
   @Path("{id}")
-  def patch( @PathParam("id") id:String, @Valid update: GroupUpdate) : Group = groupManager.patch(id, update.apply)
+  def patch( @PathParam("id") id:String, @Valid update: GroupUpdate) : Response = {
+    groupManager.patch(id, update.apply)
+    Response.noContent().build()
+  }
 
   @DELETE
   @Path("{id}")
-  def delete( @PathParam("id") id: String ) : Boolean = groupManager.expunge(id)
-
-  //map from Future[T] to T since jersey can not handle futures
-  private implicit def result[T](in:Future[T]) : T = Await.result(in, 30 seconds)
+  def delete( @PathParam("id") id: String ) : Response = {
+    val response = if (result(groupManager.expunge(id), defaultWait)) Response.ok else Response.noContent()
+    response.build()
+  }
 }
