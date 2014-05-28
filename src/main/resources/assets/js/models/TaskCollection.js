800b5cbea21e65b8504cac0e3090d52aac9e92bd
Ross Allen <rossgit@gmail.com>
2014-05-28 14:30:40 -0700

    Sort tasks ascending by default, newest on top
    It’s common to list the newest item at the top of the list. Do that with
    Tasks.

--- src/main/resources/assets/js/models/TaskCollection.js
+++ src/main/resources/assets/js/models/TaskCollection.js
@@ -2,20 +2,21 @@ define([
   "models/SortableCollection",
   "models/Task"
 ], function(SortableCollection, Task) {
-  var TaskCollection = SortableCollection.extend({
+  return SortableCollection.extend({
+    model: Task,
+
     initialize: function(models, options) {
       this.options = options;
-      this.setComparator("-updatedAt");
+      this.setComparator("updatedAt");
       this.sort();
     },
-    model: Task,
+
     parse: function(response) {
       return response.tasks;
     },
+
     url: function() {
       return "/v2/apps/" + this.options.appId + "/tasks";
     }
   });
-
-  return TaskCollection;
 });
