378c967acbd477c90025c411db93845e02f43329
mlunoe <michael.lunoe@gmail.com>
2014-06-02 23:54:42 -0700

    Adding more detailed task details

--- src/main/resources/assets/js/components/TaskDetailComponent.jsx
+++ src/main/resources/assets/js/components/TaskDetailComponent.jsx
@@ -25,6 +25,53 @@ define([
         "badge-warning": task.isStaged()
       });
 
+      var healthNodeList =
+        task.get("healthCheckResults").map(function (cResult, index) {
+          var fSuccessNode = (cResult.firstSuccess == null ?
+              <dd className="text-muted">None</dd> :
+              <dd>
+                <time timestamp={cResult.firstSuccess}>
+                  {new Date(cResult.firstSuccess).toLocaleString()}
+                </time>
+              </dd>);
+          var lSuccessNode = (cResult.lastSuccess == null ?
+              <dd className="text-muted">None</dd> :
+              <dd>
+                <time timestamp={cResult.lastSuccess}>
+                  {new Date(cResult.lastSuccess).toLocaleString()}
+                </time>
+              </dd>);
+          var lFailureNode = (cResult.lastFailure == null ?
+              <dd className="text-muted">None</dd> :
+              <dd>
+                <time timestamp={cResult.lastFailure}>
+                  {new Date(cResult.lastFailure).toLocaleString()}
+                </time>
+              </dd>);
+          var cFailuresNode = (cResult.consecutiveFailures == null ?
+              <dd className="text-muted">None</dd> :
+              <dd>{cResult.consecutiveFailures}</dd>);
+          var aliveNode = (cResult.alive == null ?
+              <dd>No</dd> :
+              <dd>Yes</dd>);
+          return <div>
+              <p className="text-right text-muted">Health Check Result {index+1}</p>
+              <dl key={index} className="dl-horizontal">
+                <dt>First Success</dt>
+                {fSuccessNode}
+                <dt>Last Success</dt>
+                {lSuccessNode}
+                <dt>Last Failure</dt>
+                {lFailureNode}
+                <dt>Consecutive Failures</dt>
+                {cFailuresNode}
+                <dt>Alive</dt>
+                {aliveNode}
+              </dl>
+              <hr />
+            </div>;
+        });
+    
       var updatedAtHead;
       var updatedAtNode;
       if (task.get("updatedAt") != null) {
@@ -43,6 +90,7 @@ define([
               ◁ Task List
             </button>
           </p>
+          <p className="text-right text-muted">Task Details</p>
           <dl className="dl-horizontal">
             <dt>ID</dt>
             <dd>{task.get("id")}</dd>
@@ -50,17 +98,21 @@ define([
             <dd>{task.get("host")}</dd>
             <dt>Ports</dt>
             <dd>[{task.get("ports").toString()}]</dd>
-            <dt>Health</dt>
-            <dd className={healthClassSet}>{task.get("healthMsg")}</dd>
             <dt>Status</dt>
             <dd>{task.get("status")}</dd>
             {updatedAtHead}
             {updatedAtNode}
             <dt>Version</dt>
-            <dd><time timestamp={task.get("version")}>
-            {task.get("version").toLocaleString()}
-          </time></dd>
+            <dd>
+              <time timestamp={task.get("version")}>
+                {task.get("version").toLocaleString()}
+              </time>
+            </dd>
+            <dt>Health</dt>
+            <dd className={healthClassSet}>{task.get("healthMsg")}</dd>
           </dl>
+          <hr />
+          {healthNodeList}
         </div>
       );
     }
