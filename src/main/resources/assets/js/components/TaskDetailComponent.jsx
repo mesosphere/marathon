bce2bc3fce70bf69f8801a6ec0951057c5be8f3a
mlunoe <michael.lunoe@gmail.com>
2014-06-10 11:12:50 -0700

    Review: restructuring task detail comp

--- src/main/resources/assets/js/components/TaskDetailComponent.jsx
+++ src/main/resources/assets/js/components/TaskDetailComponent.jsx
@@ -25,40 +25,38 @@ define([
     },
     render: function() {
       var task = this.props.task;
-      var hideError = this.props.fetchState !== this.props.STATES.STATE_ERROR && task.collection != null;
-      var errorClassSet = React.addons.classSet({
-        "text-center text-danger": true,
-        "hidden" : hideError
-      });
-
-      var taskClassSet = React.addons.classSet({
-        "hidden" : !hideError
-      });
-
+      var hasError = this.props.fetchState === this.props.STATES.STATE_ERROR || task.collection == null;
       var taskHealth = task.getHealth();
-      var healthClassSet = React.addons.classSet({
-        "text-healthy": taskHealth === Task.HEALTH.HEALTHY,
-        "text-unhealthy": taskHealth === Task.HEALTH.UNHEALTHY,
-        "text-muted": taskHealth === Task.HEALTH.UNKNOWN
-      });
+      var healthClassSet;
+      var timeNodes;
+      var timeFields;
+      if (!hasError) {
 
-      var timeNodes = [
-        {
-          label: "Staged at",
-          time: task.get("stagedAt")
-        }, {
-          label: "Started success",
-          time: task.get("startedAt")
-        }
-      ];
-      timeFields = timeNodes.map(function(timeNode, index) {
-        return (
-          <TimeFieldComponent
-            key={index}
-            label={timeNode.label}
-            time={timeNode.time} />
-        );
-      });
+        healthClassSet = React.addons.classSet({
+          "text-healthy": taskHealth === Task.HEALTH.HEALTHY,
+          "text-unhealthy": taskHealth === Task.HEALTH.UNHEALTHY,
+          "text-muted": taskHealth === Task.HEALTH.UNKNOWN
+        });
+
+        timeNodes = [
+          {
+            label: "Staged at",
+            time: task.get("stagedAt")
+          }, {
+            label: "Started success",
+            time: task.get("startedAt")
+          }
+        ];
+        timeFields = timeNodes.map(function(timeNode, index) {
+          return (
+            <TimeFieldComponent
+              key={index}
+              label={timeNode.label}
+              time={timeNode.time} />
+          );
+        });
+      }
+
       return (
         <div>
           <ol className="breadcrumb">
@@ -67,28 +65,33 @@ define([
             </li>
             <li className="active">{task.get("id")}</li>
           </ol>
-          <p className={errorClassSet}>
-            Error fetching task details. Go to <a href="#" onClick={this.handleShowTaskList}>Task List</a> to see the full list.
-          </p>
-          <h5 className={taskClassSet + " text-right text-muted"}>Task Details</h5>
-          <dl className={taskClassSet + " dl-horizontal"}>
-            <dt>Host</dt>
-            <dd>{task.get("host")}</dd>
-            <dt>Ports</dt>
-            <dd>[{task.get("ports").toString()}]</dd>
-            <dt>Status</dt>
-            <dd>{task.get("status")}</dd>
-            {timeFields}
-            <dt>Version</dt>
-            <dd>
-              <time dateTime={task.get("version")}>
-                {task.get("version").toLocaleString()}
-              </time>
-            </dd>
-            <dt>Health</dt>
-            <dd className={healthClassSet}>{this.props.formatTaskHealthMessage(task)}</dd>
-          </dl>
-          <TaskHealthComponent className={taskClassSet} task={task} />
+          <h5 className="text-right text-muted">Task Details</h5>
+          {
+            hasError ?
+              <p className="text-center text-danger">
+                Error fetching task details. Go to <a href="#" onClick={this.handleShowTaskList}>Task List</a> to see the full list.
+              </p> :
+              <div>
+                <dl className="dl-horizontal">
+                  <dt>Host</dt>
+                  <dd>{task.get("host")}</dd>
+                  <dt>Ports</dt>
+                  <dd>[{task.get("ports").toString()}]</dd>
+                  <dt>Status</dt>
+                  <dd>{task.get("status")}</dd>
+                  {timeFields}
+                  <dt>Version</dt>
+                  <dd>
+                    <time dateTime={task.get("version")}>
+                      {task.get("version").toLocaleString()}
+                    </time>
+                  </dd>
+                  <dt>Health</dt>
+                  <dd className={healthClassSet}>{this.props.formatTaskHealthMessage(task)}</dd>
+                </dl>
+                <TaskHealthComponent task={task} />
+              </div>
+          }
         </div>
       );
     }
