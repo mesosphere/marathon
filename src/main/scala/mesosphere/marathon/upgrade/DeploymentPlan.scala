92f24adecdd723a3e8453a80368259ae9b349ad5
Matthias Veit <matthias_veit@yahoo.de>
2014-06-20 17:38:14 +0200

    DeploymentPlan: first working version of dependency steps computation - needs refinement

--- src/main/scala/mesosphere/marathon/upgrade/DeploymentPlan.scala
+++ src/main/scala/mesosphere/marathon/upgrade/DeploymentPlan.scala
@@ -1,37 +1,34 @@
 package mesosphere.marathon.upgrade
 
 import mesosphere.marathon.api.v1.AppDefinition
-import mesosphere.marathon.api.v2.AppUpdate
-import mesosphere.marathon.state.{ Group, Timestamp, MarathonState }
+import mesosphere.marathon.state.{ PathId, Group, Timestamp, MarathonState }
 import mesosphere.marathon.Protos.DeploymentPlanDefinition
 import scala.concurrent.Future
 import mesosphere.marathon.MarathonSchedulerService
-import scala.concurrent.ExecutionContext.Implicits.global
 import org.apache.log4j.Logger
 
 sealed trait DeploymentAction
 //application has not been started before
-case class StartApplication(application: AppDefinition, scaleTo: Int) extends DeploymentAction
-//application is started, but old instances should be killed
-case class KillTasks(application: AppDefinition, scaleTo: Int) extends DeploymentAction
+case class StartApplication(app: AppDefinition, scaleTo: Int) extends DeploymentAction
 //application is started, but more instances should be started
-case class ScaleApplication(application: AppDefinition, scaleTo: Int) extends DeploymentAction
+case class ScaleApplication(app: AppDefinition, scaleTo: Int) extends DeploymentAction
 //application is started, but shall be completely stopped
-case class StopApplication(application: AppDefinition) extends DeploymentAction
+case class StopApplication(app: AppDefinition) extends DeploymentAction
+//application is there but should be replaced
+case class RestartApplication(app: AppDefinition, scaleOldTo: Int, scaleNewTo: Int) extends DeploymentAction
 
-case class DeploymentStep(actions: List[DeploymentAction])
+case class DeploymentStep(actions: List[DeploymentAction]) {
+  def +(step: DeploymentStep) = DeploymentStep(actions ++ step.actions)
+}
 
 case class DeploymentPlan(
     id: String,
     original: Group,
     target: Group,
-    version: Timestamp = Timestamp.now()) extends MarathonState[DeploymentPlanDefinition, DeploymentPlan] {
+    steps: List[DeploymentStep],
+    version: Timestamp) extends MarathonState[DeploymentPlanDefinition, DeploymentPlan] {
 
   private[this] val log = Logger.getLogger(getClass.getName)
-
-  def originalIds: Set[String] = original.transitiveApps.map(_.id).toSet
-
-  def targetIds: Set[String] = target.transitiveApps.map(_.id).toSet
 
   override def mergeFromProto(bytes: Array[Byte]): DeploymentPlan = mergeFromProto(DeploymentPlanDefinition.parseFrom(bytes))
 
@@ -39,6 +36,7 @@ case class DeploymentPlan(
     msg.getId,
     Group.empty.mergeFromProto(msg.getOriginial),
     Group.empty.mergeFromProto(msg.getTarget),
+    Nil, //TODO: store plan as well
     Timestamp(msg.getVersion)
   )
 
@@ -51,16 +49,9 @@ case class DeploymentPlan(
       .build()
   }
 
-  lazy val (toStart, toStop, toScale, toRestart) = {
-    val isUpdate = targetIds.intersect(originalIds)
-    val updateList = isUpdate.toList
-    val origTarget = updateList.flatMap(id => original.transitiveApps.find(_.id == id)).zip(updateList.flatMap(id => target.transitiveApps.find(_.id == id)))
-    (targetIds.filterNot(isUpdate.contains).flatMap(id => target.transitiveApps.find(_.id == id)),
-      originalIds.filterNot(isUpdate.contains).flatMap(id => original.transitiveApps.find(_.id == id)),
-      origTarget.filter{ case (from, to) => from.isOnlyScaleChange(to) }.map(_._2),
-      origTarget.filter { case (from, to) => from.isUpgrade(to) }.map(_._2))
-  }
+  def deploy(scheduler: MarathonSchedulerService, force: Boolean): Future[Boolean] = ???
 
+  /*
   def deploy(scheduler: MarathonSchedulerService, force: Boolean): Future[Boolean] = {
     log.info(s"Deploy group ${target.id}: start:${toStart.map(_.id)}, stop:${toStop.map(_.id)}, scale:${toScale.map(_.id)}, restart:${toRestart.map(_.id)}")
 
@@ -87,8 +78,64 @@ case class DeploymentPlan(
 
     deployFuture andThen { case result => log.info(s"Deployment of ${target.id} has been finished $result") }
   }
+  */
 }
 
 object DeploymentPlan {
-  def empty() = DeploymentPlan("", Group.empty, Group.empty)
+  def empty() = DeploymentPlan("", Group.empty, Group.empty, Nil, Timestamp.now())
+
+  def apply(id: String, original: Group, target: Group, version: Timestamp = Timestamp.now()): DeploymentPlan = {
+
+    val originalApps = original.transitiveApps
+    val targetApps = target.transitiveApps
+    def originalApp: Map[String, AppDefinition] = originalApps.map(app => app.id -> app).toMap
+    def targetApp: Map[String, AppDefinition] = targetApps.map(app => app.id -> app).toMap
+    val (toStart, toStop, toScale, toRestart) = {
+      val isUpdate = targetApp.keySet.intersect(originalApp.keySet)
+      val updateList = isUpdate.toList
+      val origTarget = updateList.flatMap(id => originalApps.find(_.id == id)).zip(updateList.flatMap(id => targetApps.find(_.id == id)))
+      (targetApp.keySet.filterNot(isUpdate.contains),
+        originalApp.keySet.filterNot(isUpdate.contains),
+        origTarget.filter{ case (from, to) => from.isOnlyScaleChange(to) }.map(_._2.id),
+        origTarget.filter { case (from, to) => from.isUpgrade(to) }.map(_._2.id)
+      )
+    }
+    val changedApplications = toStart ++ toRestart ++ toScale ++ toStop
+    val (dependent, nonDependent) = target.dependencyList
+
+    var pass1 = List.empty[DeploymentStep]
+    var pass2 = List.empty[DeploymentStep]
+    for (app <- dependent.filter(a => changedApplications.contains(a.id))) {
+      val origApp = originalApp(app.id)
+      var pass1Actions = List.empty[DeploymentAction]
+      var pass2Actions = List.empty[DeploymentAction]
+      if (toStart.contains(app.id)) pass1Actions ::= StartApplication(app, app.instances)
+      else if (toStop.contains(app.id)) pass1Actions ::= StopApplication(origApp)
+      else if (toScale.contains(app.id)) pass1Actions ::= ScaleApplication(app, app.instances)
+      else {
+        pass1Actions ::= RestartApplication(origApp,
+          (target.scalingStrategy.minimumHealthCapacity * origApp.instances).ceil.toInt,
+          (target.scalingStrategy.minimumHealthCapacity * app.instances).ceil.toInt)
+        pass2Actions ::= StopApplication(origApp)
+        pass2Actions ::= ScaleApplication(app, app.instances)
+      }
+      pass1 ::= DeploymentStep(pass1Actions)
+      pass2 ::= DeploymentStep(pass2Actions)
+    }
+
+    val nonDependentStep = DeploymentStep(nonDependent.toList.filter(a => changedApplications.contains(a.id)).map { app =>
+      val origApp = originalApp(app.id)
+      if (toStart.contains(app.id)) StartApplication(app, app.instances)
+      else if (toStop.contains(app.id)) StopApplication(origApp)
+      else if (toScale.contains(app.id)) ScaleApplication(app, app.instances)
+      else RestartApplication(app, 0, app.instances)
+    })
+
+    val finalSteps = pass1.reverse ::: pass2 match {
+      case head :: rest => head + nonDependentStep :: rest
+      case Nil          => List(nonDependentStep)
+    }
+
+    DeploymentPlan(id, original, target, finalSteps, version)
+  }
 }
--- src/test/scala/mesosphere/marathon/upgrade/DeploymentPlanTest.scala
+++ src/test/scala/mesosphere/marathon/upgrade/DeploymentPlanTest.scala
@@ -2,11 +2,12 @@ package mesosphere.marathon.upgrade
 
 import mesosphere.marathon.MarathonSpec
 import mesosphere.marathon.api.v1.AppDefinition
-import mesosphere.marathon.state.{ ScalingStrategy, Group }
-import org.scalatest.Matchers
+import mesosphere.marathon.state.{ Timestamp, ScalingStrategy, Group }
+import org.scalatest.{ GivenWhenThen, Matchers }
 
-class DeploymentPlanTest extends MarathonSpec with Matchers {
+class DeploymentPlanTest extends MarathonSpec with Matchers with GivenWhenThen {
 
+  /*
   test("start from empty group") {
     val app = AppDefinition("app")
     val from = Group("group", ScalingStrategy(1, None), Set.empty)
@@ -20,18 +21,48 @@ class DeploymentPlanTest extends MarathonSpec with Matchers {
   }
 
   test("start from running group") {
-    val app = AppDefinition("app", "sleep 10")
-    val appUpdate = AppDefinition("app", "sleep 30")
-    val app2 = AppDefinition("app2", "cmd2")
-    val app3 = AppDefinition("app3", "cmd3")
+    val apps = Set(AppDefinition("app", "sleep 10"), AppDefinition("app2", "cmd2"), AppDefinition("app3", "cmd3"))
+    val update = Set(AppDefinition("app", "sleep 30"), AppDefinition("app2", "cmd2", instances = 10), AppDefinition("app4", "cmd4"))
 
-    val from = Group("group", ScalingStrategy(1, None), Set(app, app2))
-    val to = Group("group", ScalingStrategy(1, None), Set(appUpdate, app2, app3))
+    val from = Group("group", ScalingStrategy(1, None), apps)
+    val to = Group("group", ScalingStrategy(1, None), update)
     val plan = DeploymentPlan("plan", from, to)
 
     plan.toStart should have size 1
     plan.toRestart should have size 1
-    plan.toScale should have size 0
-    plan.toStop should have size 0
+    plan.toScale should have size 1
+    plan.toStop should have size 1
+  }
+  */
+
+  test("when updating dependencies, the correct order is computed") {
+
+    Given("Two aplication updates with command and scale changes")
+    val scaling = ScalingStrategy(0.75, Some(1))
+    val mongoId = "/test/database/mongo"
+    val serviceId = "/test/service/srv1"
+    val mongo = AppDefinition(mongoId, "mng1", instances = 4, version = Timestamp(0)) ->
+      AppDefinition(mongoId, "mng2", instances = 8)
+    val service = AppDefinition(serviceId, "srv1", instances = 4, version = Timestamp(0)) ->
+      AppDefinition(serviceId, "srv2", dependencies = Set("/test/database/mongo"), instances = 10)
+    val from: Group = Group("/test", scaling, groups = Set(
+      Group("/test/database", scaling, Set(mongo._1)),
+      Group("/test/service", scaling, Set(service._1))
+    ))
+
+    When("the deployment plan is computed")
+    val to: Group = Group("/test", scaling, groups = Set(
+      Group("/test/database", scaling, Set(mongo._2)),
+      Group("/test/service", scaling, Set(service._2))
+    ))
+    val plan = DeploymentPlan("plan", from, to)
+
+    Then("the deployment steps are correct")
+    plan.steps should have size 4
+    plan.steps(0).actions.toSet should be(Set(RestartApplication(mongo._1, 3, 6)))
+    plan.steps(1).actions.toSet should be(Set(RestartApplication(service._1, 3, 8)))
+    plan.steps(2).actions.toSet should be(Set(StopApplication(service._1), ScaleApplication(service._2, 10)))
+    plan.steps(3).actions.toSet should be(Set(StopApplication(mongo._1), ScaleApplication(mongo._2, 8)
+    ))
   }
 }
