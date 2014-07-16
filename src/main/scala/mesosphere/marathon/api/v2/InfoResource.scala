9ec8c156992bebac330daeb371cdd693ab3f8208
drexin <dario.rexin@r3-tech.de>
2014-07-16 10:58:39 +0200

    fixes #394 add leader and frameworkId to /v2/info

--- src/main/scala/mesosphere/marathon/api/v2/InfoResource.scala
+++ src/main/scala/mesosphere/marathon/api/v2/InfoResource.scala
@@ -3,18 +3,21 @@ package mesosphere.marathon.api.v2
 import javax.ws.rs.core.{ MediaType, Response }
 import javax.ws.rs.{ Consumes, GET, Path, Produces }
 
-import mesosphere.marathon.BuildInfo
+import com.google.inject.Inject
+import mesosphere.marathon.{ MarathonSchedulerService, BuildInfo }
 
 @Path("v2/info")
 @Consumes(Array(MediaType.APPLICATION_JSON))
-class InfoResource {
+class InfoResource @Inject() (schedulerService: MarathonSchedulerService) {
   @GET
   @Produces(Array(MediaType.APPLICATION_JSON))
   def index(): Response = {
     Response.ok(
       Map(
         "name" -> BuildInfo.name,
-        "version" -> BuildInfo.version
+        "version" -> BuildInfo.version,
+        "leader" -> schedulerService.getLeader,
+        "frameworkId" -> schedulerService.frameworkId.map(_.getValue)
       )
     ).build()
   }
