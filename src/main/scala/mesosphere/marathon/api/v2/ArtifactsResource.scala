c63f1797fbf57be4fde51f067a4cf726f45c35cc
Matthias Veit <matthias_veit@yahoo.de>
2014-07-24 11:21:30 +0200

    ArtifactsResource: use jetty's media mime mapping, which is also not very exhaustive

--- src/main/scala/mesosphere/marathon/api/v2/ArtifactsResource.scala
+++ src/main/scala/mesosphere/marathon/api/v2/ArtifactsResource.scala
@@ -8,6 +8,7 @@ import javax.ws.rs.core.{ MediaType, Response }
 
 import com.sun.jersey.core.header.FormDataContentDisposition
 import com.sun.jersey.multipart.FormDataParam
+import org.eclipse.jetty.http.MimeTypes
 
 import mesosphere.marathon.MarathonConf
 import mesosphere.marathon.api.RestResource
@@ -51,9 +52,8 @@ class ArtifactsResource @Inject() (val config: MarathonConf, val storage: Storag
       notFound(s"No artifact with path $path")
     }
     else {
-      val mediaType = path.replaceFirst(".*\\.", "")
       Response.
-        ok(item.inputStream(), mediaMime(mediaType)).
+        ok(item.inputStream(), mediaMime(path)).
         lastModified(new Date(item.lastModified)).
         header("Content-Length", item.length).
         build()
@@ -72,57 +72,6 @@ class ArtifactsResource @Inject() (val config: MarathonConf, val storage: Storag
     ok()
   }
 
-  private val mediaMime = Map(
-    "avi" -> "video/avi",
-    "bmp" -> "image/bmp",
-    "bz2" -> "application/x-bzip2",
-    "css" -> "text/css",
-    "dtd" -> "application/xml-dtd",
-    "doc" -> "application/msword",
-    "eot" -> "application/vnd.ms-fontobject",
-    "exe" -> "application/octet-stream",
-    "flac" -> "audio/flac",
-    "flv" -> "video/x-flv",
-    "gif" -> "image/gif",
-    "gz" -> "application/x-gzip",
-    "html" -> "text/html",
-    "ico" -> "image/x-icon",
-    "jar" -> "application/java-archive",
-    "jpg" -> "image/jpeg",
-    "js" -> "application/javascript",
-    "json" -> "application/json",
-    "midi" -> "audio/x-midi",
-    "mp3" -> "audio/mpeg",
-    "mp4" -> "video/mp4",
-    "mpeg" -> "video/mpeg",
-    "oga" -> "audio/ogg",
-    "ogg" -> "audio/ogg",
-    "ogx" -> "application/ogg",
-    "ogv" -> "video/ogg",
-    "otf" -> "application/x-font-otf",
-    "pdf" -> "application/pdf",
-    "pl" -> "application/x-perl",
-    "png" -> "image/png",
-    "ppt" -> "application/vnd.ms-powerpoint",
-    "ps" -> "application/postscript",
-    "qt" -> "video/quicktime",
-    "ra" -> "audio/x-pn-realaudio",
-    "ram" -> "audio/x-pn-realaudio",
-    "rdf" -> "application/rdf",
-    "rtf" -> "application/rtf",
-    "sit" -> "application/x-stuffit",
-    "svg" -> "image/svg+xml",
-    "swf" -> "application/x-shockwave-flash",
-    "tar" -> "gz application/x-tar",
-    "tgz" -> "application/x-tar",
-    "tiff" -> "image/tiff",
-    "tsv" -> "text/tab-separated-values",
-    "ttf" -> "application/x-font-ttf",
-    "txt" -> "text/plain",
-    "wav" -> "audio/wav",
-    "woff" -> "application/font-woff",
-    "xls" -> "application/vnd.ms-excel",
-    "xml" -> "application/xml",
-    "zip" -> "application/zip"
-  ).withDefaultValue("application/octet-stream")
+  private[this] val mimes = new MimeTypes()
+  def mediaMime(path: String) = Option(mimes.getMimeByExtension(path)).map(_.toString("UTF-8")).getOrElse("application/octet-stream")
 }
