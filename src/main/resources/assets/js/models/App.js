4752b6471e0652805d7db316c0101311d7077370
mlunoe <michael.lunoe@gmail.com>
2014-09-02 14:28:11 -0700

    Removing update from app

--- src/main/resources/assets/js/models/App.js
+++ src/main/resources/assets/js/models/App.js
@@ -16,7 +16,6 @@ define([
   var DEFAULT_HEALTH_MSG = "Unknown";
   var EDITABLE_ATTRIBUTES = ["cmd", "constraints", "container", "cpus", "env",
     "executor", "id", "instances", "mem", "disk", "ports", "uris"];
-  var UPDATEABLE_ATTRIBUTES = ["instances", "tasksRunning", "tasksStaged", "deployments"];
   var VALID_ID_PATTERN = "^/?(([a-z0-9]|[a-z0-9][a-z0-9\\-]*[a-z0-9])\\.)*([a-z0-9]|[a-z0-9][a-z0-9\\-]*[a-z0-9])$";
   var VALID_ID_REGEX = new RegExp(VALID_ID_PATTERN);
   var VALID_CONSTRAINTS = ["unique", "cluster", "group_by"];
@@ -140,20 +139,6 @@ define([
       }
 
       return data;
-    },
-
-    /* Updates only those attributes listed in `UPDATEABLE_ATTRIBUTES` to prevent
-     * showing values that cannot be changed.
-     */
-    update: function(attrs) {
-
-      var filteredAttributes = _.filter(UPDATEABLE_ATTRIBUTES, function(attr) {
-        return attrs[attr] != null;
-      });
-
-      var allowedAttrs = _.pick(attrs, filteredAttributes);
-
-      this.set(allowedAttrs);
     },
 
     /* Sends only those attributes listed in `EDITABLE_ATTRIBUTES` to prevent
