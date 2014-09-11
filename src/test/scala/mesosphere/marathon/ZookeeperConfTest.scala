67def5a8009bfaa116e9105de8b0c7903267ab8d
Xu Lijian <xulijian@qiyi.com>
2014-09-12 03:41:03 +0800

    Allow user and password in Zookeeper URL
    This allow us to use a Zookeeper URL like: zk://user1:pass1@host1:123,host2,host3:312/path

--- src/test/scala/mesosphere/marathon/ZookeeperConfTest.scala
+++ src/test/scala/mesosphere/marathon/ZookeeperConfTest.scala
@@ -32,6 +32,14 @@ class ZookeeperConfTest extends MarathonSpec {
     assert(opts.zkPath == "/path")
   }
 
+  test("urlParameterWithAuthGetParsed") {
+    val url = "zk://user1:pass1@host1:123,host2,host3:312/path"
+    val opts = conf("--zk", url)
+    assert(opts.zkURL == url)
+    assert(opts.zkHosts == "host1:123,host2,host3:312")
+    assert(opts.zkPath == "/path")
+  }
+
   test("wrongURLIsNotParsed") {
     assert(Try(conf("--zk", "zk://host1:foo/path")).isFailure, "No port number")
     assert(Try(conf("--zk", "zk://host1")).isFailure, "No path")
