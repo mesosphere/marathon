7c2bc3c07283406e166c0c92df3eac612204e717
Dario Rexin <dario.rexin@r3-tech.de>
2014-11-18 13:38:12 -0800

    fixes #791 - Handle new TaskState value: TASK_ERROR

---
+++ src/test/scala/mesosphere/marathon/event/HistoryActorTest.scala
@@ -0,0 +1,92 @@
+package mesosphere.marathon.event
+
+import akka.actor.{ ActorRef, ActorSystem, Props }
+import akka.testkit.{ ImplicitSender, TestActorRef, TestKit }
+import mesosphere.marathon.MarathonSpec
+import mesosphere.marathon.state.PathId._
+import mesosphere.marathon.state.{ TaskFailure, TaskFailureRepository, Timestamp }
+import org.apache.mesos.Protos.TaskState
+import org.mockito.Matchers.any
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{ BeforeAndAfterAll, Matchers }
+
+class HistoryActorTest
+    extends TestKit(ActorSystem("System"))
+    with MarathonSpec
+    with MockitoSugar
+    with BeforeAndAfterAll
+    with Matchers
+    with ImplicitSender {
+  import org.apache.mesos.Protos.TaskState._
+
+  var historyActor: ActorRef = _
+  var failureRepo: TaskFailureRepository = _
+
+  before {
+    failureRepo = mock[TaskFailureRepository]
+    historyActor = TestActorRef(Props(
+      new HistoryActor(system.eventStream, failureRepo)
+    ))
+  }
+
+  test("Store TASK_FAILED") {
+    val message = statusMessage(TASK_FAILED)
+    historyActor ! message
+
+    verify(failureRepo).store(message.appId, TaskFailure.FromMesosStatusUpdateEvent(message).get)
+  }
+
+  test("Store TASK_ERROR") {
+    val message = statusMessage(TASK_ERROR)
+    historyActor ! message
+
+    verify(failureRepo).store(message.appId, TaskFailure.FromMesosStatusUpdateEvent(message).get)
+  }
+
+  test("Store TASK_LOST") {
+    val message = statusMessage(TASK_LOST)
+    historyActor ! message
+
+    verify(failureRepo).store(message.appId, TaskFailure.FromMesosStatusUpdateEvent(message).get)
+  }
+
+  test("Ignore TASK_RUNNING") {
+    val message = statusMessage(TASK_RUNNING)
+    historyActor ! message
+
+    verify(failureRepo, times(0)).store(any(), any())
+  }
+
+  test("Ignore TASK_FINISHED") {
+    val message = statusMessage(TASK_FINISHED)
+    historyActor ! message
+
+    verify(failureRepo, times(0)).store(any(), any())
+  }
+
+  test("Ignore TASK_KILLED") {
+    val message = statusMessage(TASK_KILLED)
+    historyActor ! message
+
+    verify(failureRepo, times(0)).store(any(), any())
+  }
+
+  test("Ignore TASK_STAGING") {
+    val message = statusMessage(TASK_STAGING)
+    historyActor ! message
+
+    verify(failureRepo, times(0)).store(any(), any())
+  }
+
+  private def statusMessage(state: TaskState) = MesosStatusUpdateEvent(
+    slaveId = "slaveId",
+    taskId = "taskId",
+    taskStatus = state.name(),
+    message = "message",
+    appId = "appId".toPath,
+    host = "host",
+    ports = Nil,
+    version = Timestamp.now().toString
+  )
+}
