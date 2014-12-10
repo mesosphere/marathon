58f08674184a88d2213fedcf01888d1008c5615b
Lukas Loesche <lloesche@gmail.com>
2014-12-10 16:27:34 -0700

    string cleanup

--- servicerouter/haproxycfggenerator.py
+++ servicerouter/haproxycfggenerator.py
@@ -2,6 +2,54 @@
 import re
 from operator import attrgetter
 
+HAPROXY_HEAD = '''global
+  daemon
+  log 127.0.0.1 local0
+  log 127.0.0.1 local1 notice
+  maxconn 4096
+
+defaults
+  log            global
+  retries             3
+  maxconn          2000
+  timeout connect  5000
+  timeout client  50000
+  timeout server  50000
+
+listen stats
+  bind 127.0.0.1:9090
+  balance
+  mode http
+  stats enable
+  stats auth admin:admin
+'''
+
+HAPROXY_HTTP_FRONTEND_HEAD = '''
+frontend marathon_http_in
+  bind *:80
+  mode http
+'''
+
+HAPROXY_HTTP_FRONTEND_ACL = '''  acl host_{0} hdr(host) -i {1}
+  use_backend {2} if host_{0}
+'''
+
+HAPROXY_FRONTEND_HEAD = '''
+frontend {0}
+  bind *:{1}
+'''
+
+HAPROXY_BACKEND_HEAD = '''
+backend {0}
+  balance roundrobin
+'''
+
+HAPROXY_BACKEND_HTTP_OPTIONS = '''  option forwardfor
+  http-request set-header X-Forwarded-Port %[dst_port]
+  http-request add-header X-Forwarded-Proto https if { ssl_fc }
+'''
+
+
 class MarathonBackend(object):
   def __init__(self, host, port):
     self.host = host
@@ -33,66 +81,25 @@ apps = [
   ]
 
 def config(apps):
+  config = HAPROXY_HEAD
 
-  config = str()
+  http_frontends = HAPROXY_HTTP_FRONTEND_HEAD
   frontends = str()
   backends = str()
-
-  head = '''global
-  daemon
-  log 127.0.0.1 local0
-  log 127.0.0.1 local1 notice
-  maxconn 4096
-
-defaults
-  log            global
-  retries             3
-  maxconn          2000
-  timeout connect  5000
-  timeout client  50000
-  timeout server  50000
-
-listen stats
-  bind 127.0.0.1:9090
-  balance
-  mode http
-  stats enable
-  stats auth admin:admin
-'''
-
-  http_frontends = '''
-frontend marathon_http_in
-  bind *:80
-  mode http
-'''
 
   for app in sorted(apps, key = attrgetter('appId', 'servicePort')):
     listener = app.appId[1:].replace('/', '_') + '_' + str(app.servicePort)
 
-    frontends += '''
-frontend {0}
-  bind *:{1}
-'''.format(listener, app.servicePort)
+    frontends += HAPROXY_FRONTEND_HEAD.format(listener, app.servicePort)
 
-    backends += '''
-backend {0}
-  balance roundrobin
-'''.format(listener)
+    backends += HAPROXY_BACKEND_HEAD.format(listener)
 
     # if it's a HTTP service
     if app.hostname:
       cleanedUpHostname = re.sub(r'[^a-zA-Z0-9\-]', '_', app.hostname)
-
-      http_frontends += '''  acl host_{0} hdr(host) -i {1}
-  use_backend {2} if host_{0}
-'''.format(cleanedUpHostname, app.hostname, listener)
-
+      http_frontends += HAPROXY_HTTP_FRONTEND_ACL.format(cleanedUpHostname, app.hostname, listener)
       frontends += "  mode http\n"
-
-      backends += '''  option forwardfor
-  http-request set-header X-Forwarded-Port %[dst_port]
-  http-request add-header X-Forwarded-Proto https if { ssl_fc }
-'''
+      backends += HAPROXY_BACKEND_HTTP_OPTIONS
     else:
       frontends += "  mode tcp\n"
 
@@ -102,7 +109,6 @@ backend {0}
       backends += "  server {0}:{1}\n".format(backend.host, backend.port)
 
 
-  config += head
   config += http_frontends
   config += frontends
   config += backends
