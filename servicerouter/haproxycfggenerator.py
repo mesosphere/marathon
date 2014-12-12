c2769690d046255e083437421f0892fd856517a4
Lukas Loesche <lloesche@gmail.com>
2014-12-12 00:57:24 -0700

    added logging

--- servicerouter/haproxycfggenerator.py
+++ servicerouter/haproxycfggenerator.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 import re
+import sys
 import os.path
+import logging
+from logging.handlers import SysLogHandler
 from subprocess import call
 from operator import attrgetter
 from tempfile import mkstemp
 from shutil import move
+
+if sys.platform == "darwin":
+  syslogSocket = "/var/run/syslog"
+else:
+  syslogSocket = "/dev/log"
+
+logger = logging.getLogger('haproxycfggenerator')
+logger.setLevel(logging.DEBUG)
+
+syslogHandler = SysLogHandler(syslogSocket)
+consoleHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+syslogHandler.setFormatter(formatter)
+consoleHandler.setFormatter(formatter)
+syslogHandler.setLevel(logging.ERROR)
+logger.addHandler(syslogHandler)
+logger.addHandler(consoleHandler)
 
 HAPROXY_CONFIG = '/etc/haproxy/haproxy.cfg'
 
@@ -103,7 +123,7 @@ apps = [
   ]
 
 def config(apps):
-  print "generating config"
+  logger.info("generating config")
   config = HAPROXY_HEAD
 
   http_frontends = HAPROXY_HTTP_FRONTEND_HEAD
@@ -111,8 +131,11 @@ def config(apps):
   backends = str()
 
   for app in sorted(apps, key=attrgetter('appId', 'servicePort')):
+    logger.debug("configuring app %s", app.appId)
     backend = app.appId[1:].replace('/', '_') + '_' + str(app.servicePort)
 
+    logger.debug("frontend at %s:%d with backend %s",
+      app.bindAddr, app.servicePort, backend)
     frontends += HAPROXY_FRONTEND_HEAD.format(
         bindAddr=app.bindAddr,
         backend=backend,
@@ -122,8 +145,8 @@ def config(apps):
       )
 
     if app.redirectHttpToHttps:
+      logger.debug("rule to redirect http to https traffic")
       frontends += HAPROXY_BACKEND_REDIRECT_HTTP_TO_HTTPS
-
 
     backends += HAPROXY_BACKEND_HEAD.format(backend=backend)
 
@@ -131,6 +154,7 @@ def config(apps):
     # of our haproxy config
     # TODO(lukas): Check if the hostname is already defined by another service
     if app.hostname:
+      logger.debug("adding virtual host for app with hostname %s", app.hostname)
       cleanedUpHostname = re.sub(r'[^a-zA-Z0-9\-]', '_', app.hostname)
       http_frontends += HAPROXY_HTTP_FRONTEND_ACL.format(
           cleanedUpHostname=cleanedUpHostname,
@@ -140,11 +164,13 @@ def config(apps):
       backends += HAPROXY_BACKEND_HTTP_OPTIONS
 
     if app.sticky:
+      logger.debug("turning on sticky sessions")
       backends += HAPROXY_BACKEND_STICKY_OPTIONS
 
     frontends += HAPROXY_FRONTEND_BACKEND_GLUE.format(backend=backend)
 
     for backendServer in sorted(app.backends, key=attrgetter('host', 'port')):
+      logger.debug("backend server at %s:%d", backendServer.host, backendServer.port)
       serverName = re.sub(r'[^a-zA-Z0-9\-]', '_', backendServer.host+'_'+str(backendServer.port))
       backends += HAPROXY_BACKEND_SERVER_OPTIONS.format(
           host=backendServer.host,
@@ -161,35 +187,42 @@ def config(apps):
   return config
 
 def reloadConfig():
-  print "reloading"
+  logger.debug("trying to find out how to reload the configuration")
   if os.path.isfile('/etc/init/haproxy.conf'):
+    logger.debug("we seem to be running on an Upstart based system")
     reloadCommand = ['reload', 'haproxy']
   elif ( os.path.isfile('/usr/lib/systemd/system/haproxy.service')
       or os.path.isfile('/etc/systemd/system/haproxy.service')
     ):
+    logger.debug("we seem to be running on systemd based system")
     reloadCommand = ['systemctl', 'reload', 'haproxy']
   else:
+    logger.debug("we seem to be running on a sysvinit based system")
     reloadCommand = ['/etc/init.d/haproxy', 'reload']
 
+  logger.info("reloading using %s", " ".join(reloadCommand))
   return call(reloadCommand)
 
 def writeConfig(config, configFile):
-  print "writing config"
   fd, haproxyTempConfigFile = mkstemp()
+  logger.debug("writing config to temp file %s", haproxyTempConfigFile)
   haproxyTempConfig = os.fdopen(fd, 'w')
   haproxyTempConfig.write(config)
   haproxyTempConfig.close()
+  logger.debug("moving temp file %s to %s", haproxyTempConfigFile, HAPROXY_CONFIG)
   move(haproxyTempConfigFile, HAPROXY_CONFIG)
 
 def compareWriteAndReloadConfig(config, configFile):
   runningConfig = str()
   try:
+    logger.debug("reading running config from %s", configFile)
     with open(configFile, "r") as configFile:
       runningConfig = configFile.read()
   except IOError:
-    print "couldn't open config file for reading"
+    logger.warning("couldn't open config file for reading")
 
   if runningConfig != config:
+    logger.info("running config is different from generated config - reloading")
     writeConfig(config, configFile)
     reloadConfig()
 
