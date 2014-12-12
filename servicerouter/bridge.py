b2e4fe4a35d5df165fb690090d6c5e920bbea2c2
Cody Maloney <cody@mesosphere.io>
2014-12-11 17:48:30 -0700

    Full bridge sketch

--- servicerouter/bridge.py
+++ servicerouter/bridge.py
@@ -44,33 +44,40 @@ class MarathonBackend(object):
     self.host = host
     self.port = port
 
+  def __hash__(self):
+    return hash((self.host, self.port))
+
+class MarathonService(object):
+  def __init__(self, appId, servicePort):
+    self.appId = appId
+    self.servicePort = servicePort
+    self.backends = set()
+    self.hostname = None
+
+  def add_backend(self, host, port):
+    self.backends.insert(MarathonBackend(host, port))
+
+  def __hash__(self):
+    return hash(self.servicePort)
+
+  def __eq__(self, other):
+    return self.servicePort == other.servicePort
 
 class MarathonApp(object):
-  def __init__(self, appId, hostname, servicePort, backends):
+  def __init__(self, marathon, appId):
+    self.app = marathon.get_app(appId)
     self.appId = appId
-    self.hostname = hostname
-    self.servicePort = servicePort
-    self.backends = backends
 
-apps = [
-    MarathonApp('/mm/application/portal', 'app.mesosphere.com', 9000, [
-      MarathonBackend('srv3.hw.ca1.mesosphere.com', 31006),
-      MarathonBackend('srv2.hw.ca1.mesosphere.com', 31671),
-      MarathonBackend('srv2.hw.ca1.mesosphere.com', 31030),
-      MarathonBackend('srv4.hw.ca1.mesosphere.com', 31006)
-    ]),
-    MarathonApp('/mm/service/collector', 'collector.mesosphere.com', 7070, [
-      MarathonBackend('srv4.hw.ca1.mesosphere.com', 31005)
-    ]),
-    MarathonApp('/mm/service/collector', 'collector.mesosphere.com', 9990, [
-      MarathonBackend('srv4.hw.ca1.mesosphere.com', 31006)
-    ])
-  ]
+    #port -> MarathonService
+    self.services = dict()
 
+  def __hash__(self):
+    return hash(self.appId)
 
-#TODO(cmaloney): This is a generic marathon api wrapper
+  def __eq__(self, other):
+    return self.appId == other.appId
+
 class Marathon(object):
-  # TODO(cmaloney): Pass in configuration.
   def __init__(self, hosts):
     self.__hosts = hosts
 
@@ -114,52 +121,42 @@ class Marathon(object):
     return self.api_req('DELETE', ['eventSubscriptions'], params={'callbackUrl': callbackUrl})
 
 
-"""
-{
-  "eventType": "status_update_event",
-  "timestamp": "2014-03-01T23:29:30.158Z",
-  "slaveId": "20140909-054127-177048842-5050-1494-0",
-  "taskId": "my-app_0-1396592784349",
-  "taskStatus": "TASK_RUNNING",
-  "appId": "/my-app",
-  "host": "slave-1234.acme.org",
-  "ports": [31372],
-  "version": "2014-04-04T06:26:23.051Z"
-}
-"""
-
-
 class MarathonEventSubscriber(object):
   def __init__(self, marathon, addr):
     self.__marathon = marathon
-    self.__apps = set()
+    #NOTE: Convert to a list before handing to haproxycfggenerator
+    #appId -> MarathonApp
+    self.__apps = dict()
     marathon.add_subscriber(addr)
 
     # Fetch the base data
     self.update_from_tasks()
 
-  def update_from_tasks(self):
+  def reset_from_tasks(self):
     tasks = marathon.tasks()
-    # For each task, extract self.__apps
-    # and the backends within each app.
-    raise NotImplementedError()
 
+    self.__apps = dict()
 
-  def handle_event(self, json):
-    # On "eventType": "status_update_event"
-    # Update self.__apps
-    raise NotImplementedError()
+    for task in tasks:
+      # For each task, extract self.__apps
+      # and the backends within each app.
+      for i in xrange(len(task.servicePorts)):
+        servicePort = task.servicePorts[i]
+        port = task.ports[i]
 
+        if task.appId not in self.__apps:
+          self.__apps[task.appId] = MarathonApp(task.appId)
 
-#TODO(cmaloney): servicePorts
-class Task(object):
-  def __init__(self, host, ports):
-    self.host = host
-    self.ports = ports
+        app = self.__apps[appId]
+        if task.servicePort not in app.services:
+          app.services[task.servicePort] = MarathonService(task.appId, servicePort)
 
+        service = app.services[task.servicePort]
 
-class App(object):
-  def __init__(self, appId, servicePorts, tasks):
-    self.appId = appId
-    self.servicePorts = servicePorts
-    self.tasks = tasks
+        service.add_backend(task.host, port)
+
+  def handle_event(self, event):
+    if event.eventType == 'status_update_event':
+      #TODO (cmaloney): Handle events more intelligently so that we add/remove things well
+      self.reset_from_tasks()
+
