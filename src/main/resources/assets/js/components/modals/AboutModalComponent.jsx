d9d82c1176aa5a6bfeffc36c61aaef825c48e717
mlunoe <michael.lunoe@gmail.com>
2015-01-11 00:16:28 -0800

    Reverting back style changes in AboutModalComponent

--- src/main/resources/assets/js/components/modals/AboutModalComponent.jsx
+++ src/main/resources/assets/js/components/modals/AboutModalComponent.jsx
@@ -55,60 +55,34 @@ var Info = require("../../models/Info");
             onDestroy={this.props.onDestroy}
             ref="modalComponent"
             size="lg">
-          <div
-              className="modal-header modal-header-blend">
-            <button
-                type="button"
-                className="close"
-                aria-hidden="true"
-                onClick={this.destroy}>
-              &times;
-            </button>
+          <div className="modal-header modal-header-blend">
+            <button type="button" className="close"
+              aria-hidden="true" onClick={this.destroy}>&times;</button>
             <h3 className="modal-title">
-              <img
-                  width="160"
-                  height="27"
-                  alt="Marathon"
-                  src="/img/marathon-logo.png" />
-              <small
-                  className="text-muted"
-                  style={{"marginLeft": "1em"}}>
+              <img width="160" height="27" alt="Marathon" src="/img/marathon-logo.png" />
+              <small className="text-muted" style={{"margin-left": "1em"}}>
                 Version {this.state.info.get("version")}
               </small>
             </h3>
           </div>
           <div className="modal-body">
-            <dl
-                className="dl-horizontal dl-horizontal-lg">
+            <dl className="dl-horizontal dl-horizontal-lg">
               <dt title="framework_id">Framework Id</dt>
               <dd>
-                {
-                  this.state.info.get("framework_id") ||
-                  <UNSPECIFIED_NODE />
-                }
+                {this.state.info.get("framework_id") || <UNSPECIFIED_NODE />}
               </dd>
               <dt title="leader">Leader</dt>
               <dd>
-                {
-                  this.state.info.get("leader") ||
-                  <UNSPECIFIED_NODE />
-                }
+                {this.state.info.get("leader") || <UNSPECIFIED_NODE />}
               </dd>
               <dt title="name">Name</dt>
               <dd>
-                {
-                  this.state.info.get("name") ||
-                  <UNSPECIFIED_NODE />
-                }
+                {this.state.info.get("name") || <UNSPECIFIED_NODE />}
               </dd>
             </dl>
-            <h5 title="marathon_config">
-              Marathon Config
-            </h5>
+            <h5 title="marathon_config">Marathon Config</h5>
             <ObjectDlComponent object={marathonConfig} />
-            <h5 title="zookeeper_config">
-              ZooKeeper Config
-            </h5>
+            <h5 title="zookeeper_config">ZooKeeper Config</h5>
             <ObjectDlComponent object={zookeeperConfig} />
           </div>
         </ModalComponent>
