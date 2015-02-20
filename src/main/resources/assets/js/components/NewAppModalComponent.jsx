51cccac9b3af88604c268dd683f7a6de35c31bdb
lfergon <lfergon@gmail.com>
2015-02-20 05:07:44 +0100

    modal component modifications

--- src/main/resources/assets/js/components/NewAppModalComponent.jsx
+++ src/main/resources/assets/js/components/NewAppModalComponent.jsx
@@ -1,5 +1,5 @@
 /** @jsx React.DOM */
-
+ 
 var $ = require("jquery");
 var _ = require("underscore");
 var React = require("react/addons");
@@ -7,12 +7,12 @@ var BackboneMixin = require("../mixins/BackboneMixin");
 var App = require("../models/App");
 var FormGroupComponent = require("../components/FormGroupComponent");
 var ModalComponent = require("../components/ModalComponent");
-
+ 
 function ValidationError(attribute, message) {
   this.attribute = attribute;
   this.message = message;
 }
-
+ 
 var NewAppModalComponent = React.createClass({
   displayName: "NewAppModalComponent",
   mixins: [BackboneMixin],
@@ -20,38 +20,56 @@ var NewAppModalComponent = React.createClass({
     onCreate: React.PropTypes.func,
     onDestroy: React.PropTypes.func
   },
-
+ 
   getDefaultProps: function () {
     return {
       onCreate: $.noop,
       onDestroy: $.noop
     };
   },
-
+ 
   getInitialState: function () {
     return {
       model: new App(),
+      parametersCount: 0,
+      volumesCount: 0,
       errors: []
     };
   },
-
+ 
   destroy: function () {
     // This will also call `this.props.onDestroy` since it is passed as the
     // callback for the modal's `onDestroy` prop.
     this.refs.modalComponent.destroy();
   },
-
+ 
+  addParameters: function () {
+    this.setState({parametersCount: this.state.parametersCount + 1});
+  },
+ 
+  removeParameters: function () {
+    this.setState({parametersCount: this.state.parametersCount - 1});
+  },
+ 
+  addVolumes: function () {
+    this.setState({volumesCount: this.state.volumesCount + 1});
+  },
+ 
+  removeVolumes: function () {
+    this.setState({volumesCount: this.state.volumesCount - 1});
+  },
+ 
   getResource: function () {
     return this.state.model;
   },
-
+ 
   clearValidation: function () {
     this.setState({errors: []});
   },
-
+ 
   validateResponse: function (response) {
     var errors;
-
+ 
     if (response.status === 422 && response.responseJSON != null &&
         _.isArray(response.responseJSON.errors)) {
       errors = response.responseJSON.errors.map(function (e) {
@@ -74,30 +92,30 @@ var NewAppModalComponent = React.createClass({
         )
       ];
     }
-
+ 
     this.setState({errors: errors});
   },
-
+ 
   onSubmit: function (event) {
     event.preventDefault();
-
+ 
     var attrArray = $(event.target).serializeArray();
     var modelAttrs = {};
-
+ 
     for (var i = 0; i < attrArray.length; i++) {
       var val = attrArray[i];
       if (val.value !== "") {
         modelAttrs[val.name] = val.value;
       }
     }
-
+ 
     // URIs should be an Array of Strings.
     if ("uris" in modelAttrs) {
       modelAttrs.uris = modelAttrs.uris.split(",");
     } else {
       modelAttrs.uris = [];
     }
-
+ 
     // Constraints should be an Array of Strings.
     if ("constraints" in modelAttrs) {
       var constraintsArray = modelAttrs.constraints.split(",");
@@ -107,7 +125,7 @@ var NewAppModalComponent = React.createClass({
         });
       });
     }
-
+ 
     // Ports should always be an Array.
     if ("ports" in modelAttrs) {
       var portStrings = modelAttrs.ports.split(",");
@@ -118,7 +136,7 @@ var NewAppModalComponent = React.createClass({
     } else {
       modelAttrs.ports = [];
     }
-
+ 
     // mem, cpus, and instances are all Numbers and should be parsed as such.
     if ("mem" in modelAttrs) {
       modelAttrs.mem = parseFloat(modelAttrs.mem);
@@ -132,9 +150,14 @@ var NewAppModalComponent = React.createClass({
     if ("instances" in modelAttrs) {
       modelAttrs.instances = parseInt(modelAttrs.instances, 10);
     }
-
+    if ("privileges" in modelAttrs) {
+      if (modelAttrs.privileges === "true") {
+        modelAttrs.instances = true;
+      }
+    }
+ 
     this.state.model.set(modelAttrs);
-
+ 
     if (this.state.model.isValid()) {
       this.props.onCreate(
         this.state.model,
@@ -150,7 +173,7 @@ var NewAppModalComponent = React.createClass({
             this.clearValidation();
             this.destroy();
           }.bind(this),
-
+ 
           // Wait to add the model to the collection until a successful
           // response code is received from the server.
           wait: true
@@ -158,21 +181,26 @@ var NewAppModalComponent = React.createClass({
       );
     }
   },
-
+ 
   render: function () {
     var model = this.state.model;
     var errors = this.state.errors;
-
+ 
     var generalErrors = errors.filter(function (e) {
-        return (e.attribute === "general");
-      });
-
+      return (e.attribute === "general");
+    });
+ 
+    var inputs = [];
+    for (var i=0;i<this.state.parametersCount; i++) {
+      inputs.push(i);
+    }
+ 
     /* jshint trailing:false, quotmark:false, newcap:false */
     /* jscs:disable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
     var errorBlock = generalErrors.map(function (error, i) {
       return <p key={i} className="text-danger"><strong>{error.message}</strong></p>;
     });
-
+ 
     return (
       <ModalComponent ref="modalComponent" onDestroy={this.props.onDestroy}>
         <form method="post" role="form" onSubmit={this.onSubmit}>
@@ -259,9 +287,105 @@ var NewAppModalComponent = React.createClass({
                 errors={errors}>
               <input />
             </FormGroupComponent>
+            <hr />
+            <h4>Hide docker container settings</h4>
+            <FormGroupComponent
+                attribute="image"
+                help='Image for docker'
+                label="Image"
+                model={model}
+                errors={errors}>
+              <input />
+            </FormGroupComponent>
+            <FormGroupComponent
+                attribute="network"
+                help='Network host'
+                label="Network"
+                model={model}
+                errors={errors}>
+              <select>
+                <option value="host">Host</option>
+                <option value="host_two">Host two</option>
+              </select>
+            </FormGroupComponent>
+            <hr />
+            <h4>Privileges</h4>
+            <FormGroupComponent
+                attribute="privileges"
+                help='Select to give this container access to all devices on the host'
+                label="Extend runtime privileges to this container"
+                model={model}
+                errors={errors}>
+              <input type="checkbox" value="true"/>
+            </FormGroupComponent>
+            <hr />
+            <h4>Parameters</h4>
+            <FormGroupComponent
+                attribute="key"
+                label="Key"
+                model={model}
+                errors={errors}>
+              <input />
+            </FormGroupComponent>
+            <FormGroupComponent
+                attribute="value"
+                label="Value"
+                model={model}
+                errors={errors}>
+              <input />
+            </FormGroupComponent>
+            {inputs.map(function (result) {
+              return <FormGroupComponent
+                attribute="key"
+                label="Key"
+                model={model}
+                errors={errors}>
+                <input />
+              </FormGroupComponent>;
+            })}
+            <button className="btn btn-default" type="button" onClick={this.addParameters}>
+              +
+            </button>
+            <button className="btn btn-default" type="button" onClick={this.removeParameters}>
+              -
+            </button>
+            <hr />
+            <h4>Volumes</h4>
+            <FormGroupComponent
+                attribute="container_path"
+                label="Container path"
+                model={model}
+                errors={errors}>
+              <input />
+            </FormGroupComponent>
+            <FormGroupComponent
+                attribute="host_path"
+                label="Host path"
+                model={model}
+                errors={errors}>
+              <input />
+            </FormGroupComponent>
+            <FormGroupComponent
+                attribute="mode"
+                label="Mode"
+                model={model}
+                errors={errors}>
+              <select>
+                <option value="read">Read only</option>
+                <option value="write">Write - Read</option>
+                <option value="admin">Admin</option>
+              </select>
+            </FormGroupComponent>
+            <button className="btn btn-default" type="button" onClick={this.addVolumes}>
+              +
+            </button>
+            <button className="btn btn-default" type="button" onClick={this.removeVolumes}>
+              -
+            </button>
             <div>
               {errorBlock}
-              <input type="submit" className="btn btn-success" value="+ Create" /> <button className="btn btn-default" type="button" onClick={this.destroy}>
+              <input type="submit" className="btn btn-success" value="+ Create" /> 
+              <button className="btn btn-default" type="button" onClick={this.destroy}>
                 Cancel
               </button>
             </div>
@@ -271,5 +395,5 @@ var NewAppModalComponent = React.createClass({
     );
   }
 });
-
+ 
 module.exports = NewAppModalComponent;
