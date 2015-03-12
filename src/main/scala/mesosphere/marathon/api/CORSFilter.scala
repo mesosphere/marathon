7e5cfb650f81c4e6e5d18d7e0dd3f77a301fa1ba
mlunoe <michael.lunoe@gmail.com>
2015-03-12 12:20:37 -0700

    Adding CORS headers

---
+++ src/main/scala/mesosphere/marathon/api/CORSFilter.scala
@@ -0,0 +1,36 @@
+package mesosphere.marathon.api
+
+import javax.servlet._
+import javax.servlet.http.{ HttpServletRequest, HttpServletResponse }
+
+import scala.collection.JavaConverters._
+
+class CORSFilter extends Filter {
+  override def init(filterConfig: FilterConfig): Unit = {}
+
+  override def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain): Unit = {
+
+    response match {
+      case httpResponse: HttpServletResponse =>
+        val httpRequest = request.asInstanceOf[HttpServletRequest]
+        val static = ("Accept,Content-Type,Referer,Origin,Connection,Cache-Control,Access-Control-Request-Headers," +
+          "Pragma,Access-Control-Request-Method,Accept-Language,Accept-Encoding,User-Agent,Host,Accept-Language," +
+          "Accept-Encoding,Content-Length,Origin,X-DevTools-Emulate-Network-Conditions-Client-Id").split(",")
+        val staticSet = static.map(_.toLowerCase).toSet
+        val control = httpRequest.getHeaders("Access-Control-Request-Headers").asScala
+          .flatMap(_.split(","))
+          .filter(hd => staticSet.contains(hd.toLowerCase))
+
+        //TODO: Michael wanted to have this one specific, but it should be "*"
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*")
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS")
+        httpResponse.setHeader("Access-Control-Max-Age", "86400")
+        httpResponse.setHeader("Access-Control-Allow-Headers", (static ++ control).toList.distinct.mkString(", "))
+      case _ => // ignore other responses
+
+    }
+    chain.doFilter(request, response)
+  }
+
+  override def destroy(): Unit = {}
+}
