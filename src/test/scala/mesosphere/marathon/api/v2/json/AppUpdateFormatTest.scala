7140639afd46b1b4c8faff1c1270cb7ffca17570
drexin <dario.rexin@r3-tech.de>
2015-04-02 11:20:54 +0200

    fixes #1269 - Reject app definition with 'cpus' less than or equal to 0

--- src/test/scala/mesosphere/marathon/api/v2/json/AppUpdateFormatTest.scala
+++ src/test/scala/mesosphere/marathon/api/v2/json/AppUpdateFormatTest.scala
@@ -2,16 +2,28 @@ package mesosphere.marathon.api.v2.json
 
 import mesosphere.marathon.MarathonSpec
 import mesosphere.marathon.api.v2.AppUpdate
+import org.scalatest.Matchers
 import play.api.libs.json.{ Json, JsResultException }
 
-class AppUpdateFormatTest extends MarathonSpec {
+class AppUpdateFormatTest extends MarathonSpec with Matchers {
   import Formats._
 
   // regression test for #1176
   test("should fail if id is /") {
     val json = """{"id": "/"}"""
-    intercept[JsResultException] {
-      Json.parse(json).as[AppUpdate]
-    }
+    a[JsResultException] shouldBe thrownBy { Json.parse(json).as[AppUpdate] }
+  }
+
+  test("FromJSON should fail when using / as an id") {
+    val json = Json.parse(""" { "id": "/" }""")
+    a[JsResultException] shouldBe thrownBy { json.as[AppUpdate] }
+  }
+
+  test("FromJSON should fail when 'cpus' is less than or equal to 0") {
+    var json1 = Json.parse(""" { "id": "test", "cpus": 0.0 }""")
+    a[JsResultException] shouldBe thrownBy { json1.as[AppUpdate] }
+
+    val json2 = Json.parse(""" { "id": "test", "cpus": -1.0 }""")
+    a[JsResultException] shouldBe thrownBy { json2.as[AppUpdate] }
   }
 }
