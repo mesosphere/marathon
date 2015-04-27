2bb61b9abdb834c720943feacc97173ddb09c08b
Dario Rexin <dario.rexin@r3-tech.de>
2015-04-27 17:11:29 +0200

    fixes #1431 - On app deletion only remove app definition from repo when all tasks are killed

--- src/main/scala/mesosphere/marathon/upgrade/AppStopActor.scala
+++ src/main/scala/mesosphere/marathon/upgrade/AppStopActor.scala
@@ -1,18 +1,16 @@
 package mesosphere.marathon.upgrade
 
 import akka.event.EventStream
-import mesosphere.marathon.SchedulerActions
 import mesosphere.marathon.event.MesosStatusUpdateEvent
 import mesosphere.marathon.state.{ AppDefinition, PathId }
 import mesosphere.marathon.tasks.TaskTracker
-import org.apache.mesos.SchedulerDriver
+import org.apache.mesos.{ Protos, SchedulerDriver }
 
 import scala.collection.mutable
 import scala.concurrent.Promise
 
 class AppStopActor(
     val driver: SchedulerDriver,
-    scheduler: SchedulerActions,
     val taskTracker: TaskTracker,
     val eventBus: EventStream,
     app: AppDefinition,
@@ -24,6 +22,6 @@ class AppStopActor(
 
   def initializeStop(): Unit = {
     eventBus.subscribe(self, classOf[MesosStatusUpdateEvent])
-    scheduler.stopApp(driver, app)
+    for (id <- idsToKill) driver.killTask(Protos.TaskID.newBuilder.setValue(id).build())
   }
 }
