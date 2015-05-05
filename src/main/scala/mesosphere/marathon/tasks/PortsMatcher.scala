e622a4648ec6b5805f72c3f0d2e4b7087be569b0
Sanjana Joshi Bhat <sanjanab@yahoo-inc.com>
2015-05-05 11:30:13 -0700

    This change may not be needed

--- src/main/scala/mesosphere/marathon/tasks/PortsMatcher.scala
+++ src/main/scala/mesosphere/marathon/tasks/PortsMatcher.scala
@@ -116,13 +116,7 @@ class PortsMatcher(app: AppDefinition, offer: Offer) extends Logging {
           if (!availablePorts.hasNext) throw PortResourceException(
             s"Insufficient ports in offer for app [${app.id}]"
           )
-          val nextPort = availablePorts.next
-          if(containerPort == 0) {
-            PortMapping(nextPort, nextPort, servicePort, protocol)
-          }
-          else {
-            PortMapping(containerPort, nextPort, servicePort, protocol)
-          }
+          PortMapping(containerPort, availablePorts.next, servicePort, protocol)
         case pm: PortMapping => pm
       }
 
