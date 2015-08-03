03bb7c6cba69fa7959980137e4aa7fef10bcfe73
Connor Doyle <connor@mesosphere.io>
2015-08-03 13:07:47 -0700

    Introduces RWLock utility class.

--- src/main/scala/mesosphere/marathon/health/MarathonHealthCheckManager.scala
+++ src/main/scala/mesosphere/marathon/health/MarathonHealthCheckManager.scala
@@ -1,6 +1,5 @@
 package mesosphere.marathon.health
 
-import java.util.concurrent.locks.{ Lock, ReentrantReadWriteLock }
 import javax.inject.{ Inject, Named }
 
 import akka.actor.{ ActorRef, ActorSystem, Props }
@@ -14,9 +13,11 @@ import mesosphere.marathon.event.{ AddHealthCheck, EventModule, RemoveHealthChec
 import mesosphere.marathon.health.HealthCheckActor.{ AppHealth, GetAppHealth }
 import mesosphere.marathon.state.{ AppDefinition, AppRepository, PathId, Timestamp }
 import mesosphere.marathon.tasks.{ TaskIdUtil, TaskTracker }
+import mesosphere.util.RWLock
 import mesosphere.util.ThreadPoolContext.context
 import org.apache.mesos.Protos.TaskStatus
 
+import scala.collection.mutable
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
@@ -32,41 +33,24 @@ class MarathonHealthCheckManager @Inject() (
     healthCheck: HealthCheck,
     actor: ActorRef)
 
-  protected[this] var appHealthChecks: Map[PathId, Map[Timestamp, Set[ActiveHealthCheck]]] =
-    Map.empty.withDefaultValue(Map.empty.withDefaultValue(Set.empty))
-
-  private[this] val rwLock = new ReentrantReadWriteLock
-  private[this] val readLock = rwLock.readLock
-  private[this] val writeLock = rwLock.writeLock
-
-  /**
-    * Returns the result of evaluating the supplied thunk, first obtaining the
-    * lock, and releasing it afterward.
-    */
-  private[this] def withLock[T](lock: Lock)(op: => T): T = {
-    lock.lock() // blocking
-    try { op }
-    finally { lock.unlock() }
-  }
-
-  protected[this] def withReadLock[T](op: => T): T = withLock(readLock)(op)
-  protected[this] def withWriteLock[T](op: => T): T = withLock(writeLock)(op)
+  protected[this] var appHealthChecks: RWLock[mutable.Map[PathId, Map[Timestamp, Set[ActiveHealthCheck]]]] =
+    RWLock(mutable.Map.empty.withDefaultValue(Map.empty.withDefaultValue(Set.empty)))
 
   override def list(appId: PathId): Set[HealthCheck] =
-    withReadLock { listActive(appId).map(_.healthCheck) }
+    listActive(appId).map(_.healthCheck)
 
   protected[this] def listActive(appId: PathId): Set[ActiveHealthCheck] =
-    withReadLock {
-      appHealthChecks(appId).values.flatten.toSet
+    appHealthChecks.readLock { ahcs =>
+      ahcs(appId).values.flatten.toSet
     }
 
   protected[this] def listActive(appId: PathId, appVersion: Timestamp): Set[ActiveHealthCheck] =
-    withReadLock {
-      appHealthChecks(appId)(appVersion)
+    appHealthChecks.readLock { ahcs =>
+      ahcs(appId)(appVersion)
     }
 
   override def add(appId: PathId, appVersion: Timestamp, healthCheck: HealthCheck): Unit =
-    withWriteLock {
+    appHealthChecks.writeLock { ahcs =>
       val healthChecksForApp = listActive(appId, appVersion)
 
       if (healthChecksForApp.exists(_.healthCheck == healthCheck))
@@ -80,18 +64,20 @@ class MarathonHealthCheckManager @Inject() (
         val newHealthChecksForApp =
           healthChecksForApp + ActiveHealthCheck(healthCheck, ref)
 
-        val appMap = appHealthChecks(appId) + (appVersion -> newHealthChecksForApp)
-        appHealthChecks += appId -> appMap
+        val appMap = ahcs(appId) + (appVersion -> newHealthChecksForApp)
+        ahcs += appId -> appMap
 
         eventBus.publish(AddHealthCheck(appId, appVersion, healthCheck))
       }
     }
 
   override def addAllFor(app: AppDefinition): Unit =
-    withWriteLock { app.healthChecks.foreach(add(app.id, app.version, _)) }
+    appHealthChecks.writeLock { _ => // atomically add all checks
+      app.healthChecks.foreach(add(app.id, app.version, _))
+    }
 
   override def remove(appId: PathId, appVersion: Timestamp, healthCheck: HealthCheck): Unit =
-    withWriteLock {
+    appHealthChecks.writeLock { ahcs =>
       val healthChecksForVersion: Set[ActiveHealthCheck] = listActive(appId, appVersion)
       val toRemove: Set[ActiveHealthCheck] = healthChecksForVersion.filter(_.healthCheck == healthCheck)
       for (ahc <- toRemove) {
@@ -100,7 +86,7 @@ class MarathonHealthCheckManager @Inject() (
         eventBus.publish(RemoveHealthCheck(appId))
       }
       val newHealthChecksForVersion = healthChecksForVersion -- toRemove
-      val currentHealthChecksForApp = appHealthChecks(appId)
+      val currentHealthChecksForApp = ahcs(appId)
       val newHealthChecksForApp = if (newHealthChecksForVersion.isEmpty) {
         currentHealthChecksForApp - appVersion
       }
@@ -108,18 +94,17 @@ class MarathonHealthCheckManager @Inject() (
         currentHealthChecksForApp + (appVersion -> newHealthChecksForVersion)
       }
 
-      appHealthChecks =
-        if (newHealthChecksForApp.isEmpty) appHealthChecks - appId
-        else appHealthChecks + (appId -> newHealthChecksForApp)
+      if (newHealthChecksForApp.isEmpty) ahcs -= appId
+      else ahcs += (appId -> newHealthChecksForApp)
     }
 
   override def removeAll(): Unit =
-    withWriteLock { appHealthChecks.keys foreach removeAllFor }
+    appHealthChecks.writeLock { _.keys foreach removeAllFor }
 
   override def removeAllFor(appId: PathId): Unit =
-    withWriteLock {
+    appHealthChecks.writeLock { ahcs =>
       for {
-        (version, activeHealthChecks) <- appHealthChecks(appId)
+        (version, activeHealthChecks) <- ahcs(appId)
         activeHealthCheck <- activeHealthChecks
       } {
         remove(appId, version, activeHealthCheck.healthCheck)
@@ -129,14 +114,14 @@ class MarathonHealthCheckManager @Inject() (
   override def reconcileWith(appId: PathId): Future[Unit] =
     appRepository.currentVersion(appId) flatMap {
       case None => Future(())
-      case Some(app) => withWriteLock {
+      case Some(app) => appHealthChecks.writeLock { ahcs =>
         val versionTasksMap: Map[String, MarathonTask] =
           taskTracker.get(app.id).map { task => task.getVersion -> task }.toMap
 
         // remove health checks for which the app version is not current and no tasks remain
         // since only current version tasks are launched.
         for {
-          (version, activeHealthChecks) <- appHealthChecks(appId)
+          (version, activeHealthChecks) <- ahcs(appId)
           if version != app.version && !versionTasksMap.contains(app.id.toString)
           activeHealthCheck <- activeHealthChecks
         } {
@@ -156,7 +141,7 @@ class MarathonHealthCheckManager @Inject() (
     }
 
   override def update(taskStatus: TaskStatus, version: Timestamp): Unit =
-    withReadLock {
+    appHealthChecks.readLock { ahcs =>
       // construct a health result from the incoming task status
       val taskId = taskStatus.getTaskId.getValue
       val maybeResult: Option[HealthResult] =
@@ -190,44 +175,43 @@ class MarathonHealthCheckManager @Inject() (
 
   override def status(
     appId: PathId,
-    taskId: String): Future[Seq[Health]] =
-    withReadLock {
-      import mesosphere.marathon.health.HealthCheckActor.GetTaskHealth
-      implicit val timeout: Timeout = Timeout(2, SECONDS)
-
-      val maybeAppVersion: Option[Timestamp] = taskTracker.getVersion(appId, taskId)
-
-      val taskHealth: Seq[Future[Health]] = maybeAppVersion.map { appVersion =>
-        listActive(appId, appVersion).toSeq.collect {
-          case ActiveHealthCheck(_, actor) =>
-            (actor ? GetTaskHealth(taskId)).mapTo[Health]
-        }
-      }.getOrElse(Nil)
-
-      Future.sequence(taskHealth)
-    }
-
-  override def statuses(appId: PathId): Future[Map[String, Seq[Health]]] = withReadLock {
+    taskId: String): Future[Seq[Health]] = {
+    import mesosphere.marathon.health.HealthCheckActor.GetTaskHealth
     implicit val timeout: Timeout = Timeout(2, SECONDS)
-    val futureHealths = for {
-      ActiveHealthCheck(_, actor) <- appHealthChecks(appId).values.iterator.flatten.toVector
-    } yield (actor ? GetAppHealth).mapTo[AppHealth]
 
-    Future.sequence(futureHealths) map { healths =>
-      val groupedHealth = healths.flatMap(_.health).groupBy(_.taskId)
+    val maybeAppVersion: Option[Timestamp] = taskTracker.getVersion(appId, taskId)
 
-      taskTracker.get(appId).toSeq.map { task =>
-        groupedHealth.get(task.getId) match {
-          case Some(xs) => task.getId -> xs.toSeq
-          case None     => task.getId -> Nil
-        }
-      }.toMap
-    }
+    val taskHealth: Seq[Future[Health]] = maybeAppVersion.map { appVersion =>
+      listActive(appId, appVersion).toSeq.collect {
+        case ActiveHealthCheck(_, actor) =>
+          (actor ? GetTaskHealth(taskId)).mapTo[Health]
+      }
+    }.getOrElse(Nil)
+
+    Future.sequence(taskHealth)
   }
 
-  override def healthCounts(appId: PathId): Future[HealthCounts] =
-    withReadLock {
+  override def statuses(appId: PathId): Future[Map[String, Seq[Health]]] =
+    appHealthChecks.readLock { ahcs =>
+      implicit val timeout: Timeout = Timeout(2, SECONDS)
+      val futureHealths = for {
+        ActiveHealthCheck(_, actor) <- ahcs(appId).values.iterator.flatten.toVector
+      } yield (actor ? GetAppHealth).mapTo[AppHealth]
 
+      Future.sequence(futureHealths) map { healths =>
+        val groupedHealth = healths.flatMap(_.health).groupBy(_.taskId)
+
+        taskTracker.get(appId).toSeq.map { task =>
+          groupedHealth.get(task.getId) match {
+            case Some(xs) => task.getId -> xs.toSeq
+            case None     => task.getId -> Nil
+          }
+        }.toMap
+      }
+    }
+
+  override def healthCounts(appId: PathId): Future[HealthCounts] =
+    appHealthChecks.readLock { ahcs =>
       statuses(appId).map { statusMap =>
         val builder = HealthCounts.newBuilder
         statusMap.values foreach { statuses =>
@@ -247,7 +231,7 @@ class MarathonHealthCheckManager @Inject() (
     }
 
   protected[this] def deactivate(healthCheck: ActiveHealthCheck): Unit =
-    withWriteLock { system stop healthCheck.actor }
+    appHealthChecks.writeLock { _ => system stop healthCheck.actor }
 
 }
 
