7925bd0afb389160e24052cc92ba3a0aa5b68923
Gastón Kleiman <gaston@mesosphere.io>
2015-08-27 15:43:31 +0200

    Fix typos in comments

--- src/main/scala/mesosphere/marathon/core/launcher/OfferProcessor.scala
+++ src/main/scala/mesosphere/marathon/core/launcher/OfferProcessor.scala
@@ -11,10 +11,10 @@ trait OfferProcessor {
   /**
     * Process offer by matching tasks and launching them when appropriate.
     *
-    * The offer processor while try to ensure timely execution.
+    * The offer processor will try to ensure a timely execution.
     *
     * @param offer the offer to match
-    * @return the future indicating when processing the offer has finished and if they were any errors
+    * @return the future indicating when the processing of the offer has finished and if there were any errors
     */
   def processOffer(offer: Offer): Future[Unit]
 }
