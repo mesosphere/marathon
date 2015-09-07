b2d2a11edc7dbda3dee850d663fe9f59a32e0b21
Peter Kolloch <peter@mesosphere.io>
2015-09-07 18:08:29 +0200

    UNIQUE arguments passed to run-tests

--- bin/run-tests-in-loop.sh
+++ bin/run-tests-in-loop.sh
@@ -9,11 +9,12 @@ cd "$MYDIR/.."
 
 set -x
 
+CONTAINER_SUFFIX=`date +"%Y-%m-%d_%H-%M-%S"`-$RANDOM
 
 while true ; do
   mkdir -p target/itests/{running,passed,failed}
   OUTPUT=`newOutput`
-  ./bin/run-tests.sh 2>&1 | tee "target/itests/running/$OUTPUT"
+  ./bin/run-tests.sh $CONTAINER_SUFFIX 2>&1 | tee "target/itests/running/$OUTPUT"
   if [ $PIPESTATUS -eq 0 ]; then
     mv ./target/itests/{running,passed}/"$OUTPUT"
   else
