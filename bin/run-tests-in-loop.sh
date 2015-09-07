798fae0031c1eb9db9718cabc0983affdef76c0c
Peter Kolloch <peter@mesosphere.io>
2015-09-07 16:26:32 +0200

    Added script to run integration tests in loop

---
+++ bin/run-tests-in-loop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function newOutput {
+  date +"%Y-%m-%d_%H-%M-%S.log"
+}
+
+MYDIR=`dirname "$0"`
+cd "$MYDIR/.."
+
+set -x
+
+
+while true ; do
+  mkdir -p target/itests/{running,passed,failed}
+  OUTPUT=`newOutput`
+  ./bin/run-tests.sh 2>&1 | tee "target/itests/running/$OUTPUT"
+  if [ $PIPESTATUS -eq 0 ]; then
+    mv ./target/itests/{running,passed}/"$OUTPUT"
+  else
+    mv ./target/itests/{running,failed}/"$OUTPUT"
+  fi
+done
