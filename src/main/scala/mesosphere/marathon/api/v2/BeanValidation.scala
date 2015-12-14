a7de787d7abfd44808f0a788e8a1a0827dad2d37
Alexander Weber <alex.weber@live.com>
2015-12-14 19:28:51 +0100

    #2735 - delete unnecessary files

--- src/main/scala/mesosphere/marathon/api/v2/BeanValidation.scala
+++
@@ -1,8 +0,0 @@
-package mesosphere.marathon.api.v2
-
-/**
-  * Bean validation helper trait.
-  * TODO: we should not use bean validation any longer.
-  */
-object BeanValidation
-
--- src/main/scala/mesosphere/marathon/api/v2/ModelValidation.scala
+++
@@ -1,6 +0,0 @@
-package mesosphere.marathon.api.v2
-
-/**
-  * Specific validation helper for specific model classes.
-  */
-object ModelValidation
