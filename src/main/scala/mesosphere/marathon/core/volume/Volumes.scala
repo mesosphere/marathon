2525aa5e7f055a8cbbad72e4f1a265f82374d842
James DeFelice <james@mesosphere.io>
2016-03-21 15:44:09 +0000

    code cleanup: comments, reduce pattern matching, idiomatic validation

--- src/main/scala/mesosphere/marathon/core/volume/Volumes.scala
+++ src/main/scala/mesosphere/marathon/core/volume/Volumes.scala
@@ -37,61 +37,52 @@ object Volumes {
 
   /**
     * @return the Volumes interface registered for the given name; if name is None then
-    * a default Volumes implementation is returned. None is returned if Some name is given
+    * the default Volumes implementation is returned. None is returned if Some name is given
     * but no volume provider is registered for that name.
     */
-  def apply(name: Option[String]): Option[Volumes[_ <: Volume]] = {
-    name match {
-      case None               => Some(AgentVolumes)
-      case Some(providerName) => registry.get(providerName)
-    }
-  }
+  def apply(name: Option[String]): Option[Volumes[_ <: Volume]] =
+    registry.get(name.getOrElse(AgentVolumes.name))
 
+  /** @return a validator that checks the validity of a volume provider name */
   def knownProvider(): Validator[Option[String]] =
     new NullSafeValidator[Option[String]](
       test = { !apply(_).isEmpty },
       failure = _ -> s"is not one of (${registry.keys.mkString(",")})"
     )
 
+  /** @return a validator that checks the validity of a volume given the volume provider name */
   def approved[T <: Volume](name: Option[String]): Validator[T] =
-    apply(name) match {
-      case None    => new Fail[T]("is an illegal volume specification")
-      case Some(v) => v.validation
-    }
+    apply(name).map(_.validation).getOrElse(new Fail[T]("is an illegal volume specification"))
 }
 
 object AgentVolumes extends Volumes[PersistentVolume] {
 
+  /** this is the name of the agent volume provider */
   val name = "agent"
 
+  /** validation checks that size has been specified */
   val validation = validator[Volume] { v =>
     // don't invoke validator on v because that's circular, just check the additional
     // things that we need for agent local volumes.
     // see implicit validator in the PersistentVolume class for reference.
-    v.asInstanceOf[PersistentVolume].persistent.size.isDefined is true
+    v.asInstanceOf[PersistentVolume].persistent.size is notEmpty
   }
 
   def isAgentLocal(volume: PersistentVolume): Boolean = {
-    !volume.persistent.providerName.isDefined || volume.persistent.providerName == name
+    volume.persistent.providerName.getOrElse(name) == name
   }
 
   override def apply(app: AppDefinition): Iterable[PersistentVolume] = {
-    app.persistentVolumes.filter { volume => isAgentLocal(volume) }
+    app.persistentVolumes.filter(isAgentLocal)
   }
 
+  /** @return a stream of task local volumes, extrapolating them from the app spec */
   def local(app: AppDefinition): Iterable[Task.LocalVolume] = {
     apply(app).map{ volume => Task.LocalVolume(Task.LocalVolumeId(app.id, volume), volume) }
   }
 
-  /**
-    * @return the disk resources required for volumes
-    */
+  /** @return the aggregate mesos disk resources required for volumes */
   def diskSize(app: AppDefinition): Double = {
-    apply(app).map(
-      _.persistent.size match {
-        case Some(size) => size
-        case _          => 0
-      }
-    ).sum.toDouble
+    apply(app).map(_.persistent.size).flatten.sum.toDouble
   }
 }
