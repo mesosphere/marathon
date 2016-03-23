e02765aec34148005b19c75534a4e4cd7eb31e29
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2016-03-23 14:20:46 +0100

    Activate CuratorCandidate

--- src/main/scala/mesosphere/marathon/MarathonModule.scala
+++ src/main/scala/mesosphere/marathon/MarathonModule.scala
@@ -12,8 +12,7 @@ import akka.routing.RoundRobinPool
 import com.codahale.metrics.Gauge
 import com.google.inject._
 import com.google.inject.name.Names
-import com.twitter.common.base.Supplier
-import com.twitter.common.zookeeper.{ Candidate, CandidateImpl, Group => ZGroup, ZooKeeperClient }
+import com.twitter.common.zookeeper.{ Candidate, ZooKeeperClient }
 import com.twitter.util.JavaTimer
 import com.twitter.zk.{ NativeConnector, ZkClient }
 import mesosphere.chaos.http.HttpConf
@@ -36,7 +35,6 @@ import mesosphere.util.state.zk.{ CompressionConf, ZKStore }
 import mesosphere.util.state.{ FrameworkId, FrameworkIdUtil, PersistentStore, _ }
 import mesosphere.util.{ CapConcurrentExecutions, CapConcurrentExecutionsMetrics }
 import org.apache.mesos.state.ZooKeeperState
-import org.apache.zookeeper.ZooDefs
 import org.apache.zookeeper.ZooDefs.Ids
 import org.slf4j.LoggerFactory
 
@@ -258,12 +256,8 @@ class MarathonModule(conf: MarathonConf, http: HttpConf, zk: ZooKeeperClient)
   def provideCandidate(zk: ZooKeeperClient, @Named(ModuleNames.HOST_PORT) hostPort: String): Option[Candidate] = {
     if (conf.highlyAvailable()) {
       log.info("Registering in ZooKeeper with hostPort:" + hostPort)
-      val candidate = new CandidateImpl(new ZGroup(zk, ZooDefs.Ids.OPEN_ACL_UNSAFE, conf.zooKeeperLeaderPath),
-        new Supplier[Array[Byte]] {
-          def get(): Array[Byte] = {
-            hostPort.getBytes("UTF-8")
-          }
-        })
+
+      val candidate = new core.leadership.CuratorCandidate(zk.getConnectString, conf.zooKeeperLeaderPath, hostPort)
       return Some(candidate) //scalastyle:off return
     }
     None
--- src/main/scala/mesosphere/marathon/core/leadership/CuratorCandidate.scala
+++ src/main/scala/mesosphere/marathon/core/leadership/CuratorCandidate.scala
@@ -1,24 +1,22 @@
-package mesosphere.marathon.core.leadership.impl
+package mesosphere.marathon.core.leadership
 
 import java.lang.Boolean
 import java.net.InetAddress
 
-import com.google.common.base.{Supplier, Optional}
+import com.google.common.base.{ Optional, Supplier }
 import com.twitter.common.base.ExceptionalCommand
-import com.twitter.common.zookeeper.{Group, Candidate}
 import com.twitter.common.zookeeper.Candidate.Leader
+import com.twitter.common.zookeeper.{ Candidate, Group }
 import org.apache.curator.framework.CuratorFrameworkFactory
-import org.apache.curator.framework.recipes.leader.{LeaderLatchListener, LeaderLatch}
+import org.apache.curator.framework.recipes.leader.{ LeaderLatch, LeaderLatchListener }
 import org.apache.curator.retry.ExponentialBackoffRetry
 import org.slf4j.LoggerFactory
 
-
-private[leadership] case class CuratorCandidate(
+class CuratorCandidate(
     zkConnectString: String,
     path: String,
-    id: String
-) extends Candidate {
-  val MaxZookeeperBackoffTime = 1000*300
+    id: String) extends Candidate {
+  val MaxZookeeperBackoffTime = 1000 * 300
   val MinZookeeperBackoffTime = 500
 
   private val log = LoggerFactory.getLogger(getClass.getName)
@@ -30,24 +28,19 @@ private[leadership] case class CuratorCandidate(
 
   private var latch: Option[LeaderLatch] = None
 
-  def newCandidate(zkConnectString: String, path: String, id: String): CuratorCandidate = {
-    new CuratorCandidate(zkConnectString, path, id)
-  }
-
-  def newCandidate(zkConnectString: String, path: String): CuratorCandidate = {
-    val id = InetAddress.getLocalHost.getHostAddress
-    newCandidate(zkConnectString, path, id)
+  def this(zkConnectString: String, path: String) = {
+    this(zkConnectString, path, InetAddress.getLocalHost.getHostAddress)
   }
 
   override def offerLeadership(leader: Leader): Supplier[Boolean] = {
     this.synchronized {
-      latch.foreach({l =>
+      latch.foreach({ l =>
         l.close()
       })
       var wasLeader = false
       latch = Some(new LeaderLatch(client, path, id))
 
-      latch.foreach { l=>
+      latch.foreach { l =>
         l.addListener(new LeaderLatchListener {
           override def isLeader(): Unit = latch.synchronized {
             latch.foreach { l =>
