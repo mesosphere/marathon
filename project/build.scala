7e28da41a5689b923ef723c3ab8493f098085bd7
Orlando Hohmeier <orlando@mesosphere.io>
2016-04-07 22:45:04 +0200

    Use MarathonUI 1.1.0-SNAPSHOT

--- project/build.scala
+++ project/build.scala
@@ -300,7 +300,7 @@ object Dependency {
     val PlayJson = "2.4.3"
     val JsonSchemaValidator = "2.2.6"
     val RxScala = "0.25.0"
-    val MarathonUI = "1.0.0-SNAPSHOT"
+    val MarathonUI = "1.1.0-SNAPSHOT"
     val MarathonApiConsole = "0.1.1"
     val Graphite = "3.1.2"
     val DataDog = "1.1.3"
