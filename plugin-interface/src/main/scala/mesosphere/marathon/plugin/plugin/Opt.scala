7045bd3f63621d9d13058d58744ca4643a665ba1
James DeFelice <james@mesosphere.io>
2016-05-10 21:02:18 +0000

    remove Dsl object, small optimization for combine

--- plugin-interface/src/main/scala/mesosphere/marathon/plugin/plugin/Opt.scala
+++ plugin-interface/src/main/scala/mesosphere/marathon/plugin/plugin/Opt.scala
@@ -29,20 +29,6 @@ object Opt {
     trait Plugin[P, T] extends Factory[P, T] with mesosphere.marathon.plugin.plugin.Plugin
   }
 
-  // TODO(jdef) we probably don't need the dsl stuff at all, this is just me playing with ideas.
-  // I generally dislike dsl's.
-  object Dsl {
-    import scala.language.implicitConversions
-
-    /** invoke allows for tuple notation to apply an Opt to some T: `t -> someOpt` */
-    implicit def applyOpt[T](t: Tuple2[T, Opt[T]]): Option[Opt[T]] = t._2(t._1)
-    /**
-      * invoke allows for tuple notation to apply a config parametger to some T to
-      * generate a functional option: `p -> t` (for example `secretsLabelFactory -> appDef`).
-      */
-    implicit def applyParam[P, T](t: Tuple2[Factory[P, T], P]): Option[Opt[T]] = t._1(t._2)
-  }
-
   /**
     * applyAll invokes each provided Opt, in order, on the given `t`
     * @return the result of the last invoked Opt, or else a no-op Opt
@@ -55,8 +41,13 @@ object Opt {
     last
   }
 
+  /**
+    * combine generates a single functional option that is the equivalent of invoking all of the
+    * given opts, in the order they're given here.
+    */
   def combine[T](opts: Opt[T]*): Option[Opt[T]] = {
     if (opts.isEmpty) None
+    else if (opts.size == 1) opts.headOption
     else Some(new Opt[T] {
       override def apply(t: T): Option[Opt[T]] = {
         applyAll(t, opts: _*)
