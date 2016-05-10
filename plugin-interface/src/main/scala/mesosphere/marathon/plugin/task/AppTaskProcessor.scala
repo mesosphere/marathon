85303c84a127c2faa1ef8d9c295112acccb4b038
James DeFelice <james@mesosphere.io>
2016-05-10 21:01:59 +0000

    formatting

--- plugin-interface/src/main/scala/mesosphere/marathon/plugin/task/AppTaskProcessor.scala
+++ plugin-interface/src/main/scala/mesosphere/marathon/plugin/task/AppTaskProcessor.scala
@@ -5,9 +5,9 @@ import mesosphere.marathon.plugin.plugin
 import org.apache.mesos.Protos
 
 /**
- * AppTaskProcessor is a factory func that generates functional options that mutate Mesos
- * task info's given some app specification. For example, a factory might generate functional
- * options that inject specific labels into a Mesos task info based on some properties of an
- * app specification.
- */
+  * AppTaskProcessor is a factory func that generates functional options that mutate Mesos
+  * task info's given some app specification. For example, a factory might generate functional
+  * options that inject specific labels into a Mesos task info based on some properties of an
+  * app specification.
+  */
 trait AppTaskProcessor extends plugin.Opt.Factory.Plugin[AppDefinition, Protos.TaskInfo.Builder]
