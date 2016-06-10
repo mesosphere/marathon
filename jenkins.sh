022bd68f19fd6eacd292c9fe5d2abac58940866b
Tito Sandoval <tsandoval@mesosphere.io>
2016-06-10 13:19:00 -0700

    Added a shell script that can be used to version control Jenkins build config.

---
+++ jenkins.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -v
+mkdir -p $HOME/.sbt/launchers/0.13.8/
+test -r $HOME/.sbt/launchers/0.13.8/sbt-launch.jar || curl -L -o $HOME/.sbt/launchers/0.13.8/sbt-launch.jar http://dl.bintray.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.8/sbt-launch.jar
+java -jar $HOME/.sbt/launchers/0.13.8/sbt-launch.jar -Dsbt.log.noformat=true "; clean; coverage; doc; assembly"
+java -jar $HOME/.sbt/launchers/0.13.8/sbt-launch.jar -Dfile.encoding=utf-8 -Dsbt.log.noformat=true "; coverageReport; coveralls"
