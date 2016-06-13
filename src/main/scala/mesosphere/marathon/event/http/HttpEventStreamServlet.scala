c619b7fc4d9776899f3e5df8afb6ca7061271732
Matthias Veit <aquamatthias@users.noreply.github.com>
2016-06-13 14:54:47 +0200

    Add authorization to /v2/leader, /v2/info, /v2/events and /v2/eventSubscriptions

--- src/main/scala/mesosphere/marathon/event/http/HttpEventStreamServlet.scala
+++ src/main/scala/mesosphere/marathon/event/http/HttpEventStreamServlet.scala
@@ -2,18 +2,22 @@ package mesosphere.marathon.event.http
 
 import java.util.UUID
 import javax.inject.{ Inject, Named }
-import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.{ Cookie, HttpServletRequest, HttpServletResponse }
 
 import akka.actor.ActorRef
-import mesosphere.marathon.ModuleNames
+import mesosphere.marathon.api.RequestFacade
 import mesosphere.marathon.event.http.HttpEventStreamActor._
+import mesosphere.marathon.plugin.auth.{ Authenticator, Authorizer, Identity, ViewEventStream }
+import mesosphere.marathon.plugin.http.HttpResponse
+import mesosphere.marathon.{ MarathonConf, ModuleNames }
 import org.eclipse.jetty.servlets.EventSource.Emitter
 import org.eclipse.jetty.servlets.{ EventSource, EventSourceServlet }
 
-import scala.concurrent.blocking
+import scala.concurrent.{ Await, blocking }
 
 /**
   * The Stream handle implementation for SSE.
+  *
   * @param request the initial http request.
   * @param emitter the emitter to emit data
   */
@@ -33,8 +37,54 @@ class HttpEventSSEHandle(request: HttpServletRequest, emitter: Emitter) extends 
 /**
   * Handle a server side event client stream by delegating events to the stream actor.
   */
-class HttpEventStreamServlet @Inject() (@Named(ModuleNames.HTTP_EVENT_STREAM) streamActor: ActorRef)
+class HttpEventStreamServlet @Inject() (
+  @Named(ModuleNames.HTTP_EVENT_STREAM) streamActor: ActorRef,
+  conf: MarathonConf,
+  val authenticator: Authenticator,
+  val authorizer: Authorizer)
     extends EventSourceServlet {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    val requestFacade = new RequestFacade(request)
+    val maybeIdentity = Await.result(authenticator.authenticate(requestFacade), conf.zkTimeoutDuration)
+    def withResponseFacade(fn: HttpResponse => Unit): Unit = {
+      val facade = new HttpResponse {
+        override def body(mediaType: String, bytes: Array[Byte]): Unit = {
+          response.setHeader("Content-Type", mediaType)
+          response.getWriter.write(new String(bytes))
+        }
+
+        override def sendRedirect(url: String): Unit = {
+          response.sendRedirect(url)
+        }
+
+        override def header(header: String, value: String): Unit = {
+          response.addHeader(header, value)
+        }
+
+        override def cookie(name: String, value: String, maxAge: Int, secure: Boolean): Unit = {
+          val cookie = new Cookie(name, value)
+          cookie.setMaxAge(maxAge)
+          cookie.setSecure(secure)
+          response.addCookie(cookie)
+        }
+
+        override def status(code: Int): Unit = {
+          response.setStatus(code)
+        }
+      }
+      fn(facade)
+    }
+    def isAuthorized(identity: Identity): Boolean = authorizer.isAuthorized(identity, ViewEventStream, ())
+    maybeIdentity match {
+      case Some(identity) if isAuthorized(identity) =>
+        super.doGet(request, response)
+      case Some(identity) =>
+        withResponseFacade(authorizer.handleNotAuthorized(identity, _))
+      case None =>
+        withResponseFacade(authenticator.handleNotAuthenticated(requestFacade, _))
+    }
+  }
 
   override def newEventSource(request: HttpServletRequest): EventSource = new EventSource {
     @volatile private var handler: Option[HttpEventSSEHandle] = None
---
+++ src/test/scala/mesosphere/marathon/api/v2/InfoResourceTest.scala
@@ -0,0 +1,50 @@
+package mesosphere.marathon.api.v2
+
+import mesosphere.chaos.http.HttpConf
+import mesosphere.marathon.api.TestAuthFixture
+import mesosphere.marathon.core.election.ElectionService
+import mesosphere.marathon.event.EventConfiguration
+import mesosphere.marathon.event.http.HttpEventConfiguration
+import mesosphere.marathon.{ LeaderProxyConf, MarathonConf, MarathonSchedulerService, MarathonSpec }
+import mesosphere.marathon.test.Mockito
+import mesosphere.util.state.MesosLeaderInfo
+import org.scalatest.{ GivenWhenThen, Matchers, FunSuite }
+
+class InfoResourceTest extends MarathonSpec with Matchers with Mockito with GivenWhenThen {
+
+  test("access without authentication is denied") {
+    Given("An unauthenticated request")
+    val f = new Fixture
+    val resource = f.infoResource()
+    f.auth.authenticated = false
+
+    When("we try to fetch the info")
+    val index = resource.index(f.auth.request)
+
+    Then("we receive a NotAuthenticated response")
+    index.getStatus should be(f.auth.NotAuthenticatedStatus)
+  }
+
+  test("access without authorization is denied") {
+    Given("An unauthorized request")
+    val f = new Fixture
+    val resource = f.infoResource()
+    f.auth.authenticated = true
+    f.auth.authorized = false
+
+    When("we try to fetch the info")
+    val index = resource.index(f.auth.request)
+
+    Then("we receive a NotAuthenticated response")
+    index.getStatus should be(f.auth.UnauthorizedStatus)
+  }
+
+  class Fixture {
+    val schedulerService = mock[MarathonSchedulerService]
+    val leaderInfo = mock[MesosLeaderInfo]
+    val electionService = mock[ElectionService]
+    val auth = new TestAuthFixture
+    val config = mock[MarathonConf with HttpConf with EventConfiguration with HttpEventConfiguration with LeaderProxyConf]
+    def infoResource() = new InfoResource(schedulerService, leaderInfo, electionService, auth.auth, auth.auth, config)
+  }
+}
