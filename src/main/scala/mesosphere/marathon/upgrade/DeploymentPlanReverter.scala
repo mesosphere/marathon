f6b67fd9a3ab11a61b75314722a762f572286b19
Tomasz Janiszewski <janiszt@gmail.com>
2016-06-30 21:23:58 +0200

    Fix style issue and enable scalastyle check

--- src/main/scala/mesosphere/marathon/upgrade/DeploymentPlanReverter.scala
+++ src/main/scala/mesosphere/marathon/upgrade/DeploymentPlanReverter.scala
@@ -1,6 +1,6 @@
 package mesosphere.marathon.upgrade
 
-import mesosphere.marathon.state.{ Timestamp, AppDefinition, PathId, Group }
+import mesosphere.marathon.state.{ AppDefinition, Group, PathId, Timestamp }
 import org.slf4j.LoggerFactory
 
 import scala.collection.immutable.Seq
@@ -55,106 +55,6 @@ private[upgrade] object DeploymentPlanReverter {
   }
 
   /**
-    * Reverts any group additions, changes, removals.
-    *
-    * It is more difficult than reverting any app definition changes
-    * because groups are not locked by deployments and concurrent changes are allowed.
-    */
-  //TODO: fix style issue and enable this scalastyle check
-  //scalastyle:off cyclomatic.complexity method.length
-  private[this] def revertGroupChanges(
-    version: Timestamp, groupChanges: Seq[(Option[Group], Option[Group])])(
-    group: Group): Group = {
-
-    def revertGroupRemoval(oldGroup: Group)(existingGroup: Group): Group = {
-      log.debug("re-adding group {} with dependencies {}", Seq(oldGroup.id, oldGroup.dependencies): _*)
-      if ((oldGroup.dependencies -- existingGroup.dependencies).nonEmpty) {
-        existingGroup.copy(dependencies = existingGroup.dependencies ++ oldGroup.dependencies)
-      } else {
-        existingGroup
-      }
-    }
-
-    def revertDependencyChanges(oldGroup: Group, newGroup: Group)(group: Group): Group = {
-      val removedDependencies = oldGroup.dependencies -- newGroup.dependencies
-      val addedDependencies = newGroup.dependencies -- oldGroup.dependencies
-
-      if (removedDependencies.nonEmpty || addedDependencies.nonEmpty) {
-        if (log.isDebugEnabled)
-          log.debug(
-            s"revert dependency changes in group ${oldGroup.id}, " +
-              s"readding removed {${removedDependencies.mkString(", ")}}, " +
-              s"removing added {${addedDependencies.mkString(", ")}}")
-
-        group.copy(dependencies = group.dependencies ++ removedDependencies -- addedDependencies)
-      } else {
-        // common case, unchanged
-        group
-      }
-    }
-
-    def revertGroupAddition(result: Group, newGroup: Group): Group = {
-      // We know that all group removals for groups inside of this group have been
-      // performed before. If there are no conflicts with other deployments, we could
-      // just remove the group. Unfortunately, another deployment could have
-      // added a new sub group or sub app definition. In this case, we should not remove
-      // this group.
-
-      def normalized(group: Group): Group = group.withNormalizedVersion.withoutChildren
-      def isGroupUnchanged(group: Group): Boolean =
-        !group.containsAppsOrGroups && normalized(group) == normalized(newGroup)
-
-      result.group(newGroup.id) match {
-        case Some(unchanged) if isGroupUnchanged(unchanged) =>
-
-          log.debug("remove unchanged group {}", unchanged.id)
-          result.remove(unchanged.id, version)
-        case _ if newGroup.dependencies.nonEmpty =>
-          // group dependencies have changed
-          if (log.isDebugEnabled)
-            log.debug(s"group ${newGroup.id} has changed. " +
-              s"Removed added dependencies ${newGroup.dependencies.mkString(", ")}")
-          result.update(
-            newGroup.id,
-            group => group.copy(dependencies = group.dependencies -- newGroup.dependencies),
-            version)
-        case _ =>
-          // still contains apps/groups, so we keep it
-          result
-      }
-    }
-
-    // sort groups so that inner groups are processed first
-    val sortedGroupChanges = groupChanges.sortWith {
-      case (change1, change2) =>
-        // both groups are supposed to have the same path id (if there are any)
-        def pathId(change: (Option[Group], Option[Group])): PathId = {
-          Seq(change._1, change._2).flatten.map(_.id).headOption.getOrElse(PathId.empty)
-        }
-
-        pathId(change1) > pathId(change2)
-    }
-
-    sortedGroupChanges.foldLeft(group) {
-      case (result, groupUpdate) =>
-        groupUpdate match {
-          case (Some(oldGroup), None) =>
-            result.update(oldGroup.id, revertGroupRemoval(oldGroup), version)
-
-          case (Some(oldGroup), Some(newGroup)) =>
-            result.update(oldGroup.id, revertDependencyChanges(oldGroup, newGroup), version)
-
-          case (None, Some(newGroup)) =>
-            revertGroupAddition(result, newGroup)
-
-          case (None, None) =>
-            log.warn("processing unexpected NOOP in group changes")
-            result
-        }
-    }
-  }
-
-  /**
     * Reverts app additions, changes and removals.
     *
     * The logic is quite simple because during a deployment apps are locked which
@@ -177,6 +77,105 @@ private[upgrade] object DeploymentPlanReverter {
             log.warn("processing unexpected NOOP in app changes")
             result
         }
+    }
+  }
+
+  /**
+    * Reverts any group additions, changes, removals.
+    *
+    * It is more difficult than reverting any app definition changes
+    * because groups are not locked by deployments and concurrent changes are allowed.
+    */
+  def revertGroupChanges(version: Timestamp, groupChanges: Seq[(Option[Group], Option[Group])])(group: Group): Group = {
+    // sort groups so that inner groups are processed first
+    val sortedGroupChanges = sortGroupChangesByPathId(groupChanges)
+
+    sortedGroupChanges.foldLeft(group) {
+      case (result, groupUpdate) =>
+        groupUpdate match {
+          case (Some(oldGroup), None) =>
+            result.update(oldGroup.id, revertGroupRemoval(oldGroup), version)
+
+          case (Some(oldGroup), Some(newGroup)) =>
+            result.update(oldGroup.id, revertDependencyChanges(oldGroup, newGroup), version)
+
+          case (None, Some(newGroup)) =>
+            revertGroupAddition(result, newGroup, version)
+
+          case (None, None) =>
+            log.warn("processing unexpected NOOP in group changes")
+            result
+        }
+    }
+  }
+
+  private[this] def sortGroupChangesByPathId(groupChanges: Seq[(Option[Group], Option[Group])]) = {
+    groupChanges.sortWith {
+      case (change1, change2) =>
+        // both groups are supposed to have the same path id (if there are any)
+        def pathId(change: (Option[Group], Option[Group])): PathId = {
+          Seq(change._1, change._2).flatten.map(_.id).headOption.getOrElse(PathId.empty)
+        }
+
+        pathId(change1) > pathId(change2)
+    }
+  }
+
+  private[this] def revertGroupAddition(result: Group, newGroup: Group, version: Timestamp): Group = {
+    // We know that all group removals for groups inside of this group have been
+    // performed before. If there are no conflicts with other deployments, we could
+    // just remove the group. Unfortunately, another deployment could have
+    // added a new sub group or sub app definition. In this case, we should not remove
+    // this group.
+
+    def normalized(group: Group): Group = group.withNormalizedVersion.withoutChildren
+    def isGroupUnchanged(group: Group): Boolean =
+      !group.containsAppsOrGroups && normalized(group) == normalized(newGroup)
+
+    result.group(newGroup.id) match {
+      case Some(unchanged) if isGroupUnchanged(unchanged) =>
+
+        log.debug("remove unchanged group {}", unchanged.id)
+        result.remove(unchanged.id, version)
+      case _ if newGroup.dependencies.nonEmpty =>
+        // group dependencies have changed
+        if (log.isDebugEnabled)
+          log.debug(s"group ${newGroup.id} has changed. " +
+            s"Removed added dependencies ${newGroup.dependencies.mkString(", ")}")
+        result.update(
+          newGroup.id,
+          group => group.copy(dependencies = group.dependencies -- newGroup.dependencies),
+          version)
+      case _ =>
+        // still contains apps/groups, so we keep it
+        result
+    }
+  }
+
+  private[this] def revertDependencyChanges(oldGroup: Group, newGroup: Group)(group: Group): Group = {
+    val removedDependencies = oldGroup.dependencies -- newGroup.dependencies
+    val addedDependencies = newGroup.dependencies -- oldGroup.dependencies
+
+    if (removedDependencies.nonEmpty || addedDependencies.nonEmpty) {
+      if (log.isDebugEnabled)
+        log.debug(
+          s"revert dependency changes in group ${oldGroup.id}, " +
+            s"readding removed {${removedDependencies.mkString(", ")}}, " +
+            s"removing added {${addedDependencies.mkString(", ")}}")
+
+      group.copy(dependencies = group.dependencies ++ removedDependencies -- addedDependencies)
+    } else {
+      // common case, unchanged
+      group
+    }
+  }
+
+  private[this] def revertGroupRemoval(oldGroup: Group)(existingGroup: Group): Group = {
+    log.debug("re-adding group {} with dependencies {}", Seq(oldGroup.id, oldGroup.dependencies): _*)
+    if ((oldGroup.dependencies -- existingGroup.dependencies).nonEmpty) {
+      existingGroup.copy(dependencies = existingGroup.dependencies ++ oldGroup.dependencies)
+    } else {
+      existingGroup
     }
   }
 }
