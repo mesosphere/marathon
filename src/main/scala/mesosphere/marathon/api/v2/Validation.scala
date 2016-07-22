c4131d8549dfd08df7aff22166a5a73a855b49b8
Johannes Unterstein <junterstein@mesosphere.io>
2016-07-22 13:56:39 +0200

    only 120 chars allowed

--- src/main/scala/mesosphere/marathon/api/v2/Validation.scala
+++ src/main/scala/mesosphere/marathon/api/v2/Validation.scala
@@ -283,7 +283,8 @@ object Validation {
     }
   }
 
-  def matchRegexWithFailureMessage(regex: Regex, failureMessage: String): Validator[String] = new NullSafeValidator[String](
+  def matchRegexWithFailureMessage(regex: Regex, failureMessage: String): Validator[String] =
+    new NullSafeValidator[String](
     test = _.matches(regex.regex),
     failure = _ -> failureMessage
   )
