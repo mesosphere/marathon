32384eba7905fe7787656295fe5ab8a82b45e566
Stéphane Cottin <stephane.cottin@vixns.com>
2016-08-06 17:37:29 +0200

    with docker/runtime isolator, mesos container can run without cmd and args.

--- src/main/scala/mesosphere/marathon/state/AppDefinition.scala
+++ src/main/scala/mesosphere/marathon/state/AppDefinition.scala
@@ -616,11 +616,18 @@ object AppDefinition extends GeneralPurposeCombinators {
     }
 
   private val containsCmdArgsOrContainer: Validator[AppDefinition] =
-    isTrue("AppDefinition must either contain one of 'cmd' or 'args', and/or a non-Mesos 'container'.") { app =>
+    isTrue("AppDefinition must either contain one of 'cmd' or 'args', and/or a 'container'.") { app =>
       val cmd = app.cmd.nonEmpty
       val args = app.args.nonEmpty
-      val container = app.container.exists(!_.isInstanceOf[Container.Mesos])
-      (cmd ^ args) || (!(cmd || args) && container)
+      val container = app.container.exists(
+        _ match {
+          case _: MesosDocker => true
+          case _: MesosAppC => true
+          case _: Container.Docker => true
+          case _ => false
+        }
+      )
+      (cmd ^ args) || (!(cmd && args) && container)
     }
 
   private val complyWithMigrationAPI: Validator[AppDefinition] =
