7f4496dd3a4f46d84657d6459d9591f1c4a170b8
Ken Sipe <kensipe@gmail.com>
2016-08-24 19:41:14 -0500

    adding comments

--- tests/system/test_users.py
+++ tests/system/test_users.py
@@ -22,7 +22,7 @@ def test_default_user():
     app = client.get_app(application_json['id'])
     assert app['user'] == None
 
-    # wait for deployment to finish
+    # wait for deployment
     tasks = client.get_tasks("unique-sleep")
     host = tasks[0]['host']
 
