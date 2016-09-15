5b07f0bd1ec73ecead8d37b6659c16251673c64b
Matthias Eichstedt <matthias.eichstedt@mesosphere.io>
2016-09-15 20:48:07 +0200

    fixed docs

--- src/main/scala/mesosphere/marathon/core/task/tracker/TaskStateOpProcessor.scala
+++ src/main/scala/mesosphere/marathon/core/task/tracker/TaskStateOpProcessor.scala
@@ -5,12 +5,12 @@ import mesosphere.marathon.core.instance.update.{ InstanceUpdateEffect, Instance
 import scala.concurrent.Future
 
 /**
-  * Handles the processing of [[InstanceUpdateOperation]]s. These might originate from
+  * Handles the processing of InstanceUpdateOperations. These might originate from
   * * Creating an instance
   * * Updating an instance (due to a state change, a timeout, a mesos update)
   * * Expunging an instance
   */
 trait TaskStateOpProcessor {
-  /** Process an [[InstanceUpdateOperation]] and propagate its result. */
+  /** Process an InstanceUpdateOperation and propagate its result. */
   def process(stateOp: InstanceUpdateOperation): Future[InstanceUpdateEffect]
 }
