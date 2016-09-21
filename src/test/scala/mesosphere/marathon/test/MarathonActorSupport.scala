70814b9febed6e7cb555fe627665b648c0e7bcc0
karsten <kjeschkies@mesosphere.com>
2016-09-21 14:16:07 +0200

    Add `FutureTestSupport` to `MarathonActorSupport`.

--- src/test/scala/mesosphere/marathon/test/MarathonActorSupport.scala
+++ src/test/scala/mesosphere/marathon/test/MarathonActorSupport.scala
@@ -1,16 +1,17 @@
 package mesosphere.marathon.test
 
+import mesosphere.marathon.MarathonSpec
 import akka.actor.ActorSystem
 import akka.stream.{ ActorMaterializer, Materializer }
 import akka.testkit.{ TestKit, TestKitBase }
 import com.typesafe.config.ConfigFactory
-import org.scalatest.{ BeforeAndAfterAll, Suite }
+import org.scalatest.BeforeAndAfterAll
 import org.slf4j.LoggerFactory
 
 /**
   * Start an actor system for all test methods and provide akka TestKit utility methods.
   */
-trait MarathonActorSupport extends Suite with TestKitBase with BeforeAndAfterAll {
+trait MarathonActorSupport extends MarathonSpec with BeforeAndAfterAll with TestKitBase {
 
   val log = LoggerFactory.getLogger(getClass)
 
