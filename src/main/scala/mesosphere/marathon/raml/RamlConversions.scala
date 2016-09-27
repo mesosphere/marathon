75538c6fa89cf6f97ae484fdeeed424564345a27
James DeFelice <james.defelice@gmail.com>
2016-09-27 09:16:35 -0400

    fixup

--- src/main/scala/mesosphere/marathon/raml/RamlConversions.scala
+++ src/main/scala/mesosphere/marathon/raml/RamlConversions.scala
@@ -2,5 +2,6 @@ package mesosphere.marathon.raml
 
 trait RamlConversions extends ConstraintConversion with ContainerConversion
   with EnvVarConversion with NetworkConversion with PodConversion with PodStatusConversion
+  with VolumeConversion
 
 object RamlConversions extends RamlConversions
