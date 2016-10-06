8ff8c4378599c9f0dd93c2d971b0d25c76b6a613
Johannes Unterstein <junterstein@mesosphere.io>
2016-10-06 23:43:08 +0200

    Adding unit test to verify that ScaleRunSpec is send only once if an instance reaches terminal state

---
+++ src/test/scala/mesosphere/marathon/core/task/update/impl/steps/ScaleAppUpdateStepImplTest.scala
@@ -0,0 +1,52 @@
+package mesosphere.marathon.core.task.update.impl.steps
+
+import akka.actor.{ ActorRef, ActorSystem }
+import akka.testkit.TestProbe
+import com.google.inject.Provider
+import mesosphere.marathon.MarathonSchedulerActor.ScaleRunSpec
+import mesosphere.marathon.core.event.MarathonEvent
+import mesosphere.marathon.core.instance.update.InstanceUpdated
+import mesosphere.marathon.core.instance.{ Instance, InstanceStatus, TestInstanceBuilder }
+import mesosphere.marathon.state.{ PathId, Timestamp }
+import mesosphere.marathon.test.Mockito
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{ FunSuite, GivenWhenThen, Matchers }
+
+class ScaleAppUpdateStepImplTest extends FunSuite with Matchers with GivenWhenThen with Mockito with ScalaFutures {
+
+  implicit lazy val system = ActorSystem()
+
+  test("ScaleAppUpdateStep should only send one ScaleRunSpec when receiving multiple failed tasks") {
+    val f = new Fixture
+
+    Given("an instance with terminal containers")
+    val instance = TestInstanceBuilder.newBuilder(PathId("/app"))
+      .addTaskUnreachable(containerName = Some("unreachable1"))
+      .getInstance()
+
+    When("process a task_failed update")
+    val failedUpdate1 = f.makeFailedUpdateOp(instance, InstanceStatus.Running, InstanceStatus.Failed)
+    f.step.process(failedUpdate1)
+
+    Then("a scale request is sent to the scheduler actor")
+    val answer = f.schedulerActor.expectMsgType[ScaleRunSpec]
+    answer.runSpecId should be (instance.instanceId.runSpecId)
+
+    Then("process a task_failed again")
+    val failedUpdate2 = f.makeFailedUpdateOp(instance, InstanceStatus.Failed, InstanceStatus.Failed)
+    f.step.process(failedUpdate2)
+    f.schedulerActor.expectNoMsg()
+  }
+
+  class Fixture {
+    val schedulerActor: TestProbe = TestProbe()
+    val schedulerActorProvider = new Provider[ActorRef] {
+      override def get(): ActorRef = schedulerActor.ref
+    }
+
+    def makeFailedUpdateOp(instance: Instance, lastState: InstanceStatus, newState: InstanceStatus) =
+      InstanceUpdated(instance.copy(state = instance.state.copy(status = newState)), Some(Instance.InstanceState(lastState, Timestamp.now(), instance.runSpecVersion, Some(true))), Seq.empty[MarathonEvent])
+
+    val step = new ScaleAppUpdateStepImpl(schedulerActorProvider)
+  }
+}
