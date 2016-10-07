1083e5c823c75a3391a86394ce0518e3eec395ab
James DeFelice <james.defelice@gmail.com>
2016-10-07 09:29:34 -0400

    Revert "when generating pod instance status xref the pod-spec by version" (#4563)

--- src/main/scala/mesosphere/marathon/core/appinfo/impl/AppInfoBaseData.scala
+++ src/main/scala/mesosphere/marathon/core/appinfo/impl/AppInfoBaseData.scala
@@ -4,14 +4,13 @@ import mesosphere.marathon.DeploymentService
 import mesosphere.marathon.core.appinfo.{ AppInfo, EnrichedTask, TaskCounts, TaskStatsByVersion }
 import mesosphere.marathon.core.base.Clock
 import mesosphere.marathon.core.health.{ Health, HealthCheckManager }
-import mesosphere.marathon.core.instance.Instance
 import mesosphere.marathon.core.pod.PodDefinition
 import mesosphere.marathon.core.readiness.ReadinessCheckResult
 import mesosphere.marathon.core.task.Task
 import mesosphere.marathon.core.task.tracker.InstanceTracker
 import mesosphere.marathon.raml.{ PodInstanceState, PodInstanceStatus, PodState, PodStatus, Raml }
 import mesosphere.marathon.state._
-import mesosphere.marathon.storage.repository.{ ReadOnlyPodRepository, TaskFailureRepository }
+import mesosphere.marathon.storage.repository.TaskFailureRepository
 import mesosphere.marathon.upgrade.DeploymentManager.DeploymentStepInfo
 import mesosphere.marathon.upgrade.DeploymentPlan
 import org.slf4j.LoggerFactory
@@ -27,8 +26,7 @@ class AppInfoBaseData(
     instanceTracker: InstanceTracker,
     healthCheckManager: HealthCheckManager,
     deploymentService: DeploymentService,
-    taskFailureRepository: TaskFailureRepository,
-    podRepository: ReadOnlyPodRepository) {
+    taskFailureRepository: TaskFailureRepository) {
 
   import AppInfoBaseData._
 
@@ -174,13 +172,7 @@ class AppInfoBaseData(
     async { // linter:ignore UnnecessaryElseBranch
       val now = clock.now().toOffsetDateTime
       val instances = await(instancesByRunSpecFuture).specInstances(podDef.id)
-      val specByVersion: Map[Timestamp, Option[PodDefinition]] = await(Future.sequence(
-        // TODO(jdef) if repositories ever support a bulk-load interface, use it here
-        instances.map(_.runSpecVersion).toSeq.distinct.map { version =>
-          podRepository.getVersion(podDef.id, version.toOffsetDateTime).map(version -> _)
-        }
-      )).toMap
-      val instanceStatus = instances.map { inst => podInstanceStatus(inst)(specByVersion.apply) }.toIndexedSeq.flatten
+      val instanceStatus = instances.map(instance => Raml.toRaml(podDef -> instance)).toVector
       val statusSince = if (instances.isEmpty) now else instanceStatus.map(_.statusSince).max
       val state = await(podState(podDef.instances, instanceStatus, isPodTerminating(podDef.id)))
 
@@ -195,16 +187,6 @@ class AppInfoBaseData(
         lastChanged = statusSince
       )
     }
-
-  def podInstanceStatus(instance: Instance)(f: Timestamp => Option[PodDefinition]): Option[PodInstanceStatus] = {
-    val maybePodSpec: Option[PodDefinition] = f(instance.runSpecVersion)
-
-    if (maybePodSpec.isEmpty)
-      log.warn(s"failed to generate pod instance status for instance ${instance.instanceId}, " +
-        s"pod version ${instance.runSpecVersion} failed to load from persistent store")
-
-    maybePodSpec.map { pod => Raml.toRaml(pod -> instance) }
-  }
 
   protected def isPodTerminating(id: PathId): Future[Boolean] =
     runningDeployments.map { infos =>
