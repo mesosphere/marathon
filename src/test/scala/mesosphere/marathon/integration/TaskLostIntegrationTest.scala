f566b6e57ee82fecef5a48ec6197ccd1317d5c35
Johannes Unterstein <junterstein@mesosphere.io>
2016-10-14 13:20:37 +0200

    Scaling down an app while constraints violated and lost tasks present
    Summary:
    Regression test for https://github.com/mesosphere/marathon/issues/4059 which was solved through https://github.com/mesosphere/marathon/pull/4530
    
    Test passes on master, applied to 1.3.0 this integration test fails with
    ```
     *** 1 TEST FAILED ***
    TaskLostIntegrationTest:
    
    - Scaling down an app while constraints violated and lost tasks present *** FAILED *** (32 seconds, 764 milliseconds)
      java.lang.AssertionError: Waiting for event deployment_success to arrive took longer than 30 seconds. Give up.
    ```
    which is exactly the behavior which is described in 4059.
    
    Test Plan: sbt "integration:test-only mesosphere.marathon.integration.TaskLostIntegrationTest"
    
    Reviewers: meichstedt, testjohannes
    
    Reviewed By: meichstedt, testjohannes
    
    Subscribers: jenkins, marathon-team
    
    Differential Revision: https://phabricator.mesosphere.com/D54

--- src/test/scala/mesosphere/marathon/integration/TaskLostIntegrationTest.scala
+++ src/test/scala/mesosphere/marathon/integration/TaskLostIntegrationTest.scala
@@ -1,5 +1,7 @@
 package mesosphere.marathon.integration
 
+import mesosphere.marathon.Protos.Constraint.Operator
+import mesosphere.marathon.api.v2.json.AppUpdate
 import mesosphere.marathon.integration.facades.ITEnrichedTask
 import mesosphere.marathon.integration.setup._
 import org.scalatest.{ BeforeAndAfter, GivenWhenThen, Matchers }
@@ -75,6 +77,62 @@ class TaskLostIntegrationTest extends IntegrationFunSuite with WithMesosCluster 
     waitForTasks(app.id, 1).head should be(task)
   }
 
+  // regression test for https://github.com/mesosphere/marathon/issues/4059
+  test("Scaling down an app with constraints and lost tasks will succeed") {
+    import mesosphere.marathon.Protos.Constraint
+    Given("an app that is constrained to a unique hostname")
+    val constraint: Constraint = Constraint.newBuilder
+      .setField("hostname")
+      .setOperator(Operator.UNIQUE)
+      .setValue("")
+      .build
+
+    // start both slaves
+    if (!ProcessKeeper.hasProcess(slave1)) startSlave(slave1)
+    if (!ProcessKeeper.hasProcess(slave2)) startSlave(slave2)
+
+    val app = appProxy(testBasePath / "app", "v1", instances = 2, withHealth = false).copy(constraints = Set(constraint))
+
+    marathon.createAppV2(app)
+    waitForEvent("deployment_success")
+    val enrichedTasks = waitForTasks(app.id, 2)
+    val task = enrichedTasks.find(t => t.host == slave1).getOrElse(throw new RuntimeException("No matching task found on slave1"))
+
+    When("agent1 is stopped")
+    stopMesos(slave1)
+    Then("one task is declared lost")
+    waitForEventMatching("Task is declared lost") { matchEvent("TASK_LOST", task) }
+
+    When("We try to scale down to one instance")
+    marathon.updateApp(app.id, AppUpdate(instances = Some(1)))
+    marathon.listDeploymentsForBaseGroup().value should have size 1
+
+    Then("the deployment will eventually finish")
+    waitForEventMatching("app should be scaled and deployment should be finished") { matchDeploymentSuccess(1, app.id.toString) }
+    marathon.listDeploymentsForBaseGroup().value should have size 0
+  }
+
+  test("Scaling down an app with lost tasks will succeed") {
+    val app = appProxy(testBasePath / "app", "v1", instances = 1, withHealth = false)
+
+    marathon.createAppV2(app)
+    waitForEvent("deployment_success")
+    val enrichedTasks = waitForTasks(app.id, 1)
+    val task = enrichedTasks.headOption.getOrElse(throw new RuntimeException("No matching task found"))
+
+    When("agent1 is stopped")
+    stopMesos(slave1)
+    Then("one task is declared lost")
+    waitForEventMatching("Task is declared lost") { matchEvent("TASK_LOST", task) }
+
+    When("We try to scale down to one instance")
+    marathon.updateApp(app.id, AppUpdate(instances = Some(0)))
+
+    Then("the deployment will eventually finish")
+    waitForEventMatching("app should be scaled and deployment should be finished") { matchDeploymentSuccess(1, app.id.toString) }
+    marathon.listDeploymentsForBaseGroup().value should have size 0
+  }
+
   ignore("A task lost with mesos master failover will expunge the task after gc timeout - https://github.com/mesosphere/marathon/issues/4212") {
     Given("a new app")
     val app = appProxy(testBasePath / "app", "v1", instances = 1, withHealth = false)
@@ -117,6 +175,12 @@ class TaskLostIntegrationTest extends IntegrationFunSuite with WithMesosCluster 
   def matchEvent(status: String, task: ITEnrichedTask): CallbackEvent => Boolean = { event =>
     event.info.get("taskStatus").contains(status) &&
       event.info.get("taskId").contains(task.id)
+  }
+
+  def matchDeploymentSuccess(instanceCount: Int, appId: String): CallbackEvent => Boolean = { event =>
+    val infoString = event.info.toString()
+    event.eventType == "deployment_success" && infoString.contains("instances -> 1") &&
+      infoString.contains(s"List(Map(actions -> List(Map(action -> ScaleApplication, app -> $appId)))))")
   }
 
 }
