c4a080a555682aa1887083a745d4db7d1354af49
Matthias Veit <matthias_veit@yahoo.de>
2016-10-19 12:34:28 +0200

    Use mesos 1.1.0-rc1
    Summary: Update mesos version for docker image and latest Mesos RC.
    
    Test Plan: sbt integration:test
    
    Reviewers: unterstein, jeschkies
    
    Reviewed By: jeschkies
    
    Subscribers: jenkins, marathon-team
    
    Differential Revision: https://phabricator.mesosphere.com/D95

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -74,9 +74,9 @@ object Dependency {
     val Chaos = "0.8.7"
     val Guava = "19.0"
     // FIXME (gkleiman): reenable deprecation checks after Mesos 1.0.0-rc2 deprecations are handled
-    val Mesos = "1.1.0-SNAPSHOT"
+    val Mesos = "1.1.0-rc1"
     // Version of Mesos to use in Dockerfile.
-    val MesosDebian = "1.1.0-0.0.316.pre.20161005gitb70a22b.debian81"
+    val MesosDebian = "1.1.0-0.0.326.pre.20161015git46671dd.debian81"
     val Akka = "2.4.10"
     val AsyncAwait = "0.9.6-RC5"
     val Spray = "1.3.3"
