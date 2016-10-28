31f9abe960afe261b7779835e2fe9a0278587424
Gareth Bryce <bgareth8@uk.ibm.com>
2016-10-28 09:33:51 +0100

    Fixes Leadership Deadlock & Socket Leak described in https://github.com/mesosphere/marathon/issues/4624

--- src/main/scala/mesosphere/marathon/MarathonSchedulerService.scala
+++ src/main/scala/mesosphere/marathon/MarathonSchedulerService.scala
@@ -235,7 +235,6 @@ class MarathonSchedulerService @Inject() (
         driver.foreach(_.run())
       }
     } onComplete { result =>
-      synchronized {
 
         log.info(s"Driver future completed with result=$result.")
         result match {
@@ -257,7 +256,6 @@ class MarathonSchedulerService @Inject() (
         log.info(s"Call postDriverRuns callbacks on ${prePostDriverCallbacks.mkString(", ")}")
         Await.result(Future.sequence(prePostDriverCallbacks.map(_.postDriverTerminates)), config.zkTimeoutDuration)
         log.info("Finished postDriverRuns callbacks")
-      }
     }
   }
 
