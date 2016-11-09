3cb31f642f63182a490afe1bad273106bbd76d97
Jason Gilanfarr <jgilanfarr@mesosphere.com>
2016-11-09 11:02:58 -0800

    Migrate Apps/Roots/Plans with broken constraints
    Summary:
    - Crossport of dc37f96cf68537054911b441545c5a6b336f94ea to master
      https://phabricator.mesosphere.com/D119
    - Update all instances of apps (plans, roots, and apps) to fix '*' constraints into '.*' which is a proper
      regular expression. When encountering other regular expressions that are _not_ valid regular expressions,
      the constraint will be removed and an error will be logged accordingly.
    - Interestingly enough, the original commit mistakenly doesn't actually update apps
      which wasn't caught by testing (because .equals is wrong) which I'm addressing in a separate review.
      Due to this, I've incremented the version number to 1.3.6 from the original 1.3.5. Should we need to crossport back to 1.3,
      we'll need to add the actual fixApp call and increment to 1.3.6
    
    Test Plan: sbt test
    
    Reviewers: aquamatthias, jdef
    
    Reviewed By: aquamatthias, jdef
    
    Subscribers: jenkins, marathon-team
    
    Differential Revision: https://phabricator.mesosphere.com/D159

---
+++ src/main/scala/mesosphere/marathon/storage/migration/legacy/MigrationTo_1_4_0.scala
@@ -0,0 +1,114 @@
+package mesosphere.marathon
+package storage.migration.legacy
+
+import java.util.regex.Pattern
+
+import akka.Done
+import akka.stream.Materializer
+import com.typesafe.scalalogging.StrictLogging
+import mesosphere.marathon.Protos.Constraint
+import mesosphere.marathon.core.pod.PodDefinition
+import mesosphere.marathon.metrics.Metrics
+import mesosphere.marathon.state.{ AppDefinition, Group }
+import mesosphere.marathon.storage.LegacyStorageConfig
+import mesosphere.marathon.storage.repository.{ AppRepository, DeploymentRepository, GroupRepository, PodRepository }
+import mesosphere.marathon.stream._
+import mesosphere.marathon.upgrade.DeploymentPlan
+
+import scala.async.Async._
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.Try
+
+@SuppressWarnings(Array("ClassNames"))
+class MigrationTo_1_4_0(config: Option[LegacyStorageConfig])(implicit
+  ctx: ExecutionContext,
+    metrics: Metrics,
+    mat: Materializer) extends StrictLogging {
+
+  private def isBrokenConstraint(constraint: Constraint): Boolean = {
+    (constraint.getOperator == Constraint.Operator.LIKE ||
+      constraint.getOperator == Constraint.Operator.UNLIKE) &&
+      (constraint.hasValue && Try(Pattern.compile(constraint.getValue)).isFailure)
+  }
+
+  private def fixConstraints(app: AppDefinition): AppDefinition = {
+    val newConstraints: Set[Constraint] = app.constraints.flatMap { constraint =>
+      constraint.getOperator match {
+        case Constraint.Operator.LIKE | Constraint.Operator.UNLIKE if isBrokenConstraint(constraint) =>
+          // we know we have a value and the pattern doesn't compile already.
+          if (constraint.getValue == "*") {
+            Some(constraint.toBuilder.setValue(".*").build())
+          } else {
+            logger.warn(s"Removing invalid constraint $constraint from ${app.id}")
+            None
+          }
+        case _ => Some(constraint)
+      }
+    }(collection.breakOut)
+    app.copy(constraints = newConstraints)
+  }
+
+  private def migrateApps(appRepository: AppRepository): Future[Done] = {
+    appRepository.all().collect {
+      case app: AppDefinition if app.constraints.exists(isBrokenConstraint) => app
+    }.mapAsync(Int.MaxValue) { app =>
+      logger.info(s"Fixing  with invalid constraints: ${app.id}")
+      appRepository.store(fixConstraints(app))
+    }.runForeach(_ => Done)
+  }
+
+  private def fixRoot(group: Group): Group = {
+    group.update(group.version) { g =>
+      val apps = g.apps.map {
+        case (appId, app) =>
+          app match {
+            case _ if app.constraints.exists(isBrokenConstraint) =>
+              appId -> fixConstraints(app)
+            case _ => appId -> app
+          }
+      }
+      g.copy(apps = apps)
+    }
+  }
+
+  private def migrateRoot(groupRepository: GroupRepository): Future[Done] = async {
+    val root = await(groupRepository.root())
+    if (root.transitiveApps.exists(app => app.constraints.exists(isBrokenConstraint))) {
+      logger.info("Updating apps in root group as they have invalid constraints")
+      val updatedApps: Seq[AppDefinition] = root.transitiveApps.filterAs(app => app.constraints.exists(isBrokenConstraint))(collection.breakOut)
+      await(groupRepository.storeRoot(fixRoot(root), updatedApps, Nil, Nil, Nil))
+    } else {
+      Done
+    }
+  }
+
+  private def migratePlans(deploymentRepository: DeploymentRepository): Future[Done] = {
+    deploymentRepository.all().collect {
+      case plan: DeploymentPlan if plan.original.transitiveApps.exists(app => app.constraints.exists(isBrokenConstraint)) ||
+        plan.target.transitiveApps.exists(app => app.constraints.exists(isBrokenConstraint)) => plan
+    }.mapAsync(Int.MaxValue) { plan =>
+      logger.info(s"Fixing plan ${plan.id} as it contains apps with invalid " +
+        "constraints (even if they are not affected by the plan itself)")
+      deploymentRepository.store(plan.copy(original = fixRoot(plan.original), target = fixRoot(plan.target)))
+    }.runForeach(_ => Done)
+  }
+
+  def migrate(
+    appRepository: AppRepository,
+    groupRepository: GroupRepository, deploymentRepository: DeploymentRepository): Future[Done] = async {
+    logger.info("Starting migration to 1.3.6")
+    await(Future.sequence(Seq(migrateApps(appRepository), migrateRoot(groupRepository), migratePlans(deploymentRepository))))
+    logger.info("Finished 1.3.6 migration")
+    Done
+  }
+
+  def migrate(): Future[Done] = {
+    config.fold[Future[Done]](Future.successful(Done)) { config =>
+      val appRepository = AppRepository.legacyRepository(config.entityStore[AppDefinition], config.maxVersions)
+      val podRepository = PodRepository.legacyRepository(config.entityStore[PodDefinition], config.maxVersions)
+      val groupRepository = GroupRepository.legacyRepository(config.entityStore[Group], config.maxVersions, appRepository, podRepository)
+      val deploymentRepository = DeploymentRepository.legacyRepository(config.entityStore[DeploymentPlan])
+      migrate(appRepository, groupRepository, deploymentRepository)
+    }
+  }
+}
---
+++ src/test/scala/mesosphere/marathon/storage/migration/legacy/MigrationTo_1_40Test.scala
@@ -0,0 +1,109 @@
+package mesosphere.marathon
+package storage.migration.legacy
+
+import akka.Done
+import akka.stream.scaladsl.Source
+import com.codahale.metrics.MetricRegistry
+import mesosphere.AkkaUnitTest
+import mesosphere.marathon.Protos.Constraint
+import mesosphere.marathon.Protos.Constraint.Operator
+import mesosphere.marathon.Protos.Constraint.Operator._
+import mesosphere.marathon.metrics.Metrics
+import mesosphere.marathon.state.{ AppDefinition, Group, PathId }
+import mesosphere.marathon.storage.repository.{ AppRepository, DeploymentRepository, GroupRepository, PodRepository }
+import mesosphere.marathon.upgrade.DeploymentPlan
+
+import scala.concurrent.Future
+
+class MigrationTo_1_40Test extends AkkaUnitTest {
+  implicit val metrics = new Metrics(new MetricRegistry)
+
+  def constraint(
+    field: String,
+    operator: Operator,
+    value: Option[String] = None): Constraint = {
+    val builder = Constraint.newBuilder().setField(field)
+    builder.setOperator(operator)
+    value.foreach(builder.setValue)
+    builder.build()
+  }
+
+  "Migration to 1.3.6" when {
+    "no apps/roots/plans have any broken constraints" should {
+      val appRepo = mock[AppRepository]
+      val groupRepo = mock[GroupRepository]
+      val deployRepo = mock[DeploymentRepository]
+      val podRepo = mock[PodRepository]
+
+      "not do anything" in {
+        appRepo.all() returns Source.single(AppDefinition(id = PathId("abc")))
+        groupRepo.root() returns Future.successful(Group.empty)
+        deployRepo.all() returns Source.empty[DeploymentPlan]
+
+        new MigrationTo_1_4_0(None).migrate(appRepo, groupRepo, deployRepo).futureValue
+
+        verify(appRepo).all()
+        verify(groupRepo).root()
+        verify(deployRepo).all()
+        noMoreInteractions(appRepo, podRepo, groupRepo, deployRepo)
+      }
+    }
+
+    "app has broken constraints" should {
+      val appRepo = mock[AppRepository]
+      val podRepo = mock[PodRepository]
+      val groupRepo = mock[GroupRepository]
+      val deployRepo = mock[DeploymentRepository]
+
+      "fix '*' regex's, remove bad regex's and preserve non-broken constraints" in {
+        val badApp = AppDefinition(id = PathId("/badApp"), constraints = Set(
+          constraint("hostname", LIKE, Some("*")),
+          constraint("hostname", UNLIKE, Some("*")),
+          constraint("hostname", LIKE, Some("\\w+")),
+          constraint("hostname", UNLIKE, Some("\\w+")),
+          constraint("hostname", LIKE, Some("+")), // not a valid regex
+          constraint("hostname", UNLIKE, Some("+")),
+          constraint("hostname", GROUP_BY, None)))
+        val goodApp = AppDefinition(id = PathId("/goodApp"))
+        val root = Group(id = PathId("/"), apps = Map(badApp.id -> badApp, goodApp.id -> goodApp),
+          groups = Set(Group(id = PathId("/a"), apps = Map(PathId("/a/bad") -> badApp, PathId("/a/bad") -> goodApp)))
+        )
+        val badPlan = DeploymentPlan(root, root)
+        val goodPlan = DeploymentPlan(Group.empty, Group.empty)
+
+        appRepo.all() returns Source(Seq(badApp, goodApp))
+        groupRepo.root() returns Future.successful(root)
+        deployRepo.all() returns Source(Seq(goodPlan, badPlan))
+
+        appRepo.store(any) returns Future.successful(Done)
+        groupRepo.storeRoot(any, any, any, any, any) returns Future.successful(Done)
+        deployRepo.store(any) returns Future.successful(Done)
+
+        new MigrationTo_1_4_0(None).migrate(appRepo, groupRepo, deployRepo).futureValue
+
+        val fixedApp = AppDefinition(id = PathId("/badApp"), constraints = Set(
+          constraint("hostname", LIKE, Some(".*")),
+          constraint("hostname", UNLIKE, Some(".*")),
+          constraint("hostname", LIKE, Some("\\w+")),
+          constraint("hostname", UNLIKE, Some("\\w+")),
+          constraint("hostname", GROUP_BY, None)))
+        val fixedRoot = Group(id = PathId("/"), apps = Map(badApp.id -> fixedApp, goodApp.id -> goodApp), pods = Map.empty,
+          groups = Set(Group(id = PathId("/a"), apps = Map(PathId("/a/bad") -> fixedApp, PathId("/a/bad") -> goodApp),
+            pods = Map.empty, dependencies = Set.empty,
+            version = root.version)),
+          dependencies = Set.empty,
+          version = root.version
+        )
+        val fixedPlan = badPlan.copy(original = fixedRoot, target = fixedRoot)
+
+        verify(appRepo).all()
+        verify(groupRepo).root()
+        verify(deployRepo).all()
+        verify(appRepo).store(fixedApp)
+        verify(groupRepo).storeRoot(fixedRoot, Seq(fixedApp), Nil, Nil, Nil)
+        verify(deployRepo).store(fixedPlan)
+        noMoreInteractions(appRepo, groupRepo, deployRepo)
+      }
+    }
+  }
+}
