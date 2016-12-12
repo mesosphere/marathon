2bf6fd013532c0ffe237ef92b77d343ad9c50eaa
Tim Harper <tharper@mesosphere.com>
2016-12-12 16:22:32 -0500

    Increase default expunge timeout tasks
    Summary:
    The default expunge time of 6 minutes is too aggressive and was leading
    to persistent volumes getting deleted regular maintenance tasks such as
    reboot a host.
    
    Also, update text describing the expungeAfter to make sure it's clear
    that it will result in volume deletion.
    
    addresses #4603
    
    Test Plan: n/a
    
    Reviewers: jasongilanfarr, aquamatthias, meichstedt, jdef
    
    Reviewed By: meichstedt, jdef
    
    Subscribers: jenkins, marathon-team
    
    Differential Revision: https://phabricator.mesosphere.com/D301

--- src/main/scala/mesosphere/marathon/state/UnreachableStrategy.scala
+++ src/main/scala/mesosphere/marathon/state/UnreachableStrategy.scala
@@ -9,12 +9,12 @@ import scala.concurrent.duration._
   * Defines the time outs for unreachable tasks.
   */
 case class UnreachableStrategy(
-  inactiveAfter: FiniteDuration = UnreachableStrategy.DefaultTimeUntilInactive,
-  expungeAfter: FiniteDuration = UnreachableStrategy.DefaultTimeUntilExpunge)
+  inactiveAfter: FiniteDuration = UnreachableStrategy.DefaultInactiveAfter,
+  expungeAfter: FiniteDuration = UnreachableStrategy.DefaultExpungeAfter)
 
 object UnreachableStrategy {
-  val DefaultTimeUntilInactive = 3.minutes
-  val DefaultTimeUntilExpunge = 6.minutes
+  val DefaultInactiveAfter: FiniteDuration = 15.minutes
+  val DefaultExpungeAfter: FiniteDuration = 7.days
 
   implicit val unreachableStrategyValidator = validator[UnreachableStrategy] { strategy =>
     strategy.inactiveAfter should be >= 1.second
