5069189acd2bde3a3d7126cab4406c37d7fdb1a1
Jason Gilanfarr <jgilanfarr@mesosphere.com>
2016-12-20 13:44:45 -0800

    Inform the rate limiter for pods and apps (#4863)
    Summary: The rate limiter incorrectly assumed that all runSpecs are apps.
    
    Test Plan: sbt test with new test
    
    Reviewers: meichstedt, jdef, timcharper, aquamatthias
    
    Reviewed By: jdef, timcharper, aquamatthias
    
    Subscribers: aquamatthias, jenkins, marathon-team
    
    Differential Revision: https://phabricator.mesosphere.com/D352
    
    (cherry picked from commit c8924aa8d3a21346a7728c881d6378dc969e320c)

--- src/test/scala/mesosphere/marathon/core/task/tracker/impl/InstanceOpProcessorImplTest.scala
+++ src/test/scala/mesosphere/marathon/core/task/tracker/impl/InstanceOpProcessorImplTest.scala
@@ -1,4 +1,5 @@
-package mesosphere.marathon.core.task.tracker.impl
+package mesosphere.marathon
+package core.task.tracker.impl
 
 import akka.Done
 import akka.actor.{ ActorRef, Status }
@@ -7,32 +8,29 @@ import akka.testkit.TestProbe
 import ch.qos.logback.classic.Level
 import com.codahale.metrics.MetricRegistry
 import com.google.inject.Provider
+import mesosphere.AkkaFunTest
 import mesosphere.marathon.core.CoreGuiceModule
 import mesosphere.marathon.core.base.ConstantClock
 import mesosphere.marathon.core.group.GroupManager
 import mesosphere.marathon.core.health.HealthCheckManager
 import mesosphere.marathon.core.instance.TestInstanceBuilder
-import mesosphere.marathon.core.instance.update.{ InstanceUpdateEffect, InstanceUpdateOpResolver, InstanceUpdateOperation }
+import mesosphere.marathon.core.instance.update.{ InstanceUpdateEffect, InstanceUpdateOpResolver, InstanceUpdateOperation, InstanceUpdated }
 import mesosphere.marathon.core.launchqueue.LaunchQueue
+import mesosphere.marathon.core.pod.PodDefinition
 import mesosphere.marathon.core.task.bus.{ MesosTaskStatusTestHelper, TaskStatusEmitter }
 import mesosphere.marathon.core.task.tracker.TaskUpdater
 import mesosphere.marathon.core.task.update.impl.steps.{ NotifyHealthCheckManagerStepImpl, NotifyLaunchQueueStepImpl, NotifyRateLimiterStepImpl, PostToEventStreamStepImpl, ScaleAppUpdateStepImpl, TaskStatusEmitterPublishStepImpl }
 import mesosphere.marathon.metrics.Metrics
-import mesosphere.marathon.state.{ PathId, Timestamp }
+import mesosphere.marathon.state.{ AppDefinition, PathId, Timestamp }
 import mesosphere.marathon.storage.repository.InstanceRepository
-import mesosphere.marathon.test.{ CaptureLogEvents, MarathonActorSupport, Mockito, _ }
+import mesosphere.marathon.test.{ CaptureLogEvents, _ }
 import org.apache.mesos.SchedulerDriver
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{ GivenWhenThen, Matchers }
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.util.{ Failure, Success, Try }
 
-class InstanceOpProcessorImplTest
-    extends MarathonActorSupport with MarathonSpec with Mockito with GivenWhenThen with ScalaFutures with Matchers {
-
-  import scala.concurrent.ExecutionContext.Implicits.global
+class InstanceOpProcessorImplTest extends AkkaFunTest {
 
   // ignored by the TaskOpProcessorImpl
   val deadline = Timestamp.zero
@@ -377,6 +375,36 @@ class InstanceOpProcessorImplTest
 
     And("no more interactions")
     f.verifyNoMoreInteractions()
+  }
+
+  test("the rate limiter will inform the launch queue of apps") {
+    val f = new Fixture
+    val appId = PathId("/pod")
+    val app = AppDefinition(id = appId)
+    val version = Timestamp.now()
+    val builder = TestInstanceBuilder.newBuilder(appId, version, version).addTaskDropped()
+
+    f.groupManager.appVersion(appId, version.toOffsetDateTime) returns Future.successful(Some(app))
+    f.groupManager.podVersion(appId, version.toOffsetDateTime) returns Future.successful(None)
+    f.notifyRateLimiter.process(InstanceUpdated(builder.instance, None, Nil)).futureValue
+    verify(f.groupManager).appVersion(appId, version.toOffsetDateTime)
+    verify(f.groupManager).podVersion(appId, version.toOffsetDateTime)
+    verify(f.launchQueue).addDelay(app)
+  }
+
+  test("the rate limiter will inform the launch queue of pods") {
+    val f = new Fixture
+    val podId = PathId("/pod")
+    val pod = PodDefinition(id = podId)
+    val version = Timestamp.now()
+    val builder = TestInstanceBuilder.newBuilder(podId, version, version).addTaskDropped()
+
+    f.groupManager.appVersion(podId, version.toOffsetDateTime) returns Future.successful(None)
+    f.groupManager.podVersion(podId, version.toOffsetDateTime) returns Future.successful(Some(pod))
+    f.notifyRateLimiter.process(InstanceUpdated(builder.instance, None, Nil)).futureValue
+    verify(f.groupManager).appVersion(podId, version.toOffsetDateTime)
+    verify(f.groupManager).podVersion(podId, version.toOffsetDateTime)
+    verify(f.launchQueue).addDelay(pod)
   }
 
   class Fixture {
