db9dda947629cb5a09841912aaab82b7579f6485
Tim Harper <tharper@mesosphere.com>
2017-01-04 11:16:23 +0100

    escape leader-latch lock (targets 1.4)
    Summary:
    Brad Peters in https://github.com/mesosphere/marathon/pull/4640 points
    out that LeaderLatch.setLeadership has a lock, and that this can lead
    to deadlock situations.
    
    In this patch we change the approach such that leadership change
    events are sent asynchronously to CuratorElectionService, freeing the
    LeaderLatch setLeadership lock to be closed while we try to obtain
    others in a separate thread.
    
    Test Plan: test leader election code
    
    Reviewers: jdef, jasongilanfarr, jeschkies
    
    Subscribers: marathon-team, jenkins
    
    Differential Revision: https://phabricator.mesosphere.com/D395

--- src/main/scala/mesosphere/marathon/core/election/impl/CuratorElectionService.scala
+++ src/main/scala/mesosphere/marathon/core/election/impl/CuratorElectionService.scala
@@ -1,5 +1,6 @@
 package mesosphere.marathon.core.election.impl
 
+import com.typesafe.scalalogging.StrictLogging
 import java.util
 import java.util.Collections
 import java.util.concurrent.Executors
@@ -16,10 +17,16 @@ import org.apache.curator.framework.{ AuthInfo, CuratorFramework, CuratorFramewo
 import org.apache.curator.{ RetryPolicy, RetrySleeper }
 import org.apache.zookeeper.data.ACL
 import org.apache.zookeeper.{ CreateMode, KeeperException, ZooDefs }
-import org.slf4j.LoggerFactory
+import scala.concurrent.Future
 
 import scala.util.control.NonFatal
+import scala.concurrent.ExecutionContext
 
+/**
+  * Handles our election leader election concerns.
+  *
+  * TODO - This code should be substantially simplified https://github.com/mesosphere/marathon/issues/4908
+  */
 class CuratorElectionService(
   config: MarathonConf,
   system: ActorSystem,
@@ -29,10 +36,10 @@ class CuratorElectionService(
   backoff: ExponentialBackoff,
   shutdownHooks: ShutdownHooks) extends ElectionServiceBase(
   system, eventStream, metrics, backoff, shutdownHooks
-) {
-  private lazy val log = LoggerFactory.getLogger(getClass.getName)
-
+) with StrictLogging {
   private val callbackExecutor = Executors.newSingleThreadExecutor()
+  /* We re-use the single thread executor here because code locks (via synchronized) frequently */
+  override protected implicit val executionContext: ExecutionContext = ExecutionContext.fromExecutor(callbackExecutor)
 
   private lazy val client = provideCuratorClient()
   private var maybeLatch: Option[LeaderLatch] = None
@@ -45,15 +52,15 @@ class CuratorElectionService(
       }
     } catch {
       case NonFatal(e) =>
-        log.error("error while getting current leader", e)
+        logger.error("error while getting current leader", e)
         None
     }
   }
 
   override def offerLeadershipImpl(): Unit = synchronized {
-    log.info("Using HA and therefore offering leadership")
+    logger.info("Using HA and therefore offering leadership")
     maybeLatch.foreach { l =>
-      log.info("Offering leadership while being candidate")
+      logger.info("Offering leadership while being candidate")
       l.close()
     }
 
@@ -64,32 +71,28 @@ class CuratorElectionService(
       maybeLatch = Some(latch)
     } catch {
       case NonFatal(e) =>
-        log.error(s"ZooKeeper initialization failed - Committing suicide: ${e.getMessage}")
+        logger.error(s"ZooKeeper initialization failed - Committing suicide: ${e.getMessage}")
         Runtime.getRuntime.asyncExit()(scala.concurrent.ExecutionContext.global)
     }
   }
 
+  /**
+    * Listener which forwards leadership status events asynchronously via the provided function.
+    *
+    * We delegate the methods asynchronously so they are processed outside of the synchronized lock for LeaderLatch.setLeadership
+    */
   private object Listener extends LeaderLatchListener {
-    override def notLeader(): Unit = CuratorElectionService.this.synchronized {
-      log.info(s"Defeated (LeaderLatchListener Interface). New leader: ${leaderHostPort.getOrElse("-")}")
+    override def notLeader(): Unit = Future {
+      logger.info(s"Leader defeated. New leader: ${leaderHostPort.getOrElse("-")}")
 
       // remove tombstone for twitter commons
       twitterCommonsTombstone.delete(onlyMyself = true)
-
       stopLeadership()
     }
 
-    override def isLeader(): Unit = CuratorElectionService.this.synchronized {
-      log.info("Elected (LeaderLatchListener Interface)")
-      startLeadership(error => CuratorElectionService.this.synchronized {
-        maybeLatch match {
-          case None => log.error("Abdicating leadership while not being leader")
-          case Some(l) =>
-            maybeLatch = None
-            l.close()
-        }
-        // stopLeadership() is called in notLeader
-      })
+    override def isLeader(): Unit = Future {
+      logger.info("Leader elected")
+      startLeadership(onAbdicate(_))
 
       // write a tombstone into the old twitter commons leadership election path which always
       // wins the selection. Check that startLeadership was successful and didn't abdicate.
@@ -99,8 +102,18 @@ class CuratorElectionService(
     }
   }
 
+  private[this] def onAbdicate(error: Boolean): Unit = synchronized {
+    maybeLatch match {
+      case None => logger.error(s"Abdicating leadership while not being leader (error: $error)")
+      case Some(l) =>
+        maybeLatch = None
+        l.close()
+    }
+    // stopLeadership() is called by the leader Listener in notLeader
+  }
+
   def provideCuratorClient(): CuratorFramework = {
-    log.info(s"Will do leader election through ${config.zkHosts}")
+    logger.info(s"Will do leader election through ${config.zkHosts}")
 
     // let the world read the leadership information as some setups depend on that to find Marathon
     val acl = new util.ArrayList[ACL]()
@@ -129,7 +142,7 @@ class CuratorElectionService(
       }).
       retryPolicy(new RetryPolicy {
         override def allowRetry(retryCount: Int, elapsedTimeMs: Long, sleeper: RetrySleeper): Boolean = {
-          log.error("ZooKeeper access failed - Committing suicide to avoid invalidating ZooKeeper state")
+          logger.error("ZooKeeper access failed - Committing suicide to avoid invalidating ZooKeeper state")
           Runtime.getRuntime.asyncExit()(scala.concurrent.ExecutionContext.global)
           false
         }
@@ -178,14 +191,14 @@ class CuratorElectionService(
           fallbackCreated = true
         }
 
-        log.info("Creating tombstone for old twitter commons leader election")
+        logger.info("Creating tombstone for old twitter commons leader election")
         client.create().
           creatingParentsIfNeeded().
           withMode(CreateMode.EPHEMERAL).
           forPath(path, hostPort.getBytes("UTF-8"))
       } catch {
         case NonFatal(e) =>
-          log.error(s"Exception while creating tombstone for twitter commons leader election: ${e.getMessage}")
+          logger.error(s"Exception while creating tombstone for twitter commons leader election: ${e.getMessage}")
           abdicateLeadership(error = true)
       }
     }
@@ -196,7 +209,7 @@ class CuratorElectionService(
         try {
           if (!onlyMyself ||
             new String(client.getData.forPath(memberPath(memberName))) == hostPort) {
-            log.info("Deleting existing tombstone for old twitter commons leader election")
+            logger.info("Deleting existing tombstone for old twitter commons leader election")
             client.delete().guaranteed().withVersion(tombstone.getVersion).forPath(path)
           }
         } catch {
