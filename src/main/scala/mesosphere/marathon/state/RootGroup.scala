10bacad3a1169b4e1f35eadee57dc9aeafb7d45d
Matthias Veit <matthias_veit@yahoo.de>
2017-01-05 12:37:25 +0100

    Fix scapegoat warnings and use stable identifier.
    Summary: Scapegoat prints some warnings, that do not make sense.
    
    Test Plan: sbt scapegoat
    
    Reviewers: zen-dog, jeschkies
    
    Reviewed By: zen-dog, jeschkies
    
    Subscribers: jenkins, marathon-team
    
    Differential Revision: https://phabricator.mesosphere.com/D397
    
    (cherry picked from commit 9dfbcde2ebabd639fbb7a0d58c48846205e4dc4b)

--- src/main/scala/mesosphere/marathon/state/RootGroup.scala
+++ src/main/scala/mesosphere/marathon/state/RootGroup.scala
@@ -155,12 +155,12 @@ class RootGroup(
     def updateApps(group: Group): Group = {
       Group(
         id = group.id,
-        apps = group.apps.map { case (id, appDef) => id -> app(appDef) },
+        apps = group.apps.map { case (appId, appDef) => appId -> app(appDef) },
         pods = group.pods,
-        groupsById = group.groupsById.map { case (id, group) => id -> updateApps(group) },
+        groupsById = group.groupsById.map { case (subGroupId, subGroup) => subGroupId -> updateApps(subGroup) },
         dependencies = group.dependencies,
         version = version,
-        transitiveAppsById = group.transitiveAppsById.map { case (id, appDef) => id -> app(appDef) },
+        transitiveAppsById = group.transitiveAppsById.map { case (appId, appDef) => appId -> app(appDef) },
         transitivePodsById = group.transitivePodsById)
     }
     val oldGroup = group(groupId).getOrElse(Group.empty(groupId))
@@ -307,7 +307,7 @@ class RootGroup(
       id = group.id,
       apps = group.apps,
       pods = group.pods,
-      groupsById = group.groupsById.map { case (id, group) => id -> updateVersion(group, version) },
+      groupsById = group.groupsById.map { case (subGroupId, subGroup) => subGroupId -> updateVersion(subGroup, version) },
       dependencies = group.dependencies,
       version = version,
       transitiveAppsById = group.transitiveAppsById,
@@ -398,17 +398,18 @@ class RootGroup(
   /**
     * Returns a new `RootGroup` where all transitive groups, apps, and pods have their `version` set to `Timestamp(0)`.
     */
+  @SuppressWarnings(Array("PartialFunctionInsteadOfMatch"))
   def withNormalizedVersions: RootGroup = {
     def in(group: Group): Group = {
       Group(
         id = group.id,
-        apps = group.apps.map { case (id, app) => id -> app.copy(versionInfo = VersionInfo.NoVersion) },
-        pods = group.pods.map { case (id, pod) => id -> pod.copy(version = Timestamp(0)) },
-        groupsById = group.groupsById.map { case (id, group) => id -> in(group) },
+        apps = group.apps.map { case (appId, app) => appId -> app.copy(versionInfo = VersionInfo.NoVersion) },
+        pods = group.pods.map { case (podId, pod) => podId -> pod.copy(version = Timestamp(0)) },
+        groupsById = group.groupsById.map { case (subGroupId, subGroup) => subGroupId -> in(subGroup) },
         dependencies = group.dependencies,
         version = Timestamp(0),
-        transitiveAppsById = group.transitiveAppsById.map { case (id, app) => id -> app.copy(versionInfo = VersionInfo.NoVersion) },
-        transitivePodsById = group.transitivePodsById.map { case (id, pod) => id -> pod.copy(version = Timestamp(0)) })
+        transitiveAppsById = group.transitiveAppsById.map { case (appId, app) => appId -> app.copy(versionInfo = VersionInfo.NoVersion) },
+        transitivePodsById = group.transitivePodsById.map { case (podId, pod) => podId -> pod.copy(version = Timestamp(0)) })
     }
     RootGroup.fromGroup(in(this))
   }
