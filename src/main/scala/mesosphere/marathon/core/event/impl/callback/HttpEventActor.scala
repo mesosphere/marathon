eaaa9947b1900b9ffd33a9940a82a63a48271038
janisz <janisz@users.noreply.github.com>
2017-01-06 01:43:18 +0100

    Parse JSON event only once before broadcast (#4927)
    Cherry-picked: #4771
    Current implementation parses events for every callback.
    This patch make parsing happen only once for event.

--- src/main/scala/mesosphere/marathon/core/event/impl/callback/HttpEventActor.scala
+++ src/main/scala/mesosphere/marathon/core/event/impl/callback/HttpEventActor.scala
@@ -8,6 +8,7 @@ import mesosphere.marathon.core.event.impl.callback.HttpEventActor._
 import mesosphere.marathon.core.event._
 import mesosphere.marathon.core.event.impl.callback.SubscribersKeeperActor.GetSubscribers
 import mesosphere.marathon.metrics.{ MetricPrefixes, Metrics }
+import play.api.libs.json.JsValue
 import spray.client.pipelining.{ sendReceive, _ }
 import spray.http.{ HttpRequest, HttpResponse }
 import spray.httpx.PlayJsonSupport
@@ -95,7 +96,8 @@ class HttpEventActor(
     //remove all unsubscribed callback listener
     limiter = limiter.filterKeys(subscribers.urls).iterator.toMap.withDefaultValue(NoLimit)
     metrics.skippedCallbacks.mark(limited.size)
-    active.foreach(url => Try(post(url, event, self)) match {
+    val jsonEvent = eventToJson(event)
+    active.foreach(url => Try(post(url, jsonEvent, self)) match {
       case Success(res) =>
       case Failure(ex) =>
         log.warning(s"Failed to post $event to $url because ${ex.getClass.getSimpleName}: ${ex.getMessage}")
@@ -104,12 +106,12 @@ class HttpEventActor(
     })
   }
 
-  def post(url: String, event: MarathonEvent, eventActor: ActorRef): Unit = {
+  def post(url: String, event: JsValue, eventActor: ActorRef): Unit = {
     log.info("Sending POST to:" + url)
 
     metrics.outstandingCallbacks.inc()
     val start = clock.now()
-    val request = Post(url, eventToJson(event))
+    val request = Post(url, event)
 
     val response = pipeline(context.dispatcher)(request)
 
