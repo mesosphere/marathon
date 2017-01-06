f32e85f5388a4969c8e4e817593964ef337f5180
jeschkies <jeskar@web.de>
2017-01-06 16:12:20 +0100

    Test if second Marathon fails when it binds to same port.
    Summary:
    This is tricky. Sometimes all tests would pass but sbt would still report an
    error because a test has exited with 137. As it turns out the Zookeepr client
    time out would keep the test process alive until it halted.
    
    Moreover the test did not kill all its threads. This fix starts Marathon in a
    separate process and destroys it if it times out.
    
    Test Plan: integration-test
    
    Reviewers: timcharper, meichstedt, jasongilanfarr
    
    Subscribers: jenkins, marathon-team

--- src/test/scala/mesosphere/marathon/integration/MarathonStartupIntegrationTest.scala
+++ src/test/scala/mesosphere/marathon/integration/MarathonStartupIntegrationTest.scala
@@ -1,25 +1,51 @@
 package mesosphere.marathon
 package integration
 
-import mesosphere.{ AkkaIntegrationTest }
-import mesosphere.marathon.integration.setup._
+import java.nio.file.Files
+import java.util.UUID
 
-@SerialIntegrationTest
-class MarathonStartupIntegrationTest extends AkkaIntegrationTest with EmbeddedMarathonTest {
+import mesosphere.AkkaIntegrationTest
+import mesosphere.marathon.integration.setup._
+import org.scalatest.concurrent.TimeLimits
+import org.scalatest.time.{ Seconds, Span }
+
+import scala.sys.process.Process
+
+class MarathonStartupIntegrationTest extends AkkaIntegrationTest with EmbeddedMarathonTest with TimeLimits {
+
   "Marathon" should {
     "fail during start, if the HTTP port is already bound" in {
       Given(s"a Marathon process already running on port ${marathonServer.httpPort}")
 
       When("starting another Marathon process using an HTTP port that is already bound")
 
-      val conflict = new MarathonApp(Seq("--master", marathonServer.masterUrl,
-        "--zk", marathonServer.zkUrl, "--http_port", marathonServer.httpPort.toString))
+      val args = Seq(
+        "--master", marathonServer.masterUrl,
+        "--zk", marathonServer.zkUrl,
+        "--http_port", marathonServer.httpPort.toString,
+        "--zk_timeout", "2000"
+      )
 
-      Then("An uncaught exception should be thrown")
-      intercept[Throwable] {
-        conflict.start()
+      val mainClass = "mesosphere.marathon.Main"
+      val workDir = Files.createTempDirectory(s"marathon-${marathonServer.httpPort}-conflict").toFile
+      workDir.deleteOnExit()
+      val uuid = UUID.randomUUID.toString
+      val java = sys.props.get("java.home").fold("java")(_ + "/bin/java")
+      val cp = sys.props.getOrElse("java.class.path", "target/classes")
+      val memSettings = s"-Xmx${Runtime.getRuntime.maxMemory()}"
+      val cmd = Seq(java, memSettings, s"-DmarathonUUID=$uuid -DtestSuite=$suiteName", "-classpath", cp, mainClass) ++ args
+      val marathonProcess = Process(cmd, workDir, sys.env.toSeq: _*)
+        .run(ProcessOutputToLogStream(s"$suiteName-LocalMarathon-${marathonServer.httpPort}-conflict"))
+
+      Then("The Marathon process should exit with code > 0")
+      try {
+        failAfter(Span(40, Seconds)) {
+          marathonProcess.exitValue() should be > 0
+        }
+      } finally {
+        // Destroy process if it did not exit in time.
+        marathonProcess.destroy()
       }
-      conflict.close()
     }
   }
 }
