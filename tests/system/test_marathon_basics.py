4394d180ebc7a92f6c9b675090cbba938fe385e8
jeschkies <jeskar@web.de>
2017-01-18 17:01:20 +0100

    Verify data is persisted in volumes.
    Summary:
    This defines a system integration test that persists data which is
    exposed through a simple Python server.
    
    https://mesosphere.atlassian.net/browse/MARATHON-1356
    
    Test Plan: system-integration
    
    Reviewers: kensipe, zen-dog
    
    Reviewed By: kensipe, zen-dog
    
    Subscribers: marathon-team, jenkins
    
    Differential Revision: https://phabricator.mesosphere.com/D416

--- tests/system/test_marathon_basics.py
+++ tests/system/test_marathon_basics.py
@@ -552,6 +552,39 @@ def test_pinned_task_does_not_find_unknown_host():
         tasks = client.get_tasks('/pinned')
         assert len(tasks) == 0
 
+def test_launch_container_with_presistent_volume():
+
+    with marathon_on_marathon():
+        app_def = peristent_volume_app()
+        client = marathon.create_client()
+        client.add_app(app_def)
+        deployment_wait()
+
+        tasks = client.get_tasks(app_id)
+        assert len(tasks) == 1
+
+        port = tasks[0]['ports'][0]
+        host = tasks[0]['host']
+        cmd = "curl {}:{}/data/foo".format(host, port)
+        run, data = run_command_on_master(cmd)
+
+        assert run, "{} did not succeed".format(cmd)
+        assert data == 'hello\n', "'{}' was not equal to hello\\n".format(data)
+
+        client.restart_app(app_id)
+        deployment_wait()
+
+        tasks = client.get_tasks(app_id)
+        assert len(tasks) == 1
+
+        port = tasks[0]['ports'][0]
+        host = tasks[0]['host']
+        cmd = "curl {}:{}/data/foo".format(host, port)
+        run, data = run_command_on_master(cmd)
+
+        assert run, "{} did not succeed".format(cmd)
+        assert data == 'hello\nhello\n', "'{}' was not equal to hello\\nhello\\n".format(data)
+
 
 def setup_function(function):
     with marathon_on_marathon():
