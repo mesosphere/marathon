61d1dd004bf3e8ebf8dcd7dcd53e0d84bc0fe2ef
Matthias Veit <matthias_veit@yahoo.de>
2017-01-31 15:35:07 +0100

    Enable RichRuntimeTest again, since it seems not to be Unstable (Fixes #5020)
    Summary: See https://github.com/mesosphere/marathon/issues/5020
    
    Test Plan: sbt test
    
    Reviewers: timcharper, jeschkies
    
    Reviewed By: timcharper, jeschkies
    
    Subscribers: jenkins, marathon-team
    
    Differential Revision: https://phabricator.mesosphere.com/D470

--- src/test/scala/mesosphere/marathon/core/base/RichRuntimeTest.scala
+++ src/test/scala/mesosphere/marathon/core/base/RichRuntimeTest.scala
@@ -1,13 +1,13 @@
 package mesosphere.marathon.core.base
 
-import mesosphere.{ AkkaUnitTest, Unstable }
+import mesosphere.AkkaUnitTest
 import mesosphere.marathon.test.ExitDisabledTest
 
 import scala.concurrent.duration._
 
 class RichRuntimeTest extends AkkaUnitTest with ExitDisabledTest {
   "RichRuntime.asyncExit" should {
-    "call exit" taggedAs (Unstable) in {
+    "call exit" in {
       Runtime.getRuntime.asyncExit(123, 1.millisecond)
       exitCalled(123).futureValue should be(true)
     }
