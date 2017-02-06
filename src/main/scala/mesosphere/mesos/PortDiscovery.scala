593ca148d2a6063de8b769b34356e30c9e8d92d3
James DeFelice <james.defelice@gmail.com>
2017-02-06 16:38:58 -0500

    add task and port discovery info for pod subtasks (in the same manner that we already do for apps)
    Summary:
    Pod tasks should have mesos DiscoverInfo just like app tasks do. It's actually
    required in order for certain aspects of DCOS networking to function properly.
    
    xref https://github.com/mesosphere/marathon/issues/5087
    
    Test Plan:
    sbt test
    
    manually test integration with minuteman;
    dcos-networking team is tasked with writing system integration tests
    
    Reviewers: aquamatthias, jasongilanfarr, jenkins
    
    Reviewed By: aquamatthias, jasongilanfarr, jenkins
    
    Subscribers: marathon-team
    
    JIRA Issues: MARATHON-1378
    
    Differential Revision: https://phabricator.mesosphere.com/D477

---
+++ src/main/scala/mesosphere/mesos/PortDiscovery.scala
@@ -0,0 +1,69 @@
+package mesosphere.mesos
+
+import mesosphere.marathon.api.serialization.{ PortDefinitionSerializer, PortMappingSerializer }
+import mesosphere.marathon.raml.Endpoint
+import mesosphere.marathon.state.{ AppDefinition, DiscoveryInfo, IpAddress }
+import org.apache.mesos.Protos.Port
+
+import scala.collection.immutable.Seq
+
+trait PortDiscovery {
+
+  /**
+    * @param hostModeNetworking is true if we're only using host networking (vs. bridged or container networking)
+    * @param endpoints are assumed to have had wildcard ports (e.g. 0) filled in with actual port numbers
+    */
+  def generate(hostModeNetworking: Boolean, endpoints: Seq[Endpoint]): Seq[Port] =
+    if (!hostModeNetworking) {
+      // The run spec uses bridge and user modes with portMappings, use them to create the Port messages.
+      // Note: pods only work with Mesos containerizer, which doesn't yet have bridge or port-mapping support, so
+      // we MUST use network-scope=container for any advertised ports here. This is distinctly different than how apps
+      // are implemented, which (for now) advertise network-scope=host unless there's no host-port specified (apps
+      // support bridged mode and port mappings, and so advertising the host scope can lead to better perf).
+      endpoints.flatMap { ep =>
+        val updatedEp = ep.copy(labels = ep.labels + NetworkScope.Container.discovery)
+        val containerPort: Int = ep.containerPort.getOrElse(throw new IllegalStateException(
+          "expected non-empty container port in conjunction with non-host networking"
+        ))
+        PortMappingSerializer.toMesosPorts(updatedEp, containerPort)
+      }(collection.breakOut)
+    } else {
+      // The port numbers are the allocated ports, we need to overwrite them the port numbers assigned to this particular task.
+      // network-scope is assumed to be host, no need for an additional scope label here.
+      endpoints.flatMap { ep =>
+        val hostPort: Int = ep.hostPort.getOrElse(throw new IllegalStateException(
+          "expected non-empty host port in conjunction with host networking"
+        ))
+        PortMappingSerializer.toMesosPorts(ep, hostPort)
+      }(collection.breakOut)
+    }
+
+  def generate(runSpec: AppDefinition, hostPorts: Seq[Option[Int]]): Seq[Port] =
+    runSpec.ipAddress match {
+      case Some(IpAddress(_, _, DiscoveryInfo(ports), _)) if ports.nonEmpty => ports.map(_.toProto)
+      case _ =>
+        runSpec.container.withFilter(_.portMappings.nonEmpty).map { c =>
+          // The run spec uses bridge and user modes with portMappings, use them to create the Port messages
+          c.portMappings.zip(hostPorts).collect {
+            case (portMapping, None) =>
+              // No host port has been defined. See PortsMatcher.mappedPortRanges, use container port instead.
+              val updatedPortMapping =
+                portMapping.copy(labels = portMapping.labels + NetworkScope.Container.discovery)
+              PortMappingSerializer.toMesosPort(updatedPortMapping, portMapping.containerPort)
+            case (portMapping, Some(hostPort)) =>
+              val updatedPortMapping = portMapping.copy(labels = portMapping.labels + NetworkScope.Host.discovery)
+              PortMappingSerializer.toMesosPort(updatedPortMapping, hostPort)
+          }
+        }.getOrElse(
+          // Serialize runSpec.portDefinitions to protos. The port numbers are the service ports, we need to
+          // overwrite them the port numbers assigned to this particular task.
+          // network-scope is assumed to be host, no need for an additional scope label here.
+          runSpec.portDefinitions.zip(hostPorts).collect {
+          case (portDefinition, Some(hostPort)) =>
+            PortDefinitionSerializer.toMesosProto(portDefinition).map(_.toBuilder.setNumber(hostPort).build)
+        }.flatten
+        )
+    }
+}
+
+object PortDiscovery extends PortDiscovery
