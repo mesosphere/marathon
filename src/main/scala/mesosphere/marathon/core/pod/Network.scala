96e391b74ea88ba95796c1a7885bfd6c274ad642
James DeFelice <james.defelice@gmail.com>
2017-02-07 12:41:53 -0500

    define NetworkSpec interface for pod plugins
    Summary:
    Alice wants to build a Marathon plugin that validates the labels of pod networks.
    
    blocking https://github.com/mesosphere/marathon-dcos-plugins/issues/110
    
    Test Plan: sbt test
    
    Reviewers: jenkins, jasongilanfarr, aquamatthias, timcharper
    
    Reviewed By: jenkins, jasongilanfarr, aquamatthias, timcharper
    
    Subscribers: marathon-team
    
    JIRA Issues: MARATHON-1380
    
    Differential Revision: https://phabricator.mesosphere.com/D490

--- src/main/scala/mesosphere/marathon/core/pod/Network.scala
+++ src/main/scala/mesosphere/marathon/core/pod/Network.scala
@@ -1,11 +1,16 @@
-package mesosphere.marathon.core.pod
+package mesosphere.marathon
+package core.pod
+
+import mesosphere.marathon.plugin.NetworkSpec
 
 /**
   * Network declared by a [[PodDefinition]].
   */
-sealed trait Network extends Product with Serializable
+sealed trait Network extends Product with Serializable with NetworkSpec
 
-case object HostNetwork extends Network
+case object HostNetwork extends Network {
+  val labels: Map[String, String] = Map.empty // implements NetworkSpec
+}
 
 case class ContainerNetwork(name: String, labels: Map[String, String] = Network.DefaultLabels) extends Network
 
