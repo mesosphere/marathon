90b3975868b3543a6e7b00f269293cf18161f14a
pfcoperez <contact@pablofranciscoperez.info>
2017-02-07 18:40:08 +0100

    Exponential backoff: Remove synchronized area over current duration monitor. It has been replaced by non-blocking AtomicLong logic

--- src/main/scala/mesosphere/marathon/core/election/impl/ExponentialBackoff.scala
+++ src/main/scala/mesosphere/marathon/core/election/impl/ExponentialBackoff.scala
@@ -1,5 +1,8 @@
 package mesosphere.marathon.core.election.impl
 
+import java.util.concurrent.atomic.AtomicLong
+import scala.language.postfixOps
+
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.duration._
@@ -14,20 +17,30 @@ class ExponentialBackoff(
     initialValue: FiniteDuration = 0.5.seconds,
     maximumValue: FiniteDuration = 16.seconds,
     name: String = "unnamed") extends Backoff {
+
+  private val maxMillis = maximumValue toMillis
+  private val initialMillis = initialValue toMillis
+
   private val log = LoggerFactory.getLogger(getClass.getName)
-  private var v = initialValue
+  private val v: AtomicLong = new AtomicLong(initialMillis)
 
-  def value(): FiniteDuration = synchronized { v }
+  def value(): FiniteDuration = v.get() milliseconds
 
-  def increase(): Unit = synchronized {
-    if (v <= maximumValue) {
-      v *= 2
-      log.info(s"Increasing $name backoff to $v")
+  def increase(): Unit = {
+    def atomicConditionalIncrement(): Unit = {
+      val current = v.get
+      val next = current * 2
+      if (current <= maxMillis && !v.compareAndSet(current, next))
+        atomicConditionalIncrement
+      else if (current <= maxMillis)
+        log.info(s"Increasing $name backoff to $v")
     }
+    atomicConditionalIncrement()
   }
 
   def reset(): Unit = synchronized {
     log.info(s"Reset $name backoff")
-    v = initialValue
+    v.set(initialMillis)
   }
+
 }
