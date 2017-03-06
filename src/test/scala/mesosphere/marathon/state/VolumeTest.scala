15a3801ff7d29513a45a941100e4cd0715700528
James DeFelice <james.defelice@gmail.com>
2017-03-06 07:25:21 -0500

    Fixes maxSize protobuf serialiation bug for persistent volumes
    Summary:
    unit test and fix for persistent vol maxsize proto serialization
    
    https://github.com/mesosphere/marathon/issues/5303
    MARATHON-1408
    
    backport of e59b8f9b
    
    Test Plan: sbt test
    
    Reviewers: aquamatthias, meichstedt, timcharper, jenkins, unterstein
    
    Reviewed By: meichstedt, jenkins, unterstein
    
    Subscribers: marathon-team
    
    Differential Revision: https://phabricator.mesosphere.com/D577

--- src/test/scala/mesosphere/marathon/state/VolumeTest.scala
+++ src/test/scala/mesosphere/marathon/state/VolumeTest.scala
@@ -1,20 +1,80 @@
 package mesosphere.marathon.state
 
 import com.wix.accord._
+import mesosphere.marathon.api.serialization.VolumeSerializer
 import mesosphere.marathon.api.v2.ValidationHelper
 import mesosphere.marathon.test.MarathonSpec
 import org.scalatest.Matchers
 import org.apache.mesos.Protos.Resource.DiskInfo.Source
+import org.apache.mesos.Protos.Volume.Mode
 
 class VolumeTest extends MarathonSpec with Matchers {
   import mesosphere.marathon.test.MarathonTestHelper.constraint
 
-  test("validating PersistentVolumeInfo constraints accepts an empty constraint list") {
-    val pvi = PersistentVolumeInfo(
+  def survivesProtobufSerializationRoundtrip(title: => String, volume: => Volume): Unit = {
+    test(s"$title survives protobuf serialization round-trip") {
+      val protobuf = VolumeSerializer.toProto(volume)
+      val resurrected = Volume(protobuf)
+      resurrected should be(volume)
+    }
+  }
+
+  def persistent(info: PersistentVolumeInfo, containerPath: String = "cpath", mode: Mode = Mode.RW): PersistentVolume =
+    PersistentVolume(
+      containerPath = containerPath,
+      persistent = info,
+      mode = mode
+    )
+
+  def external(info: ExternalVolumeInfo, containerPath: String = "cpath", mode: Mode = Mode.RW): ExternalVolume =
+    ExternalVolume(
+      containerPath = containerPath,
+      external = info,
+      mode = mode
+    )
+
+  trait Fixture {
+    val rootVolNoConstraints = PersistentVolumeInfo(
       1024,
       constraints = Set.empty)
+    val pathVolWithConstraint = PersistentVolumeInfo(
+      1024,
+      `type` = DiskType.Path,
+      constraints = Set(constraint("path", "LIKE", Some("valid regex"))))
+    val mountVolWithMaxSize = PersistentVolumeInfo(
+      1024,
+      `type` = DiskType.Mount,
+      maxSize = Some(2048))
+    val extVolNoSize = ExternalVolumeInfo(
+      name = "volname",
+      provider = "provider",
+      options = Map("foo" -> "bar")
+    )
+    val extVolWithSize = ExternalVolumeInfo(
+      size = Option(1),
+      name = "volname",
+      provider = "provider",
+      options = Map("foo" -> "bar", "baz" -> "qaw")
+    )
+    val hostVol = DockerVolume(
+      containerPath = "cpath",
+      hostPath = "/host/path",
+      mode = Mode.RW
+    )
+  }
+  object Fixture extends Fixture
 
-    validate(pvi).isSuccess shouldBe true
+  survivesProtobufSerializationRoundtrip("root vol, no constraints", persistent(Fixture.rootVolNoConstraints))
+  survivesProtobufSerializationRoundtrip("path vol w/ constraint", persistent(Fixture.pathVolWithConstraint))
+  survivesProtobufSerializationRoundtrip("mount vol w/ maxSize", persistent(Fixture.mountVolWithMaxSize))
+  survivesProtobufSerializationRoundtrip("ext vol w/o size", external(Fixture.extVolNoSize))
+  survivesProtobufSerializationRoundtrip("ext vol w/ size", external(Fixture.extVolWithSize))
+  survivesProtobufSerializationRoundtrip("host vol", Fixture.hostVol)
+
+  test("validating PersistentVolumeInfo constraints accepts an empty constraint list") {
+    new Fixture {
+      validate(rootVolNoConstraints).isSuccess shouldBe true
+    }
   }
 
   test("validating PersistentVolumeInfo constraints rejects unsupported fields") {
@@ -58,19 +118,19 @@ class VolumeTest extends MarathonSpec with Matchers {
   }
 
   test("validating PersistentVolumeInfo maxSize parameter wrt type") {
-    val resultRoot = validate(
-      PersistentVolumeInfo(1024, `type` = DiskType.Root, maxSize = Some(2048)))
-    resultRoot.isSuccess shouldBe false
-    ValidationHelper.getAllRuleConstrains(resultRoot).map(_.message) shouldBe Set("Only mount volumes can have maxSize")
+    new Fixture {
+      val resultRoot = validate(
+        PersistentVolumeInfo(1024, `type` = DiskType.Root, maxSize = Some(2048)))
+      resultRoot.isSuccess shouldBe false
+      ValidationHelper.getAllRuleConstrains(resultRoot).map(_.message) shouldBe Set("Only mount volumes can have maxSize")
 
-    val resultPath = validate(
-      PersistentVolumeInfo(1024, `type` = DiskType.Path, maxSize = Some(2048)))
-    resultPath.isSuccess shouldBe false
-    ValidationHelper.getAllRuleConstrains(resultPath).map(_.message) shouldBe Set("Only mount volumes can have maxSize")
+      val resultPath = validate(
+        PersistentVolumeInfo(1024, `type` = DiskType.Path, maxSize = Some(2048)))
+      resultPath.isSuccess shouldBe false
+      ValidationHelper.getAllRuleConstrains(resultPath).map(_.message) shouldBe Set("Only mount volumes can have maxSize")
 
-    validate(
-      PersistentVolumeInfo(1024, `type` = DiskType.Mount, maxSize = Some(2048))).
-      isSuccess shouldBe true
+      validate(mountVolWithMaxSize).isSuccess shouldBe true
+    }
   }
 
   test("validating that DiskSource asMesos converts to an Option Mesos Protobuffer") {
