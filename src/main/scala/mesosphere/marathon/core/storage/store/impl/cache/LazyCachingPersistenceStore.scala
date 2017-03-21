3357f792ce75718619590dc2854a43597956b76c
Matthias Veit <matthias_veit@yahoo.de>
2017-03-21 08:53:45 -0600

    Only modify the version cache if it was loaded from the underlying store
    Summary:
    Fixes MARATHON-7133
    
    See also D626 and D627
    
    Test Plan: sbt test
    
    Reviewers: jeschkies, jenkins, aquamatthias
    
    Reviewed By: jeschkies, jenkins, aquamatthias
    
    Subscribers: marathon-team
    
    JIRA Issues: MARATHON-7133
    
    Differential Revision: https://phabricator.mesosphere.com/D631

--- src/main/scala/mesosphere/marathon/core/storage/store/impl/cache/LazyCachingPersistenceStore.scala
+++ src/main/scala/mesosphere/marathon/core/storage/store/impl/cache/LazyCachingPersistenceStore.scala
@@ -207,8 +207,11 @@ case class LazyVersionCachingPersistentStore[K, Category, Serialized](
     val unversionedId = ir.toStorageId(id, None)
     maybePurgeCachedVersions()
     versionedValueCache.put((unversionedId, version), v)
-    val cached = versionCache.getOrElse((category, unversionedId), Set.empty) // linter:ignore UndesirableTypeInference
-    versionCache.put((category, unversionedId), cached + version)
+    if (versionCache.contains((category, unversionedId))) {
+      // possible race: there is no way to get/update the value in place
+      val cached = versionCache.getOrElse((category, unversionedId), Set.empty) // linter:ignore UndesirableTypeInference
+      versionCache.put((category, unversionedId), cached + version)
+    }
   }
 
   @SuppressWarnings(Array("all")) // async/await
