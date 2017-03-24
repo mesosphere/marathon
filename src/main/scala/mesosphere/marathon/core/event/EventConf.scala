76331b1a1209563f62d2c1797b137fcc63d9731f
Johannes Unterstein <junterstein@mesosphere.io>
2017-03-24 15:22:28 +0100

    Use clientIp of configuration for EventSubscription
    Summary: MoM on centos has problems getting hostname for localhost. In general the own hostname should not be calculated each time, instead a configuration should be used or it should be calculated only on startup.
    
    Test Plan:
    sbt "test"
    sbt "integration:test"
    
    Reviewers: jasongilanfarr, aquamatthias, meichstedt, jenkins
    
    Reviewed By: jasongilanfarr, aquamatthias, meichstedt, jenkins
    
    Subscribers: timcharper, marathon-team
    
    JIRA Issues: DCOS-14303
    
    Differential Revision: https://phabricator.mesosphere.com/D639

--- src/main/scala/mesosphere/marathon/core/event/EventConf.scala
+++ src/main/scala/mesosphere/marathon/core/event/EventConf.scala
@@ -4,7 +4,7 @@ package core.event
 import java.util.concurrent.TimeUnit
 
 import akka.util.Timeout
-import org.rogach.scallop.ScallopConf
+import org.rogach.scallop.{ ScallopConf, ScallopOption }
 import mesosphere.marathon.api.v2.Validation.urlIsValid
 
 import scala.concurrent.duration.FiniteDuration
@@ -56,6 +56,8 @@ trait EventConf extends ScallopConf {
   def slowConsumerDuration: FiniteDuration = httpEventCallbackSlowConsumerTimeout().millis
 
   def eventRequestTimeout: Timeout = Timeout(httpEventRequestTimeout(), TimeUnit.MILLISECONDS)
+
+  def hostname: ScallopOption[String]
 
   def zkTimeoutDuration: FiniteDuration
 }
