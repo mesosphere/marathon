8f9fb492bbe6b671519fa4c3b5fcac14eeee4ede
Matthias Veit <matthias_veit@yahoo.de>
2017-05-15 19:15:50 +0000

    [AkkaHttp] Implement HttpRequest and HttpResponse facades to bridge the plugin to akka http request and response.
    Summary:
    Bridge akka-http request and response to the plugin interface.
    Render customized results from the plugin correctly in akka-http.
    
    Test Plan:
    Start Marathon with an authorization plugin that rejects the request.
    The request should have all the necessary information.
    The customized response should be rendered correctly.
    
    Reviewers: timcharper, jenkins, jeschkies
    
    Reviewed By: timcharper, jenkins, jeschkies
    
    Subscribers: marathon-dev
    
    JIRA Issues: MARATHON-7319
    
    Differential Revision: https://phabricator.mesosphere.com/D782
    
    Signed-off-by: Mesosphere CI Robot <mesosphere-ci@users.noreply.github.com>

--- src/main/scala/mesosphere/marathon/api/akkahttp/AuthDirectives.scala
+++ src/main/scala/mesosphere/marathon/api/akkahttp/AuthDirectives.scala
@@ -1,11 +1,10 @@
 package mesosphere.marathon
 package api.akkahttp
 
-import akka.http.scaladsl.model.{ HttpRequest, StatusCodes }
-import akka.http.scaladsl.server.{ Directives => AkkaDirectives }
-import akka.http.scaladsl.server._
-import mesosphere.marathon.plugin.auth.{ Authenticator => MarathonAuthenticator, AuthorizedAction, Authorizer, Identity }
-import mesosphere.marathon.plugin.http.{ HttpRequest => PluginRequest, HttpResponse => PluginResponse }
+import akka.http.scaladsl.model.{ HttpResponse, StatusCodes }
+import akka.http.scaladsl.server.{ Directives => AkkaDirectives, _ }
+import mesosphere.marathon.plugin.auth.{ AuthorizedAction, Authorizer, Identity, Authenticator => MarathonAuthenticator }
+import mesosphere.marathon.plugin.http.{ HttpRequest => PluginRequest }
 
 import scala.util.{ Failure, Success }
 
@@ -26,11 +25,13 @@ trait AuthDirectives extends AkkaDirectives {
     * Note that an Authenticator is expected to be in scope
     */
   def authenticated(implicit authenticator: MarathonAuthenticator): Directive1[Identity] = extractRequest.flatMap { request =>
-    val pluginRequest = toPluginRequest(request)
-    onComplete(authenticator.authenticate(pluginRequest)).flatMap {
-      case Success(Some(identity)) => provide(identity)
-      case Success(None) => reject(NotAuthenticated(toResponse(authenticator.handleNotAuthenticated(pluginRequest, _))))
-      case Failure(_) => reject(AuthServiceUnavailable)
+    extractClientIP.flatMap { clientIP =>
+      val pluginRequest: PluginRequest = HttpPluginFacade.request(request, clientIP)
+      onComplete(authenticator.authenticate(pluginRequest)).flatMap {
+        case Success(Some(identity)) => provide(identity)
+        case Success(None) => reject(NotAuthenticated(HttpPluginFacade.response(authenticator.handleNotAuthenticated(pluginRequest, _))))
+        case Failure(_) => reject(AuthServiceUnavailable)
+      }
     }
   }
 
@@ -50,65 +51,24 @@ trait AuthDirectives extends AkkaDirectives {
     *     }
     *
     * @param action The action for which to check authorization for the given identity
-    * @param action The entity for which authorization should be checked
+    * @param resource The entity for which authorization should be checked
     */
   def authorized[Resource](action: AuthorizedAction[Resource], resource: Resource)(implicit authorizer: Authorizer, identity: Identity): Directive0 =
     if (authorizer.isAuthorized(identity, action, resource))
       pass
     else
-      reject(NotAuthorized(toResponse(authorizer.handleNotAuthorized(identity, _))))
+      reject(NotAuthorized(HttpPluginFacade.response(authorizer.handleNotAuthorized(identity, _))))
 }
 
 object AuthDirectives {
 
-  trait ToResponse {
-    // TODO - MARATHON-7319 actually implement this!
-  }
   private[AuthDirectives] case object AuthServiceUnavailable extends Rejection
-  private[AuthDirectives] case class NotAuthorized(toResponse: ToResponse) extends Rejection
-  private[AuthDirectives] case class NotAuthenticated(toResponse: ToResponse) extends Rejection
+  private[AuthDirectives] case class NotAuthorized(toResponse: HttpResponse) extends Rejection
+  private[AuthDirectives] case class NotAuthenticated(toResponse: HttpResponse) extends Rejection
 
   def handleAuthRejections: PartialFunction[Rejection, Route] = {
-    // TODO - MARATHON-7319 actually implement these!
-    case AuthServiceUnavailable => Directives.complete(StatusCodes.EnhanceYourCalm -> "auth service unavailable (TODO)")
-    case NotAuthorized(_) => Directives.complete(StatusCodes.EnhanceYourCalm -> "not authorized (TODO)")
-    case NotAuthenticated(_) => Directives.complete(StatusCodes.EnhanceYourCalm -> "not authenticated (TODO)")
-  }
-
-  def toPluginRequest(request: HttpRequest): PluginRequest = new PluginRequest {
-    override def method: String = request.method.value
-
-    override def cookie(name: String): Option[String] = request.cookies.find(_.name == name).map(_.value)
-
-    override def localPort: Int = 123
-
-    override def remotePort: Int = 123
-
-    override def header(name: String): Seq[String] = request.headers.filter(_.name == name).map(_.value())
-
-    override def queryParam(name: String): Seq[String] = request.uri.query().filter(_._1 == name).map(_._2)
-
-    override def localAddr: String = ""
-
-    override def requestPath: String = request.uri.path.toString()
-
-    override def remoteAddr: String = ""
-  }
-
-  def toResponse(action: (PluginResponse) => Unit): ToResponse = {
-    val builder = new PluginResponse with ToResponse {
-
-      override def cookie(name: String, value: String, maxAge: Int, secure: Boolean): Unit = ???
-
-      override def body(mediaType: String, bytes: Array[Byte]): Unit = ???
-
-      override def sendRedirect(url: String): Unit = ???
-
-      override def header(header: String, value: String): Unit = ???
-
-      override def status(code: Int): Unit = ???
-    }
-    action(builder)
-    builder
+    case AuthServiceUnavailable => Directives.complete(StatusCodes.ServiceUnavailable -> "Auth Service currently not available.")
+    case NotAuthorized(pluginResponse) => Directives.complete(pluginResponse)
+    case NotAuthenticated(pluginResponse) => Directives.complete(pluginResponse)
   }
 }
