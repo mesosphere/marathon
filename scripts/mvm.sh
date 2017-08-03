9edc91ba28d3ee36bf99d64b09aac2e4dfeae933
Keno Schwalb <kschwalb@mesosphere.io>
2017-08-03 15:58:18 +0200

    MVM fixes & features
    Summary:
    - Feature: Provide installer for persistent configuration (support for bash, zsh, sh, fish)
    - Feature: Provide --list as an alias for --tags
    - Fix / Feature remove: Do not allow to specify shell program for temporary shell (always use bash)
    - Fix: Don't switch temporary shell's version when persistent shell's version is switched
    - Fix: Always run ./bootstrap when compiling sources
    - Fix (UX): Use ✓ and ● instead of * to mark installed versions in mvm --tags (* means "active" in --installed)
    - Chore: Documentation updates
    
    Differential Revision: https://phabricator.mesosphere.com/D959

--- scripts/mvm.sh
+++ scripts/mvm.sh
@@ -25,40 +25,81 @@
 # The script clones the Apache Mesos sources files into ~/.mesos/mesos_src
 # Versions activated by the user are compiled and installed to ~/.mesos/mesos_versions/<VERSION>
 # (E.g. ~/.mesos/mesos_version/1.3.0-rc3)
-# ~/mesos/current is a symlink which always points to the currently activated version.
 # When mvm is run, it first checks if the requested version is already present
 # in ~/.mesos/mesos_versions. If not, the requested version is compiled.
-# It then updates the symlink ~/.mesos/current
-# If a shell was given as second paramter mvm will spawn an instance of the shell which was configured
-# to be able to run the chosen version of Mesos.
+# By default, MVM will then spawn a shell which was temporarily configured with the paths to
+# the requested version of Apache Mesos.
+#
+# MVM can be switched to "persistent mode", in which the shell configuration is persisted on disk.
+# In this mode, MVM will create a symlink at ~/mesos/current, which always points to the currently
+# activated version, and store the current version number in ~/mesos/current_version.
+# The following operations are carried out during the installation process:
+# Path configurations are stored in ~/.mesos/env
+# A "source" command for ~/.mesos/env is added to ~/.profile and
+# ~/.config/fish/config.fish (if fish is installed)
+# ~/.zshrc (if zsh is installed)
+# mvm.sh is copied to ~/.mesos/bin
+# The file ~/.mesos/install.lock is created to signify that mvm is installed persistently
+# By default, mvm will not spawn a new shell while persistent mode is enabled.
 #
 
 set -e
 
+MVM_BASE="$HOME/.mesos"
+MVM_BIN="$MVM_BASE/bin"
+MVM_INSTALL_LOCK_FILE="$MVM_BASE/install.lock"
+MVM_ENV_FILE="$MVM_BASE/env"
+
+MESOS_INSTALL_BASE="$MVM_BASE/mesos_versions"
+MESOS_SOURCES="$MVM_BASE/mesos_src"
+
+FISH_RC_FILE="$HOME/.config/fish/config.fish"
+ZSH_RC_FILE="$HOME/.zshrc"
+GENERIC_PROFILE_FILE="$HOME/.profile"
+ENV_SOURCE_STRING="source ${MVM_ENV_FILE/$HOME/\$HOME}"
+
+# check if persistent mode is enabled
+if [ -f "$MVM_INSTALL_LOCK_FILE" ]; then
+  PERSISTENT_MODE=true
+else
+  PERSISTENT_MODE=false
+fi
+
 function print_help {
   echo "MVM, The Mesos Version Manager."
   echo ""
-  echo "This script allows to install and switch between different versions of Apache Mesos."
+  echo "MVM allows to install and switch between different versions of Apache Mesos."
   echo "All versions are stored in the folder ~/.mesos/mesos_versions"
-  echo "The folder ~/.mesos/current always points to the currently active version."
-  echo "You should configure your shell to search for Mesos at this location."
-  echo "Use the --print-config option to display the required shell configuration."
-  echo "Alternatively, you may pass the SHELL argument to launch a configured shell."
   echo ""
-  echo "Usage: $0 [OPTION|VERSION] [SHELL]"
+  echo "By default, mvm will run in non-persistent mode."
+  echo "It will spawn a temporarily configured bash shell, and you will have to re-run"
+  echo "the script whenever you open up a new shell."
+  echo ""
+  echo "MVM can be switched to persistent mode by running $0 --self-install"
+  echo "This will configure the mesos environment variables for sh, bash, zsh, and fish."
+  echo "MVM will be copied into ~/.mesos/bin during the installation."
+  echo "If you are using a different shell, run $0 --self-install and add the line"
+  echo "$ENV_SOURCE_STRING"
+  echo "to your shell configuration or use the --print-config command."
+  echo ""
+  echo "Usage: $0 [COMMAND|VERSION]"
   echo ""
   echo "VERSION: Any tag or revision hash from the Mesos git repository may be chosen."
-  echo "SHELL: If set, the given shell will be configured to run Mesos and launched."
   echo ""
-  echo "OPTIONS:"
+  echo "Commands:"
   echo " --current                Show the currently activated Mesos version"
   echo " --delete [VERSION]       Delete an installed version of Mesos"
   echo " --fetch                  See --update"
   echo " --help                   Display this help screen"
+  if ! $PERSISTENT_MODE; then
+    echo " --self-install                Install mvm to ~/.mesos and configure shells"
+  fi
   echo " --installed              List all installed versions"
   echo " --latest                 Switch to the latest avaiable version (HEAD)"
-  echo " --print-config           Print the bash configuration"
+  echo " --list                   See --tags"
+  echo " --print-config           Print the shell configuration"
   echo " --tags                   List all available version tags"
+  echo " --shell [VERSION]        Force spawning a shell, even in persistent mode"
   echo " --update                 Update the Mesos sources"
   echo ""
   echo "Environment variables:"
@@ -69,7 +110,8 @@ function print_help {
 }
 
 function print_config {
-  local mesos_base="\$HOME/.mesos/current"
+  local version_path="$1"
+  local mesos_base="${MVM_BASE/$HOME/\$HOME}/$version_path"
   local mesos_bin="$mesos_base/bin"
   local mesos_sbin="$mesos_base/sbin"
   local mesos_lib="$mesos_base/lib"
@@ -80,6 +122,11 @@ function print_config {
   echo "export CPATH=\"$mesos_include:\$CPATH\";"
   echo "export LIBRARY_PATH=\"$mesos_lib:\$LIBRARY_PATH\";"
   echo "export PYTHONPATH=\"$mesos_lib/$PYTHON_LIB_FOLDER/site-packages:\$PYTHONPATH\""
+}
+
+function generate_env_content {
+  print_config "current"
+  echo "export PATH=\"$MVM_BASE/bin:\$PATH\""
 }
 
 function error {
@@ -101,13 +148,20 @@ function warn {
 
 function print_tags {
   cd "$MESOS_SOURCES"
+  local current_version
+
+  if [ -f "$MVM_BASE/current_version" ]; then
+    current_version=$(cat "$MVM_BASE/current_version")
+  fi
 
   for tag in $(git tag); do
     echo -ne "$tag"
 
-    # check if version is built yet
-    if [ -d "$MESOS_INSTALL_BASE/$tag" ]; then
-      echo -ne "*"
+    if [ "$current_version" == "$tag" ]; then
+      echo -ne "●"
+    elif [ -d "$MESOS_INSTALL_BASE/$tag" ]; then
+      # version is built & installed
+      echo -ne "✓"
     fi
 
     echo ""
@@ -126,7 +180,7 @@ function print_installed {
     version_name=$(basename "$version")
     echo -ne "$version_name"
     if [ "$version_name" == "$current_version" ]; then
-      echo -ne "*"
+      echo -ne "●"
     fi
     echo ""
   done
@@ -146,17 +200,15 @@ function compile_mesos_version {
   local target_dir="$MESOS_INSTALL_BASE/$requested_version"
 
   # checkout requested revision
-  if ! git checkout "$requested_version" &> /dev/null; then
+  if [ "$requested_version" = "" ] || ! git checkout "$requested_version" &> /dev/null; then
     error "Version '$requested_version' does not match a git tag or revision"
   fi
 
   echo "Version '$requested_version' is not currently installed."
   confirm "Compilation may take 30+ mins. Continue?"
 
-  # generate configure script if it does not exist
-  if [ ! -f "$MESOS_SOURCES/configure" ] || [ ! -f "$MESOS_SOURCES/Makefile.in" ]; then
-    ./bootstrap
-  fi
+  # always re-generate configure script as it may change with different versions
+  ./bootstrap
 
   mkdir -p build
   cd build
@@ -175,9 +227,9 @@ function compile_mesos_version {
   make install
 }
 
-function version_exists {
+function version_compiled {
   local requested_version="$1"
-  test -d "$MESOS_INSTALL_BASE/$requested_version"
+  [ "$requested_version" != "" ] && test -d "$MESOS_INSTALL_BASE/$requested_version"
 }
 
 function update_sources {
@@ -194,7 +246,7 @@ function get_head_revision {
 function delete_version {
   local requested_version="$1"
 
-  if ! version_exists "$requested_version"; then
+  if ! version_compiled "$requested_version"; then
     error "Mesos version '$requested_version' is not currently installed"
   fi
 
@@ -216,7 +268,7 @@ function spawn_shell {
   local mesos_include="$mesos_base/include"
 
   # spawn new shell
-  bash -c "$(print_config); export PS1=\"(mesos $requested_version) $PS1\"; ${SHELL:-bash}"
+  bash -c "$(print_config "mesos_versions/$requested_version"); export PS1=\"(mesos $requested_version) $PS1\"; bash"
 }
 
 function set_current_link {
@@ -226,7 +278,7 @@ function set_current_link {
   ln -s "$MESOS_INSTALL_BASE/$requested_version" "$MVM_BASE/current"
 }
 
-function activate_version {
+function prepare_version {
   local requested_version="$1"
   cd "$MESOS_SOURCES"
 
@@ -238,17 +290,89 @@ function activate_version {
   fi
 
   # verify that the specified Mesos version actually exists
-  if ! version_exists "$requested_version"; then
+  if ! version_compiled "$requested_version"; then
     compile_mesos_version "$requested_version"
   fi
+}
 
+function persist_version {
+  local requested_version="$1"
+  prepare_version "$requested_version"
   set_current_link "$requested_version"
   echo "$requested_version" > "$MVM_BASE/current_version"
 }
 
-MVM_BASE="$HOME/.mesos"
-MESOS_INSTALL_BASE="$MVM_BASE/mesos_versions"
-MESOS_SOURCES="$HOME/.mesos/mesos_src"
+function configure_rc_file {
+  local rc_file="$1"
+  local shell_name="$2"
+
+  if ! grep -qe "^$ENV_SOURCE_STRING\$" "$rc_file"; then
+    echo "Configuring $shell_name..."
+    echo "$ENV_SOURCE_STRING" >> "$rc_file"
+  fi
+}
+
+function install_mvm {
+  local step_env=false
+  local step_bin_dir=false
+  local step_zsh=false
+  local step_fish=false
+
+  local current_file="${BASH_SOURCE[0]}"
+  local mvm_bin_file="$MVM_BIN/mvm"
+
+  # check what needs to be done
+  if [ ! -f "$MVM_ENV_FILE" ]; then
+    step_env=true
+  fi
+
+  if [ ! -d "$MVM_BIN" ]; then
+    step_bin_dir=true
+  fi
+
+  if which zsh > /dev/null; then
+    step_zsh=true
+  fi
+
+  if which fish > /dev/null; then
+    step_fish=true
+  fi
+
+  # let user verify installation
+  echo "The following steps will be performed:"
+  if $step_env; then echo "Create environment file \"$MVM_ENV_FILE\""; fi
+  if $step_bin_dir; then echo "Create directory \"$MVM_BIN\""; fi
+  echo "Copy \"$current_file\" to \"$mvm_bin_file\""
+  echo "Source \"$MVM_ENV_FILE\" in \"$GENERIC_PROFILE_FILE\""
+  if $step_zsh; then echo "Source \"$MVM_ENV_FILE\" in \"$ZSH_RC_FILE\""; fi
+  if $step_fish; then echo "Source \"$MVM_ENV_FILE\" in \"$FISH_RC_FILE\""; fi
+  echo "Create \"$MVM_INSTALL_LOCK_FILE\""
+  confirm "Proceed?"
+
+  # perform installation
+  if $step_env; then
+    echo "Creating \"$MVM_ENV_FILE\""
+    generate_env_content > "$MVM_ENV_FILE"
+  fi
+
+  if $step_bin_dir; then
+    echo "Creating directory \"$MVM_BIN\""
+    mkdir "$MVM_BIN"
+  fi
+
+  echo "Copying $current_file to $mvm_bin_file"
+  cp "$current_file" "$mvm_bin_file"
+
+  # configure shells
+  configure_rc_file "$GENERIC_PROFILE_FILE" "bash & sh"
+  if $step_zsh; then configure_rc_file "$ZSH_RC_FILE" "zsh"; fi
+  if $step_fish; then configure_rc_file "$FISH_RC_FILE" "fish"; fi
+
+  touch "$MVM_INSTALL_LOCK_FILE"
+
+  echo "MVM successfully installed."
+  echo "Restart your shell or run \`$ENV_SOURCE_STRING\` to apply the changes."
+}
 
 # check OS compatibility
 if [ "$(uname)" == "Darwin" ]; then
@@ -261,6 +385,12 @@ else
   error "MVM currently only supports macOS and Linux."
 fi
 
+# check command line arguments for --help
+if [ "$1" == "--help" ]; then
+  print_help
+  exit 0
+fi
+
 # ensure that the MVM_BASE directory exists
 if [ ! -d "$MVM_BASE" ]; then
   mkdir "$MVM_BASE"
@@ -269,12 +399,6 @@ fi
 # ensure that the MESOS_INSTALL_BASE directory exists
 if [ ! -d "$MESOS_INSTALL_BASE" ]; then
   mkdir "$MESOS_INSTALL_BASE"
-fi
-
-# check command line arguments for --help
-if [ "$1" == "--help" ]; then
-  print_help
-  exit 0
 fi
 
 # check if mesos source directory exists
@@ -291,14 +415,28 @@ elif [ "$1" == "--current" ]; then
   print_current
   exit 0
 elif [ "$1" == "--print-config" ]; then
-  print_config
+  print_config "current"
   exit 0
-elif [ "$1" == "--tags" ]; then
+elif [ "$1" == "--tags" ] || [ "$1" == "--list" ]; then
   print_tags
+  exit 0
+elif [ "$1" == "--self-install" ]; then
+  if $PERSISTENT_MODE; then
+    echo "Error: Already installed"
+    exit 1
+  else
+    install_mvm
+  fi
   exit 0
 elif [ "$1" == "--installed" ]; then
   print_installed
   exit 0
+elif [ "$1" == "--shell" ]; then
+  if [ -z "${2+x}" ]; then
+    error "VERSION parameter required for --shell option"
+  fi
+  PERSISTENT_MODE=false
+  REQUESTED_VERSION="$2"
 elif [ "$1" == "--update" ] || [ "$1" == "--fetch" ]; then
   echo "Fetching updates..."
   update_sources
@@ -308,7 +446,6 @@ elif [ "$1" == "--delete" ]; then
   if [ -z "${2+x}" ]; then
     error "VERSION parameter required for --delete option"
   fi
-
   delete_version "$2"
   exit 0
 elif [ "$1" == "--latest" ]; then
@@ -321,18 +458,19 @@ else
   REQUESTED_VERSION="$1"
 fi
 
-activate_version "$(echo "$REQUESTED_VERSION" | tr "[:upper:]" "[:lower:]")"
+REQUESTED_VERSION_LOWER="$(echo "$REQUESTED_VERSION" | tr "[:upper:]" "[:lower:]")"
 
-if [ -n "$2" ]; then
-  SHELL="$2"
-  spawn_shell "$REQUESTED_VERSION"
-else
+if $PERSISTENT_MODE; then
+  persist_version "$REQUESTED_VERSION_LOWER"
+
   if ! echo "$PATH" | grep -q "$MVM_BASE/current"; then
     warn "Incorrect shell configuration detected."
-    echo "Run $0 --print-config and add the printed configuration to your shells' RC file."
-    echo "For bash, simply run the following command:"
-    echo "$0 --print-config >> ~/.bash_profile && source ~/.bash_profile"
-    echo ""
+    echo "MVM run in persistent mode but PATH not correctly set."
+    echo "Have you added '$ENV_SOURCE_STRING' to your shell configuration?"
   fi
+
   echo "Mesos $REQUESTED_VERSION successfully activated."
+else
+  prepare_version "$REQUESTED_VERSION_LOWER"
+  spawn_shell "$REQUESTED_VERSION"
 fi
