982704446473a070b3b12fe64998a401ea5c77f8
Tim Harper <tharper@mesosphere.com>
2017-09-14 17:46:51 -0600

    Find marathon manifest path
    Summary: The previous method for finding the manifest would only work for uber-jars
    
    Test Plan: build marathon, check that /v2/info endpoint returns correct marathon version
    
    Reviewers: jenkins, ichernetsky, unterstein
    
    Reviewed By: jenkins, ichernetsky, unterstein
    
    Subscribers: meln1k, unterstein, ichernetsky, marathon-dev, zen-dog
    
    JIRA Issues: MARATHON-7765
    
    Differential Revision: https://phabricator.mesosphere.com/D1063

--- src/main/scala/mesosphere/marathon/BuildInfo.scala
+++ src/main/scala/mesosphere/marathon/BuildInfo.scala
@@ -2,21 +2,36 @@ package mesosphere.marathon
 
 import java.util.jar.{ Attributes, Manifest }
 import scala.Predef._
-import scala.util.Try
 import scala.util.control.NonFatal
+import mesosphere.marathon.stream.Implicits._
 
 case object BuildInfo {
+  private val marathonJar = "\\bmesosphere\\.marathon\\.marathon-[0-9.]+".r
   lazy val DefaultMajor = 1
   lazy val DefaultMinor = 5
   lazy val DefaultPatch = 0
 
   lazy val DefaultBuildVersion = s"$DefaultMajor.$DefaultMinor.$DefaultPatch-SNAPSHOT"
 
-  lazy val manifest: Option[Manifest] = Try {
-    val mf = new Manifest()
-    mf.read(getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF"))
-    mf
-  }.toOption
+  /**
+    * sbt-native-package provides all of the files as individual JARs. By default, `getResourceAsStream` returns the
+    * first matching file for the first JAR in the class path. Instead, we need to enumerate through all of the
+    * manifests, and find the one that applies to the Marathon application jar.
+    */
+  private lazy val marathonManifestPath: List[java.net.URL] =
+    getClass().getClassLoader().getResources("META-INF/MANIFEST.MF").toIterator.filter { manifest =>
+      marathonJar.findFirstMatchIn(manifest.getPath).nonEmpty
+    }.toList
+
+  lazy val manifest: Option[Manifest] = marathonManifestPath match {
+    case Nil => None
+    case List(file) =>
+      val mf = new Manifest()
+      mf.read(file.openStream)
+      Some(mf)
+    case otherwise =>
+      throw new RuntimeException(s"Multiple marathon JAR manifests returned! ${otherwise}")
+  }
 
   lazy val attributes: Option[Attributes] = manifest.map(_.getMainAttributes())
 
