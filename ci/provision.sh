439a8a79df72a34fd19a285ae9d6023dbfca9343
Karsten Jeschkies <kjeschkies@mesosphere.com>
2017-09-15 13:44:02 +0200

    Apply fixes suggested by shellcheck.

--- ci/provision.sh
+++ ci/provision.sh
@@ -9,15 +9,15 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2
 # Add Mesos repo.
 sudo apt-get install -y lsb-release
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
-echo "deb http://repos.mesosphere.com/`lsb_release -is | tr '[:upper:]' '[:lower:]'` `lsb_release -cs` main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
+echo "deb http://repos.mesosphere.com/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
 sudo apt-get -y update
 
 # Install dependencies.
 sudo apt-get install -y --force-yes --no-install-recommends curl sbt
 
-if grep -q MesosDebian \$WORKSPACE/project/Dependencies.scala; then
-    MESOS_VERSION=\$(sed -n 's/^.*MesosDebian = "\\(.*\\)"/\\1/p' <\$WORKSPACE/project/Dependencies.scala)
+if grep -q MesosDebian "$WORKSPACE/project/Dependencies.scala"; then
+    MESOS_VERSION=$(sed -n 's/^.*MesosDebian = "\\(.*\\)"/\\1/p' <\$WORKSPACE/project/Dependencies.scala)
 else
-    MESOS_VERSION=\$(sed -n 's/^.*mesos=\\(.*\\)&&.*/\\1/p' <\$WORKSPACE/Dockerfile)
+    MESOS_VERSION=$(sed -n 's/^.*mesos=\\(.*\\)&&.*/\\1/p' <\$WORKSPACE/Dockerfile)
 fi
 sudo apt-get install -y --force-yes --no-install-recommends mesos=\$MESOS_VERSION
