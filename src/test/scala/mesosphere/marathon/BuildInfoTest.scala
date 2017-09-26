ce4002eb19e77a928cfe2ba643f18bb8d9e5129d
Ken Sipe <kensipe@gmail.com>
2017-09-26 17:23:56 -0500

    this test should not change for the version

--- src/test/scala/mesosphere/marathon/BuildInfoTest.scala
+++ src/test/scala/mesosphere/marathon/BuildInfoTest.scala
@@ -7,7 +7,7 @@ class BuildInfoTest extends UnitTest {
   "BuildInfo" should {
     "return a default versions" in {
       BuildInfo.scalaVersion should be("2.x.x")
-      BuildInfo.version should be("1.5.1")
+      BuildInfo.version should be("1.5.0-SNAPSHOT")
     }
   }
 }
