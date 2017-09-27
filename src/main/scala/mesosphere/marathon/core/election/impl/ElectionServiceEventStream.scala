5a48c7f67aecb3ef53448779ccf6797d5209e999
Tim Harper <tharper@mesosphere.com>
2017-09-27 17:18:19 -0600

    Akka EventsController Stream Logic and Tests
    * Replace the custom stage implementation with stream composed via the DSL.
    * Add unit tests
    
    Jira Issues: MARATHON-7317

--- src/main/scala/mesosphere/marathon/core/election/impl/ElectionServiceEventStream.scala
+++ src/main/scala/mesosphere/marathon/core/election/impl/ElectionServiceEventStream.scala
@@ -1,9 +1,13 @@
 package mesosphere.marathon
 package core.election.impl
 
-import akka.actor.ActorRef
+import akka.actor.{ ActorRef, Cancellable, PoisonPill }
 import akka.event.EventStream
+import akka.stream.OverflowStrategy
+import akka.stream.scaladsl.{ Keep, Source }
+import mesosphere.marathon.core.async.ExecutionContexts
 import mesosphere.marathon.core.election.LocalLeadershipEvent
+import mesosphere.marathon.util.CancellableOnce
 
 private[impl] trait ElectionServiceEventStream {
   protected val eventStream: EventStream
@@ -19,4 +23,19 @@ private[impl] trait ElectionServiceEventStream {
   def unsubscribe(subscriber: ActorRef): Unit = {
     eventStream.unsubscribe(subscriber, classOf[LocalLeadershipEvent])
   }
+
+  val localLeadershipEvents: Source[LocalLeadershipEvent, Cancellable] = {
+    Source.actorRef[LocalLeadershipEvent](16, OverflowStrategy.dropHead) // drop older elements
+      .watchTermination()(Keep.both)
+      .mapMaterializedValue {
+        case (ref, terminated) =>
+          subscribe(ref)
+          // If the stream terminates, for any reason, then unsubscribe from the event stream
+          terminated.onComplete(_ => unsubscribe(ref))(ExecutionContexts.callerThread)
+          // If the stream cancellable gets called, kill the actor created by Source.actorRef; this will gracefully
+          // terminate the stream
+          new CancellableOnce(() => ref ! PoisonPill)
+      }
+  }
+
 }
