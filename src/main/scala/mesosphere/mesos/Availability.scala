49fd7d87eeddff37b09c56916935813e79ef2457
Johannes Unterstein <junterstein@mesosphere.io>
2017-09-27 11:45:24 +0200

    addressed feedback

--- src/main/scala/mesosphere/mesos/Availability.scala
+++ src/main/scala/mesosphere/mesos/Availability.scala
@@ -4,7 +4,7 @@ import java.time.Clock
 
 import mesosphere.marathon.RichClock
 import mesosphere.marathon.state.Timestamp
-import org.apache.mesos.Protos.{ DurationInfo, Offer }
+import org.apache.mesos.Protos.{DurationInfo, Offer}
 
 import scala.concurrent.duration._
 import scala.language.implicitConversions
@@ -13,20 +13,34 @@ object Availability {
 
   def offerAvailable(offer: Offer, drainingTime: FiniteDuration)(implicit clock: Clock): Boolean = {
     val now = clock.now()
-    if (offer.hasUnavailability && offer.getUnavailability.hasStart) {
+    if (offerHasUnavailability(offer)) {
       val start: Timestamp = offer.getUnavailability.getStart
-      if (now.after(start - drainingTime)) {
-        offer.getUnavailability.hasDuration &&
-          now.after(start + offer.getUnavailability.getDuration.toDuration)
+
+      if (currentlyInDrainingState(now, start, drainingTime)) {
+        isAgentCurrentlyUnavailable(offer, start, now)
       } else true
     } else true
   }
 
+  private def currentlyInDrainingState(now: Timestamp, start: Timestamp, drainingTime: FiniteDuration) = {
+    now.after(start - drainingTime)
+  }
+
+  private def offerHasUnavailability(offer: Offer) = {
+    offer.hasUnavailability && offer.getUnavailability.hasStart
+  }
+
+  private def isAgentCurrentlyUnavailable(offer: Offer, start: Timestamp, now: Timestamp) = {
+    offer.getUnavailability.hasDuration && now.after(start + offer.getUnavailability.getDuration.toDuration)
+  }
+
   /**
     * Convert Mesos DurationInfo to FiniteDuration.
+    *
     * @return FiniteDuration for DurationInfo
     */
   implicit class DurationInfoHelper(val di: DurationInfo) extends AnyVal {
     def toDuration: FiniteDuration = FiniteDuration(di.getNanoseconds, NANOSECONDS)
   }
+
 }
