aa19d3d2a7e478114fc843e950e5d02b6d341c16
Karsten Jeschkies <kjeschkies@mesosphere.com>
2017-09-29 12:34:50 +0200

    Model info response in RAML.

--- src/main/scala/mesosphere/marathon/api/akkahttp/v2/InfoController.scala
+++ src/main/scala/mesosphere/marathon/api/akkahttp/v2/InfoController.scala
@@ -6,6 +6,7 @@ import com.typesafe.scalalogging.StrictLogging
 import mesosphere.marathon.api.akkahttp.Controller
 import mesosphere.marathon.core.election.ElectionService
 import mesosphere.marathon.plugin.auth.{ Authenticator, AuthorizedResource, Authorizer, ViewResource }
+import mesosphere.marathon.raml.{ HttpConfig, MarathonConfig, MarathonInfo, ZooKeeperConfig }
 import mesosphere.marathon.storage.repository.FrameworkIdRepository
 import mesosphere.util.state.MesosLeaderInfo
 import play.api.libs.json._
@@ -26,42 +27,38 @@ case class InfoController(
   import mesosphere.marathon.api.akkahttp.Directives._
   import mesosphere.marathon.api.akkahttp.EntityMarshallers._
 
-  // Marathon configurations
-  val marathonConfigValues = Json.obj(
-    "master" -> config.mesosMaster.get,
-    "failover_timeout" -> config.mesosFailoverTimeout.get,
-    "framework_name" -> config.frameworkName.get,
-    "ha" -> config.highlyAvailable.get,
-    "checkpoint" -> config.checkpoint.get,
-    "local_port_min" -> config.localPortMin.get,
-    "local_port_max" -> config.localPortMax.get,
-    "executor" -> config.defaultExecutor.get,
-    "hostname" -> config.hostname.get,
-    "webui_url" -> config.webuiUrl.get,
-    "mesos_role" -> config.mesosRole.get,
-    "task_launch_timeout" -> config.taskLaunchTimeout.get,
-    "task_reservation_timeout" -> config.taskReservationTimeout.get,
-    "reconciliation_initial_delay" -> config.reconciliationInitialDelay.get,
-    "reconciliation_interval" -> config.reconciliationInterval.get,
-    "mesos_user" -> config.mesosUser.get,
-    "leader_proxy_connection_timeout_ms" -> config.leaderProxyConnectionTimeout.get,
-    "leader_proxy_read_timeout_ms" -> config.leaderProxyReadTimeout.get,
-    "features" -> config.availableFeatures
+  val marathonConfig = MarathonConfig(
+    config.mesosMaster.get,
+    config.mesosFailoverTimeout.get,
+    config.frameworkName.get,
+    config.highlyAvailable.get,
+    config.checkpoint.get,
+    config.localPortMax.get,
+    config.localPortMax.get,
+    config.defaultExecutor.get,
+    config.hostname.get,
+    config.webuiUrl.get,
+    config.mesosRole.get,
+    config.taskLaunchTimeout.get,
+    config.taskReservationTimeout.get,
+    config.reconciliationInitialDelay.get,
+    config.reconciliationInterval.get,
+    config.mesosUser.get,
+    config.leaderProxyConnectionTimeout.get,
+    config.leaderProxyReadTimeout.get,
+    mesosLeaderInfo.currentLeaderUrl,
+    config.availableFeatures.toIndexedSeq
   )
 
-  // ZooKeeper configurations
-  val zookeeperConfigValues = Json.obj(
-    "zk" -> s"zk://${config.zkHosts}${config.zkPath}",
-    "zk_timeout" -> config.zooKeeperTimeout(),
-    "zk_connection_timeout" -> config.zooKeeperConnectionTimeout(),
-    "zk_session_timeout" -> config.zooKeeperSessionTimeout(),
-    "zk_max_versions" -> config.maxVersions()
+  val zookeeperConfig = ZooKeeperConfig(
+    s"zk://${config.zkHosts}${config.zkPath}",
+    config.zooKeeperTimeout(),
+    config.zooKeeperConnectionTimeout(),
+    config.zooKeeperSessionTimeout(),
+    config.maxVersions()
   )
 
-  //  lazy val httpConfigValues = Json.obj(
-  //    "http_port" -> config.httpPort.get,
-  //    "https_port" -> config.httpsPort.get
-  //  )
+  val httpConfig = HttpConfig(8080, 8081)
 
   def info(): Route =
     authenticated.apply { implicit identity =>
@@ -70,20 +67,17 @@ case class InfoController(
           async {
             val frameworkId = await(frameworkIdRepository.get()).map(_.id)
 
-            // Current lead might change so we have to fetch it each time.
-            val mesosLeaderUiUrl = Json.obj("mesos_leader_ui_url" -> mesosLeaderInfo.currentLeaderUrl)
-
-            Json.obj(
-              "name" -> BuildInfo.name,
-              "name" -> BuildInfo.name,
-              "version" -> BuildInfo.version,
-              "buildref" -> BuildInfo.buildref,
-              "elected" -> electionService.isLeader,
-              "leader" -> electionService.leaderHostPort,
-              "frameworkId" -> frameworkId,
-              "marathon_config" -> (marathonConfigValues ++ mesosLeaderUiUrl),
-              "zookeeper_config" -> zookeeperConfigValues
-            //          "http_config" -> httpConfigValues)).build()
+            MarathonInfo(
+              BuildInfo.name,
+              BuildInfo.version,
+              BuildInfo.buildref,
+              electionService.isLeader,
+              electionService.leaderHostPort,
+              frameworkId,
+              // Current lead might change so we have to fetch it each time.
+              marathonConfig.copy(mesos_leader_ui_url = mesosLeaderInfo.currentLeaderUrl),
+              zookeeperConfig,
+              httpConfig
             )
           }
         }
