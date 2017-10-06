a42151b897143d39ce0b6ce12c61d643da92bd00
Alena Varkockova <varkockova.a@gmail.com>
2017-10-06 14:19:42 +0200

    Add documentation to the HomeRegionProvider

--- src/main/scala/mesosphere/marathon/HomeRegionProvider.scala
+++ src/main/scala/mesosphere/marathon/HomeRegionProvider.scala
@@ -1,5 +1,9 @@
 package mesosphere.marathon
 
+/**
+  * Returns value of home region for Marathon. This is strongly connected with Marathon being region aware.
+  * The value is used for region-aware scheduling.
+  */
 trait HomeRegionProvider {
   def getHomeRegion(): Option[String]
 }
