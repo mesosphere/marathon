1beb46f430f11c04e1922eaf68a5606d9911807d
Nikita Melkozerov <nmelkozerov@mesosphere.com>
2017-10-06 13:36:02 +0200

    updated implementation and tests for already existing methods

--- src/main/scala/mesosphere/marathon/api/akkahttp/v2/AppsController.scala
+++ src/main/scala/mesosphere/marathon/api/akkahttp/v2/AppsController.scala
@@ -17,7 +17,7 @@ import mesosphere.marathon.core.appinfo._
 import mesosphere.marathon.core.deployment.DeploymentPlan
 import mesosphere.marathon.core.group.GroupManager
 import mesosphere.marathon.core.plugin.PluginManager
-import mesosphere.marathon.plugin.auth.{ Authenticator => MarathonAuthenticator, Authorizer, CreateRunSpec, Identity, ViewResource }
+import mesosphere.marathon.plugin.auth.{ Authorizer, CreateRunSpec, Identity, ViewResource, Authenticator => MarathonAuthenticator }
 import mesosphere.marathon.state.{ AppDefinition, Identifiable, PathId }
 import play.api.libs.json.Json
 import PathId._
@@ -25,6 +25,7 @@ import mesosphere.marathon.state._
 import mesosphere.marathon.core.election.ElectionService
 import mesosphere.marathon.core.task.Task.{ Id => TaskId }
 import PathMatchers._
+import mesosphere.marathon.core.event.ApiPostEvent
 
 import scala.concurrent.{ ExecutionContext, Future }
 
@@ -55,15 +56,82 @@ class AppsController(
 
   private val forceParameter = parameter('force.as[Boolean].?(false))
 
-  private def listApps(implicit identity: Identity): Route = ???
+  private def listApps(implicit identity: Identity): Route = {
+    parameters('cmd.?, 'id.?, 'label.?, 'embed.*) { (cmd, id, label, embed) =>
+      def index: Future[Seq[AppInfo]] = {
+        val resolvedEmbed = InfoEmbedResolver.resolveApp(embed.toSet) + AppInfo.Embed.Counts + AppInfo.Embed.Deployments
+        val selector = selectAuthorized(search(cmd, id, label))
+        appInfoService.selectAppsBy(selector, resolvedEmbed)
+      }
+      onSuccess(index)(apps => complete(Json.obj("apps" -> apps)))
+    }
+  }
+
+  private[v2] def search(cmd: Option[String], id: Option[String], label: Option[String]): AppSelector = {
+    def containCaseInsensitive(a: String, b: String): Boolean = b.toLowerCase contains a.toLowerCase
+    val selectors = Seq[Option[Selector[AppDefinition]]](
+      cmd.map(c => Selector(_.cmd.exists(containCaseInsensitive(c, _)))),
+      id.map(s => Selector(app => containCaseInsensitive(s, app.id.toString))),
+      label.map(new LabelSelectorParsers().parsed)
+    ).flatten
+    Selector.forall(selectors)
+  }
+
+  private[v2] def selectAuthorized(fn: => AppSelector)(implicit identity: Identity): AppSelector = {
+    Selector.forall(Seq(authzSelector, fn))
+  }
 
   private def replaceMultipleApps(implicit identity: Identity): Route = ???
 
   private def patchMultipleApps(implicit identity: Identity): Route = ???
 
-  private def createApp(implicit identity: Identity): Route = ???
+  private def createApp(implicit identity: Identity): Route = {
+    (entity(as[AppDefinition]) & forceParameter & extractClientIP & extractUri) { (rawApp, force, remoteAddr, reqUri) =>
 
-  private def showApp(appId: PathId)(implicit identity: Identity): Route = ???
+      authorized(CreateRunSpec, rawApp).apply {
+        def create: Future[(DeploymentPlan, AppInfo)] = {
+
+          val app = rawApp.copy(versionInfo = VersionInfo.OnlyVersion(clock.now()))
+
+          def createOrThrow(opt: Option[AppDefinition]) = opt
+            .map(_ => throw ConflictingChangeException(s"An app with id [${app.id}] already exists."))
+            .getOrElse(app)
+
+          groupManager.updateApp(app.id, createOrThrow, app.version, force).map { plan =>
+            val appWithDeployments = AppInfo(
+              app,
+              maybeCounts = Some(TaskCounts.zero),
+              maybeTasks = Some(Seq.empty),
+              maybeDeployments = Some(Seq(Identifiable(plan.id)))
+            )
+            plan -> appWithDeployments
+          }
+        }
+        onSuccess(create) { (plan, createdApp) =>
+          eventBus.publish(ApiPostEvent(remoteAddr.toString, reqUri.toString, createdApp.app))
+          complete((StatusCodes.Created, Seq(Headers.`Marathon-Deployment-Id`(plan.id)), createdApp))
+        }
+      }
+    }
+  }
+
+  private def showApp(appId: PathId)(implicit identity: Identity): Route = {
+    parameters('embed.*) { embed =>
+      val resolvedEmbed = InfoEmbedResolver.resolveApp(embed.toSet) ++ Set(
+        // deprecated. For compatibility.
+        AppInfo.Embed.Counts, AppInfo.Embed.Tasks, AppInfo.Embed.LastTaskFailure, AppInfo.Embed.Deployments
+      )
+
+      onSuccess(appInfoService.selectApp(appId, authzSelector, resolvedEmbed)) {
+        case None =>
+          reject(Rejections.EntityNotFound.app(appId))
+        case Some(info) =>
+          authorized(ViewResource, info.app).apply {
+            complete(Json.obj("app" -> info))
+          }
+      }
+    }
+  }
 
   private def patchSingle(appId: PathId)(implicit identity: Identity): Route = ???
 
