9e6ab0e3358bb683ba3d38bbc82b302b106dedd5
Aleksey Dukhovniy <adukhovniy@mesosphere.io>
2017-10-09 16:57:22 +0200

    Replaced `onFailure` usage with `failed.foreach`

--- src/main/scala/mesosphere/marathon/api/akkahttp/AkkaHttpModule.scala
+++ src/main/scala/mesosphere/marathon/api/akkahttp/AkkaHttpModule.scala
@@ -10,13 +10,15 @@ import com.google.inject.AbstractModule
 import com.google.inject.{ Provides, Scopes, Singleton }
 import com.typesafe.config.Config
 import mesosphere.chaos.http.HttpConf
-import mesosphere.marathon.api.MarathonHttpService
+import mesosphere.marathon.api.{ MarathonHttpService, TaskKiller }
 import mesosphere.marathon.core.appinfo._
 import mesosphere.marathon.core.election.ElectionService
 import mesosphere.marathon.core.group.GroupManager
 import mesosphere.marathon.core.plugin.PluginManager
 import mesosphere.marathon.plugin.auth._
 import mesosphere.marathon.api.akkahttp.v2.{ AppsController, EventsController, InfoController, PluginsController }
+import mesosphere.marathon.core.health.HealthCheckManager
+import mesosphere.marathon.core.task.tracker.InstanceTracker
 import mesosphere.marathon.plugin.http.HttpRequestHandler
 import mesosphere.marathon.storage.StorageModule
 import mesosphere.util.state.MesosLeaderInfo
