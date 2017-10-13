1cae379c2e954089be4c48eb6822e6db18818e3f
Karsten Jeschkies <jeskar@web.de>
2017-10-13 12:09:29 +0200

    Fix Mesos provisioning for 1.4 nodes. (#5509) (#5611)
    Summary:
    The Mesos installation failed on our 1.4 Jenkins nodes. Apparently we
    had to install lsb-release.
    
    Test Plan: PR build
    
    Reviewers: ichernetsky, meln1k
    
    Subscribers: jenkins, marathon-team

---
+++ ci/provision.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+sudo apt-get -y clean
+sudo apt-get -y update
+
+# Add sbt repo.
+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+
+# Add Mesos repo.
+sudo apt-get install -y lsb-release
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+echo "deb http://repos.mesosphere.com/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
+sudo apt-get -y update
+
+# Install dependencies.
+sudo apt-get install -y --force-yes --no-install-recommends curl sbt
+
+MESOS_VERSION=$(sed -n 's/^.*mesos=\([^[:space:]]*\) &&.*/\1/p' < "$WORKSPACE/Dockerfile")
+sudo apt-get install -y --force-yes --no-install-recommends mesos="$MESOS_VERSION"
