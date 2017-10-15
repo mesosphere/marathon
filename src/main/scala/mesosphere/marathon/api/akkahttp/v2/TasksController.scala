f7ac8b16284dd1be0ef82e37311fa1674507dde5
Alena Varkockova <varkockova.a@gmail.com>
2017-10-15 18:50:38 +0200

    Implement list tasks API method using Akka.HTTP

---
+++ src/main/scala/mesosphere/marathon/api/akkahttp/v2/TasksController.scala
@@ -0,0 +1,104 @@
+package mesosphere.marathon
+package api.akkahttp.v2
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.server.Route
+import com.typesafe.scalalogging.StrictLogging
+import mesosphere.marathon.api.akkahttp.Controller
+import mesosphere.marathon.api.akkahttp.Directives.pathEndOrSingleSlash
+import mesosphere.marathon.core.appinfo.EnrichedTask
+import mesosphere.marathon.core.condition.Condition
+import mesosphere.marathon.core.group.GroupManager
+import mesosphere.marathon.core.health.{ Health, HealthCheckManager }
+import mesosphere.marathon.core.instance.Instance
+import mesosphere.marathon.core.instance.Instance.Id
+import mesosphere.marathon.core.task.tracker.InstanceTracker
+import mesosphere.marathon.plugin.auth.{ Authenticator, _ }
+import play.api.libs.json.Json
+import mesosphere.marathon.raml.{ AnyToRaml, EnrichedTasksList, Writes }
+import mesosphere.marathon.state.PathId
+
+import scala.async.Async._
+import scala.concurrent.{ ExecutionContext, Future }
+
+class TasksController(instanceTracker: InstanceTracker, groupManager: GroupManager, healthCheckManager: HealthCheckManager)(
+    implicit
+    val actorSystem: ActorSystem,
+    val executionContext: ExecutionContext,
+    val authenticator: Authenticator,
+    val authorizer: Authorizer
+) extends Controller with StrictLogging {
+
+  import mesosphere.marathon.api.akkahttp.Directives._
+  import mesosphere.marathon.api.akkahttp.EntityMarshallers._
+  import mesosphere.marathon.raml.EnrichedTaskConversion._
+
+  override val route = {
+    get {
+      pathEndOrSingleSlash {
+        listTasks()
+      }
+    }
+  }
+
+  private def listTasks(): Route = {
+    authenticated.apply { implicit identity =>
+      authorized(ViewResource, AuthorizedResource.SystemConfig).apply {
+        parameters("status".?, "status[]".as[String].*) { (statusParameter, statusesParameter) =>
+          val statuses = statusParameter.map(s => Seq(s) ++ statusesParameter).getOrElse(Seq[String](statusesParameter.toSeq: _*))
+          onSuccess(enrichedTasks(statuses)) { tasks =>
+            complete(TasksList(tasks).toRaml)
+          }
+        }
+      }
+    }
+  }
+
+  private def enrichedTasks(statuses: Seq[String]): Future[Seq[EnrichedTask]] = async {
+    val conditionSet: Set[Condition] = statuses.flatMap(toTaskState)(collection.breakOut)
+
+    val instancesBySpec = await(instanceTracker.instancesBySpec)
+
+    val instances: Iterable[(PathId, Instance)] = instancesBySpec.instancesMap.values.flatMap { appTasks =>
+      appTasks.instances.map(i => appTasks.specId -> i)
+    }
+    val appIds: Set[PathId] = instancesBySpec.allSpecIdsWithInstances
+
+    val appToPorts: Map[PathId, Seq[Int]] = groupManager.apps(appIds).map {
+      case (appId, app) => appId -> app.map(_.servicePorts).getOrElse(Nil)
+    }
+
+    val instancesHealth = await(
+      Future.sequence(appIds.map { appId =>
+        healthCheckManager.statuses(appId)
+      })).foldLeft(Map[Id, Seq[Health]]())(_ ++ _)
+
+    val enrichedTasks: Iterable[Iterable[EnrichedTask]] = for {
+      (appId, instance) <- instances
+      if conditionSet.isEmpty || conditionSet(instance.state.condition)
+      tasks = instance.tasksMap.values
+    } yield {
+      tasks.map { task =>
+        EnrichedTask(
+          appId,
+          task,
+          instance.agentInfo,
+          instancesHealth.getOrElse(instance.instanceId, Nil),
+          appToPorts.getOrElse(appId, Nil)
+        )
+      }
+    }
+    Seq(enrichedTasks.flatten.toSeq: _*)
+  }
+
+  private def toTaskState(state: String): Option[Condition] = state.toLowerCase match {
+    case "running" => Some(Condition.Running)
+    case "staging" => Some(Condition.Staging)
+    case _ => None
+  }
+
+  case class TasksList(tasks: Seq[EnrichedTask])
+  implicit val tasksListWrite: Writes[TasksList, EnrichedTasksList] = Writes { tasksList =>
+    EnrichedTasksList(tasksList.tasks.map(_.toRaml))
+  }
+}
---
+++ src/test/scala/mesosphere/marathon/api/akkahttp/v2/TasksControllerTest.scala
@@ -0,0 +1,176 @@
+package mesosphere.marathon
+package api.akkahttp.v2
+
+import java.time.Clock
+
+import akka.http.scaladsl.model.Uri.Query
+import akka.http.scaladsl.model.{ StatusCodes, Uri }
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import com.typesafe.scalalogging.StrictLogging
+import mesosphere.UnitTest
+import mesosphere.marathon.api.{ JsonTestHelper, TestAuthFixture }
+import mesosphere.marathon.core.group.GroupManager
+import mesosphere.marathon.core.health.HealthCheckManager
+import mesosphere.marathon.core.instance.{ Instance, TestInstanceBuilder }
+import mesosphere.marathon.core.task.Task
+import mesosphere.marathon.core.task.tracker.InstanceTracker
+import mesosphere.marathon.state.AppDefinition
+import mesosphere.marathon.state.PathId._
+import mesosphere.marathon.test.SettableClock
+import org.apache.mesos
+import org.scalatest.Inside
+
+import scala.concurrent.Future
+
+class TasksControllerTest extends UnitTest with ScalatestRouteTest with Inside with RouteBehaviours with StrictLogging {
+  "TasksController" should {
+
+    "lists tasks as json" in new Fixture {
+      Given("some tasks in progress")
+      val clock = new SettableClock()
+      val app = AppDefinition(id = "/app".toPath, instances = 1)
+      val instance1 = TestInstanceBuilder.newBuilderWithLaunchedTask(app.id, clock.now()).getInstance()
+      val instance2 = TestInstanceBuilder.newBuilderWithLaunchedTask(app.id, clock.now()).getInstance()
+
+      instanceTracker.instancesBySpec() returns Future.successful(InstanceTracker.InstancesBySpec.of(InstanceTracker.SpecInstances.forInstances("app".toPath, Seq(instance1, instance2))))
+      groupManager.apps(any) returns Map(app.id -> Some(app))
+      healthCheckManager.statuses(any) returns Future.successful(Map.empty)
+
+      When("Getting the tasks list")
+      Get(Uri./) ~> controller.route ~> check {
+        Then("All tasks should be serialized into response")
+        status should be(StatusCodes.OK)
+
+        val expected = s"""{
+                         |  "tasks" : [ {
+                         |    "appId" : "app",
+                         |    "healthCheckResults" : [ ],
+                         |    "host" : "${instance1.hostname}",
+                         |    "ipAddresses" : [ ],
+                         |    "id" : "${instance1.appTask.taskId.idString}",
+                         |    "ports" : [ ],
+                         |    "servicePorts" : [ ],
+                         |    "slaveId" : "agent-1",
+                         |    "state" : "TASK_STAGING",
+                         |    "stagedAt" : "2015-04-09T12:30:00.000Z",
+                         |    "version" : "2015-04-09T12:30:00.000Z",
+                         |    "localVolumes" : [ ]
+                         |  }, {
+                         |    "appId" : "app",
+                         |    "healthCheckResults" : [ ],
+                         |    "host" : "${instance2.hostname}",
+                         |    "id" : "${instance2.appTask.taskId.idString}",
+                         |    "ipAddresses" : [ ],
+                         |    "ports" : [ ],
+                         |    "servicePorts" : [ ],
+                         |    "slaveId" : "agent-1",
+                         |    "state" : "TASK_STAGING",
+                         |    "stagedAt" : "2015-04-09T12:30:00.000Z",
+                         |    "version" : "2015-04-09T12:30:00.000Z",
+                         |    "localVolumes" : [ ]
+                         |  } ]
+                         |}""".stripMargin
+        JsonTestHelper.assertThatJsonString(responseAs[String]).correspondsToJsonString(expected)
+      }
+    }
+
+    "list tasks as json with status filter" in new Fixture {
+      Given("some tasks in progress")
+      val clock = new SettableClock()
+      val app = AppDefinition(id = "/app".toPath, instances = 1)
+      val (runningInstance, runningTask) = runningInstanceAndItsTask(app, clock)
+      val (stagingInstance, stagingTask) = stagingInstanceAndItsTask(app, clock)
+
+      instanceTracker.instancesBySpec() returns Future.successful(InstanceTracker.InstancesBySpec.of(InstanceTracker.SpecInstances.forInstances("app".toPath, Seq(runningInstance, stagingInstance))))
+      groupManager.apps(any) returns Map(app.id -> Some(app))
+      healthCheckManager.statuses(any) returns Future.successful(Map.empty)
+
+      When("Getting the tasks list only with status staging")
+      Get(Uri./.withQuery(Query("status" -> "staging"))) ~> controller.route ~> check {
+        Then("Only one task should be serialized into response")
+        status should be(StatusCodes.OK)
+
+        responseAs[String] should include (stagingTask.taskId.idString)
+        responseAs[String] should not include (runningTask.taskId.idString)
+      }
+    }
+
+    "list tasks as json with multiple statuses" in new Fixture {
+      Given("some tasks in progress")
+      val clock = new SettableClock()
+      val app = AppDefinition(id = "/app".toPath, instances = 1)
+      val (runningInstance, runningTask) = runningInstanceAndItsTask(app, clock)
+      val (stagingInstance, stagingTask) = stagingInstanceAndItsTask(app, clock)
+
+      instanceTracker.instancesBySpec() returns Future.successful(InstanceTracker.InstancesBySpec.of(InstanceTracker.SpecInstances.forInstances("app".toPath, Seq(stagingInstance, runningInstance))))
+      groupManager.apps(any) returns Map(app.id -> Some(app))
+      healthCheckManager.statuses(any) returns Future.successful(Map.empty)
+
+      When("Getting the tasks list for running as well as staging")
+      Get(Uri./.withQuery(Query("status[]" -> "staging", "status[]" -> "running"))) ~> controller.route ~> check {
+        Then("All tasks should be serialized into response")
+        status should be(StatusCodes.OK)
+
+        responseAs[String] should include (stagingTask.taskId.idString)
+        responseAs[String] should include (runningTask.taskId.idString)
+      }
+    }
+
+    "list apps when there are no apps" in new Fixture {
+      // Regression test for #4932
+      Given("no apps")
+      instanceTracker.instancesBySpec returns Future.successful(InstanceTracker.InstancesBySpec.empty)
+      groupManager.apps(any) returns Map.empty
+
+      When("Getting the tasks list")
+      Get(Uri./) ~> controller.route ~> check {
+        Then("The status should be 200")
+        status should be(StatusCodes.OK)
+      }
+    }
+
+    {
+      val controller = Fixture(authenticated = false).controller
+      behave like unauthenticatedRoute(forRoute = controller.route, withRequest = Get(Uri./))
+    }
+
+    {
+      val controller = Fixture(authorized = false).controller
+      behave like unauthorizedRoute(forRoute = controller.route, withRequest = Get(Uri./))
+    }
+  }
+
+  case class Fixture(
+      authenticated: Boolean = true,
+      authorized: Boolean = true,
+      groupManager: GroupManager = mock[GroupManager],
+      instanceTracker: InstanceTracker = mock[InstanceTracker],
+      healthCheckManager: HealthCheckManager = mock[HealthCheckManager]) {
+
+    val authFixture = new TestAuthFixture()
+    authFixture.authenticated = authenticated
+    authFixture.authorized = authorized
+
+    implicit val authenticator = authFixture.auth
+
+    val controller = new TasksController(instanceTracker, groupManager, healthCheckManager)
+
+    def runningInstanceAndItsTask(app: AppDefinition, clock: Clock): (Instance, Task) = {
+      val runningInstance = TestInstanceBuilder
+        .newBuilder(app.id, clock.now())
+        .addTaskWithBuilder().taskForStatus(mesos.Protos.TaskState.TASK_RUNNING).build()
+        .getInstance()
+
+      (runningInstance, runningInstance.tasksMap.values.head)
+    }
+
+    def stagingInstanceAndItsTask(app: AppDefinition, clock: Clock): (Instance, Task) = {
+      val stagingInstance = TestInstanceBuilder
+        .newBuilder(app.id, clock.now())
+        .addTaskStaging()
+        .getInstance()
+
+      (stagingInstance, stagingInstance.tasksMap.values.head)
+    }
+  }
+}
