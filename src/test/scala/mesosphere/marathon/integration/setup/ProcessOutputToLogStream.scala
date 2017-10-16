bc246e3c0396a1ef68d8f9bd16eacce7b0753ecb
Tim Harper <tharper@mesosphere.com>
2017-10-15 18:59:28 -0600

    Rate limit ProcessOutputToLogStream
    Sometimes, our sub-processes (such as Mesos), can get in a really tight
    loop and produce an overwhelming amount of output that overwhelms
    Jenkins and inhibits the test tear-down code from running.
    
    Looking at a few of such cases, it seems obvious that we can effectively
    limit it via our ProcessOutputToLogStream logger class.
    
    JIRA Issues: MARATHON-7809

--- src/test/scala/mesosphere/marathon/integration/setup/ProcessOutputToLogStream.scala
+++ src/test/scala/mesosphere/marathon/integration/setup/ProcessOutputToLogStream.scala
@@ -1,13 +1,40 @@
 package mesosphere.marathon
 package integration.setup
 
-import org.slf4j.LoggerFactory
+import org.slf4j.{ LoggerFactory, Logger }
 
 import scala.sys.process.ProcessLogger
 
-case class ProcessOutputToLogStream(process: String) extends ProcessLogger {
-  val log = LoggerFactory.getLogger(s"mesosphere.marathon.integration.process.$process")
-  override def out(msg: => String): Unit = log.debug(msg)
-  override def err(msg: => String): Unit = log.warn(msg)
+final case class ProcessOutputToLogStream(process: String, maxLinesPerPeriod: Int = 100, periodMillis: Long = 1000L) extends ProcessLogger {
+  private[this] var nextFlush = System.currentTimeMillis() + periodMillis
+  private[this] var callsSinceLastFlush = 0
+  val log: Logger = LoggerFactory.getLogger(s"mesosphere.marathon.integration.process.$process")
+
+  private def rateLimited(msg: String): Boolean = synchronized {
+    val now = System.currentTimeMillis
+    if (now > nextFlush) {
+      val suppressedLines = callsSinceLastFlush - maxLinesPerPeriod
+      if (suppressedLines > 0)
+        log.error(s"ProcessLogger ${process} was rate limited; ${suppressedLines} lines were supressed")
+
+      callsSinceLastFlush = 1
+      nextFlush = now + periodMillis
+      false
+    } else {
+      callsSinceLastFlush += 1
+      callsSinceLastFlush > maxLinesPerPeriod
+    }
+  }
+
+  override def out(getMsg: => String): Unit = if (log.isDebugEnabled()) {
+    val msg = getMsg
+    if (!rateLimited(msg))
+      log.debug(msg)
+  }
+  override def err(getMsg: => String): Unit = if (log.isWarnEnabled()) {
+    val msg = getMsg
+    if (!rateLimited(msg))
+      log.warn(msg)
+  }
   override def buffer[T](f: => T): T = f
 }
