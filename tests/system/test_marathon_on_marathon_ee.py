4e127a30ba1f3ee3db30c7da5d400491f328a5f5
Aleksey Dukhovniy <adukhovniy@mesosphere.io>
2017-10-20 11:52:47 +0200

    Sort MoM-EE images ORDER BY DESC

--- tests/system/test_marathon_on_marathon_ee.py
+++ tests/system/test_marathon_on_marathon_ee.py
@@ -24,9 +24,9 @@ PRIVATE_KEY_FILE = 'private-key.pem'
 PUBLIC_KEY_FILE = 'public-key.pem'
 
 DEFAULT_MOM_IMAGES = {
+    'MOM_EE_1.5': 'v1.5.1_1.10.0',
     'MOM_EE_1.4': 'v1.4.7_1.9.8',
-    'MOM_EE_1.3': '1.3.13_1.1.5',
-    'MOM_EE_1.5': 'v1.5.1_1.10.0'
+    'MOM_EE_1.3': '1.3.13_1.1.5'
 }
 
 
