5e92b3e34d5b98bfa8d3d97595fb57a07a2b6bbd
Karsten Jeschkies <kjeschkies@mesosphere.com>
2017-10-23 18:17:47 +0200

    Draft pods controller with stubs.
    Summary:
    This creates the routes as stubs for the podscontroller.

---
+++ src/main/scala/mesosphere/marathon/api/akkahttp/v2/PodsController.scala
@@ -0,0 +1,85 @@
+package mesosphere.marathon
+package api.akkahttp.v2
+
+import akka.http.scaladsl.server.Route
+import mesosphere.marathon.api.akkahttp.Controller
+import mesosphere.marathon.api.akkahttp.PathMatchers.ExistingAppPathId
+import mesosphere.marathon.core.group.GroupManager
+import mesosphere.marathon.plugin.auth.Authenticator
+import mesosphere.marathon.state.PathId
+
+class PodsController(
+    val groupManager: GroupManager)(
+    implicit
+    val authenticator: Authenticator) extends Controller {
+
+  import mesosphere.marathon.api.akkahttp.Directives._
+
+  def capability(): Route =
+    authenticated.apply { implicit identity =>
+      complete("")
+    }
+
+  def create(): Route = ???
+
+  def update(podId: PathId): Route = ???
+
+  def findAll(): Route = ???
+
+  def find(podId: PathId): Route = ???
+
+  def remove(podId: PathId): Route = ???
+
+  def status(podId: PathId): Route = ???
+
+  def versions(podId: PathId): Route = ???
+
+  def version(version: String): Route = ???
+
+  def allStatus(): Route = ???
+
+  def killInstance(instanceId: String): Route = ???
+
+  def killInstances(podId: PathId): Route = ???
+
+  override val route: Route =
+    head {
+      capability()
+    } ~
+      get {
+        findAll()
+      } ~
+      post {
+        create()
+      } ~
+      pathPrefix(ExistingAppPathId(groupManager.rootGroup)) { podId =>
+        put {
+          update(podId)
+        } ~
+          get {
+            pathEnd { find(podId) } ~
+              pathPrefix("::status") {
+                status(podId)
+              } ~
+              pathPrefix("::versions") {
+                pathEnd { versions(podId) } ~
+                  extractUnmatchedPath { v =>
+                    version(v.toString)
+                  }
+              }
+          } ~
+          delete {
+            pathEnd { remove(podId) } ~
+              pathPrefix("::instances") {
+                pathEnd { killInstances(podId) } ~
+                  extractUnmatchedPath { instanceId =>
+                    killInstance(instanceId.toString)
+                  }
+              }
+          }
+      } ~
+      pathPrefix("::status") {
+        allStatus()
+      }
+
+}
