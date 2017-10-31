25c9378da8d3a63e2eb22f4af097a22a93254fa0
Alena Varkockova <varkockova.a@gmail.com>
2017-10-31 13:39:12 +0100

    Add fixture

--- src/test/scala/mesosphere/marathon/api/akkahttp/v2/GroupsControllerTest.scala
+++ src/test/scala/mesosphere/marathon/api/akkahttp/v2/GroupsControllerTest.scala
@@ -4,9 +4,12 @@ package api.akkahttp.v2
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.typesafe.scalalogging.StrictLogging
 import mesosphere.UnitTest
+import mesosphere.marathon.core.election.ElectionService
 import mesosphere.marathon.test.GroupCreation
 import org.scalatest.Inside
 
 class GroupsControllerTest extends UnitTest with ScalatestRouteTest with Inside with RouteBehaviours with StrictLogging with GroupCreation {
-
+  case class Fixture(electionService: ElectionService = mock[ElectionService]) {
+    val groupsController: GroupsController = new GroupsController(electionService)
+  }
 }
