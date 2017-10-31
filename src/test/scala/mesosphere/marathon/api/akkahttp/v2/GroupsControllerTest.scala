e11387f3813b50a9aee54c7fbb78ea5f703e7ddf
Alena Varkockova <varkockova.a@gmail.com>
2017-10-31 11:07:52 +0100

    Groups controller test skeleton

---
+++ src/test/scala/mesosphere/marathon/api/akkahttp/v2/GroupsControllerTest.scala
@@ -0,0 +1,11 @@
+package mesosphere.marathon.api.akkahttp.v2
+
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import com.typesafe.scalalogging.StrictLogging
+import mesosphere.UnitTest
+import mesosphere.marathon.test.GroupCreation
+import org.scalatest.Inside
+
+class GroupsControllerTest extends UnitTest with ScalatestRouteTest with Inside with RouteBehaviours with StrictLogging with GroupCreation {
+
+}
