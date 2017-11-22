19694b3b0488978bb892d5be20ca9968861b30fb
Karsten Jeschkies <kjeschkies@mesosphere.com>
2017-11-22 12:16:56 +0100

    Retrieve status for pods in pods controller.
    Summary:
    Retrieve the status for one pod. Note, this fixes the RAML for the API
    endpoint as well. It seems that the current implementation as resource
    is wrong.
    
    JIRA issues: MARATHON-7878

--- src/main/scala/mesosphere/marathon/api/akkahttp/v2/PodsController.scala
+++ src/main/scala/mesosphere/marathon/api/akkahttp/v2/PodsController.scala
@@ -25,7 +25,7 @@ import mesosphere.marathon.core.election.ElectionService
 import mesosphere.marathon.core.event.PodEvent
 import mesosphere.marathon.core.plugin.PluginManager
 import mesosphere.marathon.core.pod.{ PodDefinition, PodManager }
-import mesosphere.marathon.raml.{ PodConversion, Raml }
+import mesosphere.marathon.raml.{ PodConversion, PodStatusConversion, Raml }
 import mesosphere.marathon.util.SemanticVersion
 
 import async.Async._
@@ -165,6 +165,23 @@ class PodsController(
               // We don't filter the pods with an authorization since we check for authorization before.
               val status: raml.PodStatus = maybeStatus.getOrElse(throw new IllegalStateException(s"Status for pod '$podId' was none even though pod existed at start of request."))
               complete((StatusCodes.OK, status))
+            }
+          }
+      }
+    }
+
+  @SuppressWarnings(Array("all")) // async/await
+  def status(podId: PathId): Route =
+    authenticated.apply { implicit identity =>
+      podManager.find(podId) match {
+        case None =>
+          reject(Rejections.EntityNotFound.noPod(podId))
+        case Some(pod) =>
+          authorized(ViewRunSpec, pod).apply {
+            onSuccess(podStatusService.selectPodStatus(podId)) { maybeStatus =>
+              val status = maybeStatus.get // If selectPodStatus returns None this is a bug.
+              val ramlStatus = PodStatusConversion.podInstanceStatusRamlWriter.write(status)
+              complete("")
             }
           }
       }
