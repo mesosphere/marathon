80835b34b24e9ae37365c48e1a67ee3d5aa980d4
Ken Sipe <kensipe@gmail.com>
2017-11-29 14:07:42 -0600

    aligning web site tooling to work for metronome and marathon (#5784)

--- docs/entrypoint.sh
+++ docs/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /marathon-docs
+cd /site-docs
 
 # BUNDLE_IGNORE_CONFIG=1 in case the user has a .bundle/config file from invoking bundler locally; we want to use the docker container's bundler files
 BUNDLE_IGNORE_CONFIG=1 bundle exec jekyll serve --watch -H 0.0.0.0
