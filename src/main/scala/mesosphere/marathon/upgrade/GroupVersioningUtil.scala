89175159bc3ddbd448b067bd30b46342b765ea26
Nikita Melkozerov <nmelkozerov@mesosphere.com>
2017-11-29 14:00:11 +0100

    debug prints

--- src/main/scala/mesosphere/marathon/upgrade/GroupVersioningUtil.scala
+++ src/main/scala/mesosphere/marathon/upgrade/GroupVersioningUtil.scala
@@ -86,6 +86,7 @@ object GroupVersioningUtil {
             oldPod.versionInfo.withConfigChange(newVersion = version)
           } else if (oldPod.isOnlyScaleChange(newPod)) {
             log.info(s"${newPod.id}: scaling op detected for Pod (oldVersion ${oldPod.versionInfo})")
+            println(s"œ new version: $version")
             oldPod.versionInfo.withScaleOrRestartChange(newVersion = version)
           } else if (oldPod.versionInfo != newPod.versionInfo && newPod.versionInfo == VersionInfo.NoVersion) {
             log.info(s"${newPod.id}: restart detected for Pod (oldVersion ${oldPod.versionInfo})")
