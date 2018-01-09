12fb17458eb4535a2035edefdb13e33c9d9fdb68
Nikita Melkozerov <nmelkozerov@mesosphere.com>
2018-01-09 16:27:59 +0100

    Persistent volumes for pods — ZK data migration
    Summary: added a migration to move residency from the task level to the instance level
    
    JIRA issues: MARATHON_EE-1785

---
+++ src/main/scala/mesosphere/marathon/storage/migration/MigrationTo160.scala
@@ -0,0 +1,101 @@
+package mesosphere.marathon
+package storage.migration
+
+import java.time.OffsetDateTime
+
+import akka.{ Done, NotUsed }
+import akka.http.scaladsl.unmarshalling.Unmarshaller
+import akka.stream.Materializer
+import akka.stream.scaladsl.{ Flow, Keep, Sink, Source }
+import com.typesafe.scalalogging.StrictLogging
+import mesosphere.marathon.MigrationCancelledException
+import mesosphere.marathon.core.async.ExecutionContexts
+import mesosphere.marathon.core.instance.{ Instance, Reservation }
+import mesosphere.marathon.core.instance.Instance.Id
+import mesosphere.marathon.core.storage.store.{ IdResolver, PersistenceStore }
+import mesosphere.marathon.core.storage.store.impl.zk.{ ZkId, ZkPersistenceStore, ZkSerialized }
+import mesosphere.marathon.storage.migration.MigrationTo146.Environment
+import mesosphere.marathon.storage.repository.InstanceRepository
+import play.api.libs.json.{ JsValue, Json }
+
+import scala.concurrent.{ ExecutionContext, Future }
+
+@SuppressWarnings(Array("ClassNames"))
+class MigrationTo160(instanceRepository: InstanceRepository, persistenceStore: PersistenceStore[_, _, _])(implicit ctx: ExecutionContext, mat: Materializer) extends StrictLogging {
+
+  def migrate(): Future[Done] = {
+    MigrationTo160.migrateReservations(instanceRepository, persistenceStore)(Environment(sys.env), ctx, mat)
+  }
+}
+
+object MigrationTo160 extends StrictLogging {
+  def migrateReservations(instanceRepository: InstanceRepository, persistenceStore: PersistenceStore[_, _, _])(implicit env: Environment, ctx: ExecutionContext, mat: Materializer): Future[Done] = {
+
+    logger.info("Starting unreachable strategy migration to 1.6.0")
+
+    val migrateUnreachableStrategyWanted = env.vars.getOrElse(MigrationTo146.MigrateUnreachableStrategyEnvVar, "false")
+
+    if (migrateUnreachableStrategyWanted == "true") {
+
+      val store: ZkPersistenceStore = {
+        persistenceStore match {
+          case zk: ZkPersistenceStore =>
+            zk
+          case other =>
+            throw MigrationCancelledException(s"expected ZK persistent store, but found ${other.getClass.getName}", new RuntimeException)
+        }
+      }
+
+      implicit val instanceResolver: IdResolver[Instance.Id, JsValue, String, ZkId] =
+        new IdResolver[Instance.Id, JsValue, String, ZkId] {
+          override def toStorageId(id: Id, version: Option[OffsetDateTime]): ZkId =
+            ZkId(category, id.idString, version)
+
+          override val category: String = "instance"
+
+          override def fromStorageId(key: ZkId): Id = Instance.Id(key.id)
+
+          override val hasVersions: Boolean = false
+
+          override def version(v: JsValue): OffsetDateTime = OffsetDateTime.MIN
+        }
+
+      implicit val instanceJsonUnmarshaller: Unmarshaller[ZkSerialized, JsValue] =
+        Unmarshaller.strict {
+          case ZkSerialized(byteString) =>
+            Json.parse(byteString.utf8String)
+        }
+
+      import Reservation.reservationFormat
+      import Instance.instanceJsonReads
+
+      instanceRepository
+        .ids()
+        .mapAsync(1) { instanceId =>
+          store.get(instanceId)
+        }
+        .mapConcat {
+          case Some(jsValue) =>
+            val instance = jsValue.as[Instance]
+            val maybeReservationJson = (jsValue \ "tasksMap" \\ "reservation").headOption
+
+            maybeReservationJson.map { reservationJson =>
+              reservationJson.as[Reservation] -> instance :: Nil
+            } getOrElse {
+              Nil
+            }
+
+          case _ => Nil
+        }
+        .mapAsync(1) {
+          case (reservation, instance) =>
+            val updatedInstance = instance.copy(reservation = Some(reservation))
+            instanceRepository.store(updatedInstance)
+        }
+        .runWith(Sink.ignore)
+    } else {
+      logger.info("No unreachable strategy migration to 1.6.0 wanted")
+      Future.successful(Done)
+    }
+  }
+}
---
+++ src/test/scala/mesosphere/marathon/storage/migration/MigrationTo160Test.scala
@@ -0,0 +1,129 @@
+package mesosphere.marathon
+package storage.migration
+
+import java.time.OffsetDateTime
+
+import akka.Done
+import akka.http.scaladsl.unmarshalling.Unmarshaller
+import akka.stream.scaladsl.Source
+import akka.stream.{ ActorMaterializer, Materializer }
+import com.typesafe.scalalogging.StrictLogging
+import mesosphere.AkkaUnitTest
+import mesosphere.marathon.core.condition.Condition
+import mesosphere.marathon.core.instance.Instance.Id
+import mesosphere.marathon.core.instance.{ Instance, Reservation, TestInstanceBuilder }
+import mesosphere.marathon.core.storage.store.{ IdResolver, PersistenceStore }
+import mesosphere.marathon.core.storage.store.impl.zk.{ ZkId, ZkPersistenceStore, ZkSerialized }
+import mesosphere.marathon.core.task.Task
+import mesosphere.marathon.core.task.state.NetworkInfo
+import mesosphere.marathon.state._
+import mesosphere.marathon.storage.migration.MigrationTo146.Environment
+import mesosphere.marathon.storage.repository.InstanceRepository
+import mesosphere.marathon.test.GroupCreation
+import org.apache.mesos
+import org.apache.mesos.Protos.NetworkInfo.Protocol
+import play.api.libs.json.{ JsValue, Json }
+import play.api.libs.json._
+
+import scala.concurrent.duration._
+import scala.concurrent.{ ExecutionContextExecutor, Future }
+
+class MigrationTo160Test extends AkkaUnitTest with GroupCreation with StrictLogging {
+
+  "Migration to 1.6.0" should {
+    "do nothing if env var is not configured" in new Fixture {
+      MigrationTo160.migrateReservations(instanceRepository, persistenceStore)(env, ctx, mat).futureValue
+      verify(instanceRepository, never).all()
+      verify(instanceRepository, never).store(_: Instance)
+    }
+
+    "do migration if env var is configured" in new Fixture(Map(MigrationTo146.MigrateUnreachableStrategyEnvVar -> "true")) {
+      MigrationTo160.migrateReservations(instanceRepository, persistenceStore)(env, ctx, mat).futureValue
+      val targetInstance = instance.copy(reservation = Some(Reservation(Nil, Reservation.State.Launched)))
+      val targetInstance2 = instance2.copy(reservation = Some(Reservation(Nil, Reservation.State.Launched)))
+      val targetInstance3 = instance3.copy(reservation = Some(Reservation(Nil, Reservation.State.Launched)))
+
+      logger.info(s"Migration instances ($instance, $instance2, $instance3) ")
+      verify(instanceRepository, once).ids()
+      verify(instanceRepository, once).store(targetInstance)
+      verify(instanceRepository, once).store(targetInstance2)
+      verify(instanceRepository, once).store(targetInstance3)
+    }
+  }
+
+  private class Fixture(val environment: Map[String, String] = Map.empty) {
+
+    implicit val instanceResolver: IdResolver[Instance.Id, JsValue, String, ZkId] =
+      new IdResolver[Instance.Id, JsValue, String, ZkId] {
+        override def toStorageId(id: Id, version: Option[OffsetDateTime]): ZkId =
+          ZkId(category, id.idString, version)
+        override val category: String = "instance"
+        override def fromStorageId(key: ZkId): Id = Instance.Id(key.id)
+        override val hasVersions: Boolean = false
+        override def version(v: JsValue): OffsetDateTime = OffsetDateTime.MIN
+      }
+
+    implicit val instanceJsonUnmarshaller: Unmarshaller[ZkSerialized, JsValue] =
+      Unmarshaller.strict {
+        case ZkSerialized(byteString) =>
+          Json.parse(byteString.utf8String)
+      }
+
+    import Reservation.reservationFormat
+    import Instance.instanceJsonReads
+
+    val instanceRepository: InstanceRepository = mock[InstanceRepository]
+    val persistenceStore: ZkPersistenceStore = mock[ZkPersistenceStore]
+    implicit lazy val env = Environment(environment)
+    implicit lazy val mat: Materializer = ActorMaterializer()
+    implicit lazy val ctx: ExecutionContextExecutor = system.dispatcher
+    val instanceId1 = Instance.Id.forRunSpec(PathId("/app"))
+    val instanceId2 = Instance.Id.forRunSpec(PathId("/app2"))
+    val instanceId3 = Instance.Id.forRunSpec(PathId("/app3"))
+
+    val taskMap = List(
+      Task(
+        Task.Id.forInstanceId(instanceId1, None),
+        Timestamp.now(),
+        Task.Status(
+          stagedAt = Timestamp.now(),
+          condition = Condition.Running,
+          networkInfo = NetworkInfo(
+            "127.0.0.1",
+            8888 :: Nil,
+            mesos.Protos.NetworkInfo.IPAddress.newBuilder()
+              .setProtocol(Protocol.IPv4)
+              .setIpAddress("127.0.0.1")
+              .build() :: Nil)
+        ))
+    ).map(t => t.taskId -> t).toMap
+
+    def legacyInstanceJson(i: Instance): JsValue = {
+      val fieldsMap = Json.toJson(i).asInstanceOf[JsObject].value
+
+      val res = JsObject(fieldsMap.map {
+        case ("tasksMap", taskMap) =>
+          val updatedTaskMap = "tasksMap" -> JsObject(taskMap.asInstanceOf[JsObject].value.mapValues {
+            case task: JsObject =>
+              task + ("reservation" -> Json.toJson(Reservation(Nil, Reservation.State.Launched)).asInstanceOf[JsObject])
+          })
+
+          updatedTaskMap
+
+        case (k, v) => k -> v
+      })
+
+      res
+    }
+
+    val instance = TestInstanceBuilder.emptyInstance(instanceId = instanceId1).copy(tasksMap = taskMap)
+    val instance2 = TestInstanceBuilder.emptyInstance(instanceId = instanceId2).copy(tasksMap = taskMap)
+    val instance3 = TestInstanceBuilder.emptyInstance(instanceId = instanceId3).copy(tasksMap = taskMap)
+    instanceRepository.ids() returns Source(List(instance, instance2, instance3).map(_.instanceId))
+    persistenceStore.get[Instance.Id, JsValue](equalTo(instance.instanceId))(any, any) returns Future(Some(legacyInstanceJson(instance)))
+    persistenceStore.get[Instance.Id, JsValue](equalTo(instance2.instanceId))(any, any) returns Future(Some(legacyInstanceJson(instance2)))
+    persistenceStore.get[Instance.Id, JsValue](equalTo(instance3.instanceId))(any, any) returns Future(Some(legacyInstanceJson(instance3)))
+    instanceRepository.store(any) returns Future.successful(Done)
+  }
+
+}
