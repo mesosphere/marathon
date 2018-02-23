6b104f61f4e84a915a8efe5498377ba86c3b1752
Alena Varkockova <varkockova.a@gmail.com>
2018-02-23 13:29:08 +0100

    Use make install instead of altinstall MARATHON-8102

--- ami/install.bash
+++ ami/install.bash
@@ -39,7 +39,7 @@ wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz
 tar xvf Python-3.6.2.tgz && cd Python-3.6.2/
 ./configure --enable-optimizations
 make -j
-sudo make altinstall
+sudo make install
 cd ../ && rm -r Python-3.6.2
 
 # Install pip
