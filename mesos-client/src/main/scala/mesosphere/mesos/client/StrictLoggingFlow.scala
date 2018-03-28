735c662e23fd52cbae956687e5932c9bdb885ec5
Tim Harper <tharper@mesosphere.io>
2018-03-27 18:28:58 -0600

    Update StrictLoggingFlow.scala

--- mesos-client/src/main/scala/mesosphere/mesos/client/StrictLoggingFlow.scala
+++ mesos-client/src/main/scala/mesosphere/mesos/client/StrictLoggingFlow.scala
@@ -4,7 +4,6 @@ import akka.NotUsed
 import akka.stream.scaladsl.Flow
 import com.typesafe.scalalogging.StrictLogging
 
-// TODO - move somewhere shared
 trait StrictLoggingFlow extends StrictLogging {
   protected def log[T](prefix: String): Flow[T, T, NotUsed] = Flow[T].map{ e => logger.info(s"$prefix$e"); e }
 }
