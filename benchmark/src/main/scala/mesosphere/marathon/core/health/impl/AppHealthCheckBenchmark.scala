467e34ec7f4a5e7cfe58085ecb4af17fbc950293
Clément Michaud <clement.michaud34@gmail.com>
2018-04-03 23:25:54 +0200

    Send Instance health changed event when the overall status changed (#6046)
    * Send Instance health changed event when the overall status changed
    
    Before this fix, during a deployment triggered by a restart, the old
    instances of an app were killed before the new ones were healthy. This
    was due to the "instance_health_changed_event" event sent too early to
    the ReadinessBehavior instance (when the first health check reported to be
    healthy instead of ALL health checks).
    
    The aim of this fix is to aggregate the results of health checks and
    maintain a global instance health state which is Some(true) if all health
    checks of a given instances report healthy, Some(false) if all health checks
    report unhealthy and Option.empty if there is at least one unreported result.
    The "instance_health_changed_event" event is now sent only when there is a
    transition between those 3 states.
    
    This commit relates to those tickets: MARATHON-7609, MARATHON-2683.
    
    * Purge health checks statuses of done tasks
    
    Also make some improvements to AppHealthCheckActor.
    
    * Create a benchmark for AppHealthCheckActor
    
    * Optimize the update of new health check results in AppHealthCheckActor
    
    Two optimization have been done here:
    
    * Don't browse the healthChecks map several times in updateHealthCheck method
    * Use mutable maps instead of immutable ones
    
    * Simplify the AppHealthCheckActor and factorize the code
    
    * Use genericity for health check purges
    * Use pattern matching for readability when possible
    
    * Add more documentation to AppHealthCheckActor
    
    * Fix formatting of MarathonHealthCheckManager and remove _ imports
    
    * isLaunched -> isActive
    
    Making this change compatible with change introduced by 4d20e105d72f5b690e09d206dccc4c595f0b68ec

---
+++ benchmark/src/main/scala/mesosphere/marathon/core/health/impl/AppHealthCheckBenchmark.scala
@@ -0,0 +1,105 @@
+package mesosphere.marathon
+package core.health.impl
+
+import java.util.concurrent.TimeUnit
+
+import mesosphere.marathon.core.health.{Health, MarathonHttpHealthCheck, PortReference, impl}
+import mesosphere.marathon.core.health.impl.AppHealthCheckActor.{ApplicationKey, InstanceKey}
+import mesosphere.marathon.core.instance.Instance
+import mesosphere.marathon.state.PathId
+import org.openjdk.jmh.annotations.{Mode, OutputTimeUnit, Scope, State, BenchmarkMode, Fork, Benchmark}
+import org.openjdk.jmh.infra.Blackhole
+import mesosphere.marathon.state.Timestamp
+
+object AppHealthCheckBenchmark {
+  private final val NB_APPLICATIONS = 1000
+  private final val NB_VERSIONS_PER_APPLICATION = 3
+
+  // this number is used for head revision of the app only
+  private final val NB_INSTANCES_PER_APPLICATION = 10
+  private final val NB_INSTANCES = NB_APPLICATIONS * NB_INSTANCES_PER_APPLICATION
+
+  private final val NB_STATUS_UPDATES = 10000
+
+  val randomGenerator = scala.util.Random
+
+  val healthChecks = Seq(
+    MarathonHttpHealthCheck(portIndex = Some(PortReference(80))),
+    MarathonHttpHealthCheck(portIndex = Some(PortReference(443))),
+    MarathonHttpHealthCheck(portIndex = Some(PortReference(8080)))
+  )
+  val applicationKeys = 0.to(NB_APPLICATIONS).flatMap(appId => {
+    0.to(NB_VERSIONS_PER_APPLICATION).map(version => Timestamp(version.toLong))
+      .map(version => ApplicationKey(PathId(s"$appId"), version))
+  })
+
+  val shuffledApplicationKeys = scala.util.Random.shuffle(applicationKeys)
+
+  val instanceKeys = applicationKeys.flatMap(appKey => {
+    0.to(NB_INSTANCES_PER_APPLICATION).map(instanceId => InstanceKey(appKey, Instance.Id(s"$instanceId")))
+  })
+
+  val randomlySelectedInstanceKeysWithStatus = 0.to(NB_STATUS_UPDATES).map(idx => {
+    val instanceIdx = randomGenerator.nextInt(NB_INSTANCES)
+    val hcIdx = instanceIdx % 3
+    val healthSelector = instanceIdx % 2
+    val instanceKey = instanceKeys(instanceIdx)
+    val failingHealth = Health(instanceKey.instanceId, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(10)))
+    val successHealth = Health(instanceKey.instanceId, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0)))
+    val health = healthSelector match {
+      case 0 => successHealth
+      case _ => failingHealth
+    }
+    (instanceKey.applicationKey, healthChecks(hcIdx), health)
+  })
+  val doNothingNotifier = (h: Option[Boolean]) => {}
+
+  val appHealthCheckProxy = new impl.AppHealthCheckActor.AppHealthCheckProxy
+  val appHealthCheckProxyWithAlreadyRegisteredHealthChecks = {
+    val proxy = new impl.AppHealthCheckActor.AppHealthCheckProxy
+    for(
+      applicationKey <- applicationKeys;
+      healthCheck <- healthChecks
+    ) {
+      proxy.addHealthCheck(applicationKey, healthCheck)
+    }
+    proxy
+  }
+}
+
+@State(Scope.Benchmark)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@BenchmarkMode(Array(Mode.Throughput, Mode.AverageTime))
+@Fork(1)
+class AppHealthCheckBenchmark {
+  import AppHealthCheckBenchmark._
+
+  /**
+    * Simulate Marathon starting and registering all health checks
+    * @param hole
+    */
+  @Benchmark
+  def addHealthChecks(hole: Blackhole): Unit = {
+    for(
+      applicationKey <- shuffledApplicationKeys;
+      healthCheck <- healthChecks
+    ) {
+      appHealthCheckProxy.addHealthCheck(applicationKey, healthCheck)
+    }
+  }
+
+  /**
+    * Simulate all health checks reporting at the same time, which is unlickely
+    * to happen but give an idea on the performance of this method.
+    * @param hole
+    */
+  @Benchmark
+  def updateHealthCheckStatuses(hole: Blackhole): Unit = {
+    for(
+      update <- randomlySelectedInstanceKeysWithStatus
+    ) {
+      appHealthCheckProxyWithAlreadyRegisteredHealthChecks
+        .updateHealthCheckStatus(update._1, update._2, update._3, doNothingNotifier)
+    }
+  }
+}
---
+++ src/test/scala/mesosphere/marathon/core/health/impl/AppHealthCheckActorTest.scala
@@ -0,0 +1,230 @@
+package mesosphere.marathon
+package core.health.impl
+
+import akka.testkit.{ TestActorRef, TestProbe }
+import mesosphere.AkkaUnitTest
+import mesosphere.marathon.core.event.InstanceHealthChanged
+import mesosphere.marathon.core.health.impl.AppHealthCheckActor.{
+  AddHealthCheck,
+  ApplicationKey,
+  HealthCheckStatusChanged,
+  RemoveHealthCheck,
+  InstanceKey,
+  PurgeHealthCheckStatuses
+}
+import mesosphere.marathon.core.health.{ Health, MarathonHttpHealthCheck, PortReference }
+import mesosphere.marathon.core.instance.Instance
+import mesosphere.marathon.state.PathId.StringPathId
+import mesosphere.marathon.state.Timestamp
+
+class AppHealthCheckActorTest extends AkkaUnitTest {
+  class Fixture {
+    val appId = "/test".toPath
+    val appVersion = Timestamp(1)
+    val appKey = ApplicationKey(appId, appVersion)
+    val hcPort80 = MarathonHttpHealthCheck(portIndex = Some(PortReference(80)))
+    val hcPort443 = MarathonHttpHealthCheck(portIndex = Some(PortReference(443)))
+    val instances = List(
+      Instance.Id("instance1"),
+      Instance.Id("instance2"),
+      Instance.Id("instance3")
+    )
+  }
+
+  "AppHealthCheckActor" should {
+    "send status changed event when all instances are healthy" in {
+      val f = new Fixture
+      val systemLog = TestProbe()
+
+      val actor = system.actorOf(AppHealthCheckActor.props(system.eventStream))
+      system.eventStream.subscribe(systemLog.ref, classOf[InstanceHealthChanged])
+
+      actor ! AddHealthCheck(f.appKey, f.hcPort80)
+      actor ! AddHealthCheck(f.appKey, f.hcPort443)
+
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort80,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort443,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+
+      actor ! RemoveHealthCheck(f.appKey, f.hcPort80)
+      actor ! RemoveHealthCheck(f.appKey, f.hcPort443)
+
+      systemLog.expectMsg(InstanceHealthChanged(
+        f.instances.head, f.appKey.version, f.appKey.appId, Some(true)))
+    }
+
+    "send status changed event when one instance becomes unhealthy" in {
+      val f = new Fixture
+      val systemLog = TestProbe()
+
+      val actor = system.actorOf(AppHealthCheckActor.props(system.eventStream))
+      system.eventStream.subscribe(systemLog.ref, classOf[InstanceHealthChanged])
+
+      actor ! AddHealthCheck(f.appKey, f.hcPort80)
+      actor ! AddHealthCheck(f.appKey, f.hcPort443)
+
+      // all health checks pass once
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort80,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort443,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+
+      // and suddenly one fails
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort443,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(8))))
+
+      actor ! RemoveHealthCheck(f.appKey, f.hcPort80)
+      actor ! RemoveHealthCheck(f.appKey, f.hcPort443)
+
+      systemLog.expectMsg(InstanceHealthChanged(
+        f.instances.head, f.appKey.version, f.appKey.appId, Some(true)))
+      systemLog.expectMsg(InstanceHealthChanged(
+        f.instances.head, f.appKey.version, f.appKey.appId, Some(false)))
+    }
+
+    "not send status changed even when health check is not registered" in {
+      val f = new Fixture
+      val systemLog = TestProbe()
+
+      val actor = system.actorOf(AppHealthCheckActor.props(system.eventStream))
+      system.eventStream.subscribe(systemLog.ref, classOf[InstanceHealthChanged])
+
+      // all health checks pass once
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort80,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+
+      systemLog.expectNoMsg()
+    }
+
+    "send status changed event when several instances become healthy" in {
+      val f = new Fixture
+      val systemLog = TestProbe()
+
+      val actor = system.actorOf(AppHealthCheckActor.props(system.eventStream))
+      system.eventStream.subscribe(systemLog.ref, classOf[InstanceHealthChanged])
+
+      actor ! AddHealthCheck(f.appKey, f.hcPort80)
+      actor ! AddHealthCheck(f.appKey, f.hcPort443)
+
+      // all health checks pass once
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort80,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort80,
+        Health(f.instances(1), lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort443,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+
+      systemLog.expectMsg(InstanceHealthChanged(
+        f.instances.head, f.appKey.version, f.appKey.appId, Some(true)))
+
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort443,
+        Health(f.instances(1), lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+
+      systemLog.expectMsg(InstanceHealthChanged(
+        f.instances(1), f.appKey.version, f.appKey.appId, Some(true)))
+
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort80,
+        Health(f.instances(2), lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort443,
+        Health(f.instances(2), lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+
+      systemLog.expectMsg(InstanceHealthChanged(
+        f.instances(2), f.appKey.version, f.appKey.appId, Some(true)))
+
+      actor ! RemoveHealthCheck(f.appKey, f.hcPort80)
+      actor ! RemoveHealthCheck(f.appKey, f.hcPort443)
+    }
+
+    "health checks inventories is cleaned when health checks of a given version are removed" in {
+      val f = new Fixture
+      val actor = TestActorRef[AppHealthCheckActor](AppHealthCheckActor.props(system.eventStream))
+
+      assert(!actor.underlyingActor.proxy.healthChecks.contains(f.appKey))
+      actor ! AddHealthCheck(f.appKey, f.hcPort80)
+      awaitAssert(actor.underlyingActor.proxy.healthChecks.contains(f.appKey))
+      actor ! AddHealthCheck(f.appKey, f.hcPort443)
+      awaitAssert(actor.underlyingActor.proxy.healthChecks.contains(f.appKey))
+      actor ! RemoveHealthCheck(f.appKey, f.hcPort80)
+      awaitAssert(actor.underlyingActor.proxy.healthChecks.contains(f.appKey))
+      actor ! RemoveHealthCheck(f.appKey, f.hcPort443)
+      awaitAssert(!actor.underlyingActor.proxy.healthChecks.contains(f.appKey))
+    }
+
+    "health checks results are cleaned when health checks of a given version are removed" in {
+      val f = new Fixture
+      val actor = TestActorRef[AppHealthCheckActor](AppHealthCheckActor.props(system.eventStream))
+
+      val instanceKey = InstanceKey(f.appKey, f.instances.head)
+      assert(!actor.underlyingActor.proxy.healthCheckStates.contains(instanceKey))
+      actor ! AddHealthCheck(f.appKey, f.hcPort80)
+      actor ! AddHealthCheck(f.appKey, f.hcPort443)
+
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort80,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort443,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+
+      awaitAssert(actor.underlyingActor.proxy.healthCheckStates.contains(instanceKey))
+      actor ! RemoveHealthCheck(f.appKey, f.hcPort80)
+      actor ! RemoveHealthCheck(f.appKey, f.hcPort443)
+      awaitAssert(!actor.underlyingActor.proxy.healthCheckStates.contains(instanceKey))
+    }
+
+    "health checks results are purged one by one" in {
+      val f = new Fixture
+      val actor = TestActorRef[AppHealthCheckActor](AppHealthCheckActor.props(system.eventStream))
+
+      val instanceKey = InstanceKey(f.appKey, f.instances.head)
+      assert(!actor.underlyingActor.proxy.healthCheckStates.contains(instanceKey))
+      actor ! AddHealthCheck(f.appKey, f.hcPort80)
+      actor ! AddHealthCheck(f.appKey, f.hcPort443)
+
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort80,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort443,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+
+      awaitAssert(actor.underlyingActor.proxy.healthCheckStates(instanceKey).contains(f.hcPort80))
+      awaitAssert(actor.underlyingActor.proxy.healthCheckStates(instanceKey).contains(f.hcPort443))
+
+      actor ! PurgeHealthCheckStatuses(Seq(
+        (InstanceKey(f.appKey, f.instances.head), f.hcPort80)
+      ))
+      awaitAssert(!actor.underlyingActor.proxy.healthCheckStates(instanceKey).contains(f.hcPort80))
+      awaitAssert(actor.underlyingActor.proxy.healthCheckStates(instanceKey).contains(f.hcPort443))
+
+      actor ! PurgeHealthCheckStatuses(Seq(
+        (InstanceKey(f.appKey, f.instances.head), f.hcPort443)
+      ))
+
+      awaitAssert(!actor.underlyingActor.proxy.healthCheckStates.contains(instanceKey))
+    }
+
+    "health checks results are purged all at once" in {
+      val f = new Fixture
+      val actor = TestActorRef[AppHealthCheckActor](AppHealthCheckActor.props(system.eventStream))
+
+      val instanceKey = InstanceKey(f.appKey, f.instances.head)
+      assert(!actor.underlyingActor.proxy.healthCheckStates.contains(instanceKey))
+      actor ! AddHealthCheck(f.appKey, f.hcPort80)
+      actor ! AddHealthCheck(f.appKey, f.hcPort443)
+
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort80,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+      actor ! HealthCheckStatusChanged(f.appKey, f.hcPort443,
+        Health(f.instances.head, lastSuccess = Some(Timestamp(5)), lastFailure = Some(Timestamp(0))))
+
+      awaitAssert(actor.underlyingActor.proxy.healthCheckStates(instanceKey).contains(f.hcPort80))
+      awaitAssert(actor.underlyingActor.proxy.healthCheckStates(instanceKey).contains(f.hcPort443))
+
+      actor ! PurgeHealthCheckStatuses(Seq(
+        (InstanceKey(f.appKey, f.instances.head), f.hcPort80),
+        (InstanceKey(f.appKey, f.instances.head), f.hcPort443)
+      ))
+
+      awaitAssert(!actor.underlyingActor.proxy.healthCheckStates.contains(instanceKey))
+    }
+  }
+}
