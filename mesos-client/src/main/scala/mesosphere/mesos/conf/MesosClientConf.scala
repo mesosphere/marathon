97bad66236e779507f96ae1d45a3f1936d9221a6
Tim Harper <tharper@mesosphere.io>
2018-04-03 10:05:49 -0600

    Rework Mesos v1 client
    We remove the mutable connection info and frameworkId, and instead return a Source[MesosClient, NotUsed], consistent with Akka HTTP response handling. The MesosClient is yielded after all of this data is available, and contains it immutably. To do this, the Mesos graph pulls out the initial Subscribed event, and then passes the rest of the stream for further consumption
    via a materializable-once source in mesosClient.mesosSource.

--- mesos-client/src/main/scala/mesosphere/mesos/conf/MesosClientConf.scala
+++ mesos-client/src/main/scala/mesosphere/mesos/conf/MesosClientConf.scala
@@ -7,12 +7,12 @@ import scala.concurrent.duration._
   *
   * @param master The URL of the Mesos master
   * @param sourceBufferSize Buffer size of the mesos source im messages
-  * @param redirectRetires Number of retries to follow mesos master redirect
+  * @param redirectRetries Number of retries to follow mesos master redirect
   * @param idleTimeout Time in seconds between two processed elements exceeds the provided timeout then the connection to mesos
   *                    is interrupted. Is usually set to approx. 5 hear beats.
   */
 case class MesosClientConf(
     master: String,
     sourceBufferSize: Int = 10,
-    redirectRetires: Int = 3,
+    redirectRetries: Int = 3,
     idleTimeout: FiniteDuration = 75.seconds)
--- mesos-client/src/test/scala/mesosphere/mesos/client/MesosClientIntegrationTest.scala
+++ mesos-client/src/test/scala/mesosphere/mesos/client/MesosClientIntegrationTest.scala
@@ -1,19 +1,22 @@
 package mesosphere.mesos.client
 
+import akka.Done
 import java.util.UUID
 
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, UniqueKillSwitch}
-import akka.stream.scaladsl.{Keep, Sink, SinkQueueWithCancel, Source}
+import akka.stream.{ ActorMaterializer }
+import akka.stream.scaladsl.{ Sink, Source }
 import com.typesafe.scalalogging.StrictLogging
 import mesosphere.AkkaUnitTest
 import mesosphere.marathon.IntegrationTest
 import mesosphere.marathon.integration.setup.MesosClusterTest
 import mesosphere.mesos.conf.MesosClientConf
-import org.apache.mesos.v1.mesos.{Filters, FrameworkID, FrameworkInfo}
+import org.apache.mesos.v1.mesos.{ Filters, FrameworkID, FrameworkInfo }
 import org.apache.mesos.v1.scheduler.scheduler.Event
 import org.scalatest.Inside
 import org.scalatest.concurrent.Eventually
+import scala.annotation.tailrec
+import scala.concurrent.Future
 
 @IntegrationTest
 class MesosClientIntegrationTest extends AkkaUnitTest
@@ -24,21 +27,14 @@ class MesosClientIntegrationTest extends AkkaUnitTest
 
   "Mesos client should successfully subscribe to mesos without framework Id" in withFixture() { f =>
     When("a framework subscribes")
-
-    val event: Event = f.pull(1).head
-
     Then("a framework successfully subscribes without a framework Id")
-    event.`type` shouldBe Some(Event.Type.SUBSCRIBED)
-
+    f.client.frameworkId.value shouldNot be(empty)
 
     And("connection context should be initialized")
-    inside(f.client.contextPromise.future.futureValue) {
-      case MesosConnectionContext(url, mesosStreamId, frameworkId) =>
-        url.getHost shouldBe f.mesosHost
-        url.getPort shouldBe f.mesosPort
-        mesosStreamId shouldBe defined
-        frameworkId shouldBe defined
-    }
+    f.client.connectionInfo.url.getHost shouldBe f.mesosHost
+    f.client.connectionInfo.url.getPort shouldBe f.mesosPort
+    f.client.connectionInfo.streamId.length() should be > 1
+    f.client.frameworkId.value.length() should be > 1
   }
 
   "Mesos client should successfully subscribe to mesos with framework Id" in {
@@ -46,103 +42,102 @@ class MesosClientIntegrationTest extends AkkaUnitTest
 
     When("a framework subscribes with a framework Id")
     withFixture(Some(frameworkID)) { f =>
-      val event: Event = f.pull(1).head
-
-      Then("first event received should be a subscribed event")
-      event.`type` shouldBe Some(Event.Type.SUBSCRIBED)
-      event.subscribed.get.frameworkId shouldBe frameworkID
-
-      And("connection context should be initialized")
-      inside(f.client.contextPromise.future.futureValue) {
-        case MesosConnectionContext(url, mesosStreamId, frameworkId) =>
-          url.getHost shouldBe f.mesosHost
-          url.getPort shouldBe f.mesosPort
-          mesosStreamId shouldBe defined
-          frameworkId shouldBe Some(frameworkID)
-      }
+      Then("the client should identify as the specified frameworkId")
+      f.client.frameworkId shouldBe frameworkID
     }
   }
 
   "Mesos client should successfully receive heartbeat" in withFixture() { f =>
     When("a framework subscribes")
-    val events: Seq[Event] = f.pull(2)
+    val heartbeat = f.pullUntil(_.`type`.contains(Event.Type.HEARTBEAT))
 
     Then("a heartbeat event should arrive")
-    events.count(_.`type`.contains(Event.Type.HEARTBEAT)) should be > 0
+    heartbeat shouldNot be(empty)
   }
 
   "Mesos client should successfully receive offers" in withFixture() { f =>
     When("a framework subscribes")
-    val events: Seq[Event] = f.pull(3)
+    val offer = f.pullUntil(_.`type`.contains(Event.Type.OFFERS))
 
     And("an offer should arrive")
-    events.count(_.`type`.contains(Event.Type.OFFERS)) should be > 0
+    offer shouldNot be(empty)
   }
 
   "Mesos client should successfully declines offers" in withFixture() { f =>
     When("a framework subscribes")
-    var events = f.pull(3)
-
-    val offer = events.filter(_.`type`.contains(Event.Type.OFFERS)).head
+    And("an offer event is received")
+    val Some(offer) = f.pullUntil(_.`type`.contains(Event.Type.OFFERS))
     val offerId = offer.offers.get.offers.head.id
 
-    And("an offer event is received")
-    val decline = f.client.decline(
-      offerIds = Seq(offerId),
-      filters = Some(Filters(Some(0.0f)))
-    )
-
     And("and an offer is declined")
+    val decline = f.client.calls.newDecline(
+      offerIds = Seq(offerId),
+      filters = Some(Filters(Some(0.0f))))
+
     Source.single(decline).runWith(f.client.mesosSink)
 
     Then("eventually a new offer event arrives")
-    eventually{
-      events = f.pull(1)
-      events.count(_.`type`.contains(Event.Type.OFFERS)) should be > 0
-    }
+    val nextOffer = f.pullUntil(_.`type`.contains(Event.Type.OFFERS))
+    nextOffer shouldNot be(empty)
+  }
 
-    events.count(_.`type`.contains(Event.Type.OFFERS)) should be > 0
+  "Mesos client publisher sink and event source are both stopped with the kill switch" in withFixture() { f =>
+    val sinkDone = Source.fromFuture(Future.never).runWith(f.client.mesosSink)
+
+    f.client.killSwitch.shutdown()
+    sinkDone.futureValue.shouldBe(Done)
+    eventually {
+      f.queue.pull().futureValue shouldBe None
+    }
   }
 
   def withFixture(frameworkId: Option[FrameworkID] = None)(fn: Fixture => Unit): Unit = {
     val f = new Fixture(frameworkId)
     try fn(f) finally {
-      f.switch.shutdown()
+      f.client.killSwitch.shutdown()
     }
   }
 
-  class Fixture(frameworkId: Option[FrameworkID] = None) {
+  class Fixture(existingFrameworkId: Option[FrameworkID] = None) {
     implicit val system: ActorSystem = ActorSystem()
     implicit val materializer: ActorMaterializer = ActorMaterializer()
 
     val frameworkInfo = FrameworkInfo(
       user = "foo",
       name = "Example FOO Framework",
-      id = frameworkId,
+      id = existingFrameworkId,
       roles = Seq("foo"),
       failoverTimeout = Some(0.0f),
-      capabilities = Seq(FrameworkInfo.Capability(`type` = Some(FrameworkInfo.Capability.Type.MULTI_ROLE)))
-    )
+      capabilities = Seq(FrameworkInfo.Capability(`type` = Some(FrameworkInfo.Capability.Type.MULTI_ROLE))))
 
     val mesosUrl = new java.net.URI(mesos.url)
     val mesosHost = mesosUrl.getHost
     val mesosPort = mesosUrl.getPort
 
     val conf = new MesosClientConf(master = s"${mesosUrl.getHost}:${mesosUrl.getPort}")
-    val client = new MesosClient(conf, frameworkInfo)
+    val client = MesosClient(conf, frameworkInfo).runWith(Sink.head).futureValue
 
-    val (switch: UniqueKillSwitch, queue: SinkQueueWithCancel[Event]) = client.mesosSource.toMat(Sink.queue())(Keep.both).run()
+    val queue = client.mesosSource.
+      runWith(Sink.queue())
 
     /**
-      * Method will pull n elements from the queue effectively awaiting n elements from mesos. Unlike take() e.g.
-      * `f.client.mesosSource.take(1)` it will not cancel the upstream after the number of elements is received. A
-      * TimeoutException is thrown should the element not be available withing `patienceConfig.timeout` milliseconds.
+      * Pull (and drop) elements from the queue until the predicate returns true. Does not cancel the upstream.
       *
-      * @param n number of elements to pull
-      * @return a sequence of elements
+      * Returns Some(element) when queue emits an event which matches the predicate
+      * Returns None if queue ends (client closes) before the predicate matches
+      * TimeoutException is thrown if no event is available within the `patienceConfig.timeout` duration.
+      *
+      * @param predicate Function to evaluate to see if event matches
+      * @return matching event, if any
       */
-    def pull(n: Int): Seq[Event] = {
-      (1 to n).map(_ => queue.pull().futureValue.get)
-    }
+    @tailrec final def pullUntil(predicate: Event => Boolean): Option[Event] =
+      queue.pull().futureValue match {
+        case e @ Some(event) if (predicate(event)) =>
+          e
+        case None =>
+          None
+        case _ =>
+          pullUntil(predicate)
+      }
   }
 }
