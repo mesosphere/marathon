4a00da24fff3440b3e07926e0c5592426030ce3c
Karsten Jeschkies <kjeschkies@mesosphere.com>
2018-04-03 15:32:37 +0200

    Echo pod id.

--- src/test/scala/mesosphere/marathon/integration/UpgradeIntegrationTest.scala
+++ src/test/scala/mesosphere/marathon/integration/UpgradeIntegrationTest.scala
@@ -192,8 +192,12 @@ class UpgradeIntegrationTest extends AkkaIntegrationTest with MesosClusterTest w
 
       Then(s"pods ${resident_pod_16322.id} and ${resident_pod_16322_fail.id} can be queried")
       implicit val requestTimeout = 30.seconds
-      eventually { AkkaHttpResponse.request(Get(s"http://$resident_pod_16322_address:$resident_pod_16322_port/pst1/foo")).futureValue.entityString should be("start\n") }
-      eventually { AkkaHttpResponse.request(Get(s"http://$resident_pod_16322_address_fail:$resident_pod_16322_port_fail/pst1/foo")).futureValue.entityString should be("start\n") }
+      eventually {
+        AkkaHttpResponse.request(Get(s"http://$resident_pod_16322_address:$resident_pod_16322_port/pst1/foo")).futureValue.entityString should be("start resident-pod-16322\n")
+      }
+      eventually {
+        AkkaHttpResponse.request(Get(s"http://$resident_pod_16322_address_fail:$resident_pod_16322_port_fail/pst1/foo")).futureValue.entityString should be("start resident-pod-16322-fail\n")
+      }
 
       And("All apps from 1.4.9 and 1.5.6 are still running")
       marathon16322.client.tasks(app_149.id.toPath).value should contain theSameElementsAs (originalApp149Tasks)
@@ -203,7 +207,7 @@ class UpgradeIntegrationTest extends AkkaIntegrationTest with MesosClusterTest w
       marathon16322.stop().futureValue
 
       And(s"Pod ${resident_pod_16322_fail.id} fails")
-      killTask("SimpleHTTPServer")
+      killTask("resident-pod-16322-fail")
 
       // Pass upgrade to current
       And("Marathon is upgraded to the current version")
@@ -223,10 +227,15 @@ class UpgradeIntegrationTest extends AkkaIntegrationTest with MesosClusterTest w
 
       And("All pods from 1.6.322 are still running")
       eventually { marathonCurrent.client.status(resident_pod_16322.id) should be(Stable) }
-      eventually { AkkaHttpResponse.request(Get(s"http://$resident_pod_16322_address:$resident_pod_16322_port/pst1/foo")).futureValue.entityString should be("start\n") }
+      eventually {
+        AkkaHttpResponse.request(Get(s"http://$resident_pod_16322_address:$resident_pod_16322_port/pst1/foo")).futureValue.entityString should be("start resident-pod-16322\n")
+      }
 
       val (resident_pod_16322_port_recovered, resident_pod_16322_address_recovered) = eventually { should_be_stable(resident_pod_16322_fail.id) }
-      eventually { AkkaHttpResponse.request(Get(s"http://$resident_pod_16322_address_recovered:$resident_pod_16322_port_recovered/pst1/foo")).futureValue.entityString should be("start\nstart\n") }
+      val expected_content = "start resident-pod-16322-fail\nstart resident-pod-16322-fail\n"
+      eventually {
+        AkkaHttpResponse.request(Get(s"http://$resident_pod_16322_address_recovered:$resident_pod_16322_port_recovered/pst1/foo")).futureValue.entityString should be(expected_content)
+      }
 
       marathonCurrent.close()
     }
@@ -271,23 +280,26 @@ class UpgradeIntegrationTest extends AkkaIntegrationTest with MesosClusterTest w
     * @param id Id of the pod run spec.
     * @return
     */
-  def residentPodWithService(id: String) = PodDefinition(
-    id = testBasePath / id,
-    containers = Seq(
-      MesosContainer(
-        name = "task1",
-        exec = Some(raml.MesosExec(raml.ShellCommand("cd $MESOS_SANDBOX && echo 'start' >> pst1/foo && python -m SimpleHTTPServer $ENDPOINT_TASK1"))),
-        resources = raml.Resources(cpus = 0.1, mem = 32.0),
-        endpoints = Seq(raml.Endpoint(name = "task1", hostPort = Some(0))),
-        volumeMounts = Seq(VolumeMount(Some("pst"), "pst1", true))
-      )
-    ),
-    volumes = Seq(PersistentVolume(name = Some("pst"), persistent = PersistentVolumeInfo(size = 10L))),
-    networks = Seq(HostNetwork),
-    instances = 1,
-    unreachableStrategy = state.UnreachableDisabled,
-    upgradeStrategy = state.UpgradeStrategy(0.0, 0.0)
-  )
+  def residentPodWithService(id: String) = {
+    val cmd = s"cd \$MESOS_SANDBOX && echo 'start $id' >> pst1/foo && python -m SimpleHTTPServer \$ENDPOINT_TASK1"
+    PodDefinition(
+      id = testBasePath / id,
+      containers = Seq(
+        MesosContainer(
+          name = "task1",
+          exec = Some(raml.MesosExec(raml.ShellCommand(cmd))),
+          resources = raml.Resources(cpus = 0.1, mem = 32.0),
+          endpoints = Seq(raml.Endpoint(name = "task1", hostPort = Some(0))),
+          volumeMounts = Seq(VolumeMount(Some("pst"), "pst1", true))
+        )
+      ),
+      volumes = Seq(PersistentVolume(name = Some("pst"), persistent = PersistentVolumeInfo(size = 10L))),
+      networks = Seq(HostNetwork),
+      instances = 1,
+      unreachableStrategy = state.UnreachableDisabled,
+      upgradeStrategy = state.UpgradeStrategy(0.0, 0.0)
+    )
+  }
 
   /**
     * Asserts that pod with given id is stable and has a port defined.
