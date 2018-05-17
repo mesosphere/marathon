020620196229bfbc33225ee228abbf71c61d66e8
Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>
2018-05-17 21:36:49 +0200

    Once more without `sudo`

--- ci/si_install_deps.sh
+++ ci/si_install_deps.sh
@@ -17,8 +17,8 @@ fi
 
 # Ensure amm is available.
 if ! command -v amm >/dev/null 2>&1; then
-    sudo curl -L -o /usr/local/bin/amm https://github.com/lihaoyi/Ammonite/releases/download/1.1.0/2.12-1.1.0 && \
-    sudo chmod +x /usr/local/bin/amm && \
+    curl -L -o /usr/local/bin/amm https://github.com/lihaoyi/Ammonite/releases/download/1.1.0/2.12-1.1.0 && \
+    chmod +x /usr/local/bin/amm && \
     echo "Ammonite successfully installed"
 fi
 
