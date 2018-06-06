e725a77fa6ec03eba0a11a3156f12a207a98571a
Alena Varkockova <varkockova.a@gmail.com>
2018-06-06 09:57:11 +0200

    Fix places where tasksMap might be empty

--- src/main/scala/mesosphere/marathon/MarathonSchedulerActor.scala
+++ src/main/scala/mesosphere/marathon/MarathonSchedulerActor.scala
@@ -501,7 +501,7 @@ class SchedulerActions(
   */
 object TaskStatusCollector {
   def collectTaskStatusFor(instances: Seq[Instance]): Seq[mesos.Protos.TaskStatus] = {
-    instances.collect { case Instance(_, Some(agentInfo), _, tasksMap, _, _, _) => (agentInfo, tasksMap) }
+    instances.collect { case Instance(_, Some(agentInfo), _, tasksMap, _, _, _) if tasksMap.nonEmpty => (agentInfo, tasksMap) }
       .flatMap {
         case (agentInfo, tasksMap) =>
           tasksMap.values.withFilter(task => !task.isTerminal && !task.isReserved).map { task =>
