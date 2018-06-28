4f6e6a92b47ba8bcda0a4585deef5451a5135369
Karsten Jeschkies <kjeschkies@mesosphere.com>
2018-06-28 17:00:40 +0200

    Test migration of instance goals.

--- src/main/scala/mesosphere/marathon/storage/migration/MigrationTo200.scala
+++ src/main/scala/mesosphere/marathon/storage/migration/MigrationTo200.scala
@@ -6,7 +6,7 @@ import java.time.OffsetDateTime
 import akka.Done
 import akka.http.scaladsl.unmarshalling.Unmarshaller
 import akka.stream.Materializer
-import akka.stream.scaladsl.Sink
+import akka.stream.scaladsl.{Flow, Sink}
 import com.typesafe.scalalogging.StrictLogging
 import mesosphere.marathon.core.condition.Condition
 import mesosphere.marathon.core.instance.{Goal, Instance, Reservation}
@@ -33,6 +33,63 @@ class MigrationTo200(instanceRepository: InstanceRepository, persistenceStore: P
 
 object MigrationTo200 extends MaybeStore with StrictLogging {
 
+  import Instance.agentFormat
+  import Instance.tasksMapFormat
+  import mesosphere.marathon.api.v2.json.Formats.TimestampFormat
+
+  /**
+    * Read format for instance state without goal.
+    */
+  val instanceStateReads160: Reads[InstanceState] = {
+    (
+      (__ \ "condition").read[Condition] ~
+      (__ \ "since").read[Timestamp] ~
+      (__ \ "activeSince").readNullable[Timestamp] ~
+      (__ \ "healthy").readNullable[Boolean]
+    ) { (condition, since, activeSince, healthy) =>
+        InstanceState(condition, since, activeSince, healthy)
+      }
+  }
+
+  /**
+    * Read format for old instance without goal.
+    */
+  val instanceJsonReads160: Reads[Instance] = {
+    (
+      (__ \ "instanceId").read[Instance.Id] ~
+      (__ \ "agentInfo").read[AgentInfo] ~
+      (__ \ "tasksMap").read[Map[Task.Id, Task]] ~
+      (__ \ "runSpecVersion").read[Timestamp] ~
+      (__ \ "state").read[InstanceState](instanceStateReads160) ~
+      (__ \ "unreachableStrategy").readNullable[raml.UnreachableStrategy] ~
+      (__ \ "reservation").readNullable[Reservation]
+    ) { (instanceId, agentInfo, tasksMap, runSpecVersion, state, maybeUnreachableStrategy, reservation) =>
+        val unreachableStrategy = maybeUnreachableStrategy.
+          map(Raml.fromRaml(_)).getOrElse(UnreachableStrategy.default())
+        new Instance(instanceId, agentInfo, state, tasksMap, runSpecVersion, unreachableStrategy, reservation)
+      }
+  }
+
+  implicit val instanceResolver: IdResolver[Instance.Id, JsValue, String, ZkId] =
+    new IdResolver[Instance.Id, JsValue, String, ZkId] {
+      override def toStorageId(id: Id, version: Option[OffsetDateTime]): ZkId =
+        ZkId(category, id.idString, version)
+
+      override val category: String = "instance"
+
+      override def fromStorageId(key: ZkId): Id = Instance.Id.fromIdString(key.id)
+
+      override val hasVersions: Boolean = false
+
+      override def version(v: JsValue): OffsetDateTime = OffsetDateTime.MIN
+    }
+
+  implicit val instanceJsonUnmarshaller: Unmarshaller[ZkSerialized, JsValue] =
+    Unmarshaller.strict {
+      case ZkSerialized(byteString) =>
+        Json.parse(byteString.utf8String)
+    }
+
   /**
     * This function traverses all instances in ZK and sets the instance goal field.
     */
@@ -40,102 +97,13 @@ object MigrationTo200 extends MaybeStore with StrictLogging {
 
     logger.info("Starting reservations migration to Storage version 200")
 
-    implicit val instanceResolver: IdResolver[Instance.Id, JsValue, String, ZkId] =
-      new IdResolver[Instance.Id, JsValue, String, ZkId] {
-        override def toStorageId(id: Id, version: Option[OffsetDateTime]): ZkId =
-          ZkId(category, id.idString, version)
-
-        override val category: String = "instance"
-
-        override def fromStorageId(key: ZkId): Id = Instance.Id.fromIdString(key.id)
-
-        override val hasVersions: Boolean = false
-
-        override def version(v: JsValue): OffsetDateTime = OffsetDateTime.MIN
-      }
-
-    implicit val instanceJsonUnmarshaller: Unmarshaller[ZkSerialized, JsValue] =
-      Unmarshaller.strict {
-        case ZkSerialized(byteString) =>
-          Json.parse(byteString.utf8String)
-      }
-
-    import Instance.agentFormat
-    import Instance.tasksMapFormat
-    import mesosphere.marathon.api.v2.json.Formats.TimestampFormat
-
-    /**
-      * Read format for instance state without goal.
-      */
-    val instanceStateReads160: Reads[InstanceState] = {
-      (
-        (__ \ "condition").read[Condition] ~
-        (__ \ "since").read[Timestamp] ~
-        (__ \ "activeSince").readNullable[Timestamp] ~
-        (__ \ "healthy").readNullable[Boolean]
-      ) { (condition, since, activeSince, healthy) =>
-          InstanceState(condition, since, activeSince, healthy)
-        }
-    }
-
-    /**
-      * Read format for old instance without goal.
-      */
-    val instanceJsonReads160: Reads[Instance] = {
-      (
-        (__ \ "instanceId").read[Instance.Id] ~
-        (__ \ "agentInfo").read[AgentInfo] ~
-        (__ \ "tasksMap").read[Map[Task.Id, Task]] ~
-        (__ \ "runSpecVersion").read[Timestamp] ~
-        (__ \ "state").read[InstanceState](instanceStateReads160) ~
-        (__ \ "unreachableStrategy").readNullable[raml.UnreachableStrategy] ~
-        (__ \ "reservation").readNullable[Reservation]
-      ) { (instanceId, agentInfo, tasksMap, runSpecVersion, state, maybeUnreachableStrategy, reservation) =>
-          val unreachableStrategy = maybeUnreachableStrategy.
-            map(Raml.fromRaml(_)).getOrElse(UnreachableStrategy.default())
-          new Instance(instanceId, agentInfo, state, tasksMap, runSpecVersion, unreachableStrategy, reservation)
-        }
-    }
-
-    /**
-      * Extract instance from old format without goal attached.
-      * @param jsValue The instance as JSON.
-      * @return The parsed instance.
-      */
-    def extractInstanceFromJson(jsValue: JsValue): Instance = jsValue.as[Instance](instanceJsonReads160)
-
-    /**
-      * Update the goal of the instance.
-      * @param instance The old instance.
-      * @return An instance with an updated goal.
-      */
-    def updateGoal(instance: Instance): Instance = {
-      val updatedInstanceState = if (!instance.hasReservation) {
-        instance.state.copy(goal = Goal.Running)
-      } else {
-        if (instance.isReservedTerminal) {
-          instance.state.copy(goal = Goal.Stopped)
-        } else {
-          instance.state.copy(goal = Goal.Running)
-        }
-      }
-
-      instance.copy(state = updatedInstanceState)
-    }
-
     maybeStore(persistenceStore).map { store =>
       instanceRepository
         .ids()
         .mapAsync(1) { instanceId =>
-          store.get(instanceId)
+          store.get[Instance.Id, JsValue](instanceId)
         }
-        .mapConcat {
-          case Some(jsValue) => List(extractInstanceFromJson(jsValue))
-          case None => Nil
-        }
-        .map { instance =>
-          updateGoal(instance)
-        }
+        .via(migrationFlow)
         .mapAsync(1) { updatedInstance =>
           instanceRepository.store(updatedInstance)
         }
@@ -144,4 +112,40 @@ object MigrationTo200 extends MaybeStore with StrictLogging {
       Future.successful(Done)
     }
   }
+
+  /**
+    * Update the goal of the instance.
+    * @param instance The old instance.
+    * @return An instance with an updated goal.
+    */
+  def updateGoal(instance: Instance): Instance = {
+    val updatedInstanceState = if (!instance.hasReservation) {
+      instance.state.copy(goal = Goal.Running)
+    } else {
+      if (instance.isReservedTerminal) {
+        instance.state.copy(goal = Goal.Stopped)
+      } else {
+        instance.state.copy(goal = Goal.Running)
+      }
+    }
+
+    instance.copy(state = updatedInstanceState)
+  }
+
+  /**
+    * Extract instance from old format without goal attached.
+    * @param jsValue The instance as JSON.
+    * @return The parsed instance.
+    */
+  def extractInstanceFromJson(jsValue: JsValue): Instance = jsValue.as[Instance](instanceJsonReads160)
+
+  // This flow parses all provided instances and updates their goals. It does not save the updated instances.
+  val migrationFlow = Flow[Option[JsValue]]
+    .mapConcat {
+      case Some(jsValue) => List(extractInstanceFromJson(jsValue))
+      case None => Nil
+    }
+    .map { instance =>
+      updateGoal(instance)
+    }
 }
