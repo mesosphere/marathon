53bee0bc3d9337e36b941d7adefe1b3801196faf
Karsten Jeschkies <kjeschkies@mesosphere.com>
2018-07-23 12:43:14 +0200

    Stabilize and cleanup system tests with `eventually`.
    Summary:
    This is a small follow up that replaces a few retrying blocks with the
    new `eventually` matcher. It also introduces `has_len` to match the
    length of a sequence.

--- tests/system/matcher/__init__.py
+++ tests/system/matcher/__init__.py
@@ -1,12 +1,19 @@
 from .eventually import eventually
 from .property import prop
+from precisely import has_feature
 
 # py.test integration; hide these frames from tracebacks
 __tracebackhide__ = True
 
+
+def has_len(matcher):
+    return has_feature("len", len, matcher)
+
+
 __all__ = [
     "assert_that",
     "eventually",
+    "has_len",
     "prop"
 ]
 
