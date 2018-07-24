8cd6186552c0b54cc033f3256557da276cbf6063
Karsten Jeschkies <kjeschkies@mesosphere.com>
2018-07-24 14:02:53 +0200

    Grant read access to Docker permission tarball.
    Summary:
    The system tests fail on a strict EE cluster with CentOS because the
    user `nobody` has no read access to the Docker credentail tarball in
    `/home/centos`. With this change we add read access.
    
    JIRA issues: MARATHON-8324

--- tests/system/common.py
+++ tests/system/common.py
@@ -461,6 +461,10 @@ def copy_docker_credentials_file(agents, file_name='docker.tar.gz'):
         for agent in agents:
             print("Copying docker credentials to {}".format(agent))
             shakedown.copy_file_to_agent(agent, file_name)
+
+            cmd = "setfacl -m u:nobody:r /home/centos/{}".format(pattern)
+            status, stdout = shakedown.run_command_on_agent(agent, cmd)
+            assert status, "{} failed: {}".format(cmd, output)
     except Exception as e:
         print('Failed to upload {} to agent: {}'.format(file_name, agent))
         raise e
