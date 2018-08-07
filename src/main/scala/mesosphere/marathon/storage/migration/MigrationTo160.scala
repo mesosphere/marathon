ea96c07eb3dcef80b5843712e944786fc8d06b36
Alena Varkockova <varkockova.a@gmail.com>
2018-08-07 15:09:11 +0200

    Use custom reads in 160 migration

--- src/main/scala/mesosphere/marathon/storage/migration/MigrationTo160.scala
+++ src/main/scala/mesosphere/marathon/storage/migration/MigrationTo160.scala
@@ -8,12 +8,20 @@ import akka.http.scaladsl.unmarshalling.Unmarshaller
 import akka.stream.Materializer
 import akka.stream.scaladsl.Sink
 import com.typesafe.scalalogging.StrictLogging
-import mesosphere.marathon.core.instance.{Instance, Reservation}
-import mesosphere.marathon.core.instance.Instance.Id
+import mesosphere.marathon.core.condition.Condition
+import mesosphere.marathon.core.instance.{Goal, Instance, Reservation}
+import mesosphere.marathon.core.instance.Instance.{AgentInfo, Id, InstanceState}
 import mesosphere.marathon.core.storage.store.{IdResolver, PersistenceStore}
 import mesosphere.marathon.core.storage.store.impl.zk.{ZkId, ZkSerialized}
+import mesosphere.marathon.core.task.Task
+import mesosphere.marathon.raml.Raml
+import mesosphere.marathon.state.{Timestamp, UnreachableStrategy}
+import mesosphere.marathon.storage.migration.MigrationTo17.instanceStateReads160
 import mesosphere.marathon.storage.repository.InstanceRepository
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{JsValue, Json, Reads}
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -25,6 +33,44 @@ class MigrationTo160(instanceRepository: InstanceRepository, persistenceStore: P
 }
 
 object MigrationTo160 extends MaybeStore with StrictLogging {
+
+  import Instance.agentFormat
+  import Instance.tasksMapFormat
+  import mesosphere.marathon.api.v2.json.Formats.TimestampFormat
+
+  /**
+    * Read format for instance state without goal.
+    */
+  val instanceStateReads160: Reads[InstanceState] = {
+    (
+      (__ \ "condition").read[Condition] ~
+      (__ \ "since").read[Timestamp] ~
+      (__ \ "activeSince").readNullable[Timestamp] ~
+      (__ \ "healthy").readNullable[Boolean]
+    ) { (condition, since, activeSince, healthy) =>
+        InstanceState(condition, since, activeSince, healthy, Goal.Running)
+      }
+  }
+
+  /**
+    * Read format for old instance without goal.
+    */
+  val instanceJsonReads160: Reads[Instance] = {
+    (
+      (__ \ "instanceId").read[Instance.Id] ~
+      (__ \ "agentInfo").read[AgentInfo] ~
+      (__ \ "tasksMap").read[Map[Task.Id, Task]] ~
+      (__ \ "runSpecVersion").read[Timestamp] ~
+      (__ \ "state").read[InstanceState](instanceStateReads160) ~
+      (__ \ "unreachableStrategy").readNullable[raml.UnreachableStrategy] ~
+      (__ \ "reservation").readNullable[Reservation]
+    ) { (instanceId, agentInfo, tasksMap, runSpecVersion, state, maybeUnreachableStrategy, reservation) =>
+        val unreachableStrategy = maybeUnreachableStrategy.
+          map(Raml.fromRaml(_)).getOrElse(UnreachableStrategy.default())
+        new Instance(instanceId, agentInfo, state, tasksMap, runSpecVersion, unreachableStrategy, reservation)
+      }
+  }
+
   /**
     * This function traverses all instances in ZK, and moves reservation objects from tasks to the instance level.
     */
@@ -56,7 +102,7 @@ object MigrationTo160 extends MaybeStore with StrictLogging {
     import Instance.instanceJsonReads
 
     def extractInstanceAndReservationsFromJson(jsValue: JsValue): Option[(Reservation, Instance)] = {
-      val instance = jsValue.as[Instance]
+      val instance = jsValue.as[Instance](instanceJsonReads160)
       // Prior to Marathon 1.6.0, persistent volumes are supported only with apps,
       // therefore reservation objects can only appear in app instances, and since
       // an app has only one task by definition, there is only one KV pair in a taskMap
