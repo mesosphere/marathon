9a1869107bd39fb61edcdd1c4072bd7915abe818
Alena Varkockova <varkockova.a@gmail.com>
2018-08-21 13:52:29 +0200

    Test for reserved condition as well

--- src/main/scala/mesosphere/marathon/MarathonSchedulerActor.scala
+++ src/main/scala/mesosphere/marathon/MarathonSchedulerActor.scala
@@ -509,7 +509,7 @@ object TaskStatusCollector {
     instances.collect { case i @ Instance(_, Some(agentInfo), _, tasksMap @ NonEmpty(), _, _, _) => (agentInfo, tasksMap) }
       .flatMap {
         case (agentInfo, tasksMap) =>
-          tasksMap.values.withFilter(task => !task.isTerminal).map { task =>
+          tasksMap.values.withFilter(task => !task.isTerminal && !task.isReserved).map { task =>
             task.status.mesosStatus.getOrElse(initialMesosStatusFor(task, agentInfo))
           }
       }(collection.breakOut)
--- src/test/scala/mesosphere/marathon/MarathonSchedulerActorTest.scala
+++ src/test/scala/mesosphere/marathon/MarathonSchedulerActorTest.scala
@@ -141,6 +141,7 @@ class MarathonSchedulerActorTest extends AkkaUnitTest with ImplicitSender with G
         .addTaskUnknown(containerName = Some("unknown"))
         .addTaskCreated(containerName = Some("created"))
         .addTaskKilling(containerName = Some("killing"))
+        .addTaskReserved(containerName = Some("reserved"))
         .addTaskRunning(containerName = Some("running"))
         .addTaskStaging(containerName = Some("staging"))
         .addTaskStarting(containerName = Some("starting"))
@@ -156,7 +157,7 @@ class MarathonSchedulerActorTest extends AkkaUnitTest with ImplicitSender with G
       expectMsg(TasksReconciled)
 
       val nonTerminalTasks = instance.tasksMap.values.filter(!_.task.isTerminal)
-      assert(nonTerminalTasks.size == 6, "We should have 6 non-terminal tasks")
+      assert(nonTerminalTasks.size == 7, "We should have 7 non-terminal tasks")
 
       val expectedStatus: java.util.Collection[TaskStatus] = TaskStatusCollector.collectTaskStatusFor(Seq(instance)).asJava
       assert(expectedStatus.size() == 6, "We should have 6 task status")
