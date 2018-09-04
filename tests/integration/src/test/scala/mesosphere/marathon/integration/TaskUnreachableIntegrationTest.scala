c8522628f904cf8a4803d48757b4913fc05e3a67
Nikita Melkozerov <nmelkozerov@mesosphere.io>
2018-09-04 16:12:35 +0200

    more logging to find why we start 2 pod instances

--- tests/integration/src/test/scala/mesosphere/marathon/integration/TaskUnreachableIntegrationTest.scala
+++ tests/integration/src/test/scala/mesosphere/marathon/integration/TaskUnreachableIntegrationTest.scala
@@ -214,9 +214,7 @@ class TaskUnreachableIntegrationTest extends AkkaIntegrationTest with EmbeddedMa
 
       And("a new pod with a new persistent volume is scheduled")
       waitForStatusUpdates("TASK_RUNNING")
-      eventually {
         marathon.status(pod.id).value.instances should have size 1
-      }
       When("the task associated with pod becomes reachable again")
       mesosCluster.agents(0).start()
 
@@ -238,35 +236,44 @@ class TaskUnreachableIntegrationTest extends AkkaIntegrationTest with EmbeddedMa
       waitForDeployment(createResult)
       val taskId = marathon.podTasksIds(pod.id).head
       eventually { marathon.status(pod.id) should be(Stable) }
+      logger.info(s"pod status: ${marathon.status(pod.id).value}")
 
       Then("1 instance should be running")
       val status = marathon.status(pod.id)
       status should be(OK)
       status.value.instances should have size 1
       mesosCluster.agents(1).start()
+      logger.info(s"pod status: ${marathon.status(pod.id).value}")
+
 
       When("An instance is unreachable")
       mesosCluster.agents(0).stop()
       waitForEventMatching("Task is declared unreachable") {
         matchEvent("TASK_UNREACHABLE", taskId)
       }
+      logger.info(s"pod status: ${marathon.status(pod.id).value}")
+
 
       And("Pods instance is deleted")
       val instanceId = status.value.instances.head.id
       val deleteResult = marathon.deleteInstance(pod.id, instanceId, wipe = true)
       deleteResult should be(OK)
+      logger.info(s"pod status: ${marathon.status(pod.id).value}")
+
 
       Then("pod instance is erased from marathon's knowledge ")
       val knownInstanceIds = marathon.status(pod.id).value.instances.map(_.id)
       eventually {
         knownInstanceIds should not contain instanceId
       }
+      logger.info(s"pod status: ${marathon.status(pod.id).value}")
+
 
       And("a new pod with is scheduled")
       waitForStatusUpdates("TASK_RUNNING")
-      eventually {
-        marathon.status(pod.id).value.instances should have size 1
-      }
+      logger.info(s"pod status: ${marathon.status(pod.id).value}")
+      marathon.status(pod.id).value.instances should have size 1
+
 
       When("the task associated with pod becomes reachable again")
       mesosCluster.agents(0).start()
