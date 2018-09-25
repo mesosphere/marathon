2e4ade65bce60bd395dbd927992189531b340c69
Tim Harper <tharper@mesosphere.com>
2018-09-25 14:47:56 -0600

    Be conservative when creating new framework IDs
    Previously, Marathon would automatically request a new framework ID
    from Mesos if the old one was marked as torn down in Mesos, or if the
    framework ID record was removed from Zookeeper. This has led to more
    trouble than it has helped.
    
    If Marathon's framework ID has been torn down in Mesos, Marathon will
    crash on launch, with a clear message.
    
    If Marathon's framework ID was removed / is missing, Marathon will
    refuse to create a new Framework ID if there are any defined
    instances.
    
    JIRA Issues: MARATHON-8420

---
+++ src/test/scala/mesosphere/marathon/SchedulerDriverFactoryTest.scala
@@ -0,0 +1,52 @@
+package mesosphere.marathon
+
+import mesosphere.AkkaUnitTest
+import mesosphere.marathon.core.base.CrashStrategy
+import mesosphere.marathon.core.instance.{Instance, TestInstanceBuilder}
+import mesosphere.marathon.core.storage.store.impl.memory.InMemoryPersistenceStore
+import mesosphere.marathon.metrics.dummy.DummyMetrics
+import mesosphere.marathon.state.PathId._
+import mesosphere.marathon.storage.repository.{FrameworkIdRepository, InstanceRepository}
+import mesosphere.marathon.test.TestCrashStrategy
+
+import org.scalatest.Inside
+import scala.util.{Try, Failure}
+
+class SchedulerDriverFactoryTest extends AkkaUnitTest with Inside {
+  val metrics = DummyMetrics
+  val appId = "/test/foo/bla/rest".toPath
+  val instanceId = Instance.Id.forRunSpec(appId)
+
+  class Fixture {
+    val store = new InMemoryPersistenceStore(metrics)
+    val zkTimeout = implicitly[PatienceConfig].timeout
+    val frameworkIdRepository = FrameworkIdRepository.inMemRepository(store)
+    val instanceRepository = InstanceRepository.inMemRepository(store)
+    val crashStrategy = new TestCrashStrategy()
+    store.markOpen()
+  }
+
+  "getFrameworkId throws an exception and crashes if frameworkId is undefined but there are instances defined" in new Fixture {
+    instanceRepository.store(TestInstanceBuilder.emptyInstance(instanceId = instanceId))
+
+    inside(Try(
+      MesosSchedulerDriverFactory.getFrameworkId(
+        crashStrategy,
+        zkTimeout,
+        frameworkIdRepository,
+        instanceRepository))) {
+      case Failure(ex: RuntimeException) =>
+        ex.getMessage shouldBe "Refusing to create a driver"
+    }
+
+    crashStrategy.crashedReason shouldBe Some(CrashStrategy.FrameworkIdMissing)
+  }
+
+  "getFrameworkId returns None if frameworkId is undefined and instance repository is empty" in new Fixture {
+    MesosSchedulerDriverFactory.getFrameworkId(
+      crashStrategy,
+      zkTimeout,
+      frameworkIdRepository,
+      instanceRepository) shouldBe None
+  }
+}
