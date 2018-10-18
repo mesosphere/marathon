c49a4e00970f665c1f693764f3fcf86cf666e982
Tim Harper <tharper@mesosphere.com>
2018-10-18 17:04:55 -0600

    Fix Guice issue

--- src/main/scala/mesosphere/marathon/core/CoreGuiceModule.scala
+++ src/main/scala/mesosphere/marathon/core/CoreGuiceModule.scala
@@ -18,7 +18,7 @@ import mesosphere.marathon.core.health.HealthCheckManager
 import mesosphere.marathon.core.heartbeat.MesosHeartbeatMonitor
 import mesosphere.marathon.core.instance.update.InstanceChangeHandler
 import mesosphere.marathon.core.launcher.OfferProcessor
-import mesosphere.marathon.core.launchqueue.{LaunchQueue, LaunchQueueModule, LaunchStats}
+import mesosphere.marathon.core.launchqueue.{LaunchQueue, LaunchStats}
 import mesosphere.marathon.core.leadership.{LeadershipCoordinator, LeadershipModule}
 import mesosphere.marathon.core.plugin.{PluginDefinitions, PluginManager}
 import mesosphere.marathon.core.pod.PodManager
@@ -88,7 +88,7 @@ class CoreGuiceModule(cliConf: MarathonConf) extends AbstractModule {
   final def launchQueue(coreModule: CoreModule): LaunchQueue = coreModule.launchQueueModule.launchQueue
 
   @Provides @Singleton
-  final def offerStats(launchQueueModule: LaunchQueueModule): LaunchStats = launchQueueModule.launchStats
+  final def offerStats(coreModule: CoreModule): LaunchStats = coreModule.launchQueueModule.launchStats
 
   @Provides @Singleton
   final def podStatusService(appInfoModule: AppInfoModule): PodStatusService = appInfoModule.podStatusService
