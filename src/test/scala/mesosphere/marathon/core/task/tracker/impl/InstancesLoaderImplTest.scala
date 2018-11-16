20a311ecb723ea1bea0bffc2320d099d2e80e944
Karsten Jeschkies <kjeschkies@mesosphere.com>
2018-11-16 17:22:51 +0100

    Test if instances with unknown run spec are ignored.

--- src/test/scala/mesosphere/marathon/core/task/tracker/impl/InstancesLoaderImplTest.scala
+++ src/test/scala/mesosphere/marathon/core/task/tracker/impl/InstancesLoaderImplTest.scala
@@ -19,7 +19,7 @@ class InstancesLoaderImplTest extends AkkaUnitTest {
     def verifyNoMoreInteractions(): Unit = noMoreInteractions(instanceRepository)
   }
   "InstanceLoaderImpl" should {
-    "loading no tasks" in {
+    "load no instances" in {
       val f = new Fixture
 
       Given("no tasks")
@@ -38,7 +38,7 @@ class InstancesLoaderImplTest extends AkkaUnitTest {
       f.verifyNoMoreInteractions()
     }
 
-    "loading multiple tasks for multiple apps" in {
+    "load multiple instances for multiple apps" in {
       val f = new Fixture
 
       Given("instances for multiple runSpecs")
@@ -70,6 +70,40 @@ class InstancesLoaderImplTest extends AkkaUnitTest {
       Then("the resulting data is correct")
       // we do not need to verify the mocked calls because the only way to get the data is to perform the calls
       val expectedData = InstanceTracker.InstancesBySpec.forInstances(app1Instance1, app1Instance2, app2Instance1)
+      loaded.futureValue should equal(expectedData)
+    }
+
+    "ignore instances without a run spec" in {
+      val f = new Fixture
+
+      Given("instances for multiple runSpecs")
+      val app1Id = PathId("/app1")
+      val app1Instance1 = TestInstanceBuilder.newBuilder(app1Id).getInstance()
+      val app1Instance2 = TestInstanceBuilder.newBuilder(app1Id).getInstance()
+      val app1 = app1Instance1.runSpec
+      f.groupRepository.runSpecVersion(eq(app1Id), eq(app1.version.toOffsetDateTime))(any) returns Future.successful(Some(app1))
+
+      val app2Id = PathId("/app2")
+      val app2Instance1 = TestInstanceBuilder.newBuilder(app2Id).getInstance()
+      f.groupRepository.runSpecVersion(eq(app2Id), eq(app2Instance1.runSpecVersion.toOffsetDateTime))(any) returns Future.successful(None)
+
+      val instances = Seq(
+        StateInstance.fromCoreInstance(app1Instance1),
+        StateInstance.fromCoreInstance(app1Instance2),
+        StateInstance.fromCoreInstance(app2Instance1)
+      )
+
+      f.instanceRepository.ids() returns Source(instances.map(_.instanceId)(collection.breakOut))
+      for (instance <- instances) {
+        f.instanceRepository.get(instance.instanceId) returns Future.successful(Some(instance))
+      }
+
+      When("load is called")
+      val loaded = f.loader.load()
+
+      Then("the resulting data ")
+      // we do not need to verify the mocked calls because the only way to get the data is to perform the calls
+      val expectedData = InstanceTracker.InstancesBySpec.forInstances(app1Instance1, app1Instance2)
       loaded.futureValue should equal(expectedData)
     }
   }
