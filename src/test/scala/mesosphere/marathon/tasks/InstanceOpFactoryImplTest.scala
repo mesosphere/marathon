24e1b2f0939d617aca3a11153e48bdd85aae4031
Karsten Jeschkies <kjeschkies@mesosphere.com>
2018-11-27 11:59:35 +0100

    Remove unused import.

--- src/test/scala/mesosphere/marathon/tasks/InstanceOpFactoryImplTest.scala
+++ src/test/scala/mesosphere/marathon/tasks/InstanceOpFactoryImplTest.scala
@@ -8,7 +8,6 @@ import mesosphere.marathon.core.instance.{Instance, LocalVolumeId, TestInstanceB
 import mesosphere.marathon.core.launcher.impl.InstanceOpFactoryImpl
 import mesosphere.marathon.core.launcher.{InstanceOp, InstanceOpFactory, OfferMatchResult}
 import mesosphere.marathon.core.task.Task
-import mesosphere.marathon.core.task.Task.TaskIdWithIncarnation
 import mesosphere.marathon.core.task.state.{AgentTestDefaults, NetworkInfo}
 import mesosphere.marathon.core.task.tracker.InstanceTracker
 import mesosphere.marathon.metrics.Metrics
