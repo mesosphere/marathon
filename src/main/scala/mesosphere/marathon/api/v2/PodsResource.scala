9b1fd4c2f21bd4bf36d1e29cd8c5cc1c146be706
Karsten Jeschkies <kjeschkies@mesosphere.com>
2018-11-30 10:30:37 +0100

    Respond with pod status as defined in the API.
    Summary:
    The RAML definition for the Pod instance killing defines a pod status
    for the return type. This change will use the RAML to return exactly
    that.

--- src/main/scala/mesosphere/marathon/api/v2/PodsResource.scala
+++ src/main/scala/mesosphere/marathon/api/v2/PodsResource.scala
@@ -280,7 +280,12 @@ class PodsResource @Inject() (
       validateOrThrow(instanceId)
       val parsedInstanceId = Instance.Id.fromIdString(instanceId)
       val instances = await(taskKiller.kill(id, _.filter(_.instanceId == parsedInstanceId), wipe))
-      instances.headOption.fold(unknownTask(instanceId))(instance => ok(jsonString(instance)))
+      instances.headOption match {
+        case None => (unknownTask(instanceId))
+        case Some(instance) =>
+          val raml = Raml.toRaml(pod -> instance)
+          ok(Json.stringify(Json.toJson(raml)))
+      }
     }
   }
 
