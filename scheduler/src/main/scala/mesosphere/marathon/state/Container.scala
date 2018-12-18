a3c5b632fffd844bf107cf1e9d44fef19152a5b5
Karsten Jeschkies <kjeschkies@mesosphere.com>
2018-12-18 17:25:14 +0100

    Move port mapping.

--- scheduler/src/main/scala/mesosphere/marathon/state/Container.scala
+++ scheduler/src/main/scala/mesosphere/marathon/state/Container.scala
@@ -24,3 +24,25 @@ trait Container extends Product with Serializable {
 
   def copyWith(portMappings: Seq[PortMapping] = portMappings, volumes: Seq[VolumeWithMount[Volume]] = volumes): Container
 }
+
+object Container {
+
+  /**
+    * @param containerPort The container port to expose
+    * @param hostPort      The host port to bind
+    * @param servicePort   The well-known port for this service
+    * @param protocol      Layer 4 protocol to expose (i.e. "tcp", "udp" or "udp,tcp" for both).
+    * @param name          Name of the service hosted on this port.
+    * @param labels        This can be used to decorate the message with metadata to be
+    *                      interpreted by external applications such as firewalls.
+    * @param networkNames  Specifies one or more container networks, by name, for which this PortMapping applies.
+    */
+  case class PortMapping(
+      containerPort: Int = AppDefinition.RandomPortValue,
+      hostPort: Option[Int] = None, // defaults to HostPortDefault for BRIDGE mode, None for USER mode
+      servicePort: Int = AppDefinition.RandomPortValue,
+      protocol: String = PortMapping.TCP,
+      name: Option[String] = None,
+      labels: Map[String, String] = Map.empty[String, String],
+      networkNames: Seq[String] = Nil)
+}
--- src/main/scala/mesosphere/marathon/state/Container.scala
+++ src/main/scala/mesosphere/marathon/state/Container.scala
@@ -37,26 +37,6 @@ object Container {
     }
   }
 
-  /**
-    * @param containerPort The container port to expose
-    * @param hostPort      The host port to bind
-    * @param servicePort   The well-known port for this service
-    * @param protocol      Layer 4 protocol to expose (i.e. "tcp", "udp" or "udp,tcp" for both).
-    * @param name          Name of the service hosted on this port.
-    * @param labels        This can be used to decorate the message with metadata to be
-    *                      interpreted by external applications such as firewalls.
-    * @param networkNames  Specifies one or more container networks, by name, for which this PortMapping applies.
-    */
-  case class PortMapping(
-      containerPort: Int = AppDefinition.RandomPortValue,
-      hostPort: Option[Int] = None, // defaults to HostPortDefault for BRIDGE mode, None for USER mode
-      servicePort: Int = AppDefinition.RandomPortValue,
-      protocol: String = PortMapping.TCP,
-      name: Option[String] = None,
-      labels: Map[String, String] = Map.empty[String, String],
-      networkNames: Seq[String] = Nil
-  )
-
   object PortMapping {
     val TCP = raml.NetworkProtocol.Tcp.value
     val UDP = raml.NetworkProtocol.Udp.value
