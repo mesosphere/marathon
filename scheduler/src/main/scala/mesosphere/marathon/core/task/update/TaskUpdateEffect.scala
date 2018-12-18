2d75ed57b9c7aebea0b07bb3a4d1b5441f88c939
Karsten Jeschkies <kjeschkies@mesosphere.com>
2018-12-18 17:18:00 +0100

    Move Task.

---
+++ scheduler/src/main/scala/mesosphere/marathon/core/task/update/TaskUpdateEffect.scala
@@ -0,0 +1,7 @@
+package mesosphere.marathon
+package core.task.update
+
+/**
+  * Enumeration defining the effect that an applied Mesos status update has to a task.
+  */
+trait TaskUpdateEffect extends Product with Serializable
--- src/main/scala/mesosphere/marathon/core/task/update/TaskUpdateEffect.scala
+++ src/main/scala/mesosphere/marathon/core/task/update/TaskUpdateEffect.scala
@@ -3,11 +3,6 @@ package core.task.update
 
 import mesosphere.marathon.core.task.Task
 
-/**
-  * Enumeration defining the effect that an applied Mesos status update has to a task.
-  */
-sealed trait TaskUpdateEffect extends Product with Serializable
-
 object TaskUpdateEffect {
   case object Noop extends TaskUpdateEffect
   case class Update(newState: Task) extends TaskUpdateEffect
