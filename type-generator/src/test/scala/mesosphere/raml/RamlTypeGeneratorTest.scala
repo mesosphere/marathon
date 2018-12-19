2646f056c95345e9439b59da4e8a1fc0bbbf17cd
Karsten Jeschkies <kjeschkies@mesosphere.com>
2018-12-19 11:37:07 +0100

    Introduce matcher for easier debugging.

--- type-generator/src/test/scala/mesosphere/raml/RamlTypeGeneratorTest.scala
+++ type-generator/src/test/scala/mesosphere/raml/RamlTypeGeneratorTest.scala
@@ -5,16 +5,46 @@ import java.net.URI
 import java.nio.file.Paths
 
 import com.github.difflib.DiffUtils
-import com.github.difflib.patch.{ChangeDelta, DeleteDelta, InsertDelta}
+import com.github.difflib.patch.{AbstractDelta, ChangeDelta, DeleteDelta, InsertDelta}
 import org.raml.v2.api.RamlModelBuilder
 import org.scalatest.Matchers._
+import org.scalatest.matchers.{BeMatcher, MatchResult}
 import org.scalatest.{GivenWhenThen, WordSpec}
 
 
-import collection.JavaConverters._
+import scala.collection.JavaConverters._
 import scala.collection.convert.ImplicitConversionsToScala._
+import scala.io.Source
 
 class RamlTypeGeneratorTest extends WordSpec with GivenWhenThen {
+
+
+  /**
+    * A Scalatest be matcher that compares a string to a reference. It will print the line diffs if there are deltas.
+    * @param ref Path to the reference in the Jar resources.
+    */
+  class CodeMatcher(ref: String) extends BeMatcher[String] {
+    def apply(original: String) = {
+      val refLines = Source.fromResource(ref).getLines().toList.asJava
+      val deltas = DiffUtils.diff(original.lines.toList.asJava, refLines).getDeltas.asScala
+
+      MatchResult(
+        deltas.isEmpty,
+        s"The generated code differed from reference $ref:\n\n${showDiff(deltas)}",
+        s"The generated code was equal to reference $ref"
+      )
+    }
+
+    def showDiff(deltas: Iterable[AbstractDelta[_]]): String = {
+      deltas.flatMap {
+        case d: DeleteDelta[_] => d.getSource().getLines().map { l => s"\t- $l" }
+        case i: InsertDelta[_] => i.getSource().getLines().map { l => s"\t+ $l" }
+        case c: ChangeDelta[_] => c.getSource().getLines().map { l => s"\t- $l" } ++ c.getTarget().getLines().map { l => s"\t+ $l" }
+      }.mkString("\n")
+    }
+  }
+
+  def equalToReference(ref: String) = new CodeMatcher(ref)
 
   "RamlTypeGenerator" should {
     "produce types" in {
@@ -29,19 +59,7 @@ class RamlTypeGeneratorTest extends WordSpec with GivenWhenThen {
       val typesAsStr: Map[String, String] = types.mapValues(treehugger.forest.treeToString(_))
 
       Then("the type equals our reference")
-      val original = typesAsStr("Foo").lines.toList.asJava
-      val refLines = scala.io.Source.fromResource("references/Foo.scala").getLines().toList.asJava
-      val deltas = DiffUtils.diff(original, refLines).getDeltas.asScala
-      deltas.foreach {
-        case d: DeleteDelta[_] => d.getSource().getLines().foreach { l => println(s"- $l") }
-        case i: InsertDelta[_] => i.getSource().getLines().foreach { l => println(s"+ $l") }
-        case c: ChangeDelta[_] =>
-          c.getSource().getLines().foreach { l => println(s"- $l") }
-          c.getTarget().getLines().foreach { l => println(s"+ $l") }
-        case e => println(e)
-      }
-      val ref = scala.io.Source.fromResource("references/Foo.scala").mkString
-      typesAsStr("Foo") should be(ref)
+      typesAsStr("Foo") should be(equalToReference("references/Foo.scala"))
     }
   }
 }
