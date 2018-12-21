d18fd407aa3a1c0df9c2cda433663339278be932
Karsten Jeschkies <k@jeschkies.xyz>
2018-12-21 17:12:21 +0100

    Test type generator against legacy reference.  (#6747)
    Summary:
    This will easy verifying changes such as #6743 do not break our current
    generator.

--- type-generator/src/test/scala/mesosphere/raml/RamlTypeGeneratorTest.scala
+++ type-generator/src/test/scala/mesosphere/raml/RamlTypeGeneratorTest.scala
@@ -6,17 +6,18 @@ import java.nio.file.Paths
 
 import com.github.difflib.DiffUtils
 import com.github.difflib.patch.{AbstractDelta, ChangeDelta, DeleteDelta, InsertDelta}
+import com.typesafe.scalalogging.StrictLogging
 import org.raml.v2.api.RamlModelBuilder
+import org.raml.v2.api.loader.FileResourceLoader
 import org.scalatest.Matchers._
 import org.scalatest.matchers.{BeMatcher, MatchResult}
-import org.scalatest.{GivenWhenThen, WordSpec}
-
+import org.scalatest.{GivenWhenThen, Inspectors, WordSpec}
 
 import scala.collection.JavaConverters._
 import scala.collection.convert.ImplicitConversionsToScala._
 import scala.io.Source
 
-class RamlTypeGeneratorTest extends WordSpec with GivenWhenThen {
+class RamlTypeGeneratorTest extends WordSpec with GivenWhenThen with Inspectors with StrictLogging {
 
 
   /**
@@ -25,13 +26,13 @@ class RamlTypeGeneratorTest extends WordSpec with GivenWhenThen {
     */
   class CodeMatcher(ref: String) extends BeMatcher[String] {
     def apply(original: String) = {
-      val refLines = Source.fromResource(ref).getLines().toList.asJava
+      val refLines = ref.lines.toList.asJava
       val deltas = DiffUtils.diff(original.lines.toList.asJava, refLines).getDeltas.asScala
 
       MatchResult(
         deltas.isEmpty,
-        s"The generated code differed from reference $ref:\n\n${showDiff(deltas)}",
-        s"The generated code was equal to reference $ref"
+        s"The generated code differed from reference:\n\n${showDiff(deltas)}",
+        "The generated code was equal to reference."
       )
     }
 
@@ -47,19 +48,43 @@ class RamlTypeGeneratorTest extends WordSpec with GivenWhenThen {
   def equalToCode(ref: String) = new CodeMatcher(ref)
 
   "RamlTypeGenerator" should {
-    "produce types" in {
+    "produce a primitive type" in {
       Given("A reference model")
-      val file = new File("src/test/resources/test.raml")
+      val file = new File("src/test/resources/api.raml")
       assert(file.exists(), "file does not exist")
-      val models = Seq(new RamlModelBuilder().buildApi(file))
-      models should have size(1)
+      val models = Vector(new RamlModelBuilder().buildApi(file))
 
       When("the type is generated")
-      val types = RamlTypeGenerator(models.toVector, "mesosphere.raml.test")
+      val types = RamlTypeGenerator(models, "mesosphere.raml.test")
       val typesAsStr: Map[String, String] = types.mapValues(treehugger.forest.treeToString(_))
 
       Then("the type equals our reference")
-      typesAsStr("Foo") should be(equalToCode("references/Foo.scala"))
+      typesAsStr("Foo") should be(equalToCode(Source.fromResource("references/Foo.scala").mkString))
+    }
+
+    "produce the same type as the legacy generator" in {
+      Given("The current API model")
+      val file = new File("../docs/docs/rest-api/public/api/api.raml")
+      assert(file.exists(), "file does not exist")
+      val model = new RamlModelBuilder(new FileResourceLoader(".")).buildApi(file)
+      if (model.hasErrors) {
+        model.getValidationResults.asScala.foreach { error =>
+          sys.error(error.toString)
+        }
+      }
+
+      When("the types are generated")
+      val types = RamlTypeGenerator(Vector(model), "mesosphere.raml.test")
+      val typesAsStr: Map[String, String] = types.mapValues(treehugger.forest.treeToString(_))
+
+      Then("they are the same as the legacy types")
+      val legacyTypes = LegacyRamlTypeGenerator(Vector(model), "mesosphere.raml.test")
+      val legacyTypesAsStr: Map[String, String] = legacyTypes.mapValues(treehugger.forest.treeToString(_))
+
+      forEvery(typesAsStr.keys) { typeName =>
+        legacyTypesAsStr should contain key(typeName)
+        typesAsStr(typeName) should be(equalToCode(legacyTypesAsStr(typeName)))
+      }
     }
   }
 }
