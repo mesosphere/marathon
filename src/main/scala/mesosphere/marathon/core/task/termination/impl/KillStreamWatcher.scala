fc014f9fabe7242ddcdf9f1318127294985ed66f
Tim Harper <tharper@mesosphere.com>
2019-01-16 16:53:16 -0700

    Add documentation

--- src/main/scala/mesosphere/marathon/core/task/termination/impl/KillStreamWatcher.scala
+++ src/main/scala/mesosphere/marathon/core/task/termination/impl/KillStreamWatcher.scala
@@ -14,14 +14,45 @@ import mesosphere.marathon.core.task.tracker.InstanceTracker
 import scala.concurrent.Future
 
 object KillStreamWatcher extends StrictLogging {
+  /**
+    * We have two separate predicates for determining instance terminality: tasks have since been restarted, or instance
+    * is decommissioned.
+    */
   private type TerminalPredicate = (Instance, Set[Task.Id]) => Boolean
 
+  /**
+    * Predicate used to determine whether or not the tasks associated with an instance have been killed.
+    *
+    * When a process sends some kill request, it's usually in the context of killing the tasks associated with an
+    * instance at some given point in time. We count this kill as successful if we see that the taskIds have changed,
+    * or if the instance itself is in a state we consider terminal.
+    */
   private[impl] val whenTerminalOrReplaced: TerminalPredicate = (instance: Instance, taskIds: Set[Task.Id]) =>
     considerTerminal(instance.state.condition) || instance.tasksMap.values.forall(t => !taskIds(t.taskId))
 
+  /**
+    * Predicate used to determine whether or not an instance has been decommissioned.
+    *
+    * When an instance has been marked as goal decommissioned, it's safe to assume that it is never coming back, since
+    * it's illegal to change an instance goal from Decommissioned back to running. As such, we assume an instance is
+    * decommissioned as soon as it is both marked as goal decommissioned, and the instance is considered terminal.
+    *
+    * Were we to only wait for the instance to become terminal, and not for the goal to also be decommissioned, then we
+    * could conclude too early that an instance is decommissioned, as it is possible that some random restart could
+    * precede the receipt of the update goal command.
+    */
   private[impl] val whenTerminalAndDecommissioned: TerminalPredicate = (instance: Instance, _: Set[Task.Id]) =>
     instance.state.goal == Goal.Decommissioned && considerTerminal(instance.state.condition)
 
+  /**
+    * Given instance updates, and a set of instances, emit a set of instanceIds that are not yet expunged or considered
+    * terminal. Excludes instances that are expunged or considered terminal in the initial snapshot.
+    *
+    * @param instanceUpdates InstanceTracker instance state subscription stream
+    * @param instances The instances in question to watch
+    * @param predicate The mechanism to judge instance terminality; are we watching for tasks to be killed, or instance to be decommissioned.
+    * @return
+    */
   private[impl] def emitPendingTerminal(instanceUpdates: InstanceTracker.InstanceUpdates, instances: Iterable[Instance], predicate: TerminalPredicate): Source[Set[Instance.Id], NotUsed] = {
 
     val instancesMap: Map[Instance.Id, Set[Task.Id]] =
@@ -51,7 +82,8 @@ object KillStreamWatcher extends StrictLogging {
   }
 
   /**
-    * Wait until either the tasks associated with the instances are terminal, or have been replaced with new tasks.
+    * Wait until either the instances are expunged, the tasks associated with the instances are terminal, or have been
+    * replaced with new tasks.
     */
   def watchForKilledInstances(instanceUpdates: InstanceTracker.InstanceUpdates, instances: Iterable[Instance])(implicit materializer: Materializer): Future[Done] = {
     KillStreamWatcher.
@@ -63,7 +95,8 @@ object KillStreamWatcher extends StrictLogging {
   /**
     * Wait for instances to be decommissioned and expunged from the instance tracker.
     *
-    * Since goals cannot be transitioned from decommissioned to running, it is safe to call this watcher method before or after the goal update is processed.
+    * Since goals cannot be transitioned from decommissioned to running, it is safe to call this watcher method before
+    * or after the goal update is processed.
     *
     * @param instanceUpdates InstanceTracker instanceUpdates feed
     * @param instanceIds Instance ids to wait to be expunged from the instance tracker due to decomissioning
