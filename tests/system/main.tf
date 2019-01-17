28712e7d8518b2488d93a7101071103dc765ae9d
Karsten Jeschkies <kjeschkies@mesosphere.com>
2019-01-17 16:44:31 +0100

    Use the official Terraform module.

---
+++ tests/system/main.tf
@@ -0,0 +1,31 @@
+module "dcos" {
+  source  = "dcos-terraform/dcos/aws"
+  version = "~> 0.1"
+
+  cluster_name = "${var.cluster_name}"
+
+  num_masters        = "3"
+  num_private_agents = "3"
+  num_public_agents  = "1"
+
+  dcos_version = "1.12.0"
+
+  dcos_variant = "${var.dcos_variant}"
+
+  dcos_aws_region = "us-west-2"
+
+  dcos_instance_os             = "centos_7.5"
+  bootstrap_instance_type      = "m4.larger"
+  masters_instance_type        = "m4.large"
+  private_agents_instance_type = "m4.large"
+  public_agents_instance_type  = "m4.large"
+}
+
+variable "dcos_variant" {
+  description = "Specifies whether this is an open, permissive or strict cluster."
+  default     = "open"
+}
+
+variable "cluster_name" {
+  description = "The name of the created cluster."
+}
