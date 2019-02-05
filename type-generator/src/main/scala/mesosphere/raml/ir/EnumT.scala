5953c2bfd18c37fdad3f5f960091019beb9c732c
Karsten Jeschkies <kjeschkies@mesosphere.com>
2019-02-05 14:26:47 +0100

    Use simple getter instead of `J` suffix.

--- type-generator/src/main/scala/mesosphere/raml/ir/EnumT.scala
+++ type-generator/src/main/scala/mesosphere/raml/ir/EnumT.scala
@@ -5,6 +5,6 @@ import scala.collection.JavaConverters._
 
 case class EnumT(@BeanProperty name: String, values: Set[String], default: Option[String], comments: Seq[String]) extends GeneratedClass {
   val sortedValues = values.toSeq.sorted
-  @BeanProperty val sortedValuesJ = values.toSeq.sorted.asJava
+  def getSortedValues = values.toSeq.sorted.asJava
   override def toString: String = s"Enum($name, $values)"
 }
--- type-generator/src/test/scala/mesosphere/raml/backend/liquid/EnumVisitorTest.scala
+++ type-generator/src/test/scala/mesosphere/raml/backend/liquid/EnumVisitorTest.scala
@@ -6,6 +6,37 @@ import org.scalatest.{GivenWhenThen, Matchers, WordSpec}
 
 class EnumVisitorTest extends WordSpec with GivenWhenThen with Matchers with StrictLogging {
 
+  val expected: String =
+    """// format: OFF
+      |package mesosphere.marathon.raml
+      |
+      |sealed trait myEnum extends Product with Serializable with RamlGenerated {
+      |  val value: String
+      |  override def toString: String = value
+      |}
+      |
+      |object myEnum {
+      |  case object Bar extends myEnum {
+      |    val value = BAR
+      |  }case object Foo extends myEnum {
+      |    val value = FOO
+      |  }
+      |  implicit object playJsonFormat extends play.api.libs.json.Format[myEnum] {
+      |    def reads(json: play.api.libs.json.JsValue): play.api.libs.json.JsResult[myEnum] =  {
+      |      json match {
+      |        case play.api.libs.json.JsString(s) => s.toLowerCase match {
+      |          case "bar" => play.api.libs.json.JsSuccess(Bar)
+      |          case "foo" => play.api.libs.json.JsSuccess(Foo)
+      |          case _ => play.api.libs.json.JsError(play.api.libs.json.JsonValidationError("error.unknown.enum.literal", "myEnum ([BAR, FOO])"))
+      |        }
+      |        case _ => play.api.libs.json.JsError(play.api.libs.json.JsonValidationError("error.unknown.enum.literal", "myEnum ([BAR, FOO])"))
+      |      }
+      |    }
+      |    def writes(o: myEnum): play.api.libs.json.JsValue = play.api.libs.json.JsString(o.value)
+      |  }
+      |}
+      |""".stripMargin
+
   "EnumVisitor" should {
     "generate the enum base trait" in {
       Given("An enum type")
@@ -15,7 +46,7 @@ class EnumVisitorTest extends WordSpec with GivenWhenThen with Matchers with Str
       val out = EnumVisitor.visit(enumT)
 
       Then("the source includes the base trait")
-      out.mkString("/n") should be("hello")
+      out.mkString("/n") should be(expected)
     }
   }
 }
