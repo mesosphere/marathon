8006642ec92e1aa5f7435936599a56b343162ea0
Tim Harper <tharper@mesosphere.io>
2019-02-26 15:08:16 -0700

    Handle wrapped requests
    Backport of f998588
    
    * Also, improve the error message if we still "get there"
    
    JIRA Issues: MARATHON-8587

--- src/main/scala/mesosphere/marathon/api/HttpTransferMetricsHandler.scala
+++ src/main/scala/mesosphere/marathon/api/HttpTransferMetricsHandler.scala
@@ -2,16 +2,15 @@ package mesosphere.marathon
 package api
 
 import com.typesafe.scalalogging.StrictLogging
-import javax.servlet._
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
-import javax.servlet.{AsyncEvent, AsyncListener}
+import javax.servlet.{AsyncEvent, AsyncListener, ServletRequest, ServletRequestWrapper}
 import javax.ws.rs.core.HttpHeaders
+import mesosphere.marathon.api.HttpTransferMetricsHandler._
 import mesosphere.marathon.metrics.{Counter, Metrics, ServiceMetric}
-import org.eclipse.jetty.server.Request
 import org.eclipse.jetty.server.handler.AbstractHandler
-import org.eclipse.jetty.server.{HttpChannelState, _}
+import org.eclipse.jetty.server.{AsyncContextEvent, HttpChannelState, Request}
 
-import HttpTransferMetricsHandler._
+import scala.annotation.tailrec
 
 /* Container for HTTP Metrics
  */
@@ -110,9 +109,9 @@ object HttpTransferMetricsHandler {
     * these metrics are tracked another way.
     */
   def exclude(request: ServletRequest) = {
-    val state = request match {
+    val state = unwrapRequest(request) match {
       case r: Request => r.getHttpChannelState.getState
-      case _ => throw new IllegalStateException("We should never get here")
+      case other => throw new IllegalStateException(s"Expected an org.eclipse.jetty.server.Request, got ${other.getClass} instead")
     }
 
     state match {
@@ -122,4 +121,12 @@ object HttpTransferMetricsHandler {
         request.setAttribute(SkipMetricsKey, "yes")
     }
   }
+
+  @tailrec private def unwrapRequest(request: ServletRequest): ServletRequest =
+    request match {
+      case r: ServletRequestWrapper =>
+        unwrapRequest(r.getRequest())
+      case other =>
+        other
+    }
 }
