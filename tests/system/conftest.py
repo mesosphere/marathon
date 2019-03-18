1a1f25df168fce4a356b9b0493f15662a8044ed4
Karsten Jeschkies <kjeschkies@mesosphere.com>
2019-03-18 16:49:36 +0100

    Collect report in data frame.

--- tests/system/conftest.py
+++ tests/system/conftest.py
@@ -3,6 +3,8 @@ import logging.config
 
 import json
 import os
+import pandas as pd
+import re
 import time
 
 
@@ -15,16 +17,44 @@ class PandasReport(object):
     def __init__(self, output):
         file = os.path.expanduser(os.path.expandvars(output))
         self._output_file = os.path.abspath(file)
-        self._report = list()
+        self._report = pd.DataFrame(columns=['build_id', 'testsuite', 'testcase', 'status', 'error', 'timestamp'])
+        self._py_ext_re = re.compile(r"\.py$")
+        self._build_id = 0
+
+    def mangle_test_address(self, address):
+        """Forked from https://github.com/pytest-dev/pytest/blob/master/src/_pytest/junitxml.py"""
+        path, possible_open_bracket, params = address.partition("[")
+        names = path.split("::")
+        try:
+            names.remove("()")
+        except ValueError:
+            pass
+        # convert file path to dotted path
+        names[0] = names[0].replace("/", ".")
+        names[0] = self._py_ext_re.sub("", names[0])
+        # put any params back
+        names[-1] += possible_open_bracket + params
+
+        return names
 
     def _save_report(self):
-        with open(self._output_file, 'w') as fp:
-            json.dump(self._report, fp)
+        self._report.set_index(['build_id', 'testsuite', 'testcase']).to_json(self._output_file)
 
     # Pytest hooks
 
     def pytest_runtest_logreport(self, report):
-        self._report.append({'testsuite': report.nodeid, 'testcase': 'unkown', 'status': report.outcome})
+        names = self.mangle_test_address(report.nodeid)
+        testsuite = ".".join(names[:-1])
+        testcase = names[-1]
+        self._report = self._report.append({
+            'build_id': self._build_id,
+            'testsuite': testsuite,
+            'testcase': testcase,
+            'status': report.outcome,
+            'error': 'unkown error',
+            'timesamp': self.suite_start_time
+        }, ignore_index=True)
+        self._build_id += 1 # TODO: save only once
 
     def pytest_collectreport(self, report):
         # if report.failed:
@@ -39,9 +69,16 @@ class PandasReport(object):
         pass
 
 
+def pytest_addoption(parser):
+    group = parser.getgroup('terminal reporting')
+    group.addoption('--pandas', action='store', dest='pandaspath',
+                    metavar='path', default=None,
+                    help='create Pandas dataframe report file at given path.')
+
+
 def pytest_configure(config):
     logging.config.fileConfig('logging.conf')
 
-    output = config.getoption('pandas')
+    output = config.getoption('pandaspath')
     config._pandas = PandasReport(output)
     config.pluginmanager.register(config._pandas)
