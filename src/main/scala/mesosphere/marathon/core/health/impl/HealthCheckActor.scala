dccf67115926bcf0bbaa39a0f075fef294eb33cb
Nikita Melkozerov <nmelkozerov@mesosphere.io>
2019-03-19 17:34:59 +0100

    Prototype of the new Http Health Check actor

--- src/main/scala/mesosphere/marathon/core/health/impl/HealthCheckActor.scala
+++ src/main/scala/mesosphere/marathon/core/health/impl/HealthCheckActor.scala
@@ -1,23 +1,29 @@
 package mesosphere.marathon
 package core.health.impl
 
-import akka.Done
+import akka.{ Done, NotUsed }
 import akka.actor.{ Actor, ActorRef, Cancellable, Props }
 import akka.event.EventStream
-import akka.stream.Materializer
+import akka.http.scaladsl.{ ConnectionContext, Http }
+import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.model.{ HttpRequest, HttpResponse, headers }
+import akka.stream.{ FlowShape, Materializer }
+import akka.stream.scaladsl.{ Broadcast, Flow, GraphDSL, Merge, Sink, Source }
 import com.typesafe.scalalogging.StrictLogging
 import mesosphere.marathon.core.event._
 import mesosphere.marathon.core.health._
 import mesosphere.marathon.core.health.impl.AppHealthCheckActor.{ ApplicationKey, HealthCheckStatusChanged, InstanceKey, PurgeHealthCheckStatuses }
 import mesosphere.marathon.core.health.impl.HealthCheckActor._
+import mesosphere.marathon.core.health.impl.HealthCheckWorker._
 import mesosphere.marathon.core.instance.Instance
 import mesosphere.marathon.core.task.termination.{ KillReason, KillService }
 import mesosphere.marathon.core.task.tracker.InstanceTracker
 import mesosphere.marathon.state.{ AppDefinition, Timestamp }
 
-import scala.concurrent.Future
+import scala.collection.concurrent.TrieMap
+import scala.concurrent.{ ExecutionContext, Future }
 import scala.concurrent.duration._
-import scala.util.{ Failure, Success }
+import scala.util.{ Failure, Success, Try }
 
 private[health] class HealthCheckActor(
     app: AppDefinition,
@@ -56,7 +62,9 @@ private[health] class HealthCheckActor(
     )
 
   override def postStop(): Unit = {
-    nextScheduledCheck.forall { _.cancel() }
+    nextScheduledCheck.forall {
+      _.cancel()
+    }
     logger.info(
       "Stopped health check actor for app [{}] version [{}] and healthCheck [{}]",
       app.id,
@@ -264,13 +272,350 @@ object HealthCheckActor {
 
   // self-sent every healthCheck.intervalSeconds
   case object Tick
+
   case class GetInstanceHealth(instanceId: Instance.Id)
+
   case object GetAppHealth
+
   case class ScheduleNextHealthCheck(interval: Option[FiniteDuration] = None)
 
   case class AppHealth(health: Seq[Health])
 
   case class InstanceHealth(result: HealthResult, health: Health, newHealth: Health)
+
   case class InstancesUpdate(version: Timestamp, instances: Seq[Instance])
+
+}
+
+private class MarathonHttpHealthCheckActor(
+    appDef: AppDefinition,
+    appHealthCheckActor: ActorRef,
+    killService: KillService,
+    healthCheck: MarathonHttpHealthCheck,
+    instanceTracker: InstanceTracker,
+    eventBus: EventStream)(implicit mat: Materializer, ec: ExecutionContext) extends Actor with StrictLogging {
+
+  val healthByInstanceId = TrieMap.empty[Instance.Id, Health]
+
+  import context.system
+
+  val superPool = Http().superPool[Instance](connectionContext = ConnectionContext.https(disabledSslContext, sslConfig = Some(disabledSslConfig())))
+
+  override def preStart(): Unit = {
+    logger.info(
+      "Starting health check actor for app [{}] version [{}] and healthCheck [{}]",
+      appDef.id,
+      appDef.version,
+      healthCheck
+    )
+    //Start health checking not after the default first health check
+    val start = math.min(healthCheck.interval.toMillis, HealthCheck.DefaultFirstHealthCheckAfter.toMillis).millis
+
+    Source
+      .tick(start, healthCheck.interval, Tick)
+      .map { t =>
+        logger.debug(
+          "Scheduling next health check for app [{}] version [{}] and healthCheck [{}]",
+          appDef.id,
+          appDef.version,
+          healthCheck
+        )
+        t
+      }
+      .mapAsync(1) { _ =>
+        instanceTracker.specInstances(appDef.id)
+      }
+      .map { instances =>
+        InstancesUpdate(version = appDef.version, instances = instances)
+      }
+      .collect {
+        case InstancesUpdate(version, instances) if version == appDef.version =>
+          purgeStatusOfDoneInstances(instances)
+          logger.debug("Dispatching health check jobs to workers")
+          instances.collect {
+            case instance if instance.runSpecVersion == appDef.version && instance.isRunning =>
+              logger.debug("Dispatching health check job for {}", instance.instanceId)
+              (instance, healthCheck)
+          }
+      }
+      .mapConcat(identity)
+      .map {
+        case (instance, httpHealthCheck) =>
+          prepareHttpRequest(appDef, instance, httpHealthCheck)
+      }
+      .via(httpWithBypass)
+      .mapAsync(1) {
+        case result if result.version == appDef.version =>
+          handleHealthResult(result)
+      }
+      .map(updateInstanceHealth)
+      .runWith(Sink.ignore)
+      .onComplete {
+        case Success(_) =>
+          logger.info(
+            s"HealthCheck stream for app ${appDef.id} version ${appDef.version} and healthCheck $healthCheck was stopped"
+          )
+          self ! 'restart
+
+        case Failure(ex) =>
+          logger.info(
+            s"HealthCheck stream for app ${appDef.id} version ${appDef.version} and healthCheck $healthCheck crashed due to $ex"
+          )
+          self ! 'restart
+      }
+  }
+
+  override def receive: Receive = {
+    case 'restart => throw new RuntimeException("stream stopped, restarting")
+  }
+
+  val bypassUnhealthy: Flow[Either[Unhealthy, (HttpRequest, Instance)], Unhealthy, NotUsed] =
+    Flow[Either[Unhealthy, (HttpRequest, Instance)]]
+      .mapConcat { either =>
+        either.left.toOption.toList
+      }
+
+  val checkViaHttp: Flow[Either[Unhealthy, (HttpRequest, Instance)], HealthResult, NotUsed] =
+    Flow[Either[Unhealthy, (HttpRequest, Instance)]]
+      .mapConcat { either =>
+        either.right.toOption.toList
+      }
+      .via(superPool)
+      .mapConcat { poolResponse =>
+        handleHttpResponse(poolResponse).toList
+      }
+
+  val httpWithBypass: Flow[Either[Unhealthy, (HttpRequest, Instance)], HealthResult, NotUsed] = {
+    Flow.fromGraph(GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+
+      // prepare graph elements
+      val broadcast = builder.add(Broadcast[Either[Unhealthy, (HttpRequest, Instance)]](outputPorts = 2, eagerCancel = true))
+      val merge = builder.add(Merge[HealthResult](inputPorts = 2, eagerComplete = true))
+
+      // connect the graph
+      broadcast.out(0) ~> bypassUnhealthy ~> merge.in(0)
+      broadcast.out(1) ~> checkViaHttp ~> merge.in(1)
+
+      // expose ports
+      FlowShape(broadcast.in, merge.out)
+    })
+  }
+
+  override def preRestart(reason: Throwable, message: Option[Any]): Unit =
+    logger.info(
+      "Restarting health check actor for app [{}] version [{}] and healthCheck [{}]",
+      appDef.id,
+      appDef.version,
+      healthCheck
+    )
+
+  override def postStop(): Unit = {
+    logger.info(
+      "Stopped health check actor for app [{}] version [{}] and healthCheck [{}]",
+      appDef.id,
+      appDef.version,
+      healthCheck
+    )
+  }
+
+  def handleHttpResponse(httpResult: (Try[HttpResponse], Instance)): Option[HealthResult] = httpResult match {
+    case (Success(response), instance) =>
+      response.discardEntityBytes() //forget about the body
+      if (acceptableResponses.contains(response.status.intValue())) {
+        Some(Healthy(instance.instanceId, instance.runSpecVersion))
+      } else if (healthCheck.ignoreHttp1xx && toIgnoreResponses.contains(response.status.intValue)) {
+        logger.debug(s"Ignoring health check HTTP response ${response.status.intValue} for instance=${instance.instanceId}")
+        None
+      } else {
+        logger.debug(s"Health check for instance=${instance.instanceId} responded with ${response.status}")
+        Some(Unhealthy(instance.instanceId, instance.runSpecVersion, response.status.toString()))
+      }
+
+    case (Failure(ex), instance) =>
+      logger.debug(s"Health check for instance=${instance.instanceId} did not respond due to ${ex.getMessage}.")
+      Some(Unhealthy(instance.instanceId, instance.runSpecVersion, ex.getMessage))
+  }
+
+  def prepareHttpRequest(app: AppDefinition, instance: Instance, httpHealthCheck: MarathonHttpHealthCheck): Either[Unhealthy, (HttpRequest, Instance)] = {
+
+    extractIpAndPort(app, instance, httpHealthCheck).right.flatMap {
+      case (host, port) =>
+        extractProtocol(httpHealthCheck, instance).right.map { protocol =>
+          val rawPath = httpHealthCheck.path.getOrElse("")
+          val absolutePath = if (rawPath.startsWith("/")) rawPath else s"/$rawPath"
+          val url = s"$protocol://$host:$port$absolutePath"
+
+          logger.debug(s"Checking the health of [$url] for instance=${instance.instanceId} via $protocol")
+
+          val httpRequest = RequestBuilding.Get(url)
+
+          val effectiveHost = httpRequest.uri.authority.host.toString()
+          val effectivePort = httpRequest.uri.effectivePort
+          val hostHeader = headers.Host(effectiveHost, effectivePort)
+          val effectiveRequest: HttpRequest = httpRequest
+            .withUri(httpRequest.uri.toHttpRequestTargetOriginForm)
+            .withDefaultHeaders(hostHeader)
+
+          effectiveRequest -> instance
+        }
+    }
+  }
+
+  def extractIpAndPort(app: AppDefinition, instance: Instance, httpHealthCheck: MarathonHttpHealthCheck): Either[Unhealthy, (String, Int)] = {
+    // HealthChecks are only supported for legacy App instances with exactly one task
+    val maybeEffectiveIpAddress: Option[String] = instance.appTask.status.networkInfo.effectiveIpAddress(app)
+
+    val effectiveIpAddress: Either[Unhealthy, String] = maybeEffectiveIpAddress
+      .map(Right(_))
+      .getOrElse {
+        val message = "Health check failed: unable to get the task's effective IP address"
+        logger.warn(message)
+        Left(createUnhealthy(message, instance))
+      }
+
+    val maybePort: Option[Int] = httpHealthCheck.effectivePort(app, instance)
+
+    val port: Either[Unhealthy, Int] =
+      maybePort
+        .map(Right(_))
+        .getOrElse {
+          val message = "Health check failed: unable to get the task's effectivePort"
+          logger.warn(message)
+          Left(createUnhealthy(message, instance))
+        }
+
+    effectiveIpAddress.right.flatMap(ip => port.right.map(p => ip -> p))
+  }
+
+  def extractProtocol(hc: MarathonHttpHealthCheck, instance: Instance): Either[Unhealthy, String] = {
+    hc.protocol match {
+      case Protos.HealthCheckDefinition.Protocol.HTTPS => Right("https")
+      case Protos.HealthCheckDefinition.Protocol.HTTP => Right("http")
+      case invalidProtocol: Protos.HealthCheckDefinition.Protocol =>
+        val message = s"Health check failed: HTTP health check contains invalid protocol: $invalidProtocol"
+        logger.warn(message)
+        Left(createUnhealthy(message, instance))
+    }
+  }
+
+  def createUnhealthy(message: String, instance: Instance): Unhealthy = {
+    val ex = new UnsupportedOperationException(message)
+    Unhealthy(
+      instance.instanceId,
+      instance.runSpecVersion,
+      s"${ex.getClass.getSimpleName}: ${ex.getMessage}"
+    )
+  }
+
+  def handleHealthResult(result: HealthResult): Future[InstanceHealth] = {
+    val instanceId = result.instanceId
+    val health = healthByInstanceId.getOrElse(instanceId, Health(instanceId))
+
+    val updatedHealth = result match {
+      case Healthy(_, _, _, _) =>
+        Future.successful(health.update(result))
+      case Unhealthy(_, _, _, _, _) =>
+        instanceTracker.instance(instanceId).map {
+          case Some(instance) =>
+            if (ignoreFailures(instance, health)) {
+              // Don't update health
+              health
+            } else {
+              logger.debug("{} is {}", instance.instanceId, result)
+              if (result.publishEvent) {
+                eventBus.publish(FailedHealthCheck(appDef.id, instanceId, healthCheck))
+              }
+              checkConsecutiveFailures(instance, health)
+              health.update(result)
+            }
+          case None =>
+            logger.error(s"Couldn't find instance $instanceId")
+            health.update(result)
+        }
+    }
+    updatedHealth.map(newHealth => InstanceHealth(result, health, newHealth))
+  }
+
+  def checkConsecutiveFailures(instance: Instance, health: Health): Unit = {
+    val consecutiveFailures = health.consecutiveFailures
+    val maxFailures = healthCheck.maxConsecutiveFailures
+
+    // ignore failures if maxFailures == 0
+    if (consecutiveFailures >= maxFailures && maxFailures > 0) {
+      val instanceId = instance.instanceId
+      logger.info(
+        s"Detected unhealthy $instanceId of app [${appDef.id}] version [${appDef.version}] on host ${instance.agentInfo.host}"
+      )
+
+      // kill the instance, if it is reachable
+      if (instance.isUnreachable) {
+        logger.info(s"Instance $instanceId on host ${instance.agentInfo.host} is temporarily unreachable. Performing no kill.")
+      } else {
+        logger.info(s"Send kill request for $instanceId on host ${instance.agentInfo.host} to driver")
+        require(instance.tasksMap.size == 1, "Unexpected pod instance in HealthCheckActor")
+        val taskId = instance.appTask.taskId
+        eventBus.publish(
+          UnhealthyInstanceKillEvent(
+            appId = instance.runSpecId,
+            taskId = taskId,
+            instanceId = instanceId,
+            version = appDef.version,
+            reason = health.lastFailureCause.getOrElse("unknown"),
+            host = instance.agentInfo.host,
+            slaveId = instance.agentInfo.agentId,
+            timestamp = health.lastFailure.getOrElse(Timestamp.now()).toString
+          )
+        )
+        killService.killInstancesAndForget(Seq(instance), KillReason.FailedHealthChecks)
+      }
+    }
+  }
+
+  def ignoreFailures(instance: Instance, health: Health): Boolean = {
+    // ignore all failures during the grace period aa well as for instances that are not running
+    if (instance.isRunning) {
+      // ignore if we haven't had a successful health check yet and are within the grace period
+      health.firstSuccess.isEmpty && instance.state.since + healthCheck.gracePeriod > Timestamp.now()
+    } else {
+      true
+    }
+  }
+
+  def updateInstanceHealth(instanceHealth: InstanceHealth): Unit = {
+    val result = instanceHealth.result
+    val instanceId = result.instanceId
+    val health = instanceHealth.health
+    val newHealth = instanceHealth.newHealth
+
+    logger.info("Received health result for app [{}] version [{}]: [{}]", appDef.id, appDef.version, result)
+    healthByInstanceId += (instanceId -> instanceHealth.newHealth)
+    appHealthCheckActor ! HealthCheckStatusChanged(ApplicationKey(appDef.id, appDef.version), healthCheck, newHealth)
+
+    if (health.alive != newHealth.alive && result.publishEvent) {
+      eventBus.publish(HealthStatusChanged(appDef.id, instanceId, result.version, alive = newHealth.alive))
+    }
+  }
+
+  def purgeStatusOfDoneInstances(instances: Seq[Instance]): Unit = {
+    logger.debug(
+      "Purging health status of inactive instances for app [{}] version [{}] and healthCheck [{}]",
+      appDef.id,
+      appDef.version,
+      healthCheck
+    )
+    val activeInstanceIds: Set[Instance.Id] = instances.withFilter(_.isLaunched).map(_.instanceId)(collection.breakOut)
+    // The Map built with filterKeys wraps the original map and contains a reference to activeInstanceIds.
+    // Therefore we materialize it into a new map.
+    activeInstanceIds.foreach { activeId =>
+      healthByInstanceId.remove(activeId)
+    }
+
+    val hcToPurge = instances.withFilter(!_.isActive).map(instance => {
+      val instanceKey = InstanceKey(ApplicationKey(instance.runSpecId, instance.runSpecVersion), instance.instanceId)
+      (instanceKey, healthCheck)
+    })
+    appHealthCheckActor ! PurgeHealthCheckStatuses(hcToPurge)
+  }
 
 }
