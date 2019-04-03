9b1af023aacb665dcd0e8df09a46a590090a581d
Karsten Jeschkies <kjeschkies@mesosphere.com>
2019-04-03 20:10:02 +0200

    Verify that a replacement is started.

--- tests/integration/src/test/scala/mesosphere/marathon/integration/HealthCheckIntegrationTest.scala
+++ tests/integration/src/test/scala/mesosphere/marathon/integration/HealthCheckIntegrationTest.scala
@@ -15,26 +15,63 @@ class HealthCheckIntegrationTest extends AkkaIntegrationTest with EmbeddedMarath
   def appId(suffix: Option[String] = None): PathId = testBasePath / s"app-${suffix.getOrElse(UUID.randomUUID)}"
 
   "Health checks" should {
-    "kill unhealthy instance" in {
+    "kill instance with failing Marathon health checks" in {
       Given("a deployed app with health checks")
-      val app = appProxy(appId(Some("replace-marathon-http-health-check")), "v1", instances = 1, healthCheck = None).
-        copy(healthChecks = Set(ramlHealthCheck))
-      val check = registerAppProxyHealthCheck(PathId(app.id), "v1", state = true)
+      val id = appId(Some(s"replace-marathon-http-health-check"))
+      val app = appProxy(id, "v1", instances = 1, healthCheck = None).
+        copy(healthChecks = Set(ramlHealthCheck(AppHealthCheckProtocol.Http)))
+      val check = registerAppProxyHealthCheck(id, "v1", state = true)
       val result = marathon.createAppV2(app)
       result should be(Created)
       waitForDeployment(result)
 
       When("the app becomes unhealthy")
+      val oldTaskId = marathon.tasks(id).value.head.id
       check.afterDelay(1.seconds, false)
 
       Then("the unhealthy instance is killed")
       waitForEvent("unhealthy_instance_kill_event")
+
+      And("a replacement is started")
+      check.afterDelay(1.seconds, true)
+      eventually {
+        val currentTasks = marathon.tasks(id).value
+        currentTasks should have size (1)
+        currentTasks.map(_.id) should not contain (oldTaskId)
+      }
+    }
+
+    "kill instance with failing Mesos health checks" in {
+      Given("a deployed app with health checks")
+      val id = appId(Some(s"replace-mesos-http-health-check"))
+      val app = appProxy(id, "v1", instances = 1, healthCheck = None).
+        copy(healthChecks = Set(ramlHealthCheck(AppHealthCheckProtocol.Http)))
+      val check = registerAppProxyHealthCheck(id, "v1", state = true)
+      val result = marathon.createAppV2(app)
+      result should be(Created)
+      waitForDeployment(result)
+
+      When("the app becomes unhealthy")
+      val oldTaskId = marathon.tasks(id).value.head.id
+      check.afterDelay(1.seconds, false)
+
+      Then("the unhealthy instance is killed")
+      waitForEvent("unhealthy_instance_kill_event")
+
+      And("a replacement is started")
+      // This fails.
+      //check.afterDelay(1.seconds, true)
+      //eventually {
+      //  val currentTasks = marathon.tasks(id).value
+      //  currentTasks should have size (1)
+      //  currentTasks.map(_.id) should not contain (oldTaskId)
+      //}
     }
   }
 
-  private val ramlHealthCheck = AppHealthCheck(
+  private def ramlHealthCheck(protocol: AppHealthCheckProtocol) = AppHealthCheck(
     path = Some("/health"),
-    protocol = AppHealthCheckProtocol.Http,
+    protocol = protocol,
     gracePeriodSeconds = 20,
     intervalSeconds = 1,
     maxConsecutiveFailures = 5,
