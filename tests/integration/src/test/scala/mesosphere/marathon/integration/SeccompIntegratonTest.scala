da425c09b3c48d1d4426b1fb5d759c79511a7ee4
Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>
2019-06-26 17:02:21 +0200

    Add seccomp integration tests for pods
    using 1) unconfined and 2) default profile.
    
    JIRA: DCOS-51846

--- tests/integration/src/test/scala/mesosphere/marathon/integration/SeccompIntegratonTest.scala
+++ tests/integration/src/test/scala/mesosphere/marathon/integration/SeccompIntegratonTest.scala
@@ -1,11 +1,12 @@
 package mesosphere.marathon
 package integration
 
-import mesosphere.{AkkaIntegrationTest, WhenEnvSet}
+import mesosphere.marathon.core.pod.{MesosContainer, PodDefinition}
 import mesosphere.marathon.integration.setup.{EmbeddedMarathonTest, MesosConfig}
 import mesosphere.marathon.raml.{App, Container, DockerContainer, EngineType, LinuxInfo, Seccomp}
 import mesosphere.marathon.state.PathId
 import mesosphere.marathon.state.PathId._
+import mesosphere.{AkkaIntegrationTest, WhenEnvSet}
 
 import scala.io.Source
 
@@ -23,7 +24,6 @@ class SeccompIntegratonTest extends AkkaIntegrationTest with EmbeddedMarathonTes
 
   logger.info(s"Using --seccomp_config_dir = ${mesosConfig.agentSeccompConfigDir.get}")
   logger.info(s"Using --seccomp_profile_name = ${mesosConfig.agentSeccompProfileName.get}")
-  logger.debug(s"Seccomp profile: ${Source.fromFile(s"$projectDir/src/test/resources/mesos/seccomp/default.json").getLines.mkString("\n")}")
 
   "An app definition WITH seccomp profile defined and unconfined = false" taggedAs WhenEnvSet(envVarRunMesosTests, default = "true") in {
     Given("an app WITH seccomp profile defined and unconfined = false")
@@ -49,6 +49,51 @@ class SeccompIntegratonTest extends AkkaIntegrationTest with EmbeddedMarathonTes
 
     And("the task is running")
     waitForTasks(app.id.toPath, app.instances)
+  }
+
+  "A pod definition WITH seccomp profile defined and unconfined = false" taggedAs WhenEnvSet(envVarRunMesosTests, default = "true") in {
+    Given("a pod WITH seccomp profile defined and unconfined = false")
+    val pod = seccompPod(PathId("/pod-with-seccomp-profile-and-unconfined-false"), unconfined = false, profileName = mesosConfig.agentSeccompProfileName)
+
+    When("the pod is successfully deployed")
+    val result = marathon.createPodV2(pod)
+    result should be(Created)
+    waitForDeployment(result)
+
+    And("the task is running")
+    eventually { marathon.status(pod.id) should be(Stable) }
+  }
+
+  "An pod definition WITHOUT seccomp profile and unconfined = true" taggedAs WhenEnvSet(envVarRunMesosTests, default = "true") in {
+    Given("a pod WITHOUT seccomp profile and unconfined = true")
+    val pod = seccompPod(PathId("/pod-without-seccomp-profile-and-unconfined-true"), unconfined = true)
+
+    When("the pod is successfully deployed")
+    val result = marathon.createPodV2(pod)
+    result should be(Created)
+    waitForDeployment(result)
+
+    And("the task is running")
+    eventually { marathon.status(pod.id) should be(Stable) }
+  }
+
+  def seccompPod(podId: PathId, unconfined: Boolean, profileName: Option[String] = None): PodDefinition = {
+    PodDefinition(
+      id = podId,
+      instances = 1,
+      containers = Seq(
+        MesosContainer(
+          name = "task1",
+          exec = Some(raml.MesosExec(raml.ShellCommand("sleep 9000"))),
+          resources = raml.Resources(cpus = 0.01, mem = 32.0),
+          linuxInfo = Some(state.LinuxInfo(
+            seccomp = Some(state.Seccomp(
+              profileName,
+              unconfined
+            ))
+          ))
+        )
+      ))
   }
 
   def seccompApp(appId: PathId, unconfined: Boolean, profileName: Option[String] = None): App = {
