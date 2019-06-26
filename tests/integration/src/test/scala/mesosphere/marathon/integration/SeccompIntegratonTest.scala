aea2ee8a7bcea9a00999c2ab64c3edf1ce188b0f
Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>
2019-06-26 15:32:33 +0200

    Make appIds absolute PathIds

--- tests/integration/src/test/scala/mesosphere/marathon/integration/SeccompIntegratonTest.scala
+++ tests/integration/src/test/scala/mesosphere/marathon/integration/SeccompIntegratonTest.scala
@@ -11,7 +11,7 @@ import scala.io.Source
 
 class SeccompIntegratonTest extends AkkaIntegrationTest with EmbeddedMarathonTest {
 
-  val projectDir = sys.props.getOrElse("user.dir", ".")
+  val projectDir: String = sys.props.getOrElse("user.dir", ".")
   override lazy val mesosConfig = MesosConfig(
     launcher = "linux",
     containerizers = "docker,mesos",
@@ -27,7 +27,7 @@ class SeccompIntegratonTest extends AkkaIntegrationTest with EmbeddedMarathonTes
 
   "An app definition WITH seccomp profile defined and unconfined = false" taggedAs WhenEnvSet(envVarRunMesosTests, default = "true") in {
     Given("an app WITH seccomp profile defined and unconfined = false")
-    val app = seccompApp(PathId("app-with-seccomp-profile-and-unconfined-false"), unconfined = false, profileName = mesosConfig.agentSeccompProfileName)
+    val app = seccompApp(PathId("/app-with-seccomp-profile-and-unconfined-false"), unconfined = false, profileName = mesosConfig.agentSeccompProfileName)
 
     When("the app is successfully deployed")
     val result = marathon.createAppV2(app)
@@ -35,12 +35,12 @@ class SeccompIntegratonTest extends AkkaIntegrationTest with EmbeddedMarathonTes
     waitForDeployment(result)
 
     And("the task is running")
-    waitForTasks(app.id.toPath, 1)
+    waitForTasks(app.id.toPath, app.instances)
   }
 
   "An app definition WITHOUT seccomp profile and unconfined = true" taggedAs WhenEnvSet(envVarRunMesosTests, default = "true") in {
     Given("an app WITHOUT seccomp profile and unconfined = true")
-    val app = seccompApp(PathId("app-without-seccomp-profile-and-unconfined-true"), unconfined = true)
+    val app = seccompApp(PathId("/app-without-seccomp-profile-and-unconfined-true"), unconfined = true)
 
     When("the app is successfully deployed")
     val result = marathon.createAppV2(app)
@@ -48,14 +48,13 @@ class SeccompIntegratonTest extends AkkaIntegrationTest with EmbeddedMarathonTes
     waitForDeployment(result)
 
     And("the task is running")
-    waitForTasks(app.id.toPath, 1)
+    waitForTasks(app.id.toPath, app.instances)
   }
 
   def seccompApp(appId: PathId, unconfined: Boolean, profileName: Option[String] = None): App = {
     App(
       id = appId.toString,
       cmd = Some("sleep 232323"),
-      instances = 1,
       cpus = 0.01,
       mem = 32.0,
       container = Some(
