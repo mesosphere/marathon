17069ab0fb2460f8d13f1d9a7a2c1bbca89f4b39
Andreas Neumann <aneumann@mesosphere.com>
2019-07-15 17:16:30 +0200

    Added Instance Migration to 19100-Migration

--- src/main/scala/mesosphere/marathon/storage/migration/MigrationTo19100.scala
+++ src/main/scala/mesosphere/marathon/storage/migration/MigrationTo19100.scala
@@ -7,16 +7,21 @@ import java.time.OffsetDateTime
 import akka.http.scaladsl.marshalling.Marshaller
 import akka.http.scaladsl.unmarshalling.Unmarshaller
 import akka.stream.Materializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.util.ByteString
 import akka.{Done, NotUsed}
 import com.typesafe.scalalogging.StrictLogging
+import mesosphere.marathon.core.instance.Instance.{AgentInfo, Id, InstanceState}
+import mesosphere.marathon.core.instance.Reservation
 import mesosphere.marathon.core.storage.store.impl.zk.{ZkId, ZkSerialized}
 import mesosphere.marathon.core.storage.store.{IdResolver, PersistenceStore}
-import mesosphere.marathon.state.{AppDefinition, PathId, Timestamp}
-import mesosphere.marathon.storage.repository.{AppRepository, PodRepository}
+import mesosphere.marathon.core.task.Task
+import mesosphere.marathon.state.{AppDefinition, Instance, PathId, Timestamp}
+import mesosphere.marathon.storage.repository.{AppRepository, InstanceRepository, PodRepository}
 import mesosphere.marathon.storage.store.ZkStoreSerialization
-import play.api.libs.json.Json
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
 
 import scala.async.Async.{async, await}
 import scala.concurrent.{ExecutionContext, Future}
@@ -25,13 +30,54 @@ class MigrationTo19100(
     defaultMesosRole: String,
     appRepository: AppRepository,
     podRepository: PodRepository,
+    instanceRepository: InstanceRepository,
     persistenceStore: PersistenceStore[ZkId, String, ZkSerialized]) extends MigrationStep with StrictLogging {
 
   override def migrate()(implicit ctx: ExecutionContext, mat: Materializer): Future[Done] = async {
     logger.info("Starting migration to 1.9.100")
     await(MigrationTo19100.migrateApps(defaultMesosRole, persistenceStore))
     await(MigrationTo19100.migratePods(defaultMesosRole, persistenceStore))
+    await(InstanceMigration.migrateInstances(instanceRepository, persistenceStore, instanceMigrationFlow))
   }
+
+  /**
+    * Read format for old instance without reservation id.
+    */
+  val instanceJsonReads19100: Reads[Instance] = {
+    import mesosphere.marathon.api.v2.json.Formats.TimestampFormat
+    import mesosphere.marathon.core.instance.Instance.tasksMapFormat
+
+    (
+      (__ \ "instanceId").read[Id] ~
+      (__ \ "agentInfo").read[AgentInfo] ~
+      (__ \ "tasksMap").read[Map[Task.Id, Task]] ~
+      (__ \ "runSpecVersion").read[Timestamp] ~
+      (__ \ "state").read[InstanceState] ~
+      (__ \ "reservation").readNullable[Reservation] ~
+      (__ \ "role").readNullable[String]
+    ) { (instanceId, agentInfo, tasksMap, runSpecVersion, state, reservation, rawRole) =>
+        logger.info(s"Migrate $instanceId")
+
+        val role = Some(defaultMesosRole)
+
+        new Instance(instanceId, Some(agentInfo), state, tasksMap, runSpecVersion, reservation, role)
+      }
+  }
+
+  /**
+    * Extract instance from old format
+    *
+    * @param jsValue The instance as JSON.
+    * @return The parsed instance.
+    */
+  def extractInstanceFromJson(jsValue: JsValue): Instance = jsValue.as[Instance](instanceJsonReads19100)
+
+  val instanceMigrationFlow = Flow[JsValue]
+    .filter { jsValue =>
+      // Only migrate instances that don't have a role
+      (jsValue \ "role").isEmpty
+    }
+    .map(extractInstanceFromJson)
 }
 
 object MigrationTo19100 extends MaybeStore with StrictLogging {
@@ -143,4 +189,5 @@ object MigrationTo19100 extends MaybeStore with StrictLogging {
       Future.successful(Done)
     }
   }
+
 }
