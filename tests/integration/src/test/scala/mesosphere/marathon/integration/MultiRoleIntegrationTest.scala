e19e8db1a428e98c20e0352c24bb033dfeb4b791
Andreas Neumann <aneumann@mesosphere.com>
2019-07-17 21:18:47 +0200

    Deploy and check for deployment in order

--- tests/integration/src/test/scala/mesosphere/marathon/integration/MultiRoleIntegrationTest.scala
+++ tests/integration/src/test/scala/mesosphere/marathon/integration/MultiRoleIntegrationTest.scala
@@ -14,21 +14,25 @@ class MultiRoleIntegrationTest extends AkkaIntegrationTest with EmbeddedMarathon
   "MultiRole" should {
 
     "Marathon should launch tasks as specified role" in {
-      Given("a new pod")
+      Given("an app in role dev")
       val appInDev = appProxy(PathId("/dev/app-with-role"), "v1", instances = 1, role = Some("dev"))
-      val appInMetrics = appProxy(PathId("/metrics/app-with-role"), "v1", instances = 1, role = Some("metrics"))
 
-      When("The apps are deployed")
+      When("The app is deployed")
       val resultInDev = marathon.createAppV2(appInDev)
-      val resultInMetrics = marathon.createAppV2(appInMetrics)
 
       Then("The apps are created")
       resultInDev should be(Created)
-      resultInMetrics should be(Created)
-
       waitForDeployment(resultInDev)
       waitForTasks(PathId(appInDev.id), 1) //make sure, the pod has really started
 
+      Given("an app in role metrics")
+      val appInMetrics = appProxy(PathId("/metrics/app-with-role"), "v1", instances = 1, role = Some("metrics"))
+
+      When("The app is deployed")
+      val resultInMetrics = marathon.createAppV2(appInMetrics)
+
+      Then("The apps are created")
+      resultInMetrics should be(Created)
       waitForDeployment(resultInMetrics)
       waitForTasks(PathId(appInMetrics.id), 1) //make sure, the pod has really started
 
