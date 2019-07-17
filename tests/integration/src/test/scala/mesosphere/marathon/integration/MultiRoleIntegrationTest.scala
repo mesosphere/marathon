bed107b115d73b39b887452f97ccbf1e29e117fb
Andreas Neumann <aneumann@mesosphere.com>
2019-07-17 18:30:58 +0200

    Started integration test

---
+++ tests/integration/src/test/scala/mesosphere/marathon/integration/MultiRoleIntegrationTest.scala
@@ -0,0 +1,33 @@
+package mesosphere.marathon
+package integration
+
+import mesosphere.AkkaIntegrationTest
+import mesosphere.marathon.integration.setup.EmbeddedMarathonTest
+
+class MultiRoleIntegrationTest extends AkkaIntegrationTest with EmbeddedMarathonTest {
+
+  "MultiRole" should {
+
+    "Marathon should launch tasks as specified role" in {
+      Given("a new pod")
+      val podInDev = simplePod("/dev/podWithRole", role = "dev")
+      val podInMetrics = simplePod("/metrics/podWithRole", role = "metrics")
+
+      When("The pods are deployed")
+      val resultInDev = marathon.createPodV2(podInDev)
+      val resultInMetrics = marathon.createPodV2(podInMetrics)
+
+      Then("The pods are created")
+      resultInDev should be(Created)
+      resultInMetrics should be(Created)
+      //      extractDeploymentIds(resultInDev) should have size 1
+      waitForDeployment(resultInDev)
+      waitForTasks(podInDev.id, 1) //make sure, the pod has really started
+
+      waitForDeployment(resultInMetrics)
+      waitForTasks(podInMetrics.id, 1) //make sure, the pod has really started
+
+    }
+
+  }
+}
