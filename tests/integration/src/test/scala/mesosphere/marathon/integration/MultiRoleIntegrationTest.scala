bd62b19a75c06416d389f11605cb65b2351607fa
Andreas Neumann <aneumann@mesosphere.com>
2019-07-19 11:31:00 +0200

    Started to add new integration test

--- tests/integration/src/test/scala/mesosphere/marathon/integration/MultiRoleIntegrationTest.scala
+++ tests/integration/src/test/scala/mesosphere/marathon/integration/MultiRoleIntegrationTest.scala
@@ -10,6 +10,9 @@ class MultiRoleIntegrationTest extends AkkaIntegrationTest with EmbeddedMarathon
   override lazy val mesosConfig = MesosConfig(
     restrictedToRoles = None
   )
+  override lazy val marathonArgs = Map(
+    "mesos_role" -> "*"
+  )
 
   "MultiRole" should {
 
@@ -37,18 +40,30 @@ class MultiRoleIntegrationTest extends AkkaIntegrationTest with EmbeddedMarathon
       waitForTasks(PathId(appInMetrics.id), 1) //make sure, the pod has really started
 
     }
+
+    "Marathon should launch an resident app as non-default role" in {
+      Given("an app in role dev")
+      val appInDev = residentApp(PathId("/dev/simple-resident-app-with-role"), role = Some("dev"))
+
+      When("The app is deployed")
+      val resultInDev = marathon.createAppV2(appInDev)
+
+      Then("The apps are created")
+      resultInDev should be(Created)
+      waitForDeployment(resultInDev)
+      waitForTasks(PathId(appInDev.id), 1) //make sure the app has really started
+    }
+//
+//    "Marathon should decline an resident app without a role if mesos_role is *" in {
+//      Given("an app in role dev")
+//      val appInDev = residentApp(PathId("/dev/resident-app-without-role"))
+//
+//      When("The app is created")
+//      val resultInDev = marathon.createAppV2(appInDev)
+//
+//      Then("The apps is not created")
+//      resultInDev should be(Created)
+//    }
   }
 
-  "Marathon should launch an resident app as non-default role" in {
-    Given("an app in role dev")
-    val appInDev = residentApp(PathId("/dev/simple-resident-app-with-role"), role = Some("dev"))
-
-    When("The app is deployed")
-    val resultInDev = marathon.createAppV2(appInDev)
-
-    Then("The apps are created")
-    resultInDev should be(Created)
-    waitForDeployment(resultInDev)
-    waitForTasks(PathId(appInDev.id), 1) //make sure the app has really started
-  }
 }
