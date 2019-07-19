5a2ba3e864746d07cfdd60cea63780f51e40d76d
Karsten Jeschkies <kjeschkies@mesosphere.com>
2019-07-19 13:35:00 +0200

    Document group role behavior.

--- src/main/scala/mesosphere/marathon/GroupRoleBehavior.scala
+++ src/main/scala/mesosphere/marathon/GroupRoleBehavior.scala
@@ -1,18 +1,25 @@
 package mesosphere.marathon
 
+/**
+  * The group role behavior defines whether groups enforce their role or not by default.
+  *
+  *  - [[GroupRoleBehavior.Off]] indicates that groups will not enforce their role and apps will use
+  *    the default Mesos role.
+  *  - [[GroupRoleBehavior.Top]] indicates that only top-level groups, ie groups directly unser `/`
+  *    will enforce their role, thus assign their name as the role for any [[raml.App]] or [[raml.Pod]].
+  */
 sealed trait GroupRoleBehavior {
   val name: String
+  override def toString: String = name
 }
 
 object GroupRoleBehavior {
   case object Off extends GroupRoleBehavior {
-    override val name: String = "off"
-
-    override def toString: String = name
+    override val name: String = "Off"
   }
 
   case object Top extends GroupRoleBehavior {
-    override val name: String = "top"
+    override val name: String = "Top"
   }
 
   val all = Seq(Off, Top)
