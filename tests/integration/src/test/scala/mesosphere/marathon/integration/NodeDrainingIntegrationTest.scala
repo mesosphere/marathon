02cb9cf2dd3dfe079e70601ce2aaa813d0ac3b63
Matthias Eichstedt <matthias.eichstedt@mesosphere.io>
2019-08-01 10:32:36 +0200

    Update tests/integration/src/test/scala/mesosphere/marathon/integration/NodeDrainingIntegrationTest.scala
    Co-Authored-By: Karsten Jeschkies <kjeschkies@mesosphere.com>

--- tests/integration/src/test/scala/mesosphere/marathon/integration/NodeDrainingIntegrationTest.scala
+++ tests/integration/src/test/scala/mesosphere/marathon/integration/NodeDrainingIntegrationTest.scala
@@ -47,7 +47,7 @@ class NodeDrainingIntegrationTest extends AkkaIntegrationTest with EmbeddedMarat
       }
 
       When("the node is drained")
-      mesos.drainAgent(agentId, maxGracePeriod = Some(42)).success shouldBe true
+      mesos.drainAgent(agentId, maxGracePeriod = Some(42)) should be(Ok)
 
       Then("the delay is reset")
       eventually {
