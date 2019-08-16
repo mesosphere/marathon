caa9684648cd9cd01e77ec2c147a1e31d3fbd761
Karsten Jeschkies <kjeschkies@mesosphere.com>
2019-08-16 19:24:34 +0200

    Accept default Mesos role for `acceptedResourceRoles`. (#7014)
    Fixing a regression where a valid case of having an acceptedResourceRoles: [--mesos_role] would fail validation. Additionally, acceptedResourceRole is now sanitized when sanitize_accepted_resource_roles is active:
    
    if an invalid resource role is passed, it is removed
    If acceptedResourceRole array is empty, * (Unreserved) is used
    
    JIRA: MARATHON-8683
    
    Co-authored-by: Aleksey Dukhovniy <adukhovniy@mesosphere.io>

--- src/main/scala/mesosphere/marathon/api/v2/AppHelpers.scala
+++ src/main/scala/mesosphere/marathon/api/v2/AppHelpers.scala
@@ -17,10 +17,11 @@ object AppHelpers {
   def appNormalization(config: AppNormalization.Config, validRoles: Set[String]): Normalization[raml.App] = Normalization { app =>
 
     validateOrThrow(app)(AppValidation.validateOldAppAPI)
-    val migrated = AppNormalization.forDeprecated(config).normalized(app)
-    validateOrThrow(migrated)(AppValidation.validateCanonicalAppAPI(config.enabledFeatures, () => config.defaultNetworkName, validRoles))
-    AppNormalization(config).normalized(migrated)
 
+    val migrated = AppNormalization.forDeprecated(config).normalized(app)
+    val preNormalized = AppNormalization.forPreValidation(config).normalized(migrated)
+    validateOrThrow(preNormalized)(AppValidation.validateCanonicalAppAPI(config.enabledFeatures, () => config.defaultNetworkName, validRoles))
+    AppNormalization.forPostValidation(config).normalized(preNormalized)
   }
 
   def appUpdateNormalization(config: AppNormalization.Config): Normalization[raml.AppUpdate] = Normalization { app =>
--- src/test/scala/mesosphere/marathon/api/v2/AppsResourceTest.scala
+++ src/test/scala/mesosphere/marathon/api/v2/AppsResourceTest.scala
@@ -55,12 +55,7 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
     implicit val authenticator: Authenticator = auth.auth
     implicit val authorizer: Authorizer = auth.auth
 
-    val normalizationConfig = AppNormalization.Configuration(
-      config.defaultNetworkName.toOption,
-      config.mesosBridgeName(),
-      config.availableFeatures,
-      ResourceRole.Unreserved,
-      config.availableDeprecatedFeatures.isEnabled(DeprecatedFeatures.sanitizeAcceptedResourceRoles))
+    val normalizationConfig = AppNormalization.Configuration(config, config.mesosRole())
     implicit lazy val appDefinitionValidator = AppDefinition.validAppDefinition(config.availableFeatures, ValidationHelper.roleSettings())(PluginManager.None)
 
     implicit val validateAndNormalizeApp: Normalization[raml.App] =
@@ -244,7 +239,7 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
       val app = App(
         id = "/app", cmd = Some("cmd"), container = Some(container),
         secrets = Map("pullConfigSecret" -> SecretDef("/config")))
-      val (body, plan) = prepareApp(app, groupManager, enabledFeatures = Set("secrets"))
+      val (body, _) = prepareApp(app, groupManager, enabledFeatures = Set("secrets"))
 
       When("The create request is made")
       clock += 5.seconds
@@ -1948,7 +1943,7 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
       val app = App(
         id = "/app", cmd = Some("cmd"), container = Some(container),
         networks = Seq(Network(name = Some("name_with_underscore"), mode = NetworkMode.Container)))
-      val (body, plan) = prepareApp(app, groupManager)
+      val (body, _) = prepareApp(app, groupManager)
 
       When("The create request is made")
       clock += 5.seconds
@@ -2047,7 +2042,6 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
       }
 
       withClue(response.getEntity.toString) {
-        println("Response Status: " + response.getStatus + " >> " + response.getEntity.toString)
         Then("The return code indicates a validation error")
         response.getStatus should be(422)
         response.getEntity.toString should include("/role")
@@ -2067,7 +2061,6 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
       }
 
       withClue(response.getEntity.toString) {
-        println("Response Status: " + response.getStatus + " >> " + response.getEntity.toString)
         Then("The return code indicates success")
         response.getStatus should be(201)
       }
@@ -2089,7 +2082,6 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
       }
 
       withClue(response.getEntity.toString) {
-        println("Response Status: " + response.getStatus + " >> " + response.getEntity.toString)
         Then("The return code indicates a validation error")
         response.getStatus should be(422)
         response.getEntity.toString should include("/role")
@@ -2112,7 +2104,6 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
       }
 
       withClue(response.getEntity.toString) {
-        println("Response Status: " + response.getStatus + " >> " + response.getEntity.toString)
         Then("The return code indicates success")
         response.getStatus should be(201)
       }
@@ -2135,7 +2126,6 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
       }
 
       withClue(response.getEntity.toString) {
-        println("Response Status: " + response.getStatus + " >> " + response.getEntity.toString)
         Then("The return code indicates a validation error")
         response.getStatus should be(422)
         response.getEntity.toString should include("/role")
@@ -2155,7 +2145,6 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
       }
 
       withClue(response.getEntity.toString) {
-        println("Response Status: " + response.getStatus + " >> " + response.getEntity.toString)
         Then("The return code indicates success")
         response.getStatus should be(201)
       }
@@ -2173,7 +2162,6 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
       }
 
       withClue(response.getEntity.toString) {
-        println("Response Status: " + response.getStatus + " >> " + response.getEntity.toString)
         Then("The return code indicates success")
         response.getStatus should be(201)
       }
@@ -2195,7 +2183,6 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
       }
 
       withClue(response.getEntity.toString) {
-        println("Response Status: " + response.getStatus + " >> " + response.getEntity.toString)
         Then("The return code indicates success")
         response.getStatus should be(422)
         response.getEntity.toString should include("/role")
@@ -2215,7 +2202,6 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
       }
 
       withClue(response.getEntity.toString) {
-        println("Response Status: " + response.getStatus + " >> " + response.getEntity.toString)
         Then("The return code indicates success")
         response.getStatus should be(201)
       }
@@ -2248,5 +2234,79 @@ class AppsResourceTest extends AkkaUnitTest with GroupCreation with JerseyTest {
       app.role shouldBe "dev"
     }
 
+    "Create an app in root with acceptedResourceRoles = default Mesos role" in new Fixture(configArgs = Seq("--mesos_role", "customMesosRole")) {
+      Given("An app with the mesos_role role")
+      val app = App(
+        id = "/app-with-accepted-default-mesos-role",
+        cmd = Some("cmd"),
+        acceptedResourceRoles = Some(Set("customMesosRole")))
+
+      val (body, _) = prepareApp(app, groupManager, validate = false)
+
+      When("The create request is made")
+      clock += 5.seconds
+      val response = asyncRequest { r =>
+        appsResource.create(body, force = false, auth.request, r)
+      }
+
+      withClue(response.getEntity.toString) {
+        Then("The return code indicates success")
+        response.getStatus should be(201)
+      }
+    }
+
+    "Create an app in root with acceptedResourceRoles = customMesosRole and sanitizeAcceptedResourceRoles = true" in new Fixture(
+      configArgs = Seq("--deprecated_features", "sanitize_accepted_resource_roles")
+    ) {
+
+      Given("An app with an unknown acceptedResourceRole")
+      val app = App(
+        id = "/app-with-accepted-unknown-mesos-role",
+        cmd = Some("cmd"),
+        acceptedResourceRoles = Some(Set("customMesosRole")))
+
+      val (body, _) = prepareApp(app, groupManager, validate = false)
+
+      When("The create request is made")
+      clock += 5.seconds
+      val response = asyncRequest { r =>
+        appsResource.create(body, force = false, auth.request, r)
+      }
+
+      withClue(response.getEntity.toString) {
+        Then("The return code indicates success")
+        response.getStatus should be(201)
+      }
+
+      And("resulting app has acceptedResourceRoles sanitized (equals default one)")
+      val appJson = Json.parse(response.getEntity.asInstanceOf[String])
+      (appJson \ "acceptedResourceRoles" \ 0 ) should be (JsDefined(JsString(ResourceRole.Unreserved)))
+    }
+
+    "Create an app in root with acceptedResourceRoles = customMesosRole and sanitizeAcceptedResourceRoles = false" in new Fixture {
+
+      // TODO(ad) Instead of BuildInfo hack, I'd love to keep this test activated and somehow disable DeprecatedFeature.sanitizeAcceptedResourceRoles
+      if (BuildInfo.version >= DeprecatedFeatures.sanitizeAcceptedResourceRoles.softRemoveVersion) {
+        Given("An app with an unknown acceptedResourceRole")
+        val app = App(
+          id = "/app-with-not-accepted-unknown-mesos-role",
+          cmd = Some("cmd"),
+          acceptedResourceRoles = Some(Set("customMesosRole")))
+
+        val (body, _) = prepareApp(app, groupManager, validate = false)
+
+        When("The create request is made")
+        clock += 5.seconds
+        val response = asyncRequest { r =>
+          appsResource.create(body, force = false, auth.request, r)
+        }
+
+        withClue(response.getEntity.toString) {
+          Then("The return code indicates a failure")
+          response.getStatus should be(422)
+          response.getEntity.asInstanceOf[String] contains "acceptedResourceRoles can only contain *"
+        }
+      }
+    }
   }
 }
