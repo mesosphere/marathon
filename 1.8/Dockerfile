c4b45e1d806a2afcc036637a6b0f40bb1cbeec8f
Tim Harper <tim@bastion.lan>
2019-09-11 20:27:11 -0600

    Syncing docs with release branch

--- 1.8/Dockerfile
+++ 1.8/Dockerfile
@@ -5,8 +5,9 @@ COPY Gemfile Gemfile.lock /jekyll/
 RUN apt-get update && \
   curl -sL https://deb.nodesource.com/setup_6.x | bash - && \
   apt-get install -y gcc g++ git libxml2 zlib1g-dev libxml2-dev ruby ruby-dev make autoconf nodejs python python-dev && \
-  gem install bundler && \
-  cd /jekyll && bundle install && \
+  gem install bundler
+
+RUN cd /jekyll && bundle install && \
   apt-get purge -y gcc g++ ruby-dev python-dev && \
   apt-get -y autoremove && \
   rm -rf /var/lib/apt/lists
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,10 @@ case "$MODE" in
   build)
     if [ -z $MARATHON_DOCS_VERSION ]; then
       echo "Generating top level docs"
-      bundle exec jekyll build --config _config.yml -d _site
+      BUNDLE_GEMFILE=/jekyll/Gemfile bundle exec jekyll build --config _config.yml -d _site
     else
       echo "Generating docs for version $MARATHON_DOCS_VERSION"
-      bundle exec jekyll build --config "_config.yml,_config.$MARATHON_DOCS_VERSION.yml" -d "_site/$MARATHON_DOCS_VERSION/"
+      BUNDLE_GEMFILE=/jekyll/Gemfile bundle exec jekyll build --config "_config.yml,_config.$MARATHON_DOCS_VERSION.yml" -d "_site/$MARATHON_DOCS_VERSION/"
     fi
     ;;
   watch)
