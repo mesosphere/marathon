390768ac872e091f7b369b8ada8fd57e7deb2f7f
Tim Harper <tharper@mesosphere.com>
2019-12-10 08:40:47 -0700

    Unused import cleanup

--- src/main/scala/mesosphere/marathon/state/AppDefinition.scala
+++ src/main/scala/mesosphere/marathon/state/AppDefinition.scala
@@ -15,7 +15,7 @@ import mesosphere.marathon.core.plugin.PluginManager
 import mesosphere.marathon.core.pod.{HostNetwork, Network}
 import mesosphere.marathon.core.readiness.ReadinessCheck
 import mesosphere.marathon.plugin.validation.RunSpecValidator
-import mesosphere.marathon.raml.{App, Apps, ExecutorResources, ResourceLimitNumber, ResourceLimitUnlimited, Resources}
+import mesosphere.marathon.raml.{App, Apps, ExecutorResources, Resources}
 import mesosphere.marathon.state.Container.{Docker, MesosDocker}
 import mesosphere.marathon.state.VersionInfo._
 import mesosphere.marathon.stream.Implicits._
