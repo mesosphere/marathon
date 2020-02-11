f90ea5c4f5b556a5604f1c64e9b1dfca155f6936
Karsten Jeschkies <kjeschkies@mesosphere.com>
2020-02-11 11:29:45 +0100

    Use Tim's Docker image.

--- ci/docker/build.sh
+++ ci/docker/build.sh
@@ -2,4 +2,4 @@ mkdir target
 rsync -avz ../../ target/marathon/ --exclude docker --exclude target --delete
 sed -i.bak '/mem/d' target/marathon/.sbtopts
 
-docker build . --build-arg MESOS_PKG_VERSION=1.9.0-2.0.1
+docker build -t mesosphere/marathon-build-env:unstable . --build-arg MESOS_PKG_VERSION=1.9.0-2.0.1
