9e23c23a015441d131ad35ac4549f7d59643d0e2
Karsten Jeschkies <kjeschkies@mesosphere.com>
2020-05-27 11:23:30 +0200

    Report instance loading status.
    Summary:
    When the loading of the instance state from Zookeeper takes a long time
    it would be great to report the current progress. This change report
    every ten seconds how many instances have been loaded.

--- src/main/scala/mesosphere/marathon/core/task/tracker/impl/InstancesLoaderImpl.scala
+++ src/main/scala/mesosphere/marathon/core/task/tracker/impl/InstancesLoaderImpl.scala
@@ -3,14 +3,16 @@ package core.task.tracker.impl
 
 import akka.Done
 import akka.stream.Materializer
-import akka.stream.scaladsl.Sink
+import akka.stream.scaladsl.{Sink, Source}
 import com.typesafe.scalalogging.StrictLogging
 import mesosphere.marathon.core.instance.Instance
+import mesosphere.marathon.core.launchqueue.impl.ReviveOffersStreamLogic.Tick
 import mesosphere.marathon.core.task.tracker.{InstanceTracker, InstanceTrackerConfig}
 import mesosphere.marathon.storage.repository.InstanceView
 
 import scala.async.Async.{async, await}
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
 /**
   * Loads all task data into an [[InstanceTracker.InstancesBySpec]] from an [[mesosphere.marathon.storage.repository.InstanceRepository]].
@@ -23,18 +25,22 @@ private[tracker] class InstancesLoaderImpl(
 
   override def load(): Future[InstanceTracker.InstancesBySpec] = async {
 
-    // Join instances with app or pod.
+    case object Tick
+    val progressReportTick = 10.seconds
+    var loadedInstancesCount = 0
+
+    // Join instances with app or pod spec.
     val coreInstances = await(
-      repo.ids().mapAsync(parallelism = config.internalInstanceTrackerNumParallelLoads()) { instanceId =>
-        async {
-          await(repo.get(instanceId)) match {
-            case None =>
-              await(expungeOrphanedInstance(instanceId)); None
-            case Some(coreInstance) => Some(coreInstance)
-          }
+      repo.ids().mapAsync(parallelism = config.internalInstanceTrackerNumParallelLoads())(loadInstance)
+        .map(Left(_))
+        .merge(Source.tick(progressReportTick, progressReportTick, Right(Tick)), eagerComplete = true)
+        .map {
+          case Left(maybeInstance) => maybeInstance
+          case Right(Tick) =>
+            logger.info(s"Still loading instances. loadedInstances=$loadedInstancesCount")
+            None // Ignore tick.
         }
-      }
-        .collect{ case Some(x) => x }
+        .collect{ case Some(x) => loadedInstancesCount += 1; x }
         .runWith(Sink.seq)
     )
 
@@ -46,5 +52,15 @@ private[tracker] class InstancesLoaderImpl(
     logger.warn(s"No run spec ${instanceId.runSpecId} with any version was found for instance ${instanceId}. Expunging.")
     await(repo.delete(instanceId))
     Done
+  }
+
+  def loadInstance(instanceId: Instance.Id): Future[Option[Instance]] = {
+    async {
+      await(repo.get(instanceId)) match {
+        case None =>
+          await(expungeOrphanedInstance(instanceId)); None
+        case Some(coreInstance) => Some(coreInstance)
+      }
+    }
   }
 }
