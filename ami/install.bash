374a72cf2709edca1dc51aa5b649b286e61fcd4d
Tim Harper <tharper@mesosphere.com>
2021-07-23 14:48:51 -0600

    Appease CI

--- ami/install.bash
+++ ami/install.bash
@@ -9,7 +9,7 @@ apt-get install -y dirmngr
 
 # Install sbt
 echo -e "\n=== Install SBT ==="
-echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 apt-get -y update
 apt-get install -y openjdk-8-jdk-headless sbt
